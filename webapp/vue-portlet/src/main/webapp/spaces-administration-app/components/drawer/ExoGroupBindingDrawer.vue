7b3fbd73dce887db9a6d79b91cfd468b3937761e
Moez Diha <mdiha@exoplatform.com>
2020-03-21 19:58:29 +0100

    SOC-6150: Select_Group_drawer: Add the drawer's second level with groups in tree view.

--- webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/drawer/ExoGroupBindingDrawer.vue
+++ webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/drawer/ExoGroupBindingDrawer.vue
@@ -1,6 +1,6 @@
 <template>
-  <div v-if="!showSelectGroupsTree">
-    <v-flex id="GroupBindingForm">
+  <div>
+    <v-flex v-show="!showSelectGroupsTree" id="GroupBindingForm">
       <v-card-title 
         class="title">
         <v-layout class="pa-2" row>
@@ -39,7 +39,8 @@
           class="pt-5 pl-3 mb-4"
           wrap>
           <v-flex xs9>
-            <input id="add-groups" type="text"/>
+            <exo-suggester v-model="groups" :options="suggesterOptions" :source-providers="[findGroups]"
+                           :placeholder="$t('social.spaces.administration.manageSpaces.spaceBindingForm.textField.placeHolder')"/>
           </v-flex>
           <v-flex xs1/>
           <v-flex xs1>
@@ -52,7 +53,22 @@
             </v-btn>
           </v-flex>
         </v-layout>
-        <v-layout v-if="groupSpaceBindings.length > 0" column>
+        <v-layout v-show="boundGroupsLoading" column>
+          <v-flex pl-4>
+            <v-skeleton-loader
+              class="mx-auto"
+              type="heading">
+            </v-skeleton-loader>
+          </v-flex>
+          <v-spacer></v-spacer>
+          <v-flex pt-4 pl-4 pr-8>
+            <v-skeleton-loader
+              class="mx-auto"
+              type="paragraph@3">
+            </v-skeleton-loader>
+          </v-flex>
+        </v-layout>
+        <v-layout v-if="groupSpaceBindings.length > 0 && !boundGroupsLoading" column>
           <v-flex>
             <span class="subtitle-1">
               {{ $t('social.spaces.administration.manageSpaces.spaceBindingForm.boundGroups') }}
@@ -84,7 +100,7 @@
       </div>
       <v-card-actions absolute class="drawerActions">
         <v-layout>
-          <v-flex class="xs7"></v-flex>
+          <v-flex class="xs6"></v-flex>
           <button type="button" class="btn ml-2" @click="cancelBinding">{{ $t('social.spaces.administration.manageSpaces.spaceBindingForm.cancel') }}</button>
           <button :disabled="!isAllowToSave" type="button" class="btn btn-primary ml-6" @click="$emit('openBindingModal', groups)">
             {{ $t('social.spaces.administration.manageSpaces.spaceBindingForm.apply') }}
@@ -92,44 +108,9 @@
         </v-layout>
       </v-card-actions>      
     </v-flex>
-  </div>
-  <div v-else>
-    <v-card-title 
-      class="title">
-      <v-layout 
-        class="pa-2" 
-        justify-center
-        align-baseline 
-        row>
-        <v-flex
-          align-self-end
-          class="xs1">
-          <v-btn
-            icon
-            class="leftIcon"
-            @click="back">
-            <v-icon small>arrow_back</v-icon>
-          </v-btn>
-        </v-flex>
-        <v-flex>
-          <span class="subtitle-1">
-            {{ $t('social.spaces.administration.manageSpaces.spaceBindingForm.selectGroups') }}
-          </span>
-        </v-flex>
-        <v-flex class="xs1 mr-2">
-          <v-btn
-            icon
-            class="rightIcon"
-            @click="closeDrawer">
-            <v-icon 
-              large
-              class="closeIcon">
-              close
-            </v-icon>
-          </v-btn>
-        </v-flex>
-      </v-layout>
-    </v-card-title>      
+    <div v-show="showSelectGroupsTree">
+      <exo-group-binding-second-level-drawer :already-selected="groups" @selectionSaved="selectionSaved" @back="back" @close="closeDrawer"></exo-group-binding-second-level-drawer>
+    </div>
   </div>
 </template>
 
@@ -146,14 +127,38 @@ export default {
     groupSpaceBindings: {
       type: Array,
       default: null,
-    }
+    },
+    boundGroupsLoading: {
+      type: Boolean,
+      default: true,
+    },
   },
   data() {
+    const component = this;
     return {
       textAreaValue: '',
       groups: [],
       showSelectGroupsTree: false,
       avatar: spacesConstants.DEFAULT_SPACE_AVATAR,
+      suggesterOptions: {
+        type: 'tag',
+        plugins: ['remove_button', 'restore_on_backspace'],
+        create: false,
+        createOnBlur: false,
+        highlight: false,
+        openOnFocus: false,
+        valueField: 'text',
+        labelField: 'text',
+        searchField: ['text'],
+        closeAfterSelect: false,
+        dropdownParent: 'body',
+        hideSelected: true,
+        placeholder:`@ ${this.$t('social.spaces.administration.manageSpaces.spaceBindingForm.textField.placeHolder')}`,
+        renderMenuItem (item, escape) {
+          return component.renderMenuItem(item, escape);
+        },
+        sortField: [{field: 'order'}, {field: '$score'}],
+      }
     };
   },
   computed : {
@@ -162,87 +167,13 @@ export default {
     },
     spaceDisplayName() {
       return  this.spaceToBind ? this.spaceToBind.displayName : '';
-    },
-    boundGroups() {
-      return this.groupSpaceBindings? this.groupSpaceBindings.map(binding => binding.group) : [];
     }
   },
-  mounted() {
-    this.initSuggesterGroupsToBind();
-  },
-  updated() {
-    this.initSuggesterGroupsToBind();
-  },
   methods : {
-    initSuggesterGroupsToBind() {
-      const suggesterContainer = $('#add-groups');
-      if(suggesterContainer && suggesterContainer.length && suggesterContainer.suggester) {
-        const component = this;
-        const suggesterData = {
-          type: 'tag',
-          plugins: ['remove_button', 'restore_on_backspace'],
-          create: false,
-          createOnBlur: false,
-          highlight: false,
-          openOnFocus: false,
-          sourceProviders: ['exo:spacesAdministration'],
-          valueField: 'text',
-          labelField: 'text',
-          searchField: ['text'],
-          closeAfterSelect: false,
-          dropdownParent: 'body',
-          hideSelected: true,
-          placeholder:`@ ${this.$t('social.spaces.administration.manageSpaces.spaceBindingForm.textField.placeHolder')}`,
-          renderMenuItem (item, escape) {
-            return component.renderMenuItem(item, escape);
-          },
-          renderItem(item) {
-            return `<div class="item">${item.text}</div>`;
-          },
-          onItemAdd(item) {
-            component.addSuggestedItemCreate(item);
-          },
-          onItemRemove(item) {
-            component.removeSuggestedItemCreators(item);
-          },
-          sortField: [{field: 'order'}, {field: '$score'}],
-          providers: {
-            'exo:spacesAdministration': component.findGroups
-          }
-        };
-        suggesterContainer.suggester(suggesterData);
-        $('#add-groups')[0].selectize.clear();
-        if(this.groups && this.groups !== null) {
-          for(const group of this.groups) {
-            suggesterContainer[0].selectize.addOption({text: group});
-            suggesterContainer[0].selectize.addItem(group);
-          }
-        }
-      }
-    },
-    addSuggestedItemCreate(item) {
-      if($('#add-groups') && $('#add-groups').length && $('#add-groups')[0].selectize) {
-        const selectize = $('#add-groups')[0].selectize;
-        item = selectize.options[item];
-      }
-      if(!this.groups.find(creator => creator === item.text)) {
-        this.groups.push(item.text);
-      }
-    },
-    removeSuggestedItemCreators(item) {
-      const suggesterContainer = $('#add-groups');
-      for(let i=this.groups.length-1; i>=0; i--) {
-        if(this.groups[i] === item) {
-          this.groups.splice(i, 1);
-          suggesterContainer[0].selectize.removeItem(item);
-        }
-      }
-    },
     findGroups (query, callback) {
       if (!query.length) {
         return callback();
       }
-
       spacesAdministrationServices.getGroups(query).then(data => {
         const groups = [];
         const boundGroups = this.groupSpaceBindings.map(binding => binding.group);
@@ -276,24 +207,18 @@ export default {
     },
     cancelBinding() {
       this.groups = [];
-      let items;
-      const selectizeInput = document.getElementById('GroupBindingForm').getElementsByClassName('has-items')[0];
-      if (selectizeInput && selectizeInput !== null) {
-        items = selectizeInput.getElementsByClassName('item');
-        if (items) {
-          Array.prototype.forEach.call(items, function (item) {
-            item.remove();
-          });
-        }
-      }
-      this.initSuggesterGroupsToBind();
       this.$emit('close');
+      this.showSelectGroupsTree = false;
     },
     renderGroupName(groupName) {
       let groupPrettyName = groupName.slice(groupName.lastIndexOf('/') + 1, groupName.length);
       groupPrettyName = groupPrettyName.charAt(0).toUpperCase() + groupPrettyName.slice(1);
       return `${groupPrettyName} (${groupName})`;
-    }
+    },
+    selectionSaved(groupsIds) {
+      this.showSelectGroupsTree = false;
+      this.groups.push(...groupsIds);
+    },
   }
 };
 </script>
