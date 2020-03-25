841107ecca403d071573270bc22a5b02c0190e49
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-03-25 16:00:35 +0100

    SOC-6150: Select_Group_drawer: Add the drawer's second level with groups in tree view (#11)
    *Select_Group_drawer_US01: Groups tree
    *Select_Group_drawer_US02: Drawer components
    *Select_Group_drawer_US03: Close icon
    *Select_Group_drawer_US04: Cancel button
    *Select_Group_drawer_US05: Back icon
    *Select_Group_drawer_US06: Save button
    *Select_Group_drawer_US08: Already seized groups
    *Select_Group_drawer_US09: Search results
    *Select_Group_drawer_US10: Sub groups in search results

---
+++ webapp/vue-portlet/src/main/webapp/spaces-administration-app/components/suggester/ExoSuggester.vue
@@ -0,0 +1,79 @@
+<template>
+  <input type="text"/>
+</template>
+
+<script>
+
+export default {
+  model: {
+    prop: 'selectedItems',
+    event: 'change'
+  },
+  props: {
+    options: {
+      type: Object,
+      default: function() { return {}; }
+    },
+    sourceProviders: {
+      type: Array,
+      default: () => []
+    },
+    selectedItems: {
+      type: Array,
+      default: () => []
+    }
+  },
+  watch: {
+    selectedItems() {
+      this.$emit('change', this.selectedItems);
+      this.bindSelectedItems();
+    }
+  },
+  mounted() {
+    const thiss = this;
+    const suggesterOptions = {
+      type: 'tag',
+      sourceProviders: [],
+      plugins: ['remove_button'],
+      providers: {},
+      onChange(items) {
+        thiss.selectedItems = items.split(',');
+      },
+    };
+    
+    //
+    this.sourceProviders.forEach(sourceProvider => {
+      if(typeof sourceProvider === 'function') {
+        suggesterOptions.providers[sourceProvider.name] = sourceProvider;
+        suggesterOptions.sourceProviders.push(sourceProvider.name);
+      } else {
+        suggesterOptions.sourceProviders.push(sourceProvider);
+      }
+    });
+    
+    const options = Object.assign({}, suggesterOptions, this.options);
+    
+    //init suggester
+    $(this.$el).suggester(options);
+  },
+  methods: {
+    bindSelectedItems() {
+      const selectize = $(this.$el)[0].selectize;
+      //
+      const removeItems = [];
+      selectize.items.forEach(item => {
+        if (!this.selectedItems.includes(item)) {
+          removeItems.push(item);
+        }
+      });
+      removeItems.forEach(item => {
+        selectize.removeItem(item, true);
+      });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
