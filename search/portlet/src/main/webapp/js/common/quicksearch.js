0cdd4467cc8406151e680149fb8eba140436f04b
Ayoub Zayati <azayati@exoplatform.com>
2020-03-31 20:05:33 +0200

    Update quicksearch.js

--- search/portlet/src/main/webapp/js/common/quicksearch.js
+++ search/portlet/src/main/webapp/js/common/quicksearch.js
@@ -459,10 +459,8 @@ window.initQuickSearch = function initQuickSearch(portletId,seeAllMsg, noResultM
       $(this).toggleClass('uiIconCloseSearchBox')
              .parents('#ToolBarSearch').find('input[type="text"]').toggleClass("showInputSearch").removeClass('loadding').focus()
              .parents('#ToolBarSearch').find('.uiQuickSearchResult').hide()
-             .parents('#PlatformAdminToolbarContainer').toggleClass('activeInputSearch')
+             .parents('.SearchPortletTDContainer').toggleClass('activeInputSearch')
              .parents('body').toggleClass('quickSearchDisplay');
-			 
-	  $(this).parents('#UITopBarContainerParent').toggleClass('activeInputSearch');
     });
 
     //collapse the input search field when clicking outside the search box
@@ -471,8 +469,7 @@ window.initQuickSearch = function initQuickSearch(portletId,seeAllMsg, noResultM
         // $(txtQuickSearchQuery_id).hide();
         $(txtQuickSearchQuery_id).removeClass("showInputSearch");
         $("#ToolBarSearch .uiIconPLF24x24Search").removeClass('uiIconCloseSearchBox');
-        $('#PlatformAdminToolbarContainer').removeClass('activeInputSearch');
-		$('#UITopBarContainerParent').removeClass('activeInputSearch');
+        $('.SearchPortletTDContainer').removeClass('activeInputSearch');
         $('#ToolBarSearch').find('input[type="text"]').removeClass('loadding');
         $('body').removeClass('quickSearchDisplay');
         $(quickSearchResult_id).hide();        
