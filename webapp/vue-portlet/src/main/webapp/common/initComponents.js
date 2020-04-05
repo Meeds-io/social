fbe3658a1fd9b22a110e2b9246ea112a8137bb73
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-05 10:54:56 +0100

    Declare Common Vue Suggester component

--- webapp/vue-portlet/src/main/webapp/common/initComponents.js
+++ webapp/vue-portlet/src/main/webapp/common/initComponents.js
@@ -2,12 +2,14 @@ import ExoDrawer from './components/ExoDrawer.vue';
 import ExoConfirmDialog from './components/ExoConfirmDialog.vue';
 import ExoUserAvatarsList from './components/ExoUserAvatarsList.vue';
 import ExoUserAvatar from './components/ExoUserAvatar.vue';
+import ExoIdentitySuggester from './components/ExoIdentitySuggester.vue';
 
 const components = {
   'exo-user-avatars-list': ExoUserAvatarsList,
   'exo-user-avatar': ExoUserAvatar,
   'exo-drawer': ExoDrawer,
   'exo-confirm-dialog': ExoConfirmDialog,
+  'exo-identity-suggester': ExoIdentitySuggester,
 };
 
 for (const key in components) {
