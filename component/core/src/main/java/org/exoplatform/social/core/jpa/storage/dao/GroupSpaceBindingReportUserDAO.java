b19e0c0e6e5e1ef2ae38541b03839ffd4d81b857
Moez Diha <mdiha@exoplatform.com>
2020-04-06 15:31:16 +0100

    SOC-6150: BindingReports-US03-To-US12: Add GroupSpaceBindingReportUserDAO.

---
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/storage/dao/GroupSpaceBindingReportUserDAO.java
@@ -0,0 +1,4 @@
+package org.exoplatform.social.core.jpa.storage.dao;
+
+public interface GroupSpaceBindingReportUserDAO {
+}
---
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/storage/dao/jpa/GroupSpaceBindingReportUserDAOImpl.java
@@ -0,0 +1,6 @@
+package org.exoplatform.social.core.jpa.storage.dao.jpa;
+
+import org.exoplatform.social.core.jpa.storage.dao.GroupSpaceBindingReportUserDAO;
+
+public class GroupSpaceBindingReportUserDAOImpl implements GroupSpaceBindingReportUserDAO {
+}
