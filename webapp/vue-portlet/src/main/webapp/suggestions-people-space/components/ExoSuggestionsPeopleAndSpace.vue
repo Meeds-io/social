b5e1b2d4bbda012baf32237f11708703904f3ef8
saraBoutej <sboutej@exoplatform.com>
2020-04-07 15:05:13 +0100

    Add suggestions services

--- webapp/vue-portlet/src/main/webapp/suggestions-people-space/components/ExoSuggestionsPeopleAndSpace.vue
+++ webapp/vue-portlet/src/main/webapp/suggestions-people-space/components/ExoSuggestionsPeopleAndSpace.vue
@@ -1,13 +1,14 @@
 <template>
-  <v-app v-if="spacesSuggestionsList.length > 0 || peopleSuggestionsList > 0" id="SuggestionsPeopleAndSpace">
+  <v-app v-if="spacesSuggestionsList.length > 0 || peopleSuggestionsList.length > 0" id="SuggestionsPeopleAndSpace">
     <v-flex d-flex xs12 sm12>
       <v-layout row wrap mx-0>
         <v-flex d-flex xs12>
-          <v-card flat class="flex">
+          <v-card flat class="flex suggestions-wrapper">
             <v-card-title class="suggestions-title subtitle-1 text-uppercase pb-0"> {{ $t('suggestions.label') }}</v-card-title>
             <v-list v-if="peopleSuggestionsList.length > 0" dense class="suggestions-list people-list py-4 mx-4">
-              <v-list-item v-for="(people, index) in peopleSuggestionsList.slice(0, 2)"
+              <v-list-item v-for="(people, index) in getLastSuggestionsPeople"
                            :key="index"
+                           :id="'people-item-'+people.suggestionId"
                            class="suggestions-list-item suggestions-people-list-item pa-0">
                 <v-list-item-avatar size="37">
                   <v-img :src="people.avatar"></v-img>
@@ -18,18 +19,19 @@
                 </v-list-item-content>
                 <v-list-item-action class="suggestions-list-item-actions">
                   <v-btn-toggle class="transparent" dark>
-                    <a text icon small min-width="auto" class="px-0 suggestions-btn-action connexion-accept-btn">
+                    <a text icon small min-width="auto" class="px-0 suggestions-btn-action connexion-accept-btn" @click="connectionRequest(people)">
                       <i class="uiIconInviteUser"></i>
                     </a>
-                    <a text small min-width="auto" class="px-0 suggestions-btn-action connexion-refuse-btn">
+                    <a text small min-width="auto" class="px-0 suggestions-btn-action connexion-refuse-btn" @click="ignoredConnection(currentUser, people)">
                       <i class="uiIconCloseCircled"></i>
                     </a>
                   </v-btn-toggle>
                 </v-list-item-action>
               </v-list-item>
+            
             </v-list>
             <v-list v-if="spacesSuggestionsList.length > 0" dense class="suggestions-list space-list py-4 mx-4">
-              <v-list-item v-for="(space, index) in spacesSuggestionsList.slice(0, 2)"
+              <v-list-item v-for="(space, index) in getLastSuggestionsSpace"
                            :key="index"
                            class="suggestions-list-item suggestions-space-list-item pa-0">
                 <v-list-item-avatar tile size="37">
@@ -41,10 +43,10 @@
                 </v-list-item-content>
                 <v-list-item-action class="suggestions-list-item-actions">
                   <v-btn-toggle class="transparent" dark>
-                    <a text icon small min-width="auto" class="px-0 suggestions-btn-action connexion-accept-btn">
+                    <a text icon small min-width="auto" class="px-0 suggestions-btn-action connexion-accept-btn" @click="joinSpace(space)">
                       <i class="uiIconPlusLight"></i>
                     </a>
-                    <a text small min-width="auto" class="px-0 suggestions-btn-action connexion-refuse-btn">
+                    <a text small min-width="auto" class="px-0 suggestions-btn-action connexion-refuse-btn" @click="ignoredSuggestionSpace(space)">
                       <i class="uiIconCloseCircled"></i>
                     </a>
                   </v-btn-toggle>
@@ -63,8 +65,17 @@ export default {
   data () {
     return {
       peopleSuggestionsList: [],
-      spacesSuggestionsList: []
+      spacesSuggestionsList: [],
+      currentUser: ''
     };
+  },
+  computed : {
+    getLastSuggestionsPeople() {
+      return this.peopleSuggestionsList.slice(0, 2);
+    },
+    getLastSuggestionsSpace() {
+      return this.spacesSuggestionsList.slice(0, 2);
+    }
   },
   created() {
     this.initPeopleSuggestionsList();
@@ -74,13 +85,31 @@ export default {
     initPeopleSuggestionsList() {
       suggestionsServices.getPeopleSuggestions().then(data => {
         this.peopleSuggestionsList = data.items;
+        this.currentUser = data.username;
       });
     },
     initSpaceSuggestionsList() {
       suggestionsServices.getSpaceSuggestions().then(data => {
         this.spacesSuggestionsList = data.items;
       });
-    }
-  }
+    },
+    connectionRequest(item) {
+      suggestionsServices.sendConnectionRequest(item.suggestionId);
+      this.peopleSuggestionsList.splice(this.peopleSuggestionsList.indexOf(item),1);
+    },
+    ignoredConnection(sender, receiverItem) {
+      suggestionsServices.ignoredSuggestionConnection(sender, receiverItem.username);
+      this.peopleSuggestionsList.splice(this.peopleSuggestionsList.indexOf(receiverItem),1);
+    },
+    joinSpace(item) {
+      suggestionsServices.joinSpace(item.spaceId);
+      this.spacesSuggestionsList.splice(this.spacesSuggestionsList.indexOf(item),1);
+    },
+    ignoredSuggestionSpace(item) {
+      suggestionsServices.ignoredSuggestionSpace(item);
+      this.spacesSuggestionsList.splice(this.spacesSuggestionsList.indexOf(item),1);
+    },
+  },
 };
 </script>
+
--- webapp/vue-portlet/src/main/webapp/suggestions-people-space/suggestionsServices.js
+++ webapp/vue-portlet/src/main/webapp/suggestions-people-space/suggestionsServices.js
@@ -5,3 +5,35 @@ export function getPeopleSuggestions() {
 export function getSpaceSuggestions(){
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/homepage/intranet/spaces/suggestions`, {credentials: 'include'}).then(resp => resp.json());
 }
+
+export function sendConnectionRequest(userID) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/homepage/intranet/people/contacts/connect/${userID}`, null);
+}
+
+export function ignoredSuggestionConnection(sender, receiver) {
+  const data = {'sender': sender,'receiver': receiver,'status':'IGNORED'};
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/usersRelationships/`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
+
+export function joinSpace(spaceId) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/homepage/intranet/spaces/request/${spaceId}`, null);
+}
+
+export function ignoredSuggestionSpace(item) {
+  const data = {'user': item.username,'space': item.displayName, 'status':'IGNORED'};
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/spacesMemberships/`, {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
