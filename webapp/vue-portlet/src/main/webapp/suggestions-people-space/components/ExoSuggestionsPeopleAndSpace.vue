d1d8510b3f35bdfa3f4b7ce82974ad501a12912f
saraBoutej <sboutej@exoplatform.com>
2020-04-13 16:37:46 +0100

    Edit loading suggestions space variable name

--- webapp/vue-portlet/src/main/webapp/suggestions-people-space/components/ExoSuggestionsPeopleAndSpace.vue
+++ webapp/vue-portlet/src/main/webapp/suggestions-people-space/components/ExoSuggestionsPeopleAndSpace.vue
@@ -42,7 +42,7 @@ export default {
     return {
       peopleSuggestionsList: [],
       spacesSuggestionsList: [],
-      firstLoadingSpacesAndPeople: true,
+      firstLoadingSpaces: true,
       firstLoadingPeoples: true,
     };
   },
