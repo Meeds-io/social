322b66fc68d63c6facd0bc1b28091c50a02cdb84
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2020-04-16 12:15:28 +0100

    Add Getting Started and who is online skeleton feature/meeds(#50)

--- webapp/vue-portlet/src/main/webapp/getting-started/components/ExoGettingStarted.vue
+++ webapp/vue-portlet/src/main/webapp/getting-started/components/ExoGettingStarted.vue
@@ -14,15 +14,25 @@
           <v-card
             flat
             class="flex">
-            <v-card-title class="getting-started-title subtitle-1 text-uppercase pb-0"> {{ $t('locale.portlet.gettingStarted.title') }}</v-card-title>
+            <v-card-title class="getting-started-title subtitle-1 text-uppercase pb-0">
+              <span :class="firstLoadingSteps && 'skeleton-background skeleton-text skeleton-header skeleton-border-radius'">
+                {{ $t('locale.portlet.gettingStarted.title') }}
+              </span>
+            </v-card-title>
             <v-list dense class="getting-started-list">
               <v-list-item v-for="(step, index) in gettingStratedSteps" :key="index" class="getting-started-list-item">
-                <v-list-item-icon class="mr-3 steps-icon">
-                  <i v-if="step.status" class="UICheckIcon white--text"></i>
-                  <span v-else class="step-number font-weight-bold text-center white--text">{{ index+1 }}</span>
+                <v-list-item-icon :class="firstLoadingSteps && 'skeleton-text'" class="mr-3 steps-icon">
+                  <i v-if="step.status" :class="firstLoadingSteps && 'skeleton-background skeleton-text'" class="UICheckIcon white--text"></i>
+                  <span v-else :class="firstLoadingSteps && 'skeleton-background'" class="step-number font-weight-bold text-center white--text" >
+                    <span :class="firstLoadingSteps && 'skeleton-text'" >{{ index+1 }}</span>
+                  </span>
                 </v-list-item-icon>
                 <v-list-item-content :class="{ inactiveStep: !step.status }" class="pb-3">
-                  <v-list-item-title class="body-2">{{ $t(`locale.portlet.gettingStarted.step.${step.name}`) }}</v-list-item-title>
+                  <v-list-item-title class="body-2">
+                    <span :class="firstLoadingSteps && 'skeleton-background skeleton-text skeleton-list-item-title skeleton-border-radius'">
+                      {{ $t(`locale.portlet.gettingStarted.step.${step.name}`) }}
+                    </span>
+                  </v-list-item-title>
                 </v-list-item-content>
               </v-list-item>
             </v-list>
@@ -37,7 +47,8 @@ import * as gettingStartedService from '../gettingStartedService.js';
 export default {
   data () {
     return {
-      gettingStratedSteps: []
+      gettingStratedSteps: [],
+      firstLoadingSteps: true
     };
   },
   created() {
@@ -47,6 +58,9 @@ export default {
     initGettingStarted() {
       gettingStartedService.getGettingStartedSteps().then(data => {
         this.gettingStratedSteps = data;
+        if(this.firstLoadingSteps) {
+          this.firstLoadingSteps = false;
+        }
       });
     }
   }
