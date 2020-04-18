3998fb2058a86022ff1da7ca95db1d7fd59d6ae7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-18 21:20:45 +0100

    Enahnce Profile Header components definition

---
+++ webapp/vue-portlet/src/main/webapp/profile-header/components/ProfileHeaderText.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-flex>
+    <v-card-title
+      :class="skeleton && 'skeleton-background skeleton-text skeleton-text-width skeleton-text-height pa-0 my-3' || ''"
+      class="headline white--text">
+      {{ !skeleton && user && user.fullname || '&nbsp;' }}
+    </v-card-title>
+    <v-card-subtitle
+      :class="skeleton && 'skeleton-background skeleton-text skeleton-text-width skeleton-text-height pa-0 my-3' || ''"
+      class="subtitle white--text"
+      dark>
+      {{ !skeleton && user && user.position || '&nbsp;' }}
+    </v-card-subtitle>
+  </v-flex>
+</template>
+
+<script>
+export default {
+  props: {
+    user: {
+      type: Object,
+      default: () => null,
+    },
+    skeleton: {
+      type: Boolean,
+      default: () => true,
+    },
+  },
+};
+</script>
