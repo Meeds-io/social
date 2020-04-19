d0804494af29c058ed94ff8fdc2a8c99566c3bc9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-19 11:45:48 +0100

    Implement AboutMe application for Profile page

---
+++ webapp/vue-portlet/src/main/webapp/profile-about-me/main.js
@@ -0,0 +1,39 @@
+import './initComponents.js';
+
+// get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('ProfileAboutMe');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+
+Vue.use(Vuetify);
+Vue.use(VueEllipsis);
+const vuetify = new Vuetify({
+  dark: true,
+  iconfont: '',
+});
+
+//getting language of user
+const lang = eXo && eXo.env.portal.language || 'en';
+
+const appId = 'ProfileAboutMe';
+
+//should expose the locale ressources as REST API 
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.social.${appId}-${lang}.json`;
+
+export function init(aboutMe) {
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+  // init Vue app when locale ressources are ready
+    new Vue({
+      template: `<profile-about-me id="${appId}" about-me="${aboutMe || ''}" />`,
+      i18n,
+      vuetify,
+    }).$mount(`#${appId}`);
+  });
+}
