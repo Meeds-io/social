b70d3e6fee9b7cab57df68d885533e660e08fc81
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-26 19:54:52 +0100

    Open Profile URLs in a new tab

--- webapp/vue-portlet/src/main/webapp/profile-contact-information/components/ProfileContactUrls.vue
+++ webapp/vue-portlet/src/main/webapp/profile-contact-information/components/ProfileContactUrls.vue
@@ -5,7 +5,7 @@
     </div>
     <div v-if="user.urls && user.urls.length" class="align-end flex-grow-1 text-truncate text-end">
       <div v-for="(url, i) in user.urls" :key="i" class="text-no-wrap text-truncate">
-        <a :href="url.url" :title="url.url">
+        <a :href="url.url" :title="url.url" target="_blank" rel="noopener noreferrer">
           {{ url.url }}
         </a>
       </div>
