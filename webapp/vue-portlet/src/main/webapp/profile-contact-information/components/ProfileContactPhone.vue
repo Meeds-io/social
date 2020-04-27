0adcea9947bc462c63793ae642c3654ea2c7959b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-27 10:21:14 +0100

    Improve Profile Contact Information

--- webapp/vue-portlet/src/main/webapp/profile-contact-information/components/ProfileContactPhone.vue
+++ webapp/vue-portlet/src/main/webapp/profile-contact-information/components/ProfileContactPhone.vue
@@ -1,9 +1,16 @@
 <template>
   <v-flex class="d-flex">
-    <div class="align-start text-no-wrap font-weight-bold mr-3">
+    <div
+      :class="skeleton && 'skeleton-text skeleton-text-width skeleton-background skeleton-text-height skeleton-border-radius'"
+      class="align-start text-no-wrap font-weight-bold mr-3">
       {{ $t('profileContactInformation.phones') }}
     </div>
-    <div v-if="user && user.phones && user.phones.length" class="align-end flex-grow-1 text-truncate text-end">
+    <div
+      v-if="skeleton"
+      class="align-end flex-grow-1 text-truncate text-end skeleton-text skeleton-text-width skeleton-background skeleton-text-height-fine skeleton-border-radius">
+      &nbsp;&nbsp;&nbsp;&nbsp;
+    </div>
+    <div v-else-if="user && user.phones && user.phones.length" class="align-end flex-grow-1 text-truncate text-end">
       <div v-for="(phone, i) in user.phones" :key="i" :title="phone.phoneNumber" class="text-no-wrap text-truncate">
         <span class="pr-1 text-capitalize">
           {{ getLabel(`profileContactInformation.phone.${phone.phoneType}`, phone.phoneType) }}:
@@ -20,6 +27,10 @@ export default {
     user: {
       type: Object,
       default: () => null,
+    },
+    skeleton: {
+      type: Boolean,
+      default: false,
     },
   },
   methods: {
