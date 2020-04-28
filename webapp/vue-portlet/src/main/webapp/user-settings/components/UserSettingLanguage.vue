f55ee1ccdc89ef3ee473e808c5ad170fb3d1be74
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-04-28 11:17:08 +0100

    Add user Settings application

---
+++ webapp/vue-portlet/src/main/webapp/user-settings/components/UserSettingLanguage.vue
@@ -0,0 +1,45 @@
+<template>
+  <v-card class="ma-4 border-radius" flat>
+    <v-list two-line>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title text-color">
+            {{ $t('UserSettings.language') }}
+          </v-list-item-title>
+          <v-list-item-subtitle class="text-sub-title">
+            {{ languageLabel }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon>
+            <i class="uiIconEdit uiIconLightBlue pb-2"></i>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    languageLabel() {
+      const langLabelInEnglish = new Intl.DisplayNames(['en'], {type: 'language'}).of(this.value);
+      const langLabelInSameLang = new Intl.DisplayNames([eXo.env.portal.language], {type: 'language'}).of(this.value);
+      return `${langLabelInEnglish} - ${langLabelInSameLang}`;
+    },
+  },
+  watch: {
+    value() {
+      this.$emit('input', this.value);
+    },
+  },
+};
+</script>
+
---
+++ webapp/vue-portlet/src/main/webapp/user-settings/components/UserSettingTimezone.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card class="ma-4 border-radius" flat>
+    <v-list two-line>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title text-color">
+            {{ $t('UserSettings.timezone') }}
+          </v-list-item-title>
+          <v-list-item-subtitle class="text-sub-title">
+            {{ timezoneLabel }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+        <v-list-item-action>
+          <v-btn icon>
+            <i class="uiIconEdit uiIconLightBlue pb-2"></i>
+          </v-btn>
+        </v-list-item-action>
+      </v-list-item>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    timezoneLabel() {
+      const langTimezone = new Intl.DateTimeFormat().resolvedOptions().timeZone;
+      return langTimezone;
+    },
+  },
+  watch: {
+    value() {
+      this.$emit('input', this.value);
+    },
+  },
+};
+</script>
+
