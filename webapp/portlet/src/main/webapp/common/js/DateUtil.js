914dbc3b993d98874e426afe126b37d44758e983
Uoc Nguyen <uoc@newlogic.io>
2020-04-28 17:45:35 +0700

    Update license header for Meeds

--- webapp/portlet/src/main/webapp/common/js/DateUtil.js
+++ webapp/portlet/src/main/webapp/common/js/DateUtil.js
@@ -1,15 +1,20 @@
 const DIGIT_PATTERN = /^[1-9][0-9]*$/g;
 
-/**
- * Return Date object from string value.
- * The string value can of type:
- * - Date with format: dd/MM/yyyy
- * - Date with ISO 8601 format : yyyy-MM-dd
- * - Date timestamp
- * 
- * @param {String} value String or Integer value of date
- * @param {Boolean} isISOString if true, the date will be parsed by ISO 8601 format
- * @returns {Date} Date object
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2020 Meeds Association
+ * contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 export function getDateObjectFromString(value, isISOString) {
   value = String(value).trim();
