aa9eef7f9887f14f9b3e85d755a12efdc5beb385
Lotfi Ben Gayed <lbengayed@exoplatform.com>
2020-06-10 10:21:37 +0100

    Maint-23038: add disabled user label (#104)
    When a user is disabled , there a missing indications in  :
    - User profile page
    - User popup
    The fix will add the word disabled next to the username

--- webapp/vue-portlet/src/main/webapp/profile-header/components/ProfileHeaderText.vue
+++ webapp/vue-portlet/src/main/webapp/profile-header/components/ProfileHeaderText.vue
@@ -3,8 +3,7 @@
     <v-card-title
       :class="skeleton && 'skeleton-background skeleton-text skeleton-text-width skeleton-text-height pa-0 my-3' || ''"
       class="headline white--text">
-      {{ !skeleton && user && user.fullname || '&nbsp;' }}
-    </v-card-title>
+      {{ !skeleton && user && user.fullname || '&nbsp;' }} {{ !skeleton && user.enabled ? '' : '(' + $t('profileHeader.label.disabled') + ')' }}    </v-card-title>
     <v-card-subtitle
       :class="skeleton && 'skeleton-background skeleton-text skeleton-text-width skeleton-text-height pa-0 my-3' || ''"
       class="subtitle white--text"
