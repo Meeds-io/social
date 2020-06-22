a41ae922f654447b9176889515958a37fc2f907a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2020-06-22 15:44:45 +0100

    Redesign Activity Search card

--- webapp/portlet/src/main/webapp/search-activity/components/SearchActivityCard.vue
+++ webapp/portlet/src/main/webapp/search-activity/components/SearchActivityCard.vue
@@ -1,36 +1,42 @@
 <template>
   <v-card class="d-flex flex-column border-radius box-shadow mr-2 mb-4" flat min-height="227">
-    <v-card-text class="pa-2">
+    <v-card-text class="px-2 pt-2 pb-1">
       <exo-user-avatar
         :username="poster.username"
         :fullname="poster.fullname"
-        :title="poster.fullname">
+        :title="poster.fullname"
+        avatar-class="border-color">
+        <template slot="subTitle">
+          <date-format :value="postedTime" />
+        </template>
       </exo-user-avatar>
     </v-card-text>
-    <v-divider class="box-shadow no-border" />
-    <v-card-text class="pb-0 flex-grow-1 flex-shrink-0">
-      <blockquote
-        v-if="excerpt"
-        class="text-color mb-0"
-        v-html="excerpt">
-      </blockquote>
+    <v-card-text class="py-0 flex-grow-1 flex-shrink-0">
+      <div
+        ref="excerptNode"
+        :title="excerptText"
+        class="text-wrap text-break caption">
+      </div>
     </v-card-text>
-    <v-card-actions class="pt-1">
-      <v-btn
-        :href="link"
-        link
-        text
-        color="primary"
-        class="text-capitalize">
-        {{ $t('Search.activity.open') }}
-      </v-btn>
-    </v-card-actions>
+    <v-list class="light-grey-background flex-grow-0 border-top-color no-border-radius pa-0">
+      <v-list-item :href="link" class="px-0 pt-1 pb-2">
+        <v-list-item-icon class="mx-0 my-auto">
+          <span :class="activityIcon" class="tertiary--text px-1 display-1"></span>
+        </v-list-item-icon>
+        <v-list-item-content>
+          <v-list-item-title :title="activityReactions">
+            {{ activityReactions }}
+          </v-list-item-title>
+          <v-list-item-subtitle>
+            {{ activityStreamOwner }}
+          </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
   </v-card>
 </template>
 
 <script>
-const MAX_EXCERPT_CHARS_LENGTH = 130;
-
 export default {
   props: {
     term: {
@@ -43,10 +49,17 @@ export default {
     },
   },
   data: () => ({
+    excerptLines: 6,
+    lineHeight: 22,
     profileActionExtensions: [],
-    displayDrawers: false,
+    format: {
+      
+    },
   }),
   computed: {
+    maxEllipsisHeight() {
+      return this.lineHeight * this.excerptLines;
+    },
     isComment() {
       return this.result && this.result.comment;
     },
@@ -65,46 +78,51 @@ export default {
     excerpts() {
       return this.activity && this.activity.excerpts;
     },
-    excerpt() {
-      const excerpt = this.excerpts && this.excerpts.length && this.excerpts[0];
-      if (!excerpt) {
+    excerptHtml() {
+      return this.excerpts && this.excerpts.join('\r\n...');
+    },
+    excerptText() {
+      return $('<div />').html(this.excerptHtml).text();
+    },
+    activityType() {
+      if (!this.result) {
         return '';
       }
-      let excerptText = $('<div />').html(excerpt).text();
-      if (excerptText.length > MAX_EXCERPT_CHARS_LENGTH) {
-        const excerptParts = excerpt.split(new RegExp('<span class=["\']searchMatchExcerpt["\']>(.*)</span>'));
-        let startPart = excerptParts[0];
-        let endPart = excerptParts[excerptParts.length - 1];
-        const excerptMandatoryParts = [];
-        for (let i = 1; i < excerptParts.length - 1; i++) {
-          excerptMandatoryParts.push(`<span class="searchMatchExcerpt">${excerptParts[i]}</span>`);
-        }
-        const exceptToDisplay = excerptMandatoryParts.join();
-        excerptText = $('<div />').html(exceptToDisplay).text();
-        if (excerptText.length > MAX_EXCERPT_CHARS_LENGTH) {
-          return exceptToDisplay;
-        }
-
-        const excerptHighlightLength = excerptText.length;
-        const quoteCharsBoudaries = parseInt((MAX_EXCERPT_CHARS_LENGTH - excerptHighlightLength) / 2);
-
-        const startIndex = startPart.length - quoteCharsBoudaries;
-        let endIndex = quoteCharsBoudaries;
-
-        if (startIndex > 0) {
-          startPart = `...${startPart.substring(startIndex)}`;
-        } else {
-          endIndex -= startIndex;
-        }
-
-        if (endIndex < endPart.length) {
-          endPart = `${endPart.substring(0, endIndex)}...`;
-        }
-
-        return `${startPart}${exceptToDisplay}${endPart}`;
-      } else {
-        return excerpt;
+      return this.result.comment && this.result.comment.type || this.result.type;
+    },
+    activityIcon() {
+      if (!this.result) {
+        return '';
       }
+      let typeIcon = this.activityType.replace(':', '_');
+      typeIcon = `uiIcon${typeIcon.charAt(0).toUpperCase()}${typeIcon.substring(1)}`;
+      return `uiIconActivity ${typeIcon}`;
+    },
+    activityLikes() {
+      if (!this.result) {
+        return '';
+      }
+      const likesCount = this.result.likesCount || 0;
+      return this.$t('Search.activity.likesCount', {0: likesCount});
+    },
+    activityComments() {
+      const commentsCount = this.result.commentsCount || 0;
+      return this.$t('Search.activity.commentsCount', {0: commentsCount});
+    },
+    activityReactions() {
+      return `${this.activityLikes}, ${this.activityComments}`;
+    },
+    activityStreamOwner() {
+      if (!this.result) {
+        return '';
+      }
+      return this.result.streamOwner.profile && this.result.streamOwner.profile.fullname || this.result.streamOwner.space && this.result.streamOwner.space.displayName || '';
+    },
+    postedTime() {
+      if (!this.result) {
+        return '';
+      }
+      return this.result.comment && this.result.comment.postedTime || this.result.postedTime;
     },
     link() {
       if (this.isComment) {
@@ -117,5 +135,58 @@ export default {
   created() {
     this.profileActionExtensions = extensionRegistry.loadExtensions('profile-extension', 'action') || [];
   },
+  mounted() {
+    this.computeEllipsis();
+  },
+  methods: {
+    computeEllipsis() {
+      if (!this.excerptHtml || this.excerptHtml.length === 0) {
+        return;
+      }
+      const stNode = this.$refs.excerptNode;
+      if (!stNode) {
+        return;
+      }
+      stNode.innerHTML = this.excerptHtml;
+
+      let stNodeHeight = stNode.getBoundingClientRect().height || this.lineHeight;
+      if (stNodeHeight > this.maxEllipsisHeight) {
+        while (stNodeHeight > this.maxEllipsisHeight) {
+          const newHtml = this.deleteLastChars(stNode.innerHTML.replace(/&[a-z]*;/, ''), 10);
+          if (newHtml.length === stNode.innerHTML.length) {
+            break;
+          }
+          stNode.innerHTML = newHtml;
+          stNodeHeight = stNode.getBoundingClientRect().height || this.lineHeight;
+        }
+
+        stNode.innerHTML = this.deleteLastChars(stNode.innerHTML, 4);
+        stNode.innerHTML = `${stNode.innerHTML}...`;
+      }
+    },
+    deleteLastChars(html, charsToDelete) {
+      if (html.slice(-1) === '>') {
+        // Replace empty tags
+        html = html.replace(/<[a-zA-Z 0-9 "'=]*><\/[a-zA-Z 0-9]*>$/g, '');
+      }
+      html = html.replace(/<br>(\.*)$/g, '');
+
+      charsToDelete = charsToDelete || 1;
+
+      let newHtml = '';
+      if (html.slice(-1) === '>') {
+        // Delete last inner html char
+        html = html.replace(/(<br>)*$/g, '');
+        newHtml = html.replace(new RegExp(`([^>]{${charsToDelete}})(</)([a-zA-Z 0-9]*)(>)$`), '$2$3');
+        newHtml = $('<div />').html(newHtml).html().replace(/&[a-z]*;/, '');
+        if (newHtml.length === html.length) {
+          newHtml = html.replace(new RegExp('([^>]*)(</)([a-zA-Z 0-9]*)(>)$'), '$2$3');
+        }
+      } else {
+        newHtml = html.substring(0, html.trimRight().length - charsToDelete);
+      }
+      return newHtml;
+    },
+  }
 };
 </script>
