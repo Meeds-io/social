f1843b7333fadd93ad91027219a50918d5828831
Jihed525 <31882754+Jihed525@users.noreply.github.com>
2020-09-27 19:49:14 +0100

    Patch 6.0.0:3 (#281)
    * MAINT-28042 : Fix problem of loading add News button (#254) (#258)
    
    * MAINT-28042 : Make sure to add extension asynchronously to composer
    
    * MAINT-28042 : Use a function to check if user is redactor in space
    
    * MAINT-28042 : use resource bundle name from JS extension
    
    (cherry picked from commit 6c15b2e747078e66bdef26910abefc519990f774)
    (cherry picked from commit 1455c3f436d7d193295426e8719406f94dcf5962)
    
    * [6.0.0-3]
    
    Co-authored-by: Ali HAMDI <ahamdi@exoplatform.com>

--- webapp/portlet/src/main/webapp/activity-composer-app/components/ExoActivityComposer.vue
+++ webapp/portlet/src/main/webapp/activity-composer-app/components/ExoActivityComposer.vue
@@ -186,13 +186,18 @@ export default {
   },
   methods: {
     refreshExtensions: function() {
+      const urls = [];
       this.activityComposerActions = getActivityComposerActionExtensions();
       this.activityComposerActions.forEach(action => {
         if (action.component) {
           this.actionsData[action.key] = action.component.model.value;
           this.actionsEvents[action.key] = action.component.events;
         }
+        if(action.resourceBundle) {
+          urls.push(`/portal/rest/i18n/bundle/${action.resourceBundle}-${eXo.env.portal.language}.json`);
+        }
       });
+      exoi18n.loadLanguageAsync(eXo.env.portal.language, urls);
     },
     editActivity(params) {
       params = params && params.detail;
