b3e3842e8f07c4b51b31b64100901ef32e50dc88
ahmed <achakroun@exoplatform.com>
2020-10-02 23:54:29 +0100

    Update and clean code

--- component/core/src/main/java/org/exoplatform/social/core/space/impl/SpaceRenamedListenerImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/space/impl/SpaceRenamedListenerImpl.java
@@ -18,6 +18,7 @@ import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceLifeCycleEvent;
 import org.exoplatform.web.application.RequestContext;
 import org.exoplatform.web.url.navigation.NodeURL;
+import org.exoplatform.container.PortalContainer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -92,7 +93,8 @@ public class SpaceRenamedListenerImpl extends SpaceListenerPlugin {
   @Override
   public void spaceRenamed(SpaceLifeCycleEvent event) {
     Space space = event.getSpace();
-    renameNavigationAndGroup(space);
+    SpaceUtils.renamePageNode(space);
+    SpaceUtils.renameGroupLabel(space);
   }
 
   @Override
@@ -113,92 +115,6 @@ public class SpaceRenamedListenerImpl extends SpaceListenerPlugin {
   @Override
   public void addPendingUser(SpaceLifeCycleEvent event) {
 
-  }
-
-  /**
-   * Rename navigation and group
-   *
-   * @param space
-   * @return
-   */
-  public static void renameNavigationAndGroup(Space space) {
-    try {
-
-      String oldSpaceName = space.getGroupId().split("/spaces/")[1];
-      if (!oldSpaceName.equals(space.getDisplayName())) {
-        String cleanedString = SpaceUtils.cleanString(space.getDisplayName());
-        UserNode node = renamePageNode(cleanedString, space);
-        OrganizationService organizationService = CommonsUtils.getService(OrganizationService.class);
-        GroupHandler groupHandler = organizationService.getGroupHandler();
-        Group group = groupHandler.findGroupById(space.getGroupId());
-        group.setLabel(space.getDisplayName());
-        groupHandler.saveGroup(group, true);
-        if (node != null) {
-          PortalRequestContext prContext = Util.getPortalRequestContext();
-          prContext.createURL(NodeURL.TYPE).setNode(node);
-          space.setUrl(getSpaceURL(node));
-        }
-      }
-    } catch (Exception e) {
-      LOG.warn(e.getMessage(), e);
-    } finally {
-      RequestLifeCycle.end();
-    }
-  }
-
-  /**
-   * Rename page node.
-   *
-   * @param newNodeLabel
-   * @param space
-   * @return
-   */
-  public static UserNode renamePageNode(String newNodeLabel, Space space) {
-
-    try {
-
-      DataStorage dataService = CommonsUtils.getService(DataStorage.class);
-      UserNode renamedNode = SpaceUtils.getSpaceUserNode(space);
-      UserNode parentNode = renamedNode.getParent();
-      String newNodeName = SpaceUtils.cleanString(newNodeLabel);
-      //
-      renamedNode.setLabel(newNodeLabel);
-      renamedNode.setName(newNodeName);
-
-      Page page = dataService.getPage(renamedNode.getPageRef().format());
-      if (page != null) {
-        page.setTitle(newNodeLabel);
-        dataService.save(page);
-      }
-
-      SpaceUtils.getUserPortal().saveNode(parentNode, null);
-
-      space.setUrl(newNodeName);
-      SpaceUtils.changeSpaceUrlPreference(renamedNode, space, newNodeLabel);
-      SpaceUtils.changeAppPageTitle(renamedNode, newNodeLabel);
-
-      List<UserNode> userNodes = new ArrayList<>(renamedNode.getChildren());
-      for (UserNode childNode : userNodes) {
-        SpaceUtils.changeSpaceUrlPreference(childNode, space, newNodeLabel);
-        SpaceUtils.changeAppPageTitle(childNode, newNodeLabel);
-      }
-      return renamedNode;
-    } catch (Exception e) {
-      LOG.warn(e.getMessage(), e);
-      return null;
-    }
-  }
-
-  /**
-   * Get the space url.
-   *
-   * @param node
-   * @return
-   */
-  public static String getSpaceURL(UserNode node) {
-    RequestContext ctx = RequestContext.getCurrentInstance();
-    NodeURL nodeURL = ctx.createURL(NodeURL.TYPE);
-    return nodeURL.setNode(node).toString();
   }
 
 }
