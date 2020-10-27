a07159f4786705890b11d88bbf6a03ba271c5aa0
AzmiTouil <atouil@exoplatform.com>
2020-10-27 15:26:56 +0100

    Update PR

--- webapp/portlet/src/main/webapp/spaces-administration/components/ExoSpaceTemplate.vue
+++ webapp/portlet/src/main/webapp/spaces-administration/components/ExoSpaceTemplate.vue
@@ -31,7 +31,7 @@ export default {
   },
   computed: {
     applications() {
-      let applications = `${this.$t('Stream.label.name')}, `;
+      let applications = `${this.$t(`${this.template.spaceHomeApplication.portletName}.label.name`)}, `;
       const size = this.template.spaceApplicationList.length;
       for (let i=0; i<size-1; i++) {
         applications += `${this.$t(`${this.template.spaceApplicationList[i].portletName}.label.name`)}, `;
