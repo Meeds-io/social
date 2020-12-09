39c57a638ce2a5afc9fb5cd49924935f6efd8193
Nick Riabovol <45879164+NickEngineer@users.noreply.github.com>
2020-12-09 18:12:18 +0200

    Clean SpaceMenu component from extension and extract init to mounted for SpaceTitleActionComponents (#454)
    * Code cleanup for SpaceTitleActionComponents
    
    * Removed initTitleActionComponents() from template, moved it to mounted(), code cleanup
    
    * Turn back check for container length
    
    * Cleaned logs in spaceMenu
    
    Co-authored-by: Kate Nechyporchuk <kachankatrin@gmail.com>

--- webapp/portlet/src/main/webapp/space-menu/components/SpaceTitleActionComponents.vue
+++ webapp/portlet/src/main/webapp/space-menu/components/SpaceTitleActionComponents.vue
@@ -2,22 +2,9 @@
   <v-app class="white">
     <div class="space-title-action-components">
       <div
-        v-for="action in spaceTitleActionComponents"
-        v-if="action.enabled"
+        v-for="action in enabledComponents"
         :key="action.key"
-        :class="`${action.appClass} ${action.typeClass}`"
-        :ref="action.key"
-      >
-        <div v-if="action.component">
-          <component
-            v-dynamic-events="action.component.events"
-            v-bind="action.component.props ? action.component.props : {}"
-            :is="action.component.name"
-          ></component>
-        </div>
-        <div v-else-if="action.element" v-html="action.element.outerHTML"></div>
-        <div v-else-if="action.html" v-html="action.html"></div>
-        {{ initTitleActionComponents }}
+        :class="`${action.appClass} ${action.typeClass}`" :ref="action.key">
       </div>
     </div>
   </v-app>
@@ -25,44 +12,30 @@
 <script>
 import { spaceTitleActionComponents } from '../extension.js';
 export default {
-  data() {
-    return {
-      spaceTitleActionComponents: spaceTitleActionComponents,
-      isMounted: null,
-      resolveMounting: null
-    };
+  components: {
+    spaceTitleActionComponents
   },
   computed: {
-    initTitleActionComponents() {
-      let components;
-      spaceTitleActionComponents.map(action => components = this.initTitleActionComponent(action));
-      return components;
-    }
-  },
-  created() {
-    const thevue = this;
-    this.isMounted = new Promise(function(resolve) {
-      thevue.resolveMounting = resolve;
-    });
+    enabledComponents() {
+      return spaceTitleActionComponents.filter(action => action.enabled);
+    },
   },
   mounted() {
-    this.resolveMounting();
+    spaceTitleActionComponents.map(action => this.initTitleActionComponent(action));
   },
   methods: {
     initTitleActionComponent(action) {
       if (action.init && !action.isStartedInit && action.enabled) {
         action.isStartedInit = true;
-        this.isMounted.then(() => {
-          let container = this.$refs[action.key];
-          if (container && container.length > 0) {
-            container = container[0];
-            action.init(container, eXo.env.portal.spaceName);
-          } else {
-            console.error(
-              `Error initialization of the ${action.key} action component: empty container`
-            );
-          }
-        });
+        let container = this.$refs[action.key];
+        if (container && container.length > 0) {
+          container = container[0];
+          action.init(container, eXo.env.portal.spaceName);
+        } else {
+          console.error(
+            `Error initialization of the ${action.key} action component: empty container`
+          );
+        }
       }
     }
   }
