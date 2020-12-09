704d7249382d8eb34a66eabf912edadb0ed5fe6f
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2020-12-09 10:32:55 +0100

    Task-37293: Ability to enable/disable the external invitation feature from administration (#450)

--- webapp/portlet/src/main/webapp/spaces-administration/spacesAdministrationServices.js
+++ webapp/portlet/src/main/webapp/spaces-administration/spacesAdministrationServices.js
@@ -113,3 +113,41 @@ export function getBindingReportOperations() {
 export function getReport(spaceId, action, groupId, groupBindingId) {
   window.open(`${spacesConstants.SPACE_GROUP_BINDING_API}/getExport?spaceId=${spaceId}&action=${action}&group=${groupId}&groupBindingId=${groupBindingId}`, '_blank');
 }
+
+export function saveExternalFeatureStatus(status) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/settings/GLOBAL/GLOBAL/exo:externalUsers`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      value: status,
+    }),
+  }).then((resp) => {
+    if (resp && resp.ok) {
+      return resp;
+    } else {
+      throw new Error('Error when setting External Feature settings');
+    }
+  });
+}
+export function isExternalFeatureActive() {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/settings/GLOBAL/GLOBAL/exo:externalUsers`, {
+    method: 'GET',
+    credentials: 'include',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  }).then((resp) => {
+    if (resp && resp.ok) {
+      return resp.json();
+    } else if (resp && resp.status === 404) {
+      return null;
+    } else {
+      throw new Error('Error when getting External Feature settings');
+    }
+  });
+}
