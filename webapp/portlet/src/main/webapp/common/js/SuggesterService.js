7936d466dc014cf35834e76951aafc3cc7e57dc1
El kamel Hassen <helkamel@exoplatform.com>
2020-12-23 09:34:01 +0100

    TASK-38016: Enhance suggester service to allow restrict assigning to project participants

--- webapp/portlet/src/main/webapp/common/js/SuggesterService.js
+++ webapp/portlet/src/main/webapp/common/js/SuggesterService.js
@@ -38,11 +38,19 @@ function searchUsers(filter, items, typeOfRelation, searchOptions) {
     typeOfRelation: typeOfRelation || 'mention_activity_stream',
     currentUser: eXo.env.portal.userName,
   };
-  if (searchOptions) {
+
+  let params = null;
+  let url = null;
+  if (searchOptions && !searchOptions.projectId) {
     Object.assign(options, searchOptions);
+    params = $.param(options);
+    url = '/portal/rest/social/people/suggest.json?'.concat(params);
+  } else {
+    params = searchOptions.projectId;
+    url = '/portal/rest/projects/projectParticipants/'.concat(params);
   }
-  const params = $.param(options);
-  return fetch(`/portal/rest/social/people/suggest.json?${params}`, {credentials: 'include'})
+
+  return fetch(url, {credentials: 'include'})
     .then(resp => resp && resp.ok && resp.json())
     .then(data => {
       if (data) {
