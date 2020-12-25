73f0b402525b7b9420a9551af70abb411ef6733c
Moez Diha <38471028+mdiha@users.noreply.github.com>
2020-12-25 17:13:08 +0100

    MAINT-37615: Allow the space manager to enable or disable the chat of a space. (#453)
    I implemented a solution in order to acquire external components from add-ons.

--- webapp/portlet/src/main/webapp/space-settings/components/SpaceSettings.vue
+++ webapp/portlet/src/main/webapp/space-settings/components/SpaceSettings.vue
@@ -1,7 +1,12 @@
 <template>
   <v-app class="transparent" flat>
     <space-setting-general :space-id="spaceId" class="mb-6" />
-    <space-setting-applications :space-id="spaceId" />
+    <space-setting-applications :space-id="spaceId" class="mb-6" />
+    <template v-if="displaySpaceExternalSettings">
+      <template v-for="(spaceExternalSetting, index) in spaceExternalSettings">
+        <component :index="index" :space-id="spaceId" :is="spaceExternalSetting" :key="spaceExternalSetting.name"></component>
+      </template>
+    </template>
   </v-app>
 </template>
 
@@ -9,7 +14,21 @@
 export default {
   data: () => ({
     spaceId: eXo.env.portal.spaceId,
+    displaySpaceExternalSettings: true,
+    spaceExternalSettings: []
   }),
+  created() {
+    // get external components
+    document.addEventListener('chat-external-updated', () => {
+      if (extensionRegistry) {
+        const components = extensionRegistry.loadComponents('SpaceSettings-external-component').map(component => component.componentOptions.componentImpl);
+        this.spaceExternalSettings.push(...components);
+      }
+    });
+    
+    document.addEventListener('hideSettingsApps', () => this.displaySpaceExternalSettings = false);
+    document.addEventListener('showSettingsApps', () => this.displaySpaceExternalSettings = true);
+  },
   mounted() {
     this.$nextTick().then(() => this.$root.$emit('application-loaded'));
   },
