99bac96fba3ac0d858ac10fd6b0531e943c4bad3
AzmiTouil <atouil@exoplatform.com>
2021-01-27 15:06:46 +0100

    Edit DLP keywords via drawer

--- webapp/portlet/src/main/webapp/dlp-quarantine/components/DlpKeywordsEditor.vue
+++ webapp/portlet/src/main/webapp/dlp-quarantine/components/DlpKeywordsEditor.vue
@@ -1,38 +1,98 @@
 <template>
-  <div>
-    <v-card v-if="keywordsEdit" flat class="py-0">
-      <v-card-text class="pb-0">
-        <extended-textarea
-          v-model="keywords"
-          :max-length="keywordsTextLength"
-          :placeholder="$t('items.dlp.editKeyword.placeholder')"
-          class="py-0"
-          type="text"
-        />
-      </v-card-text>
-      <v-card-actions class="px-4">
-        <v-spacer />
-        <v-btn
-          class="btn mr-2"
-          @click="keywordsEdit = false;getDlpKeywords()">
-          {{ $t('items.dlp.editKeyword.cancel') }}
-        </v-btn>
-        <v-btn
-          class="btn btn-primary"
-          @click="saveDlpKeywords">
-          {{ $t('items.dlp.editKeyword.save') }}
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    <v-card-text v-else>
-      <div>
-        <p @mouseover="showEditBtn = true"
-           @mouseleave="showEditBtn = false">
-          {{ keywords }} <v-btn v-if="showEditBtn" dark width="20" height="20" icon @click="keywordsEdit = true"><v-icon size="20" color="primary">mdi-pencil</v-icon></v-btn>
-        </p>
-      </div>
+  <v-app>
+    <v-card-text>
+      <v-row no-gutters>
+        <div class="my-auto mr-6">
+          <h4 class="font-weight-bold ma-0">keywords</h4>
+        </div>
+        <div>
+          <v-chip-group
+            active-class="primary--text"
+            column
+          >
+            <v-chip
+              v-for="keyword in keywords"
+              :key="keyword"
+              outlined
+              class="my-0"
+            >
+              {{ keyword }}
+            </v-chip>
+          </v-chip-group>
+        </div>
+        <div class="my-auto">     
+          <v-btn
+            icon
+            outlined
+            small
+            @click="editKeywords">
+            <i v-if="keywords.length" class="uiIconEdit uiIconLightBlue pb-2" />
+            <i v-else class="uiIconSocSimplePlus uiIconLightBlue" />
+          </v-btn>
+        </div>
+      </v-row>
     </v-card-text>
-  </div>
+    <exo-drawer
+      ref="keywordsDrawer"
+      class="keywordsDrawer"
+      right>
+      <template slot="title">
+        {{ $t('items.dlp.editKeyword.title') }}
+      </template>
+      <template slot="content">
+        <v-card flat>
+          <v-card-text class="pb-0">
+            <input
+              ref="InputKeyword"
+              :placeholder="$t('items.dlp.editKeyword.placeholder')"
+              class="ignore-vuetify-classes InputKeyword"
+              @keyup.enter="AddKeyword"
+            />
+          </v-card-text>
+          <v-card-text>
+            <v-chip-group
+              active-class="primary--text"
+              column
+            >
+              <v-chip
+                v-for="keyword in modifyingKeywords"
+                :key="keyword"
+                close
+                outlined
+                class="my-0"
+                @click:close="removeKeyword(keyword)"
+              >
+                {{ keyword }}
+              </v-chip>
+            </v-chip-group>
+          </v-card-text>
+          <v-card-text v-if="error">
+            <v-alert type="error">
+              {{ error }} 
+            </v-alert>
+          </v-card-text>
+        </v-card>
+      </template>
+      <template slot="footer">
+        <div class="d-flex">
+          <v-spacer />
+          <v-btn
+            :disabled="saving"
+            class="btn mr-2"
+            @click="cancel">
+            {{ $t('items.dlp.editKeyword.cancel') }}
+          </v-btn>
+          <v-btn
+            :disabled="saving"
+            :loading="saving"
+            class="btn btn-primary"
+            @click="saveDlpKeywords">
+            {{ $t('items.dlp.editKeyword.save') }}
+          </v-btn>
+        </div>
+      </template>
+    </exo-drawer>
+  </v-app>
 </template>
 
 <script>
@@ -42,11 +102,18 @@ export default {
   data () {
     return {
       keywordsEdit: false,
-      keywords :'',
-      keywordsTextLength : 1000,
+      keywords: [],
+      modifyingKeywords : [],
+      keywordTextLength : 100,
       showEditBtn : false, 
-      saving : null
+      saving : null,
+      error : null,
     };
+  },
+  watch: {
+    modifyingKeywords() {
+      this.error = null;
+    },
   },
   created() {
     this.getDlpKeywords();
@@ -54,20 +121,52 @@ export default {
   methods : {
     getDlpKeywords() {
       getDlpKeywords().then(keywords => {
-        this.keywords = keywords;
-        this.keywordsEdit = keywords === '';
+        if (keywords !== '') {
+          this.keywords = keywords.split(',');
+          this.modifyingKeywords = keywords.split(',');
+        }
       });
     },
-    saveDlpKeywords() {
-      if (this.keywords.trim()) {
-        this.$settingService.setSettingValue('GLOBAL', 'DLP', 'GLOBAL', 'DlpKeywords', 'exo:dlpKeywords', this.keywords);
-        this.keywordsEdit = false;
-      } else {
-        this.$settingService.setSettingValue('GLOBAL', 'DLP', 'GLOBAL', 'DlpKeywords', 'exo:dlpKeywords', '');
-        this.keywords ='';
-        this.keywordsEdit = true;
+    AddKeyword() {
+      const input = this.$refs.InputKeyword.value;
+      if (input !== '') {
+        const index = this.modifyingKeywords.indexOf(input);
+        if (input.split(' ').length > 1) {
+          this.error = this.$t('items.dlp.editKeyword.formatError');
+          window.setTimeout(() => {
+            this.error = null;
+          }, 5000);
+        } else if (index > -1) {
+          this.error = this.$t('items.dlp.editKeyword.alreadyAdded');
+          window.setTimeout(() => {
+            this.error = null;
+          }, 5000);
+        } else {
+          this.modifyingKeywords.push(this.$refs.InputKeyword.value);
+          this.$refs.InputKeyword.value = '';
+        }
       }
-    }
+    },
+    removeKeyword(keyword) {
+      const index = this.modifyingKeywords.indexOf(keyword);
+      if (index > -1) {
+        this.modifyingKeywords.splice(index, 1);
+      }
+    },
+    editKeywords() {
+      this.$refs.keywordsDrawer.open();
+      this.getDlpKeywords();
+    },
+    saveDlpKeywords() {
+      this.$settingService.setSettingValue('GLOBAL', 'DLP', 'GLOBAL', 'DlpKeywords', 'exo:dlpKeywords', this.modifyingKeywords.join());
+      this.$refs.InputKeyword.value = '';
+      this.$refs.keywordsDrawer.close();
+      this.keywords = this.modifyingKeywords;
+    },
+    cancel() {
+      this.$refs.InputKeyword.value = '';
+      this.$refs.keywordsDrawer.close();
+    },
   }
 };
 </script>
