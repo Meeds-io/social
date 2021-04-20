9a06744f7beab747e0732390c069b5c5d51444c0
AzmiTouil <atouil@exoplatform.com>
2021-04-20 15:17:33 +0100

    Task-43073: Hide the add users button for the delegated administrator

--- webapp/portlet/src/main/webapp/common/js/UserService.js
+++ webapp/portlet/src/main/webapp/common/js/UserService.js
@@ -24,6 +24,19 @@ export function isSuperUser() {
   });
 }
 
+export function isDelegatedAdministrator() {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/users/isDelegatedAdministrator`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then(resp => {
+    if (!resp || !resp.ok) {
+      throw new Error('Response code indicates a server error', resp);
+    } else {
+      return resp.json();
+    }
+  });
+}
+
 export function isSynchronizedUserAllowedToChangePassword() {
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/users/isSynchronizedUserAllowedToChangePassword`, {
     method: 'GET',
