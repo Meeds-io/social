8f6c1eaa5953adc48bb028dcd916edf1cee75b46
Ahmed <achakroun@exoplatform.com>
2021-05-18 10:18:46 +0100

    Task-44094 : User Type on UserManegement search is not updated when an internal user become external (#705)
    Before this fix, when we add an internal user to an external group, the profile property external isn't updated.
    This fix will ensure that when we add users to an external group, the profile external property is changed to true.

---
+++ component/core/src/main/java/org/exoplatform/social/core/listeners/ExternalUsersListenerImpl.java
@@ -0,0 +1,59 @@
+package org.exoplatform.social.core.listeners;
+
+import org.exoplatform.commons.utils.CommonsUtils;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+import org.exoplatform.services.organization.Membership;
+import org.exoplatform.services.organization.MembershipEventListener;
+import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.identity.model.Profile;
+import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.webui.exception.MessageException;
+
+public class ExternalUsersListenerImpl extends MembershipEventListener {
+  private static final Log LOG = ExoLogger.getLogger(ExternalUsersListenerImpl.class);
+
+  private static final String PLATFORM_EXTERNALS_GROUP  = "/platform/externals";
+
+  private IdentityManager identityManager;
+
+  @Override
+  public void postSave(Membership m, boolean isNew) {
+    if (m.getGroupId().equals(PLATFORM_EXTERNALS_GROUP)) {
+      Identity userIdentity = getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, m.getUserName());
+      Profile profile = userIdentity.getProfile();
+      if (profile != null) {
+        profile.setProperty(Profile.EXTERNAL, String.valueOf(true));
+        try {
+          getIdentityManager().updateProfile(profile, true);
+        } catch (MessageException e) {
+          LOG.error("Error while saving the external property for user profile {}", m.getUserName(), e);
+        }
+      }
+    }
+  }
+
+  @Override
+  public void postDelete(Membership m) {
+    if (m.getGroupId().equals(PLATFORM_EXTERNALS_GROUP)) {
+      Identity userIdentity = getIdentityManager().getOrCreateIdentity(OrganizationIdentityProvider.NAME, m.getUserName());
+      Profile profile = userIdentity.getProfile();
+      if (profile != null) {
+        profile.setProperty(Profile.EXTERNAL, String.valueOf(false));
+        try {
+          getIdentityManager().updateProfile(profile, true);
+        } catch (MessageException e) {
+          LOG.error("Error while saving the external property for user profile {}", m.getUserName(), e);
+        }
+      }
+    }
+  }
+
+  private IdentityManager getIdentityManager() {
+    if (identityManager == null) {
+      identityManager = CommonsUtils.getService(IdentityManager.class);
+    }
+    return identityManager;
+  }
+}
