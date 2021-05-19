b68703cc09c3aba49d8092378288f8f542ad4147
Romain Dénarié <romain.denarie@exoplatform.com>
2021-05-19 11:58:38 +0200

    Task-43955 : Make MFA extensible
    Before this fix, all mfa solutions are present in social, and it is not possible to extend it
    This commit add the possibility to extend and configure which MFA system is active

--- webapp/portlet/src/main/webapp/mfa-access/components/MfaAccess.vue
+++ webapp/portlet/src/main/webapp/mfa-access/components/MfaAccess.vue
@@ -1,8 +1,29 @@
 <template>
   <v-app>
-    <mfa-fido-access />
+    <!--mfa-fido-access /-->
+    <component
+      v-if="extension"
+      :ref="extension.id"
+      v-model="extension.component.model.value"
+      :is="extension.component.name" />
   </v-app>
 </template>
 <script>
+import {getSettings} from '../mfaSettings.js';
+
+export default {
+  data() {
+    return {
+      extension: null,
+    };
+  },
+  mounted() {
+    getSettings().then(settings => {
+      this.extension = extensionRegistry.loadExtensions('mfa-extension', 'mfa-extension')
+        .find(plugin => plugin.id === settings.mfaSystem);
+      console.log(this.extension);
+    });
+  },
+};
 
 </script>
