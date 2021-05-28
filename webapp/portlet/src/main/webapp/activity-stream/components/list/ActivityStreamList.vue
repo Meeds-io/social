8d3f92ce59290636c5a9a974d07c08d9ba2cd3a3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-05-28 04:38:20 +0100

    TASK-44600 Add activity Stream load more button (#731)

--- webapp/portlet/src/main/webapp/activity-stream/components/list/ActivityStreamList.vue
+++ webapp/portlet/src/main/webapp/activity-stream/components/list/ActivityStreamList.vue
@@ -5,6 +5,15 @@
       :key="activity.id"
       :activity="activity"
       class="mb-6" />
+    <v-btn
+      v-if="hasMore"
+      :loading="loading"
+      :disabled="loading"
+      block
+      class="btn border-box-sizing"
+      @click="loadMore">
+      {{ $t('Search.button.loadMore') }}
+    </v-btn>
   </div>
 </template>
 
@@ -16,6 +25,7 @@ export default {
     limit: 10,
     spaceId: eXo.env.portal.spaceId,
     userName: eXo.env.portal.userName,
+    hasMore: false,
     loading: false,
     display: false,
   }),
@@ -43,15 +53,28 @@ export default {
   methods: {
     loadActivities() {
       this.loading = true;
+      // Load 'limit + 1' instead of only 'limit' to avoid retrieving count of user activities
+      // Which can be CPU consuming in server side.
+      // If the retrieved elements count > 'limit', then there are more elements to retrieve,
+      // else no more elements to retrieve
+      const limitToRetrieve = this.limit + 1;
       if (this.spaceId) {
-        this.$spaceService.getSpaceActivities(this.spaceId, this.limit, 'identity')
-          .then(data => this.activities = data && data.activities || [])
+        this.$spaceService.getSpaceActivities(this.spaceId, limitToRetrieve, 'identity')
+          .then(data => this.handleRetrievedActivities(data && data.activities || []))
           .finally(() => this.loading = false);
       } else {
-        this.$userService.getUserActivities(this.userName, this.limit, 'identity')
-          .then(data => this.activities = data && data.activities || [])
+        this.$userService.getUserActivities(this.userName, limitToRetrieve, 'identity')
+          .then(data => this.handleRetrievedActivities(data && data.activities || []))
           .finally(() => this.loading = false);
       }
+    },
+    handleRetrievedActivities(activities) {
+      this.activities = activities.slice(0, this.limit);
+      this.hasMore = activities.length > this.limit;
+    },
+    loadMore() {
+      this.limit += this.pageSize;
+      this.loadActivities();
     },
   },
 };
