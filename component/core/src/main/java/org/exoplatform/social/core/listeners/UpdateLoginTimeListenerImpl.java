0ac418b627bbf7431c96583e4829aa92de746f8e
ahmadmido <chakroun.ahmed.enetcom@gmail.com>
2021-06-08 16:59:51 +0100

    Task-44498 : No result found when search a user after applying Connected filter

--- component/core/src/main/java/org/exoplatform/social/core/listeners/UpdateLoginTimeListenerImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/listeners/UpdateLoginTimeListenerImpl.java
@@ -1,5 +1,6 @@
 package org.exoplatform.social.core.listeners;
 
+import org.exoplatform.commons.search.index.IndexingService;
 import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.services.listener.Asynchronous;
 import org.exoplatform.services.listener.Event;
@@ -12,6 +13,7 @@ import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.identity.model.Profile;
 import org.exoplatform.social.core.identity.provider.OrganizationIdentityProvider;
+import org.exoplatform.social.core.jpa.search.ProfileIndexingServiceConnector;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.web.CacheUserProfileFilter;
 import org.exoplatform.webui.exception.MessageException;
@@ -24,16 +26,20 @@ public class UpdateLoginTimeListenerImpl extends Listener<ConversationRegistry, 
 
   public void onEvent(Event<ConversationRegistry, ConversationState> event) {
     IdentityManager identityManager = CommonsUtils.getService(IdentityManager.class);
+    IndexingService indexingService = CommonsUtils.getService(IndexingService.class);
     ConversationState state = event.getData();
     User user = (User) state.getAttribute(CacheUserProfileFilter.USER_PROFILE);
     Identity userIdentity = identityManager.getOrCreateIdentity(OrganizationIdentityProvider.NAME,
                                                                 event.getData().getIdentity().getUserId());
     Profile profile = userIdentity.getProfile();
-    profile.setProperty(Profile.LAST_LOGIN_TIME, user != null ? user.getLastLoginTime() : Calendar.getInstance().getTime());
-    try {
-      identityManager.updateProfile(profile, true);
-    } catch (MessageException e) {
-      LOG.error("Error while updating the last login time for user profile {}", user.getUserName(), e);
+    if (profile != null) {
+      profile.setProperty(Profile.LAST_LOGIN_TIME, user != null ? user.getLastLoginTime() : Calendar.getInstance().getTimeInMillis());
+      try {
+        identityManager.updateProfile(profile, true);
+        indexingService.reindex(ProfileIndexingServiceConnector.TYPE, userIdentity.getId());
+      } catch (MessageException e) {
+        LOG.error("Error while updating the last login time for user profile {}", user.getUserName(), e);
+      }
     }
   }
 }
