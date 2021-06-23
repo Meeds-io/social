4b732de897c61f78edccd463f1c2872259dc12df
Romain Dénarié <romain.denarie@exoplatform.com>
2021-06-23 14:54:28 +0200

    Remove fido code (#787)

--- webapp/portlet/src/main/webapp/mfa-access/initComponents.js
+++ webapp/portlet/src/main/webapp/mfa-access/initComponents.js
@@ -1,30 +1,15 @@
 import MfaAccess from './components/MfaAccess.vue';
 import OtpAccess from './components/OtpAccess.vue';
-import FidoAccess from './components/FidoAccess.vue';
 
 const components = {
   'mfa-otp-access': OtpAccess,
   'mfa-access': MfaAccess,
-  'mfa-fido-access': FidoAccess,
 };
 
 for (const key in components) {
   Vue.component(key, components[key]);
 }
 
-
-const fidoExtensionPlugin = {
-  id: 'fido2',
-  title: 'fido',
-  enabled: () => true,
-  component: {
-    name: 'mfa-fido-access',
-    model: {
-      value: [],
-      default: []
-    }
-  }
-};
 
 const otpExtensionPlugin = {
   id: 'otp',
@@ -39,6 +24,4 @@ const otpExtensionPlugin = {
   }
 };
 
-
-extensionRegistry.registerExtension('mfa-extension', 'mfa-extension', fidoExtensionPlugin);
 extensionRegistry.registerExtension('mfa-extension', 'mfa-extension', otpExtensionPlugin);
