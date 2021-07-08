50fa3a739b2cccfc6af16b72b33b79f851fbe67b
ahmadmido <chakroun.ahmed.enetcom@gmail.com>
2021-07-08 14:09:05 +0100

    Fix sonnar issue

--- component/core/src/main/java/org/exoplatform/social/core/listeners/UpdateLoginTimeListenerImpl.java
+++ component/core/src/main/java/org/exoplatform/social/core/listeners/UpdateLoginTimeListenerImpl.java
@@ -5,7 +5,8 @@ import java.util.stream.Collectors;
 
 import org.exoplatform.commons.utils.CommonsUtils;
 import org.exoplatform.services.listener.*;
-import org.exoplatform.services.organization.Group;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.User;
 import org.exoplatform.services.security.ConversationRegistry;
@@ -18,6 +19,9 @@ import org.exoplatform.web.CacheUserProfileFilter;
 
 @Asynchronous
 public class UpdateLoginTimeListenerImpl extends Listener<ConversationRegistry, ConversationState> {
+
+  private static final Log LOG = ExoLogger.getLogger(UpdateLoginTimeListenerImpl.class);
+
   public void onEvent(Event<ConversationRegistry, ConversationState> event) {
     IdentityManager identityManager = CommonsUtils.getService(IdentityManager.class);
     OrganizationService organizationService = CommonsUtils.getService(OrganizationService.class);
@@ -28,12 +32,12 @@ public class UpdateLoginTimeListenerImpl extends Listener<ConversationRegistry, 
     Profile profile = userIdentity.getProfile();
     if (profile != null) {
       profile.setProperty(Profile.LAST_LOGIN_TIME, user != null ? user.getLastLoginTime() : Calendar.getInstance().getTimeInMillis());
-      if(profile.getProperty(Profile.GROUPS_IDS) == null) {
+      if(profile.getProperty(Profile.GROUPS_IDS) == null && user != null) {
         try {
           List<String> userGroups = organizationService.getGroupHandler().findGroupsOfUser(user.getUserName()).stream().map(g -> g.getId()).collect(Collectors.toList());
           profile.setProperty(Profile.GROUPS_IDS, String.join(",", userGroups));
         } catch (Exception e) {
-          throw new RuntimeException("Can't get the groups of user '" + user.getUserName(), e);
+          LOG.error("Can't get the groups of user '" + user.getUserName(), e);
         }
       }
       identityManager.updateProfile(profile, true);
