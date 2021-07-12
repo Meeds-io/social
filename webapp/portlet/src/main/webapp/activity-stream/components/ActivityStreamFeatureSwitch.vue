2938e3b8d27c1ba215ec3356de7824f80124ad54
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-07-12 08:42:58 +0100

    TASK-46600 Install skeleton loader for Activity stream

--- webapp/portlet/src/main/webapp/activity-stream/components/ActivityStreamFeatureSwitch.vue
+++ webapp/portlet/src/main/webapp/activity-stream/components/ActivityStreamFeatureSwitch.vue
@@ -3,11 +3,10 @@
     <v-img
       src="/social-portlet/activity-stream/comingSoonV2.png"
       width="190px"
+      height="50px"
       max-width="100%"
-      class="mx-auto mt-6"
-      @load="displayText" />
+      class="mx-auto mt-6" />
     <div
-      v-if="imageLoaded"
       class="v-label font-weight-bold mt-0 mb-6 text-color"
       v-sanitized-html="comingSoonText">
     </div>
@@ -16,20 +15,12 @@
 
 <script>
 export default {
-  data: () => ({
-    imageLoaded: false,
-  }),
   computed: {
     comingSoonText() {
       return this.$t('activityStream.comingSoonV2', {
         0: `<a href="${eXo.env.portal.context}/${eXo.env.portal.portalName}/tribe-feedback">`,
         1: '</a>',
       });
-    },
-  },
-  methods: {
-    displayText() {
-      window.setTimeout(() => this.imageLoaded = true, 200);
     },
   },
 };
