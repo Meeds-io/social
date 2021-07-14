d96c98def6ed9f6dfe0c2aea5f65f960529f0848
NaderSai <nsay@exoplatform.com>
2021-07-14 10:57:39 +0100

    Task-46724: Add design for qr code

--- webapp/portlet/src/main/webapp/user-setting-mobile/components/MobileSettingsWindow.vue
+++ webapp/portlet/src/main/webapp/user-setting-mobile/components/MobileSettingsWindow.vue
@@ -20,38 +20,47 @@
       </v-toolbar-title>
       <v-spacer />
     </v-toolbar>
-    <div class="d-flex py-6 my-6 justify-space-between align-center text-left">
+    <div class="d-flex mx-11 py-6 my-6 justify-space-between flex-wrap align-center text-left">
       <div>
         <v-list-item>
-          <v-list-item-content class="px-11 pb-0 pt-2 mt-auto mb-12">
-            <v-list-item-title class="dark-grey-color font-weight-bold">
+          <v-list-item-content class="pb-0 pt-2 mt-auto mb-12">
+            <v-list-item-title class="dark-grey-color font-weight-bold text-wrap">
               {{ $t('MobileSettings.subtitle') }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
-          <v-list-item-content class="px-11 pb-0 pt-2 mt-auto mb-7">
-            <v-list-item-title class="text-color">
+          <v-list-item-content class="pb-0 pt-2 mt-auto mb-5">
+            <v-list-item-title class="text-color text-wrap">
               {{ $t('MobileSettings.step.one') }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
-          <v-list-item-content class="px-11 pb-0 pt-2 mt-auto mb-7">
-            <v-list-item-title class="text-color">
+          <v-list-item-content class="pb-0 pt-2 mt-auto mb-5">
+            <v-list-item-title class="text-color text-wrap">
               {{ $t('MobileSettings.step.two') }}<v-icon class="primary--text primaryBackground mx-1 py-1 px-1" size="14">mdi-scan-helper</v-icon><span class="font-weight-medium">{{ $t('MobileSettings.bg') }}</span>
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
         <v-list-item>
-          <v-list-item-content class="px-11 pb-0 pt-2 mt-auto mb-7">
-            <v-list-item-title class="text-color">
+          <v-list-item-content class="pb-0 pt-2 mt-auto mb-5">
+            <v-list-item-title class="text-color text-wrap">
               {{ $t('MobileSettings.step.three') }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </div>
-      <div class="mx-16" id="qrcode"></div>
+      <div class="ma-7">
+        <div style="background-image: url(/social-portlet/images/cadre.png); background-position: center; background-size: contain;height: 190px;width: 190px;display: flex;align-items: center;justify-content: space-around;">
+          <div
+            class="mt-2"
+            height="145"
+            width="145"
+            id="qrcode">
+          </div>
+        </div>
+      </div>
     </div>
   </v-card>
 </template>
@@ -61,7 +70,16 @@ export default {
   mounted(){
     // Options
     const options = {
-      text: ((window.location.port > 0) ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}/portal/login?username=${eXo.env.portal.userName}` : `${window.location.protocol}//${window.location.hostname}/portal/login?username=${eXo.env.portal.userName}`)
+      text: ((window.location.port > 0) ? `${window.location.protocol}//${window.location.hostname}:${window.location.port}/portal/login?username=${eXo.env.portal.userName}` : `${window.location.protocol}//${window.location.hostname}/portal/login?username=${eXo.env.portal.userName}`),
+      width: 123,
+      height: 123,
+      colorLight: '#e1e8ee',
+      logo: '/social-portlet/images/logo.png',
+      logoBackgroundColor: '#e1e8ee',
+      logoWidth: 38,
+      quietZone: 8,
+      quietZoneColor: '#e1e8ee',
+      drawer: 'svg',
     };
 
     // Create new QRCode Object
