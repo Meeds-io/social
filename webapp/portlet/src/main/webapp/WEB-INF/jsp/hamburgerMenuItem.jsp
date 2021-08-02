091e6fc9db1eab240efbc5c3973b3411b75a244b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-02 17:02:29 +0100

    TASK-47234 Defer loading of Javascript of widgets after Page Loading (#869)

--- webapp/portlet/src/main/webapp/WEB-INF/jsp/hamburgerMenuItem.jsp
+++
@@ -1,4 +0,0 @@
-<script type="text/javascript" defer="defer">
-  <% String jsModule = ((String[])request.getAttribute("jsModule"))[0]; %>
-  eXo.env.portal.onLoadCallbacks.push(() => require(['<%=jsModule%>']));
-</script>
--- webapp/portlet/src/main/webapp/vue-apps/hamburger-menu/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/hamburger-menu/main.js
@@ -18,11 +18,16 @@ const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
 const lang = eXo && eXo.env && eXo.env.portal && eXo.env.portal.language || 'en';
 const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portal.HamburgerMenu-${lang}.json`;
 
+document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+
 export function init() {
   exoi18n.loadLanguageAsync(lang, url)
     .then(i18n => {
       // init Vue app when locale ressources are ready
       new Vue({
+        mounted() {
+          document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
+        },
         template: '<exo-hamburger-menu-navigation></exo-hamburger-menu-navigation>',
         i18n,
         vuetify,
