e5fbe18b79bf0e85992d0a2fda88733f6944f1fa
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-05 08:20:04 +0100

    TASK-47592 Fix Avatars New Style with Img HTML Tag Prior to this change, the user avatars and the activity link image hadn't preserved its style after changing the usage of <v-img> to <img>. This fix will ensure to preserve exactly the same behavior

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/header/ActivityHeadUser.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/header/ActivityHeadUser.vue
@@ -7,8 +7,7 @@
     <v-list-item-avatar :size="size" class="ma-0">
       <img
         :src="avatarUrl"
-        :width="`${size}px`"
-        :height="`${size}px`"
+        :style="{minHeight: 'fit-content', minWidth: 'fit-content', objectFit: 'cover'}"
         class="my-auto">
     </v-list-item-avatar>
   </a>
