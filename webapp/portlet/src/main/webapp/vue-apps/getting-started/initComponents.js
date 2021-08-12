40ac825e182e05734c2191af0aad5c279b73068a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-12 18:41:07 +0100

    TASK-47872 Enhance Getting Started Widget Performances Display Prior to this change, the widget was initialized using Javascript which can lead to bad FrontEnd performances by multiplying the REST calls and JS execution to initialize Vue application. This change will make the application HTML built in Server side since it's a simple widget display

--- webapp/portlet/src/main/webapp/vue-apps/getting-started/initComponents.js
+++
@@ -1,9 +0,0 @@
-import ExoGettingStarted from './components/ExoGettingStarted.vue';
-
-const components = {
-  'exo-getting-started': ExoGettingStarted,
-};
-
-for (const key in components) {
-  Vue.component(key, components[key]);
-}
--- webapp/portlet/src/main/webapp/vue-apps/getting-started/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/getting-started/main.js
@@ -1,42 +1,17 @@
-import './initComponents.js';
-
-import * as gettingStartedService from './js/gettingStartedService.js';
-
-window.Object.defineProperty(Vue.prototype, '$gettingStartedService', {
-  value: gettingStartedService,
-});
-
-// getting language of the PLF 
-const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
-
-// should expose the locale ressources as REST API 
-const url = `${Vue.prototype.$spacesConstants.PORTAL}/${Vue.prototype.$spacesConstants.PORTAL_REST}/i18n/bundle/locale.portlet.social.GettingStartedPortlet-${lang}.json`;
-
-
-// get overrided components if exists
-if (extensionRegistry) {
-  const components = extensionRegistry.loadComponents('GettingStarted');
-  if (components && components.length > 0) {
-    components.forEach(cmp => {
-      Vue.component(cmp.componentName, cmp.componentOptions);
-    });
-  }
+export function hideGettingStarted() {
+  return fetch(`${Vue.prototype.$spacesConstants.PORTAL_CONTEXT}/${Vue.prototype.$spacesConstants.PORTAL_REST}/getting-started`, {
+    method: 'DELETE',
+    credentials: 'include',
+  }).then((resp) => {
+    if (!resp || !resp.ok) {
+      throw new Error('Error deleting getting started');
+    }
+  });
 }
 
-Vue.use(Vuetify);
-const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
-
-const appId = 'GettingStartedPortlet';
-
-export function init() {
-  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
-    const appElement = document.createElement('div');
-    appElement.id = appId;
-
-    Vue.createApp({
-      template: `<exo-getting-started v-cacheable id="${appId}"></exo-getting-started>`,
-      i18n,
-      vuetify,
-    }, appElement, 'Getting Started');
-  });
+const parentAppElement = document.querySelector('#GettingStartedPortlet .btClose');
+if (parentAppElement) {
+  parentAppElement.onclick = () => {
+    hideGettingStarted().then(() => document.querySelector('#GettingStartedPortlet').parentElement.remove());
+  };
 }
