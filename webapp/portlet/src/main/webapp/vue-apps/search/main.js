c044779feaaeaea24e95a34793927a7cc382c503
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-12 09:20:59 +0100

    TASK-47812 Defer initialization of Search assets until it's required by user (#916)
    Prior to this change, the Search app initializes its assets (CSS & Resource Bundles) in page display phase. This change will defer the initialization of Search app only when the user clicks on Search icon or displaying the search page. The Search button display will be made using JSP page.

--- webapp/portlet/src/main/webapp/vue-apps/search/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/search/main.js
@@ -1,37 +1,19 @@
 import './initComponents.js';
 
-Vue.use(Vuetify);
-const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
-
 const appId = 'SearchApplication';
 const appName = 'Search';
 
-//getting language of the PLF 
-const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
-
-//should expose the locale ressources as REST API 
-const urls = [`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Portlets-${lang}.json`];
-
-export function init(connectors) {
-  document.dispatchEvent(new CustomEvent('vue-app-loading-start', {detail: appName}));
-  if (connectors && connectors.length) {
-    connectors.forEach(connector => {
-      if (connector.i18nBundle) {
-        urls.push(`${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/${connector.i18nBundle}-${lang}.json`);
-      }
-    });
-  }
-
-  exoi18n.loadLanguageAsync(lang, urls, 'defer')
-    .then(i18n => {
-  
-      Vue.createApp({
-        data: {
-          connectors: connectors,
-        },
-        template: `<search-application id="${appId}" :connectors="connectors" />`,
-        vuetify,
-        i18n
-      }, `#${appId}`, appName);
-    });
+export function init() {
+  document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+  const connectors = JSON.parse(document.getElementById('searchConnectorsDefaultValue').value);
+  const skinUrls = JSON.parse(document.getElementById('searchSkinUrlsDefaultValue').value);
+  Vue.createApp({
+    data: {
+      connectors,
+      skinUrls,
+    },
+    template: `<search-application id="${appId}" :connectors="connectors" :skin-urls="skinUrls" />`,
+    vuetify: Vue.prototype.vuetifyOptions,
+    i18n: exoi18n.i18n,
+  }, `#${appId}`, appName);
 }
