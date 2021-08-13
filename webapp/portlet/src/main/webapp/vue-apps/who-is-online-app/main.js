fd9d42e991d1a91d270b47e47db872b88ba4caf4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-13 10:28:12 +0100

    TASK-47877 Enhance Who Is Online Widget UI Loading Performances Prior tot his change, the online users are retrieved from server end by a seperate request, this change will get the list of online users from server end at the same time than the document DOM.

--- webapp/portlet/src/main/webapp/vue-apps/who-is-online-app/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/who-is-online-app/main.js
@@ -6,9 +6,6 @@ const lang = `${Vue.prototype.$spacesConstants.LANG}`;
 // should expose the locale ressources as REST API
 const url = `${Vue.prototype.$spacesConstants.PORTAL}/${Vue.prototype.$spacesConstants.PORTAL_REST}/i18n/bundle/locale.portlet.whoisonline.whoisonline-${lang}.json`;
 
-Vue.use(Vuetify);
-const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
-
 // get overrided components if exists
 if (extensionRegistry) {
   const components = extensionRegistry.loadComponents('WhoIsOnLinePortlet');
@@ -20,18 +17,30 @@ if (extensionRegistry) {
 }
 
 const appId = 'OnlinePortlet';
-const cacheId = `${appId}_${eXo.env.portal.spaceId || ''}`;
+const appName = 'Who is Online';
 
 // getting locale ressources
 export function init() {
-  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
-    const appElement = document.createElement('div');
-    appElement.id = appId;
-
-    Vue.createApp({
-      template: `<exo-who-is-online v-cacheable="{cacheId: '${cacheId}'}" id="${appId}"></exo-who-is-online>`,
-      i18n,
-      vuetify
-    }, appElement, 'Who is Online');
-  });
+  document.dispatchEvent(new CustomEvent('vue-app-loading-start', {detail: {
+    appName: appName,
+    time: Date.now(),
+  }}));
+  exoi18n.loadLanguageAsync(lang, url)
+    .then(() => {
+      const onlineUsers = JSON.parse(document.getElementById('whoIsOnlineDefaultValue').value);
+      if (onlineUsers && onlineUsers.length) {
+        const avatars = JSON.parse(document.getElementById('whoIsOnlineAvatarsDefaultValue').value);
+        onlineUsers.forEach(user => {
+          user.avatar = avatars[user.userId];
+        });
+      }
+      Vue.createApp({
+        data: {
+          onlineUsers,
+        },
+        template: `<exo-who-is-online id="${appId}"></exo-who-is-online>`,
+        vuetify: Vue.prototype.vuetifyOptions,
+        i18n: exoi18n.i18n,
+      }, `#${appId}`, appName);
+    });
 }
