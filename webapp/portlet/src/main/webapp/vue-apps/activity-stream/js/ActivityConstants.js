fdb6b52c43ba4d1d54c7a2859731a1b1a10333c3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-08-15 09:08:45 +0100

    TASK-47892 Improve Stream App loading Performances Prior tot his change, the activity list fetch Request is retrieved late in Page display process. This change will ensure to preload early in page display process the activity ids to display in page (computed in dispatched JSP file of Generic Portlet) and in the REST API call of activity ids, preload requested activities with single Link Header query per activity.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
@@ -1,6 +1,7 @@
 export default {
   // Attention!!! when changing this, the list of preloaded
   // URLs has to change in JSP as well
+  FULL_ACTIVITY_IDS_EXPAND: 'ids,identity,likes,shared,commentsPreview,subComments',
   FULL_ACTIVITY_EXPAND: 'identity,likes,shared,commentsPreview,subComments',
   ACTIVITY_EXPAND: 'identity,likes,shared',
   FULL_COMMENT_EXPAND: 'identity,likes,subComments',
