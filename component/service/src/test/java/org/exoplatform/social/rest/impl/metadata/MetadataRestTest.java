11fb7700f91065f3f2286d3abdfaaf506536e970
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-09-22 10:53:50 +0100

    TASK-49104 Allow to override or ignore existing Metadata Item

--- component/service/src/test/java/org/exoplatform/social/rest/impl/metadata/MetadataRestTest.java
+++ component/service/src/test/java/org/exoplatform/social/rest/impl/metadata/MetadataRestTest.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 
 import org.exoplatform.services.rest.impl.ContainerResponse;
 import org.exoplatform.social.core.identity.model.Identity;
+import org.exoplatform.social.core.jpa.storage.dao.jpa.MetadataDAO;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.metadata.MetadataService;
 import org.exoplatform.social.metadata.MetadataTypePlugin;
@@ -37,11 +38,14 @@ public class MetadataRestTest extends AbstractResourceTest {
 
   private MetadataRest           metadataRest;
 
+  private MetadataDAO            metadataDAO;
+
   @Override
   public void setUp() throws Exception {
     super.setUp();
     identityManager = getContainer().getComponentInstanceOfType(IdentityManager.class);
     metadataService = getContainer().getComponentInstanceOfType(MetadataService.class);
+    metadataDAO = getContainer().getComponentInstanceOfType(MetadataDAO.class);
     try {
       metadataService.addMetadataTypePlugin(userMetadataTypePlugin);
     } catch (UnsupportedOperationException e) {
@@ -74,6 +78,7 @@ public class MetadataRestTest extends AbstractResourceTest {
     identityManager.deleteIdentity(raulIdentity);
     identityManager.deleteIdentity(paulIdentity);
 
+    metadataDAO.deleteAll();
     super.tearDown();
   }
 
@@ -105,6 +110,70 @@ public class MetadataRestTest extends AbstractResourceTest {
     assertEquals(metadataItemEntity.getAudienceId(), entity.getAudienceId());
   }
 
+  public void testCreateDuplicatedMetadataItem() throws Exception {
+    MetadataItemEntity metadataItemEntity = new MetadataItemEntity(0,
+                                                                   "name",
+                                                                   "objectType",
+                                                                   "objectId",
+                                                                   "parentObjectId",
+                                                                   Long.parseLong(johnIdentity.getId()),
+                                                                   Long.parseLong(johnIdentity.getId()),
+                                                                   Collections.singletonMap("testMetadataItem1Key",
+                                                                                            "testMetadataItem1Value"));
+
+    String metadataItemEntityJson = toJsonString(metadataItemEntity);
+
+    startSessionAs(rootIdentity.getRemoteId());
+    ContainerResponse response = getResponse("POST",
+                                             getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName())
+                                                 + "?ignoreWhenExisting=true",
+                                             metadataItemEntityJson);
+    assertEquals(401, response.getStatus());
+
+    startSessionAs(johnIdentity.getRemoteId());
+    response = getResponse("POST",
+                           getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName()),
+                           metadataItemEntityJson);
+    assertEquals(200, response.getStatus());
+    MetadataItemEntity entity = (MetadataItemEntity) response.getEntity();
+    assertNotNull(entity);
+    assertTrue(entity.getId() > 0);
+    assertEquals(metadataItemEntity.getObjectType(), entity.getObjectType());
+    assertEquals(metadataItemEntity.getObjectId(), entity.getObjectId());
+    assertEquals(metadataItemEntity.getParentObjectId(), entity.getParentObjectId());
+    assertEquals(metadataItemEntity.getName(), entity.getName());
+    assertEquals(metadataItemEntity.getCreatorId(), entity.getCreatorId());
+    assertEquals(metadataItemEntity.getAudienceId(), entity.getAudienceId());
+
+    response = getResponse("POST",
+                           getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName()),
+                           metadataItemEntityJson);
+    assertEquals(409, response.getStatus());
+
+    startSessionAs(rootIdentity.getRemoteId());
+    response = getResponse("POST",
+                           getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName())
+                               + "?ignoreWhenExisting=true",
+                           metadataItemEntityJson);
+    assertEquals(401, response.getStatus());
+
+    startSessionAs(johnIdentity.getRemoteId());
+    response = getResponse("POST",
+                           getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName())
+                               + "?ignoreWhenExisting=true",
+                           metadataItemEntityJson);
+    assertEquals(200, response.getStatus());
+    MetadataItemEntity existingEntity = (MetadataItemEntity) response.getEntity();
+    assertNotNull(existingEntity);
+    assertEquals(entity.getId(), existingEntity.getId());
+    assertEquals(metadataItemEntity.getObjectType(), existingEntity.getObjectType());
+    assertEquals(metadataItemEntity.getObjectId(), existingEntity.getObjectId());
+    assertEquals(metadataItemEntity.getParentObjectId(), existingEntity.getParentObjectId());
+    assertEquals(metadataItemEntity.getName(), existingEntity.getName());
+    assertEquals(metadataItemEntity.getCreatorId(), existingEntity.getCreatorId());
+    assertEquals(metadataItemEntity.getAudienceId(), existingEntity.getAudienceId());
+  }
+
   public void testDeleteMetadataItem() throws Exception {
     MetadataItemEntity metadataItemEntity = new MetadataItemEntity(0,
                                                                    "name",
@@ -117,19 +186,27 @@ public class MetadataRestTest extends AbstractResourceTest {
                                                                                             "testMetadataItem1Value"));
 
     startSessionAs(johnIdentity.getRemoteId());
-    String metadataItemEntityJson = toJsonString(metadataItemEntity);
     ContainerResponse response = getResponse("POST",
                                              getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName()),
-                                             metadataItemEntityJson);
+                                             toJsonString(metadataItemEntity));
     assertEquals(200, response.getStatus());
     MetadataItemEntity entity = (MetadataItemEntity) response.getEntity();
     assertNotNull(entity);
     assertTrue(entity.getId() > 0);
 
+    startSessionAs(rootIdentity.getRemoteId());
     response = getResponse("DELETE",
                            getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName() + "/name/"
                                + entity.getId()),
-                           metadataItemEntityJson);
+                           null);
+    assertEquals(401, response.getStatus());
+
+    startSessionAs(johnIdentity.getRemoteId());
+    response = getResponse("DELETE",
+                           getURLResource("metadatas/" + userMetadataTypePlugin.getMetadataType().getName() + "/name/"
+                               + entity.getId()),
+                           null);
+    assertEquals(200, response.getStatus());
     entity = (MetadataItemEntity) response.getEntity();
     assertNotNull(entity);
     assertTrue(entity.getId() > 0);
@@ -139,6 +216,19 @@ public class MetadataRestTest extends AbstractResourceTest {
     assertEquals(metadataItemEntity.getName(), entity.getName());
     assertEquals(metadataItemEntity.getCreatorId(), entity.getCreatorId());
     assertEquals(metadataItemEntity.getAudienceId(), entity.getAudienceId());
+  }
+
+  public void testDeleteNotExistingMetadataItem() throws Exception {
+    startSessionAs(johnIdentity.getRemoteId());
+    ContainerResponse response = getResponse("DELETE",
+                                             getURLResource("metadatas/user/name/522"),
+                                             null);
+    assertEquals(404, response.getStatus());
+
+    response = getResponse("DELETE",
+                           getURLResource("metadatas/user/name/522") + "?ignoreNotExisting=true",
+                           null);
+    assertEquals(204, response.getStatus());
   }
 
   public final class UserOnlyMetadataPlugin extends MetadataTypePlugin {
@@ -174,6 +264,11 @@ public class MetadataRestTest extends AbstractResourceTest {
     @Override
     public boolean canDeleteMetadataItem(MetadataItem metadataItem, long userIdentityId) {
       return userIdentityId > 0 && metadataItem.getMetadata().getAudienceId() == userIdentityId;
+    }
+
+    @Override
+    public boolean allowMultipleItemsPerObject() {
+      return false;
     }
   }
 
