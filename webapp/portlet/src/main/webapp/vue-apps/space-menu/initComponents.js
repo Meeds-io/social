6de924c1b484715f67065743736045e41835bca4
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2021-10-14 17:42:53 +0100

    MAINT-42333: Implement a new space Top banner Logo popover (#1020)

--- webapp/portlet/src/main/webapp/vue-apps/space-menu/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/space-menu/initComponents.js
@@ -1,10 +1,7 @@
 import SpaceMenu from './components/SpaceMenu.vue';
-import SpaceTitleActionComponents from './components/SpaceTitleActionComponents.vue';
-
 
 const components = {
   'space-menu': SpaceMenu,
-  'space-title-action-components': SpaceTitleActionComponents,
 };
 
 for (const key in components) {
