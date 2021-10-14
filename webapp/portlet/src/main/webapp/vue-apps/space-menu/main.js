6878156b9c6338c3011fb14d6abd67e21b13fc09
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2021-10-14 17:42:53 +0100

    MAINT-42333: Implement a new space Top banner Logo popover (#1020)
    MAINT-42333:Space Top bar Logo portlet config adjustments (#1028)
    
    Space Top bar Logo portlet config adjustments by adding missing module and also add a check to return an empty string when no description was added to a space .
    
    Fix new space top logo banner popover feedback (#1034)
    
    MAINT-42333: Show space popover on space title hovering (#1037)

--- webapp/portlet/src/main/webapp/vue-apps/space-menu/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/space-menu/main.js
@@ -13,9 +13,6 @@ if (extensionRegistry) {
 const appId = 'SpaceMenu';
 const cacheId = `${appId}_${eXo.env.portal.spaceId}`;
 
-const appIdAction = 'SpaceTitleActionComponnetsContainer';
-const cachedIdActions = `${appIdAction}_${eXo.env.portal.spaceId}`;
-
 export function init(settings) {
   const appElement = document.createElement('div');
   appElement.id = appId;
@@ -28,16 +25,4 @@ export function init(settings) {
     template: `<space-menu v-cacheable="{cacheId: '${cacheId}'}" id="${appId}" :navigations="navigations" :selected-navigation-uri="selectedNavigationUri" />`,
     vuetify: Vue.prototype.vuetifyOptions,
   }, appElement, 'Space Menu');
-
-  const actionsElement = document.createElement('div');
-  actionsElement.id = appIdAction;
-
-  new Vue({
-    mounted() {
-      this.$root.$emit('application-loaded');
-    },
-    template: `<space-title-action-components v-cacheable="{cacheId: '${cachedIdActions}'}" id="${appIdAction}" />`,
-    vuetify: Vue.prototype.vuetifyOptions,
-  }).$mount(actionsElement);
-
 }
