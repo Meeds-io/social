30ee013cb94dc3def91a89519712d9e16f70ae22
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2021-10-19 12:41:00 +0100

    TASK-50322 Improve activity likers computing while displaying list in stream Prior to this change, a lot of liker identities are retrieved when displaying the activity in Stream page, while we display only 4 at most in Activity Reactions section. This improvement will make likers loading made lazy when displaying Activity Reactions Drawer, and limit the retrieved list of likers to 4 when retrieving Activity Details to display on stream.

--- webapp/portlet/src/main/webapp/vue-apps/common/js/ActivityService.js
+++ webapp/portlet/src/main/webapp/vue-apps/common/js/ActivityService.js
@@ -118,6 +118,19 @@ export function deleteActivity(id, hide) {
   });
 }
 
+export function getActivityLikers(id, offset, limit) {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/activities/${id}/likes?offset=${offset || 0}&limit=${limit || 50}`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then(resp => {
+    if (!resp || !resp.ok) {
+      throw new Error('Response code indicates a server error', resp);
+    } else {
+      return resp.json();
+    }
+  });
+}
+
 export function likeActivity(id) {
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/activities/${id}/likes`, {
     method: 'POST',
