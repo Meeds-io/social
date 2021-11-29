8679dac6175539ed496014c05649d36d9b85f56b
mohamed <mrdidi@exoplatform.com>
2021-11-29 15:06:14 +0100

    update name of event

--- webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
@@ -25,13 +25,13 @@ export default {
   }),
   created() {
     // add external components
-    const externalComponents = extensionRegistry.loadComponents('external-space').map(component => component.componentOptions.componentImpl);
+    const externalComponents = extensionRegistry.loadComponents('external-apps-space-settings').map(component => component.componentOptions.componentImpl);
     this.spaceExternalSettings.push(...externalComponents);
     document.addEventListener('hideSettingsApps', () => this.displayed = true);
     document.addEventListener('showSettingsApps', () => this.displayed = true);
   },
   mounted() {
-    document.addEventListener('component-external-space-settings-updated', (event) => {
+    document.addEventListener('component-external-apps-space-settings-space-settings-updated', (event) => {
       if (event && event.detail ) {
         this.$spaceService.getSpaceApplications(this.spaceId).then(applications => {
           if (applications.find(app => app.id === event.detail.componentOptions.appId) || !event.detail.componentOptions.appId) {
