981599b4beb44d8bc12ee9143dfd7c1946ebdda5
Mohamed El Hadi Rdid <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2021-11-29 17:18:58 +0100

    Add condition if application is installed in space (#1094)

--- webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
@@ -1,16 +1,17 @@
 <template>
-  <v-app class="transparent" flat v-if="displayed">
+  <v-app
+    class="transparent"
+    flat
+    v-if="displayed">
     <space-setting-general :space-id="spaceId" class="mb-6" />
     <space-setting-applications :space-id="spaceId" class="mb-6" />
-    <template>
-      <template v-for="(spaceExternalSetting, index) in spaceExternalSettings">
-        <component
-          class="mb-6"
-          :index="index"
-          :space-id="spaceId"
-          :is="spaceExternalSetting"
-          :key="spaceExternalSetting.name" />
-      </template>
+    <template v-for="(spaceExternalSetting, index) in spaceExternalSettings">
+      <component
+        class="mb-6"
+        :index="index"
+        :space-id="spaceId"
+        :is="spaceExternalSetting"
+        :key="spaceExternalSetting.name" />
     </template>
   </v-app>
 </template>
@@ -24,19 +25,21 @@ export default {
   }),
   created() {
     // add external components
-    const externalComponents = extensionRegistry.loadComponents('external-space').map(component => component.componentOptions.componentImpl);
+    const externalComponents = extensionRegistry.loadComponents('external-apps-space-settings').map(component => component.componentOptions.componentImpl);
     this.spaceExternalSettings.push(...externalComponents);
-
-    document.addEventListener('addSpaceSettingsExternalComponents', (event) => {
-      if (event && event.detail) {
-        this.spaceExternalSettings.push(event.detail.componentImpl);
-      }
-    });
-
     document.addEventListener('hideSettingsApps', () => this.displayed = true);
     document.addEventListener('showSettingsApps', () => this.displayed = true);
   },
   mounted() {
+    document.addEventListener('component-external-apps-space-settings-space-settings-updated', (event) => {
+      if (event && event.detail ) {
+        this.$spaceService.getSpaceApplications(this.spaceId).then(applications => {
+          if (applications.find(app => app.id === event.detail.componentOptions.appId) || !event.detail.componentOptions.appId) {
+            this.spaceExternalSettings.push(event.detail.componentOptions.componentImpl);
+          }
+        });
+      }
+    });
     this.$nextTick().then(() => this.$root.$applicationLoaded());
   },
 };
