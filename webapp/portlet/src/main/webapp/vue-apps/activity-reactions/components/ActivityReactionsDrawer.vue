d12be7996ed9d9baf11f7d5ee8e1755b29b4440f
nidhalnidhal <nmbarki@exoplatform.com>
2021-11-30 15:38:15 +0100

    TASK-48499 : Updates

--- webapp/portlet/src/main/webapp/vue-apps/activity-reactions/components/ActivityReactionsDrawer.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-reactions/components/ActivityReactionsDrawer.vue
@@ -103,7 +103,7 @@ export default {
         this.openSelectedTab(event.detail.activityId, event.detail.tab, event.detail.activityType);
       }
     });
-    this.$root.$on(`open-reaction-drawer-selected-tab-${this.activityId}`,event => {
+    this.$root.$on(`open-reaction-drawer-selected-tab-${this.activityId}`, event => {
       this.openSelectedTab(event.activityId, event.tab, event.activityType);
     });
   },
