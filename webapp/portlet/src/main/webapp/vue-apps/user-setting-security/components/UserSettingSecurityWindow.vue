1b512d7bee80d02d295e627da93f394889d901ec
Noussairhamrit <noussair.hamrit@esprit.tn>
2021-12-21 17:34:55 +0100

    code refactoring

--- webapp/portlet/src/main/webapp/vue-apps/user-setting-security/components/UserSettingSecurityWindow.vue
+++ webapp/portlet/src/main/webapp/vue-apps/user-setting-security/components/UserSettingSecurityWindow.vue
@@ -106,7 +106,7 @@
 const USER_NOT_FOUND_ERROR_CODE = 'USER_NOT_FOUND';
 const WRONG_USER_PASSWORD_ERROR_CODE = 'WRONG_USER_PASSWORD';
 const PASSWORD_UNKNOWN_ERROR_CODE = 'PASSWORD_UNKNOWN_ERROR_CODE';
-const WRONG_NEW_PASSWORD_ERROR_CODE = 'WRONG_NEW_PASSWORD';
+const UNCHANGED_NEW_PASSWORD_ERROR_CODE = 'UNCHANGED_NEW_PASSWORD';
 
 export default {
   data: () => ({
@@ -168,7 +168,7 @@ export default {
               this.error = this.$t('UserSettings.label.accountNotExist');
             } else if (error.indexOf(PASSWORD_UNKNOWN_ERROR_CODE) > -1) {
               this.error = this.$t('UserSettings.label.changePasswordFail');
-            } else if (error.indexOf(WRONG_NEW_PASSWORD_ERROR_CODE) > -1) {
+            } else if (error.indexOf(UNCHANGED_NEW_PASSWORD_ERROR_CODE) > -1) {
               this.error = this.$t('UserSettings.label.changePasswordIdentical');
             } else {
               this.error = error;
