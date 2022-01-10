cd870b42fdf36584b30031de65e9be61d58f62a7
Amine0008 <60577231+Amine0008@users.noreply.github.com>
2022-01-10 09:55:29 +0100

    task45533: added a settings page where the user can download the mobile application
    added a settings page where the user can download the mobile application by either scanning a QR code or navigating directly to the relevant app store by clicking on the appropriate store icon

--- webapp/portlet/src/main/webapp/vue-apps/user-setting-mobile/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/user-setting-mobile/initComponents.js
@@ -1,10 +1,11 @@
 import MobileSettings from './components/MobileSettings.vue';
 import MobileSettingsWindow from './components/MobileSettingsWindow.vue';
-
+import MobileSettingsDownloadWindow from './components/MobileSettingsDownloadWindow.vue';
 
 const components = {
   'mobile-settings': MobileSettings,
   'mobile-settings-window': MobileSettingsWindow,
+  'mobile-settings-download-window': MobileSettingsDownloadWindow,
 };
 
 for (const key in components) {
