4ed310a33497e4414753d96c9c8932f62ca4bd27
Noussairhamrit <noussair.hamrit@esprit.tn>
2022-01-11 09:09:51 +0100

    code refactoring

--- component/api/src/main/java/org/exoplatform/social/common/ObjectAlreadyExistsException.java
+++ component/api/src/main/java/org/exoplatform/social/common/ObjectAlreadyExistsException.java
@@ -17,11 +17,11 @@
 package org.exoplatform.social.common;
 
 /**
- * @since 6.3.x
- * @deprecated Deprecated from 6.3.x. Replaced by a new Exception org.exoplatform.commons.ObjectAlreadyExistsException
- *             will be removed at 6.3.x
+ * @since 6.3.0
+ * @deprecated Deprecated from 6.3.0. Replaced by a new Exception org.exoplatform.commons.ObjectAlreadyExistsException
+ *             will be removed at 6.3.0
  */
-@Deprecated(since="6.3.x", forRemoval=true)
+@Deprecated(since="6.3.0", forRemoval=true)
 public class ObjectAlreadyExistsException extends org.exoplatform.commons.ObjectAlreadyExistsException {
 
     public ObjectAlreadyExistsException(Object existingObject) {
