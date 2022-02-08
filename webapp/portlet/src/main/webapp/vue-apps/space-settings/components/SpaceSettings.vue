f163e8eaf911a6f852673e4e16cf2620ec87f44f
Mohamed El Hadi Rdid <54801286+Rdidmohamedelhadi@users.noreply.github.com>
2022-02-08 15:34:46 +0100

    Add condition if application is installed in space (#1291)

--- webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettings.vue
@@ -25,25 +25,26 @@ export default {
     spaceExternalSettings: []
   }),
   created() {
-    this.$spaceService.getSpaceApplications(this.spaceId)
-      .then(applications => 
-        applications.map(app =>  {
-          console.log(`${app.id} ${app}`);
-          this.spaceApplications.push(app.id);
-        })
-      ).then(() => {
-        document.addEventListener('component-external-apps-space-settings-space-settings-updated', (event) => {
-          if (event && event.detail ) {
-            if (this.spaceApplications.find(app => app.id === event.detail.componentOptions.appId) || !event.detail.componentOptions.appId) {
-              this.spaceExternalSettings.push(event.detail.componentOptions.componentImpl);
+    if (this.spaceId) {
+      this.$spaceService.getSpaceApplications(this.spaceId)
+        .then(applications =>
+          applications.map(app =>  {
+            this.spaceApplications.push(app.id);
+          })
+        ).then(() => {
+          document.addEventListener('component-external-apps-space-settings-space-settings-updated', (event) => {
+            if (event && event.detail ) {
+              if (this.spaceApplications.find(app => app.id === event.detail.componentOptions.appId) || !event.detail.componentOptions.appId) {
+                this.spaceExternalSettings.push(event.detail.componentOptions.componentImpl);
+              }
             }
-          }
-        });
-        document.addEventListener('hideSettingsApps', () => this.displayed = true);
-        document.addEventListener('showSettingsApps', () => this.displayed = true);
-        const externalComponents = extensionRegistry.loadComponents('external-apps-space-settings').map(component => component.componentOptions.componentImpl);
-        this.spaceExternalSettings.push(...externalComponents);
-      }).finally(() => this.$root.$applicationLoaded());
+          });
+          document.addEventListener('hideSettingsApps', () => this.displayed = false);
+          document.addEventListener('showSettingsApps', () => this.displayed = true);
+          const externalComponents = extensionRegistry.loadComponents('external-apps-space-settings').map(component => component.componentOptions.componentImpl);
+          this.spaceExternalSettings.push(...externalComponents);
+        }).finally(() => this.$root.$applicationLoaded());
+    }
   }
 };
 </script>
