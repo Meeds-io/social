5fa074eb55e1ea138c43baad492d014319ab57b9
nidhalnidhal <nmbarki@exoplatform.com>
2022-03-01 15:47:11 +0100

    TASK-53671 : Opening the search on tags from comments drawer

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/list/ActivityCommentsDrawer.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/list/ActivityCommentsDrawer.vue
@@ -5,6 +5,7 @@
     id="activityCommentsDrawer"
     allow-expand
     right
+    fixed
     disable-pull-to-refresh
     @closed="reset">
     <template slot="title">
@@ -101,6 +102,7 @@ export default {
   created() {
     document.addEventListener('activity-comments-display', this.displayActivityComments);
     document.addEventListener('activity-comment-edit', this.editActivityComments);
+    document.addEventListener('search-metadata-tag', this.closeDrawer);
 
     this.$root.$on('activity-comment-created', this.hideCommentRichEditor);
     this.$root.$on('activity-comment-updated', this.hideCommentRichEditor);
@@ -121,6 +123,10 @@ export default {
       this.limit = this.pageSize;
       this.hideCommentRichEditor();
       this.$root.selectedCommentId = null;
+    },
+    closeDrawer() {
+      this.reset();
+      this.$refs.activityCommentsDrawer.close();
     },
     addComment(comment) {
       this.$root.selectedCommentId = comment.id;
