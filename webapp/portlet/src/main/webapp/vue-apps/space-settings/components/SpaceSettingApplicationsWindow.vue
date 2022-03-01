67adf7b62327bc8cda9147d1e818688c80e09b68
GouadriaHanen <hgouadria@exoplatform.com>
2022-03-01 10:52:51 +0100

    clean up code

--- webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettingApplicationsWindow.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettingApplicationsWindow.vue
@@ -95,9 +95,7 @@ export default {
     },
     removeConfirm() {
       this.$spaceService.removeApplication(this.spaceId, this.selectedApplication.id)
-        .then(() => {
-          this.$emit('refresh');
-        });
+        .then(() => this.$emit('refresh'));
     },
     openDrawer() {
       this.$refs.spaceSettingAddApplicationDrawer.open();
