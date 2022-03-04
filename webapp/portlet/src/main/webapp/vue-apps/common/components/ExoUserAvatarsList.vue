d61c6a4c6ab9426453905dd62f34c249ea0f58bd
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-03-04 17:05:02 +0100

    Task-54855: Enhance the user avatar list display (#1353)

--- webapp/portlet/src/main/webapp/vue-apps/common/components/ExoUserAvatarsList.vue
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ExoUserAvatarsList.vue
@@ -1,9 +1,30 @@
 <template>
-  <div class="d-flex flex-nowrap position-relative">
+  <div v-if="!retrieveExtraInformation" class="d-flex flex-nowrap position-relative">
     <exo-user-avatar
       v-for="user in usersToDisplay"
       :key="user"
       :identity="user"
+      :size="iconSize"
+      :extra-class="'mx-1'"
+      :popover="popover"
+      avatar />
+    <v-avatar
+      v-if="notDisplayedItems"
+      :size="iconSize"
+      :class="avatarOverlayPosition && 'position-relative' || 'position-absolute'"
+      :style="overlayStyle"
+      class="notDisplayedIdentitiesOverlay clickable"
+      @click="$emit('open-detail')">
+      <div class="notDisplayedIdentities d-flex align-center justify-center">
+        +{{ notDisplayedItems }}
+      </div>
+    </v-avatar>
+  </div>
+  <div v-else class="d-flex flex-nowrap position-relative">
+    <exo-user-avatar
+      v-for="user in usersToDisplay"
+      :key="user"
+      :profile-id="user.userName"
       :size="iconSize"
       :extra-class="'mx-1'"
       :popover="popover"
@@ -33,6 +54,10 @@ export default {
       type: Number,
       default: () => 0,
     },
+    defaultLength: {
+      type: Number,
+      default: () => 0,
+    },
     iconSize: {
       type: Number,
       default: () => 37,
@@ -57,14 +82,14 @@ export default {
   },
   computed: {
     usersToDisplay() {
-      if (this.retrieveExtraInformation) {
-        return this.spaceManagers && this.spaceManagers.slice(0, this.max);
-      } else {
-        return this.users && this.users.slice(0, this.max);
-      }
+      return this.users && this.users.slice(0, this.max);
     },
     notDisplayedItems() {
-      return this.users && this.users.length > this.max ? this.users.length - this.max : 0;
+      if (this.defaultLength) {
+        return this.defaultLength > this.max ? this.defaultLength - this.max : 0;
+      } else {
+        return this.users && this.users.length > this.max ? this.users.length - this.max : 0;
+      }
     },
     overlayStyle(){
       if (this.avatarOverlayPosition) {
@@ -74,14 +99,9 @@ export default {
     },
   },
   created() {
-    if (this.retrieveExtraInformation) {
-      this.users.forEach(user => {
-        this.$userService.getUser(user.userName)
-          .then(item => {
-            this.spaceManagers.push(item);
-          });
-      });
-    }
-  }
+    this.$root.$on('refresh-avatars-list', username => {
+      this.users.push({'userName': username});   
+    });
+  },
 };
 </script>
