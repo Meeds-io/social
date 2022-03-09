5c939e2f96fc472c92c7959fb871e2267fe49efe
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-03-09 15:05:17 +0100

    Task-54679: Inform the user of the new way to attach a file in the activity composer drawer (#1358)
    This PR will add a new plugin related to the attach file feature to determines whether we should notify the user or not by the new way of attaching a file in the activity composer

---
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ChangesReminder.vue
@@ -0,0 +1,81 @@
+<template>
+  <v-app class="border-box-sizing transparent" flat>
+    <v-dialog
+      v-model="dialog"
+      content-class="rounded-lg"
+      overlay-opacity="0.7"
+      width="500"
+      @click:outside="dialog = false">
+      <v-card class="pb-4">
+        <v-card-title class="text-h5 font-weight-bold dark-grey-color pb-6">
+          {{ $t('changes.reminder.WhatNew') }}
+          <v-spacer />
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on, attrs }">
+              <div
+                v-bind="attrs"
+                v-on="on">
+                <v-btn
+                  icon
+                  @click="dialog = false">
+                  <v-icon size="18">mdi-close</v-icon>
+                </v-btn>
+              </div>
+            </template>
+            <span>
+              {{ $t('label.close') }}
+            </span>
+          </v-tooltip>
+        </v-card-title>
+        <v-card-text><img :src="reminder.img"></v-card-text>
+        <v-card-text class="text-subtitle-1 font-weight-bold dark-grey-color">{{ reminder.title }}</v-card-text>
+        <v-card-text>
+          {{ reminder.description }}
+        </v-card-text>
+        <v-card-actions class="pe-6">
+          <v-spacer />
+          <v-btn
+            text
+            class="primary--text text-subtitle-1 font-weight-bold"
+            @click="doNotRemindMe">
+            {{ $t('changes.reminder.doNotRemind') }}
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  props: {
+    reminder: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data () {
+    return {
+      dialog: false,
+    };
+  },
+  created() {
+    document.addEventListener('changes-reminder-open' , () => this.getReminderStatus());
+  },
+  methods: {
+    getReminderStatus() {
+      return this.$featureService.isFeatureEnabled(this.reminder.name).then(status => {
+        this.dialog = status;
+      });
+    },
+    doNotRemindMe() {
+      this.$settingService.setSettingValue('USER', eXo.env.portal.userName, 'APPLICATION', 'changesReminder', this.reminder.name, true)
+        .then(() => {
+          this.dialog = false;
+        });
+    }
+  }
+
+};
+</script>
