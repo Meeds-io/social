a215cc4e1b563e798b711485403bb8f543cfd7a5
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-03-21 20:55:48 +0100

    Task-55473: Hide the attach option in the share drawer editor and in the kudos drawer (#1377)
    Prior this change, attach file icon is displayed in share activity and send kudos composer

--- webapp/portlet/src/main/webapp/vue-apps/common/components/ExoActivityRichEditor.vue
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ExoActivityRichEditor.vue
@@ -165,7 +165,7 @@ export default {
       }
 
       const ckEditorExtensions = extensionRegistry.loadExtensions('ActivityComposer', 'ckeditor-extensions');
-      if (ckEditorExtensions && ckEditorExtensions.length && (this.ckEditorType === 'activityContent' || this.ckEditorType.startsWith('comment_'))) {
+      if (ckEditorExtensions && ckEditorExtensions.length && !['activityShare', 'kudosContent'].includes(this.ckEditorType)) {
         ckEditorExtensions.forEach(ckEditorExtension => {
           if (ckEditorExtension.extraPlugin) {
             extraPlugins = `${extraPlugins},${ckEditorExtension.extraPlugin}`;
