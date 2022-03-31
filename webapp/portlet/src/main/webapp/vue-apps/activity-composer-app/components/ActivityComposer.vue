e39f5f9e9a8084c69dc93c9b04f4d2eefda632e3
nidhal mbarki <75621053+nidhalnidhal@users.noreply.github.com>
2022-03-31 17:31:53 +0100

    TASK-54518 : display the activity filter posting activities is enabled (#1397)

--- webapp/portlet/src/main/webapp/vue-apps/activity-composer-app/components/ActivityComposer.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-composer-app/components/ActivityComposer.vue
@@ -12,7 +12,7 @@
         {{ composerButtonLabel }}
       </a>
       <select
-        v-if="standalone"
+        v-if="displayStreamFilter"
         id="filterStream"
         v-model="selectedFilter"
         name="status"
@@ -57,6 +57,9 @@ export default {
         return this.$t('activity.composer.post');
       }
     },
+    displayStreamFilter() {
+      return !eXo.env.portal.spaceId;
+    }
   },
   watch: {
     selectedFilter(newValue, oldValue) {
