0897fe723f8ece9c0d68eafdd9132d954d7cb644
hchaabene <99255294+hchaabene@users.noreply.github.com>
2022-05-24 10:38:46 +0100

    Apply suggestions from code review
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>

--- webapp/portlet/src/main/webapp/vue-apps/common/components/ExoIdentitySuggester.vue
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ExoIdentitySuggester.vue
@@ -239,7 +239,6 @@ export default {
           if (!this.previousSearchTerm || this.previousSearchTerm !== this.searchTerm) {
             this.loadingSuggestions = 0;
             this.items = [];
-            this.searchOptions.filterType='all';
             this.$suggesterService.searchSpacesOrUsers(value,
               this.items,
               this.typeOfRelations,
