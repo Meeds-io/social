c1ff2fce7ba76dbeeb51f3000e3f99cc160e5829
Montassar-Hamadi <92787967+Montassar-Hamadi@users.noreply.github.com>
2022-05-25 16:34:19 +0100

    TASK-57092 Fix redirection to members page in specific space when his admin  accept Request join (#1508)
    Problem: When an administrator of a space approved a membership request, the redirection was to the settings page.
    Fix: set the correct url redirection to page members in space  when the administrator clicks on the approve button in the mail.
    Improvement: added a new component vue AlertSpaceMembers which will be displayed when the administrator re-approves the request to join and the user is already a member of this space.

--- webapp/portlet/src/main/webapp/vue-apps/space-members/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/space-members/initComponents.js
@@ -1,11 +1,13 @@
 import SpaceMembers from './components/SpaceMembers.vue';
 import SpaceMembersToolbar from './components/SpaceMembersToolbar.vue';
 import SpaceInvitationDrawer from './components/SpaceInvitationDrawer.vue';
+import AlertSpaceMembers from './components/AlertSpaceMembers.vue';
 
 const components = {
   'space-members': SpaceMembers,
   'space-members-toolbar': SpaceMembersToolbar,
   'space-invitation-drawer': SpaceInvitationDrawer,
+  'alert-space-members': AlertSpaceMembers,
 };
 
 for (const key in components) {
