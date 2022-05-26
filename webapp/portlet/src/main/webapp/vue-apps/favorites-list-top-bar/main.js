bda97216fd1e0f39f81f9a0481ea28daf830c7d4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-05-26 04:03:45 +0100

    TASK-56737 Delete Metadatas when deleting the associated Space (#1522)
    Prior to this change, when deleting a space the associated metadatas remains, especially when displaying favorites list for a user. In fact, in favorites list drawer, the items are requested from Metadata API without referring to spaces user is member of. This fix will introduce a Space Listener to delete user personal metadata when he leave the space or the space has been deleted.

--- webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/main.js
@@ -10,17 +10,17 @@ if (extensionRegistry) {
   }
 }
 
+const appId = 'favoritesListPortlet';
+const lang = eXo.env.portal.language;
+const url = `${Vue.prototype.$spacesConstants.PORTAL}/${Vue.prototype.$spacesConstants.PORTAL_REST}/i18n/bundle/locale.portlet.Portlets-${lang}.json`;
+
 //getting locale ressources
 export function init() {
-
-  const appId = 'favoritesListPortlet';
-  const lang = eXo.env.portal.language;
-  const url = `${Vue.prototype.$spacesConstants.PORTAL}/${Vue.prototype.$spacesConstants.PORTAL_REST}/i18n/bundle/locale.portlet.Portlets-${lang}.json`;
   exoi18n.loadLanguageAsync(lang, url)
     .then(() => {
       // init Vue app when locale ressources are ready
       Vue.createApp({
-        template: `<exo-top-bar-favorites id="${appId}"></exo-top-bar-favorites>`,
+        template: `<top-bar-favorites id="${appId}" />`,
         vuetify: Vue.prototype.vuetifyOptions,
         i18n: exoi18n.i18n,
       }, `#${appId}`, 'Topbar Favorites');
