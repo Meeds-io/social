2a7c9582456702bd41e38df8f3996827791cf017
Amine Fourati <60577231+Amine0008@users.noreply.github.com>
2022-05-31 10:22:26 +0100

    TASK:56691: disable the share and comment tooltips for mobile devices
    ISSUE: v-tooltip is not compatibale with the safari browser in Mobile Device. In fact, the tooltip message stays displayed after the component is closed.
    FIX: disable Vuetify tooltip component for mobile devices.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/footer/actions/ActivityShareAction.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/footer/actions/ActivityShareAction.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="d-inline-flex ms-lg-4">
     <!-- Added for mobile -->
-    <v-tooltip bottom>
+    <v-tooltip :disabled="isMobile" bottom>
       <template #activator="{ on, attrs }">
         <v-btn
           v-if="isShareable"
@@ -67,6 +67,9 @@ export default {
     },
     shareIconColorClass() {
       return this.hasShared && 'primary--text' || 'disabled--text';
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'sm' || this.$vuetify.breakpoint.name === 'xs';
     },
   },
   created() {
