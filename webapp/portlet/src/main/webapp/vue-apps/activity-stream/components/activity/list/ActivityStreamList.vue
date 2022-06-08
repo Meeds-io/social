50564c76050abc322dc8fbdd2ffe6046ac840908
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-06-08 13:56:26 +0100

    Task-161: Fix redirect to the space when deleting an activity (#1549)
    Prior to this change when deleting an activity from the page activity details a new message with "Activity not found" is displayed.
    After this change when deleting an activity, it will be redirect to to the space's home page if it is a space activity, else it will be redirect to the default home link.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/list/ActivityStreamList.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/list/ActivityStreamList.vue
@@ -35,7 +35,7 @@
         class="mx-auto my-10" />
     </div>
     <template v-else>
-      <activity-not-found v-if="activityId" />
+      <activity-not-found v-if="activityId && !isDeleted" />
       <template v-else-if="!error">
         <activity-stream-empty-message-space v-if="spaceId" />
         <activity-stream-empty-message-user v-else />
@@ -90,6 +90,7 @@ export default {
     hasMore: false,
     loading: false,
     error: false,
+    isDeleted: false,
   }),
   computed: {
     activitiesToDisplay() {
@@ -114,6 +115,9 @@ export default {
   created() {
     document.addEventListener('activity-deleted', event => {
       const activityId = event && event.detail;
+      if (this.isActivityDetail) {
+        this.isDeleted = true;
+      }
       if (activityId) {
         const index = this.activities.findIndex(activity => activityId === activity.id);
         if (index >= 0) {
