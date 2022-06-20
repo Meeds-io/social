295e99cc049ba373698df818975a2011b73e5328
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-06-20 10:46:03 +0100

    MEED-169: Add space favorite item to favorites topbar drawer
    This PR will add a bookmarked space to the topbar favorites drawer.

--- webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/extensions.js
+++ webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/extensions.js
@@ -3,3 +3,10 @@ extensionRegistry.registerComponent('favorite-activity', 'favorite-drawer-item',
   id: 'activity',
   vueComponent: Vue.options.components['activity-favorite-item'],
 }); 
+
+ 
+extensionRegistry.registerComponent('favorite-space', 'favorite-drawer-item', {
+  id: 'space',
+  vueComponent: Vue.options.components['space-favorite-item'],
+}); 
+
