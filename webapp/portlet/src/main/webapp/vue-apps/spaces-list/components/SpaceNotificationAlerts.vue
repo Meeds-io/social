43d324296e04894c4c746dbe68faf4cafea73ef5
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-06-20 12:30:56 +0100

    MEED-167: Add bookmark a space as favorite in spaces card list (#1592)
    Add the possibility to bookmark a space from the space card list

---
+++ webapp/portlet/src/main/webapp/vue-apps/spaces-list/components/SpaceNotificationAlerts.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-snackbar
+    v-model="snackbar"
+    :left="!$vuetify.rtl"
+    :right="$vuetify.rtl"
+    color="transparent"
+    elevation="0"
+    app>
+    <exo-notification-alert
+      :alert="alert"
+      @dismissed="clear" />
+  </v-snackbar>
+</template>
+<script>
+export default {
+  data: () => ({
+    snackbar: false,
+    alert: null,
+  }),
+  watch: {
+    alert() {
+      this.snackbar = !!this.alert;
+    },
+  },
+  created() {
+    this.$root.$on('space-notification-alert', alert => this.alert = alert);
+  },
+  methods: {
+    clear() {
+      this.alert = null;
+    },
+  },
+};
+</script>
