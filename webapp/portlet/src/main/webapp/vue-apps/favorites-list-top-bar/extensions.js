d52b8af6592333fe3abafeea56ad579256b51b7f
saraboutej <sboutej@exoplatform.com>
2022-06-20 16:02:28 +0100

    MEED-169: remove empty line

--- webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/extensions.js
+++ webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/extensions.js
@@ -4,7 +4,6 @@ extensionRegistry.registerComponent('favorite-activity', 'favorite-drawer-item',
   vueComponent: Vue.options.components['activity-favorite-item'],
 }); 
 
- 
 extensionRegistry.registerComponent('favorite-space', 'favorite-drawer-item', {
   id: 'space',
   vueComponent: Vue.options.components['space-favorite-item'],
