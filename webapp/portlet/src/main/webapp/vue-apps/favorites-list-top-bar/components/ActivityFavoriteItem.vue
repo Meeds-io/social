12c77fc71309aaa3874cda4aac4814a046f92aad
saraboutej <sboutej@exoplatform.com>
2022-06-21 15:36:09 +0100

    Meed-288: Add extension for activity favorite icon

--- webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/components/ActivityFavoriteItem.vue
+++ webapp/portlet/src/main/webapp/vue-apps/favorites-list-top-bar/components/ActivityFavoriteItem.vue
@@ -1,14 +1,20 @@
 <template>
   <v-list-item class="clickable" :href="activityUrl">
-    <v-list-item-icon class="me-3 my-auto">
-      <v-icon size="22" class="icon-default-color"> fas fa-file-alt </v-icon>
+    <v-list-item-icon v-if="activityTypeExtension && activityTypeExtension.img" class="me-3 my-auto">
+      <v-img
+        :src="activityTypeExtension.img"
+        max-height="28"
+        max-width="25" />
+    </v-list-item-icon>
+    <v-list-item-icon v-else class="me-3 my-auto">
+      <v-icon size="24" :class="activityTypeExtension.class"> {{ activityTypeExtension.icon }} </v-icon> 
     </v-list-item-icon>
 
     <v-list-item-content>
       <v-list-item-title class="text-color body-2">
         <p
           class="ma-auto"
-          v-sanitized-html="activityTitle"></p>
+          v-sanitized-html="activityTypeExtension.title"></p>
       </v-list-item-title>
     </v-list-item-content>
 
@@ -32,13 +38,38 @@ export default {
       type: String,
       default: () => null,
     },
+    activityExtensions: {
+      type: Object,
+      default: () => null,
+    }
   },
   data: () => ({
     activity: null,
     activityTitle: '',
     activityUrl: '#',
-    isFavorite: true
+    isFavorite: true,
+    defaultIcon: 'fas fa-feather-alt',
+    defaultClass: 'primary--text',
+    activityTypeExtension: {},
   }),
+  watch: {
+    activity() {
+      if (this.activity?.type) {
+        this.activityExtensions.forEach(extension => {
+          if (extension.type === this.activity.type) {
+            this.activityTypeExtension.icon = extension?.icon ? extension.icon : '';
+            this.activityTypeExtension.img = extension?.img ? extension.img : '';  
+            this.activityTypeExtension.class = extension?.class ? extension.class : '';
+            this.activityTypeExtension.title = extension?.title ? extension.title(this.activity) : this.$t('UITopBarFavoritesPortlet.label.activity');
+          }
+        });
+      } else {
+        this.activityTypeExtension.icon = this.defaultIcon;
+        this.activityTypeExtension.class = this.defaultClass;
+        this.activityTypeExtension.title = this.activity?.title ? this.favoriteTitle(this.activity.title) : this.$t('UITopBarFavoritesPortlet.label.activity');
+      }
+    }
+  },
   computed: {
     spaceId() {
       return this.activity?.activityStream?.space?.id;
@@ -49,18 +80,12 @@ export default {
     this.$activityService.getActivityById(this.id)
       .then(fullActivity => {
         this.activity = fullActivity;
-        if (fullActivity && fullActivity.title ) {
-          this.activityTitle = this.favoriteTitle(fullActivity.title);
-        } else {
-          this.activityTitle = this.$t('UITopBarFavoritesPortlet.label.activity');
-        }
       });
   },
   methods: {
     favoriteTitle(title) {
       const regex = /(<([^>]+)>)/ig;
-      const activityTitle = title.replace(regex, '').split(' ').slice(0, 5).join(' ');
-      return activityTitle;
+      return title.replace(regex, '').split(' ').slice(0, 5).join(' ');
     },
     removed() {
       this.isFavorite = !this.isFavorite;
