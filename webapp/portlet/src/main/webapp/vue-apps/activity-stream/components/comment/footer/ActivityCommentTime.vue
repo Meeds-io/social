50db5a46e797a398f360ff7f6ffe0a29e3ca2b1d
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-06-28 11:01:00 +0100

    MEED-217: Access to comments (Link) (#1627)
    Prior change, user cannot share comment link.
    Update : Add browser option for comment timestamp to copy link and open in new tab

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/footer/ActivityCommentTime.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/footer/ActivityCommentTime.vue
@@ -4,7 +4,8 @@
       <template #activator="{ on, attrs }">
         <v-btn
           :height="20"
-          class="hover-underline width-auto text-capitalize-first-letter d-inline px-0"
+          :href="commentLink"
+          class="d-flex hover-underline width-auto text-capitalize-first-letter d-inline px-0"
           x-small
           link
           text
@@ -65,9 +66,16 @@ export default {
     activityPostedTime() {
       return this.comment && (this.comment.updateDate || this.comment.createDate);
     },
+    commentLink() {
+      return `${this.$root.activityBaseLink}?id=${this.commentId}`;
+    },
   },
   methods: {
-    openCommentsDrawer() {
+    openCommentsDrawer(event) {
+      if (event) {
+        event.preventDefault();
+        event.stopPropagation();
+      }
       document.dispatchEvent(new CustomEvent('activity-comments-display', {detail: {
         activity: this.activity,
         selectedCommentId: this.commentId,
