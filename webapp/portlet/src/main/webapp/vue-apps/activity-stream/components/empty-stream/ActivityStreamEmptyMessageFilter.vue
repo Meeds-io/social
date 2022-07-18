0303fefbf99f837634d740df76f9e605a6a722bb
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-07-18 16:37:18 +0100

    MEED-175: Filter the stream display (no result) (#1677)
    This PR will improve the display of the stream filter when no result found

---
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageFilter.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="d-flex flex-column white border-radius">
+    <v-flex class="d-flex my-auto border-box-sizing">
+      <div class="d-flex flex-column ma-auto py-10 text-center text-sub-title">
+        <img
+          width="70px"
+          height="70px"
+          :src="emptyStreamImage"
+          :alt="streamFilter"
+          class="mx-auto mt-8"
+          loading="lazy">
+        <span class="text-sub-title my-7">{{ emptyStreamLabel }}</span>
+      </div>
+    </v-flex>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    streamFilter: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    emptyStreamLabel() {
+      return this.$t(`activity.filter.empty_${this.streamFilter}.msg`);
+    },
+    emptyStreamImage() {
+      return `/social-portlet/images/empty_${this.streamFilter}.png`;
+    },
+  }
+};
+</script>
