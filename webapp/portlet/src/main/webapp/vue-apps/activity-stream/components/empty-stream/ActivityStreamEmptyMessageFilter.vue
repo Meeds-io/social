d5726561b1373cc8ac7cdc10285c6d7f31028767
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-07-19 16:44:43 +0100

    MEED-175: Filter the stream display (no result) (#1680)
    This PR will improve the display of the stream filter when no result found

---
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageFilter.vue
@@ -0,0 +1,50 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <div class="d-flex flex-column white border-radius">
+    <v-flex class="d-flex my-auto border-box-sizing">
+      <div class="d-flex flex-column ma-auto py-10 text-center text-sub-title">
+        <img
+          width="70px"
+          height="70px"
+          :src="emptyStreamImage"
+          :alt="streamFilter"
+          class="mx-auto mt-8"
+          loading="lazy">
+        <span class="text-sub-title my-7">{{ emptyStreamLabel }}</span>
+      </div>
+    </v-flex>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    streamFilter: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    emptyStreamLabel() {
+      return this.$t(`activity.filter.empty_${this.streamFilter}.msg`);
+    },
+    emptyStreamImage() {
+      return `/social-portlet/images/empty_${this.streamFilter}.png`;
+    },
+  }
+};
+</script>
