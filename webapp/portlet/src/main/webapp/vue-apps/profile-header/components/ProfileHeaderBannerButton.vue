2221d80116e0433c70ca1a11f1fe788831618809
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2022-07-21 17:18:52 +0100

    TASK-58470: When I modify the banner of profile I can no longer find the default banner (#1687)
    Prior to this change, when the banner of the profile changes, the banner that has been set by default can be put back.
    To fix this, add a delete button when profile banner is changed, and when clicked change the profile banner to the default banner.

--- webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderBannerButton.vue
+++ webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderBannerButton.vue
@@ -1,13 +1,39 @@
 <template>
-  <v-file-input
-    v-if="!sendingImage"
-    v-show="hover"
-    ref="bannerInput"
-    prepend-icon="fa-file-image"
-    class="changeBannerButton me-4"
-    accept="image/*"
-    clearable
-    @change="uploadBanner" />
+  <div 
+    class="d-flex flex-row changeBannerButton"
+    v-if="hover">
+    <div 
+      v-if="!isDefaultBanner"
+      class="changeBannerButtonIcon me-2">
+      <v-btn 
+        class="d-flex justify-center"
+        dark
+        outlined
+        icon
+        x-small
+        rounded
+        border
+        @click="removeBanner">
+        <v-icon
+          class="text-center"
+          icon
+          rounded>
+          mdi-delete
+        </v-icon>
+      </v-btn>
+    </div>
+    <div class="changeBannerButtonIcon">
+      <v-file-input
+        v-if="!sendingImage"
+        ref="bannerInput"
+        class="d-flex justify-center"
+        prepend-icon="fa-file-image"
+        rounded
+        accept="image/*"
+        clearable
+        @change="uploadBanner" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -21,9 +47,14 @@ export default {
       type: Boolean,
       default: () => false,
     },
+    user: {
+      type: Object,
+      default: () => null,
+    },
   },
   data: () => ({
     sendingImage: false,
+    isDefaultBanner: false,
   }),
   watch: {
     sendingImage() {
@@ -33,8 +64,19 @@ export default {
         document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
       }
     },
+    user(){
+      this.isDefaultBanner=this.user.banner.startsWith('/portal/rest/v1/social/users/default-image/');
+    }
   },
   methods: {
+    removeBanner(){     
+      return this.$userService.updateProfileField(eXo.env.portal.userName, 'banner', null)
+        .then(() => {
+          this.isDefaultBanner = true;
+          this.$emit('refresh');
+        })
+        .catch(error => this.$emit('error', error));
+    },
     uploadBanner(file) {
       if (file && file.size) {
         if (file.type && file.type.indexOf('image/') !== 0) {
@@ -48,7 +90,10 @@ export default {
         this.sendingImage = true;
         return this.$uploadService.upload(file)
           .then(uploadId => this.$userService.updateProfileField(eXo.env.portal.userName, 'banner', uploadId))
-          .then(() => this.$emit('refresh'))
+          .then(() => {
+            this.isDefaultBanner = false;
+            this.$emit('refresh');
+          })
           .catch(error => this.$emit('error', error))
           .finally(() => {
             this.sendingImage = false;
