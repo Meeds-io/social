3060332c32ecb3c9d7a2c36a558c4f752a2a98ce
Anwar khanfir <akhanfir@exoplatform.com>
2022-07-26 11:06:41 +0100

    TASK-59079: Hide the delete icon when the default banner is set Prior to this change, When the default banner is displayed on a profile, the delete icon display. To fix this,add the method update , assign the value of isDefaultBanner.

--- webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderBannerButton.vue
+++ webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderBannerButton.vue
@@ -56,6 +56,9 @@ export default {
     sendingImage: false,
     isDefaultBanner: false,
   }),
+  updated() {
+    this.isDefaultBanner=this.user.banner.startsWith('/portal/rest/v1/social/users/default-image/');
+  },
   watch: {
     sendingImage() {
       if (this.sendingImage) {
@@ -72,7 +75,6 @@ export default {
     removeBanner(){     
       return this.$userService.updateProfileField(eXo.env.portal.userName, 'banner', null)
         .then(() => {
-          this.isDefaultBanner = true;
           this.$emit('refresh');
         })
         .catch(error => this.$emit('error', error));
@@ -91,7 +93,6 @@ export default {
         return this.$uploadService.upload(file)
           .then(uploadId => this.$userService.updateProfileField(eXo.env.portal.userName, 'banner', uploadId))
           .then(() => {
-            this.isDefaultBanner = false;
             this.$emit('refresh');
           })
           .catch(error => this.$emit('error', error))
