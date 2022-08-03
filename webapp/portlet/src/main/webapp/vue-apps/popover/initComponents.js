58b5de3616f54f2bdc899f44104d4690c039f755
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-08-03 15:59:14 +0100

    MEED-309: Adjust the space popover component (#1710)
    Refactor space popover
    Fix update popover extension when the popover component is being created

--- webapp/portlet/src/main/webapp/vue-apps/popover/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/popover/initComponents.js
@@ -1,10 +1,14 @@
 import Popover from './components/Popover.vue';
-import PopoverUser from './components/PopoverUser.vue';
+import PopoverMenu from './components/PopoverMenu.vue';
+import UserPopoverContent from './components/UserPopoverContent.vue';
+import SpacePopoverContent from './components/SpacePopoverContent.vue';
 import NotificationAlerts from './components/NotificationAlerts.vue';
 
 const components = {
   'popover': Popover,
-  'popover-user': PopoverUser,
+  'popover-menu': PopoverMenu,
+  'user-popover-content': UserPopoverContent,
+  'space-popover-content': SpacePopoverContent,
   'notification-alerts': NotificationAlerts,
 };
 
