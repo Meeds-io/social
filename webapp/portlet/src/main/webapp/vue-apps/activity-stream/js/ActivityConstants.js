803dc94bcda39174a8b98b6403f2a98bf40d7386
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-08-04 15:30:12 +0100

    MEED-240 Bookmark a space as a favorite from space popover (#1717)
    Add favorite action inside space popover.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
@@ -2,7 +2,7 @@ export default {
   // Attention!!! when changing this, the list of preloaded
   // URLs has to change in JSP as well
   FULL_ACTIVITY_IDS_EXPAND: 'ids,identity,likes,shared,commentsPreview,subComments',
-  FULL_ACTIVITY_EXPAND: 'identity,likes,shared,commentsPreview,subComments',
+  FULL_ACTIVITY_EXPAND: 'identity,likes,shared,commentsPreview,subComments,favorite',
   ACTIVITY_EXPAND: 'identity,likes,shared',
   FULL_COMMENT_EXPAND: 'identity,likes,subComments',
   COMMENT_MAX_LENGTH: 1300,
