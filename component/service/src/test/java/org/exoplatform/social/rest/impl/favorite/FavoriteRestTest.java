9bf020bb25367b52202ce81b5c07281da21d1f0d
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2022-08-26 16:30:27 +0100

    MEED-456 Bug: Set a space not a member of as a favorite shouldn't be possible (#1756)
    Prior this change, not a member user can mark space a favorite by executing a fetch command
    Fix : Add restriction to API

--- component/service/src/test/java/org/exoplatform/social/rest/impl/favorite/FavoriteRestTest.java
+++ component/service/src/test/java/org/exoplatform/social/rest/impl/favorite/FavoriteRestTest.java
@@ -23,9 +23,18 @@ import java.util.List;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.container.xml.ObjectParameter;
 import org.exoplatform.services.rest.impl.ContainerResponse;
+import org.exoplatform.social.core.activity.model.ExoSocialActivity;
+import org.exoplatform.social.core.activity.model.ExoSocialActivityImpl;
 import org.exoplatform.social.core.identity.model.Identity;
 import org.exoplatform.social.core.jpa.storage.dao.jpa.MetadataDAO;
+import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.manager.IdentityManager;
+import org.exoplatform.social.core.plugin.ActivityFavoriteACLPlugin;
+import org.exoplatform.social.core.plugin.SpaceFavoriteACLPlugin;
+import org.exoplatform.social.core.space.SpaceUtils;
+import org.exoplatform.social.core.space.model.Space;
+import org.exoplatform.social.core.space.spi.SpaceService;
+import org.exoplatform.social.metadata.FavoriteACLPlugin;
 import org.exoplatform.social.metadata.MetadataService;
 import org.exoplatform.social.metadata.MetadataTypePlugin;
 import org.exoplatform.social.metadata.favorite.FavoriteService;
@@ -38,6 +47,8 @@ public class FavoriteRestTest extends AbstractResourceTest {
 
   private Identity        johnIdentity;
 
+  private Identity        maryIdentity;
+
   private IdentityManager identityManager;
 
   private FavoriteRest    metadataRest;
@@ -45,6 +56,10 @@ public class FavoriteRestTest extends AbstractResourceTest {
   private FavoriteService favoriteService;
 
   private MetadataService metadataService;
+  
+  private SpaceService    spaceService;
+
+  private ActivityManager activityManager;
 
   private MetadataDAO     metadataDAO;
 
@@ -55,6 +70,8 @@ public class FavoriteRestTest extends AbstractResourceTest {
     metadataService = getContainer().getComponentInstanceOfType(MetadataService.class);
     favoriteService = getContainer().getComponentInstanceOfType(FavoriteService.class);
     metadataDAO = getContainer().getComponentInstanceOfType(MetadataDAO.class);
+    spaceService = getContainer().getComponentInstanceOfType(SpaceService.class);
+    activityManager = getContainer().getComponentInstanceOfType(ActivityManager.class);
 
     try {
       InitParams params = new InitParams();
@@ -63,6 +80,10 @@ public class FavoriteRestTest extends AbstractResourceTest {
       parameter.setObject(FavoriteService.METADATA_TYPE);
       params.addParameter(parameter);
       metadataService.addMetadataTypePlugin(new MetadataTypePlugin(params));
+      FavoriteACLPlugin favoriteACLPlugin = new SpaceFavoriteACLPlugin(spaceService, identityManager);
+      FavoriteACLPlugin favoriteACLPlugin1 = new ActivityFavoriteACLPlugin(activityManager);
+      favoriteService.addFavoriteACLPlugin(favoriteACLPlugin);
+      favoriteService.addFavoriteACLPlugin(favoriteACLPlugin1);
     } catch (UnsupportedOperationException e) {
       // Expected when already added
     }
@@ -71,6 +92,7 @@ public class FavoriteRestTest extends AbstractResourceTest {
     registry(metadataRest);
 
     johnIdentity = identityManager.getOrCreateUserIdentity("john");
+    maryIdentity = identityManager.getOrCreateUserIdentity("mary");
   }
 
   @Override
@@ -121,6 +143,54 @@ public class FavoriteRestTest extends AbstractResourceTest {
     assertEquals(objectId, metadataItem.getObjectId());
     assertEquals(objectType, metadataItem.getObjectType());
     assertEquals(parentObjectId, metadataItem.getParentObjectId());
+  }
+
+  public void testCreateFavoritesACL() throws Exception {
+
+    Space space1 = new Space();
+    space1.setDisplayName("space1");
+    space1.setPrettyName(space1.getDisplayName());
+    space1.setRegistration("validation");
+    space1.setVisibility("public");
+    space1.setPriority("2");
+    String[] manager = new String[] { "john" };
+    String[] members = new String[] { "john" };
+    space1.setManagers(manager);
+    space1.setMembers(members);
+
+    spaceService.createSpace(space1, johnIdentity.getRemoteId());
+
+    Space createdSpace = spaceService.getSpaceByPrettyName(space1.getPrettyName());
+
+    ExoSocialActivity activity = new ExoSocialActivityImpl();
+    activity.setUserId(johnIdentity.getId());
+    activity.setTitle("activity 1");
+    Identity spaceIdentity = identityManager.getOrCreateSpaceIdentity(space1.getPrettyName());
+    activityManager.saveActivityNoReturn(spaceIdentity, activity);
+
+    startSessionAs(maryIdentity.getRemoteId());
+    ContainerResponse response = getResponse("POST",
+                                             getURLResource("favorites/space/" + createdSpace.getId())
+                                                 + "?parentObjectId=parentObjectId",
+                                             null);
+    assertEquals(401, response.getStatus());
+
+    response = getResponse("POST",
+                           getURLResource("favorites/activity/" + activity.getId()) + "?parentObjectId=parentObjectId",
+                           null);
+    assertEquals(401, response.getStatus());
+
+    spaceService.addMember(space1, maryIdentity.getRemoteId());
+
+    response = getResponse("POST",
+                           getURLResource("favorites/space/" + createdSpace.getId()) + "?parentObjectId=parentObjectId",
+                           null);
+    assertEquals(204, response.getStatus());
+
+    response = getResponse("POST",
+                           getURLResource("favorites/activity/" + activity.getId()) + "?parentObjectId=parentObjectId",
+                           null);
+    assertEquals(204, response.getStatus());
   }
 
   public void testDeleteFavorite() throws Exception {
