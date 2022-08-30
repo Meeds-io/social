2036e1df8114c922d3c49831fe8322dd88bf3503
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-08-30 13:40:23 +0100

    MEED-352-362: Unify the topbar space popover UI (#1761)
    This Pr allows to unify the topbar space popover UI with the UI of the platfrom, and add space Hosts drawer when cliking on hosts list.

--- webapp/portlet/src/main/webapp/vue-apps/space-top-bannerlogo/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/space-top-bannerlogo/initComponents.js
@@ -1,9 +1,12 @@
 import ExoSpaceLogoBanner from './components/ExoSpaceLogoBanner.vue';
 import SpacePopoverActionComponents from './components/SpacePopoverActionComponents.vue';
+import SpaceHostsDrawer from './components/SpaceHostsDrawer.vue';
+
 
 const components = {
   'exo-space-logo-banner': ExoSpaceLogoBanner,
-  'space-popover-action-component': SpacePopoverActionComponents
+  'space-popover-action-component': SpacePopoverActionComponents,
+  'space-hosts-drawer': SpaceHostsDrawer,
 };
 
 for (const key in components) {
