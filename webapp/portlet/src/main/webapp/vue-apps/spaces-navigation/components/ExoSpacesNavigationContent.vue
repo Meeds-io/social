577dfdfe8e33b668ad1cc69a1263cb5cda5e6bab
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-09-19 18:01:30 +0100

    feat: left menu review Display second panels (recent spaces / admin)-Desktop - MEED-609 - Meeds-io/meeds#16 (#1815)
    This changes allows to enhance the UI/UX of the recent spaces menu in the left navigation.

--- webapp/portlet/src/main/webapp/vue-apps/spaces-navigation/components/ExoSpacesNavigationContent.vue
+++ webapp/portlet/src/main/webapp/vue-apps/spaces-navigation/components/ExoSpacesNavigationContent.vue
@@ -55,7 +55,7 @@
               <v-list-item
                 v-if="homeLink !== url(space)"
                 @click="selectHome($event, space)">
-                <v-icon size="16" class="fas fa-home icon-default-color mr-3" />
+                <v-icon size="16" class="fas fa-house-user icon-default-color mr-3" />
                 <span class="text-color mt-1">{{ $t('menu.spaces.makeAsHomePage') }}</span>
               </v-list-item>
             </v-list>
