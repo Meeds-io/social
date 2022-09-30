0a792eba6e99763e9dcbcada378e9e180837765e
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-09-30 20:15:31 +0100

    feat: Remove vue app initilaizing from version history module - EXO-59624 -  Meeds-io/MIPs#19 (#1849)
    Remove the unneeded vue app initilaizing from version history module
    
    Co-authored-by: Helmi Akermi <hakermi@exoplatform.org>

--- webapp/portlet/src/main/webapp/vue-apps/version-history-drawer/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/version-history-drawer/main.js
@@ -9,21 +9,3 @@ if (extensionRegistry) {
     });
   }
 }
-const appId = 'VersionHistoryDrawer';
-
-Vuetify.prototype.preset = eXo.env.portal.vuetifyPreset;
-Vue.use(Vuetify);
-
-//getting language of user
-const lang = eXo && eXo.env.portal.language || 'en';
-
-const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Portlets-${lang}.json`;
-
-export function init() {
-  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
-    Vue.createApp({
-      i18n,
-      vuetify: Vue.prototype.vuetifyOptions,
-    }, appId, 'Version History drawer');
-  });
-}
