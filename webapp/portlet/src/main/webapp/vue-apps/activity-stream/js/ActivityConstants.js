d67df70f41f5bf3b60a517d70cf11ffad02ee95b
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-10-18 10:21:29 +0100

    fix : Activity Stream some pre-fetched REST URLs aren't used in page - MEED-645 - Meeds-io/meeds#33 (#1888)
    Prior to change some social activities URLs are prefetched while it's
    not used immediately in page rendering phase
    this change is going to inhance performance by prefetch only necessary
    URLs used for the first rendering of the page

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/js/ActivityConstants.js
@@ -1,7 +1,7 @@
 export default {
   // Attention!!! when changing this, the list of preloaded
   // URLs has to change in JSP as well
-  FULL_ACTIVITY_IDS_EXPAND: 'ids,identity,likes,shared,commentsPreview,subComments',
+  FULL_ACTIVITY_IDS_EXPAND: 'ids,identity,likes,shared,commentsPreview,subComments,favorite',
   FULL_ACTIVITY_EXPAND: 'identity,likes,shared,commentsPreview,subComments,favorite',
   ACTIVITY_EXPAND: 'identity,likes,shared',
   FULL_COMMENT_EXPAND: 'identity,likes,subComments',
