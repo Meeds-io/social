da560029cd4eb777453e023e6ab06e1f08d249ad
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-11-08 15:30:24 +0100

    feat: Add Feature Flag for Left Menu Navigation - MEED-995 (#1943)
    Prior to this change, the Left Menu Navigation was completely replaced by the new implementation without possibility to get back previous legacy Left Menu behavior. this change will apply a feature flag to allow switching from newer implementation to the legacy one. (Default value = true to enable newer implementation by default).

--- webapp/portlet/src/main/webapp/vue-apps/administration-navigation/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/administration-navigation/initComponents.js
@@ -1,15 +1,21 @@
 // eslint-disable-next-line max-len
 import ExoAdministrationHamburgerNavigation from './components/ExoAdministrationHamburgerNavigation.vue';
+import LegacyExoAdministrationHamburgerNavigation from './components/legacy/ExoAdministrationHamburgerNavigation.vue';
 // eslint-disable-next-line max-len
 import ExoAdministrationMenuItem from './components/ExoAdministrationMenuItem.vue';
 import ExoAdministrationNavigations from './components/ExoAdministrationNavigations.vue';
 
 const components = {
   // eslint-disable-next-line max-len
-  'exo-administration-hamburger-menu-navigation': ExoAdministrationHamburgerNavigation,
   'exo-administration-menu-item': ExoAdministrationMenuItem,
   'exo-administration-navigations': ExoAdministrationNavigations,
 };
+
+if (eXo.env.portal.leftMenuEnabled) {
+  components['exo-administration-hamburger-menu-navigation'] = ExoAdministrationHamburgerNavigation;
+} else {
+  components['exo-administration-hamburger-menu-navigation'] = LegacyExoAdministrationHamburgerNavigation;
+}
 
 for (const key in components) {
   Vue.component(key, components[key]);
@@ -22,7 +28,7 @@ if (extensionRegistry) {
       id: 'HamburgerMenuNavigationAdministration',
       priority: 40,
       secondLevel: true,
-      vueComponent: ExoAdministrationHamburgerNavigation,
+      vueComponent: components['exo-administration-hamburger-menu-navigation'],
     },
   );
 }
