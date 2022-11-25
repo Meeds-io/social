f67cce0c981a4922949f2d785ac3b66047d11dea
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2022-11-25 16:22:42 +0100

    feat: Unread activities information Display number of unread activities for a space from the left menu - MEED-700- Meeds-io/MIPs#17 (#1992)
    This Task allows to add a badge of the number of unread items per application to the recent viewed spaces from the left navigation menu. For now this information will be retrieve only when the value "unread" is added to "&expand=...,unread" Path parameter.

--- component/api/src/main/java/org/exoplatform/social/metadata/MetadataService.java
+++ component/api/src/main/java/org/exoplatform/social/metadata/MetadataService.java
@@ -299,6 +299,17 @@ public interface MetadataService {
   int countMetadataItemsByMetadataTypeAndCreator (String metadataTypeName, long creatorId);
 
   /**
+   * Count the size of Metadata items to a given {@link Metadata} type,
+   * {@link MetadataItem} creatorId
+   * by a given {@link Space} identifier
+   *
+   * @param metadataTypeName {@link Metadata} type
+   * @param creatorId {@link MetadataItem} creatorId
+   * @param spaceId {@link Space} technical identifier
+   * @return map of Metadata items grouped by objectId {@link MetadataObject}
+   */
+  Map<String, Long> countMetadataItemsByMetadataTypeAndAudienceId(String metadataTypeName, long creatorId, long spaceId);
+  /**
    * Retrieves the list of Metadata items attached to a {@link MetadataKey} and
    * an object identified by its name and identifier
    * 
