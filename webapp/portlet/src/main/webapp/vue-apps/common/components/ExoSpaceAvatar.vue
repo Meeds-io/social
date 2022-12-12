a754b2020cdbd575c91aa672e02571738abe6355
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-12-12 10:05:58 +0100

    MEED

--- webapp/portlet/src/main/webapp/vue-apps/common/components/ExoSpaceAvatar.vue
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ExoSpaceAvatar.vue
@@ -5,7 +5,7 @@
     :class="extraClass"
     v-identity-popover="space">
     <a
-      v-if="notAccessibleSpace && !avatar && !fullname"
+      v-if="notAccessibleSpace"
       class="flex-nowrap flex-shrink-0 d-flex spaceAvatar not-clickable-link">
       <v-avatar
         v-if="avatar || !isMobile"
