01c75a74df787fdede40f8b0d7dc9ac3df9caa3e
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2022-12-19 13:48:13 +0100

    feat: Add a dynamic sub navigations for top bar navigation menu - EXO-61274 - Meeds-io/MIPs#36 (#2053)
    Add a dynamic sub navigations for top bar navigation menu

--- webapp/portlet/src/main/webapp/vue-apps/top-bar-menu/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/top-bar-menu/initComponents.js
@@ -1,11 +1,13 @@
 import TopBarNavigationMenu from './components/TopBarNavigationMenu.vue';
 import NavigationMenuItem from './components/NavigationMenuItem.vue';
+import NavigationMenuSubItem from './components/NavigationMenuSubItem.vue';
 
 import * as NavigationService from '../common/js/NavigationService.js';
 
 const components = {
   'top-bar-navigation-menu': TopBarNavigationMenu,
-  'navigation-menu-item': NavigationMenuItem
+  'navigation-menu-item': NavigationMenuItem,
+  'navigation-menu-sub-item': NavigationMenuSubItem
 };
 
 for (const key in components) {
