4a006cf5302ab9322c22b4548bfbec4418ab590b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-20 15:16:38 +0100

    feat: Restyle the display of external Login Providers - MEED-1305 - Meeds-io/MIPs#29 (#2064)
    Prior to this change, the external providers was displayed at the bottom
    of the login form. This change will move them in top and display them
    using a re-usable style which will be uniform.

--- webapp/portlet/webpack.common.js
+++ webapp/portlet/webpack.common.js
@@ -23,7 +23,6 @@ let config = {
     loginCommon: './src/main/webapp/vue-apps/login-common/main.js',
     register: './src/main/webapp/vue-apps/user-register/main.js',
     registerOnboarding: './src/main/webapp/vue-apps/user-register-onboarding/main.js',
-    oAuthLoginExtension: './src/main/webapp/vue-apps/login-oauth/main.js',
     spacesAdministration: './src/main/webapp/vue-apps/spaces-administration/main.js',
     hamburgerMenu: './src/main/webapp/vue-apps/hamburger-menu/main.js',
     administrationHamburgerMenu: './src/main/webapp/vue-apps/administration-navigation/main.js',
