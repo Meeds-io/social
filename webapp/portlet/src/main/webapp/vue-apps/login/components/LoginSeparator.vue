09f58aa29ed16145dec7a994ef60660baa4b75b7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-20 15:16:38 +0100

    feat: Restyle the display of external Login Providers - MEED-1305 - Meeds-io/MIPs#29 (#2064)
    Prior to this change, the external providers was displayed at the bottom
    of the login form. This change will move them in top and display them
    using a re-usable style which will be uniform.

--- webapp/portlet/src/main/webapp/vue-apps/login/components/LoginSeparator.vue
+++ webapp/portlet/src/main/webapp/vue-apps/login/components/LoginSeparator.vue
@@ -19,32 +19,11 @@
 
 -->
 <template>
-  <a :id="id" :href="link">
-    <i :class="cssClass"></i>
-  </a>
+  <div class="d-flex">
+    <v-divider class="my-auto secondary login-separator" />
+    <span class="mx-3 text-uppercase">
+      {{ $t('UILoginForm.label.or') }}
+    </span>
+    <v-divider class="my-auto secondary login-separator" />
+  </div>
 </template>
-<script>
-export default {
-  props: {
-    provider: {
-      type: Object,
-      default: null,
-    },
-    rememberme: {
-      type: Boolean,
-      default: false,
-    },
-  },
-  computed: {
-    id() {
-      return `login-${this.provider.key}`;
-    },
-    link() {
-      return this.rememberme && `${this.provider.url}&_rememberme=true` || this.provider.url;
-    },
-    cssClass() {
-      return `${this.provider.key.toLowerCase()} uiIconOauth white rounded-lg`;
-    },
-  },
-};
-</script>
