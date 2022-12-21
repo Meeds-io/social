93da50af8c2506c46c8d04ba791a36d1e1e065a1
ArfaouiMohamedAmine <marfaoui@exoplatform.com>
2022-12-21 09:51:06 +0100

    MEED-941: fix

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageUser.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageUser.vue
@@ -1,31 +1,79 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
 <template>
   <div class="d-flex flex-column white border-radius">
-    <img
-      :height="height"
-      :style="{maxHeight: height}"
-      src="/social-portlet/images/emptyUserStream.png"
-      class="mx-auto"
-      loading="lazy">
-    <div class="mx-4 mb-4">
+    <div class="mx-4 my-4">
       <p v-sanitized-html="welcomeTitle"></p>
-      <div v-sanitized-html="thanksForJoingMeeds"></div>
-      <div v-sanitized-html="welcomeParagraph1"></div>
-      <div v-sanitized-html="welcomeParagraph2" class="mb-4"></div>
-      <div v-sanitized-html="welcomeParagraph3"></div>
-      <div v-sanitized-html="welcomeParagraph4" class="mb-4"></div>
-      <div v-sanitized-html="welcomeParagraph5" class="mb-4"></div>
-      <div v-sanitized-html="conclusionParagraph"></div>
+      <v-card
+        max-width="500"
+        flat
+        class="mx-auto my-8">
+        <v-row>
+          <activity-stream-empty-message-space-icons-column
+            :icon-size="iconSize"
+            :icon-ref="userGearIcon"
+            :info-message="emptyStreamForSpacesThatUserManage1" />
+          <activity-stream-empty-message-space-icons-column
+            :icon-size="iconSize"
+            :icon-ref="shareIcon"
+            :info-message="emptyStreamForSpacesThatUserManage2" 
+            icon-color="secondary" />
+          <activity-stream-empty-message-space-icons-column
+            :icon-size="iconSize"
+            :icon-ref="awardIcon"
+            :info-message="emptyStreamForSpacesThatUserManage3" />
+          <activity-stream-empty-message-space-icons-column
+            :icon-size="iconSize"
+            :icon-ref="trophyIcon"
+            :info-message="emptyStreamForSpacesThatUserManage4"      
+            icon-color="secondary" /> 
+        </v-row>
+      </v-card>
+      <div v-sanitized-html="welcomeSpaceParagraph1" class="mb-4"></div>
+      <div v-sanitized-html="welcomeSpaceParagraph2" class="mb-4"></div>
+      <div v-sanitized-html="welcomeSpaceParagraph3" class="mb-4"></div>
+      <div>
+        <ol>
+          <li v-html="welcomeSpaceNumberedParagraph1"></li>
+          <li v-sanitized-html="welcomeSpaceNumberedParagraph2"></li>
+          <li v-sanitized-html="welcomeSpaceNumberedParagraph3"></li>
+        </ol>
+      </div>
+      <div v-sanitized-html="welcomeSpaceParagraph4" class="my-4"></div>
     </div>
   </div>
 </template>
 <script>
 export default {
-  data: function() {
+  data() {
     return {
-      branding: null,
-      height: '380px',
-      productName: eXo.env.portal.productName,
+      emptyActivityStreamActionName: 'empty-activity-stream-spaces',
+      iconSize: '30',
+      userGearIcon: 'fa-user-cog',
+      shareIcon: 'fa-share-alt',
+      awardIcon: 'fa-award',
+      trophyIcon: 'fa-trophy',
     };
+  },
+  created() {
+    document.addEventListener(this.emptyActivityStreamActionName, this.clickOnSpacesActivityStreamEmptyActionLink);
+  },
+  beforeDestroy() {
+    document.removeEventListener(this.emptyActivityStreamActionName, this.clickOnSpacesActivityStreamEmptyActionLink);
   },
   computed: {
     profileName() {
@@ -42,44 +90,47 @@ export default {
         'user full name': this.profilelink,
       });
     },
-    companyName() {
-      return this.branding && this.branding.companyName;
+    emptyStreamForSpacesThatUserManage1() {
+      return this.$t('UIActivity.label.empty_space_stream_manage_community');
     },
-    thanksForJoingMeeds() {
-      return this.$t && this.$t('UIActivity.label.Thanks_for_joing_company', {
-        'company name': this.companyName,
+    emptyStreamForSpacesThatUserManage2() {
+      return this.$t('UIActivity.label.empty_space_stream_share_knowledge');
+    },
+    emptyStreamForSpacesThatUserManage3() {
+      return this.$t('UIActivity.label.empty_space_stream_congrats');
+    },
+    emptyStreamForSpacesThatUserManage4() {
+      return this.$t('UIActivity.label.empty_space_stream_value_skills');
+    },
+    welcomeSpaceParagraph1() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_one');
+    },
+    welcomeSpaceParagraph2() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_two');
+    },
+    welcomeSpaceParagraph3() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_three');
+    },
+    welcomeSpaceParagraph4() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_four');
+    },
+    welcomeSpaceNumberedParagraph1() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_numbered_one', {
+        0: `<a class="primary--text font-weight-bold" href="javascript:void(0)" onclick="document.dispatchEvent(new CustomEvent('${this.emptyActivityStreamActionName}'))">`,
+        1: '</a>',
       });
     },
-    welcomeParagraph1() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_paragraph_one', {
-        'product name': this.productName,
-      });
+    welcomeSpaceNumberedParagraph2() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_numbered_two');
     },
-    welcomeParagraph2() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_paragraph_two', {
-        'product name': this.productName,
-      });
-    },
-    welcomeParagraph3() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_paragraph_three');
-    },
-    welcomeParagraph4() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_paragraph_four');
-    },
-    welcomeParagraph5() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_paragraph_five');
-    },
-    conclusionParagraph() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_lets_begin');
+    welcomeSpaceNumberedParagraph3() {
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_numbered_three');
     },
   },
-  created() {
-    fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/platform/branding`, {
-      method: 'GET',
-      credentials: 'include',
-    })
-      .then(resp => resp && resp.ok && resp.json())
-      .then(branding => this.branding = branding);
-  }
+  methods: {
+    clickOnSpacesActivityStreamEmptyActionLink() {
+      window.location.href = `${eXo.env.portal.context}/${eXo.env.portal.portalName}/spaces`;
+    },
+  },
 };
 </script>
