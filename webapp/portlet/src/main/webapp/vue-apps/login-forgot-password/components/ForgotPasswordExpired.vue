adb953bdcf76e04ef181e1583d0469bb91220ae5
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-21 13:52:45 +0100

    feat: Enhance Forgot Password UI by using Vue & Vuetify - MEED-1304 - Meeds-io/MIPs#29 (#2067)
    Prior to this change, the UI of forgot Password was retrieved using jsp
    Server Rendering which is not convenient to new FrontEnd tools used for
    other UIs. This change will rework the UI to use Vue & Vuetify
    applications for this handler.

---
+++ webapp/portlet/src/main/webapp/vue-apps/login-forgot-password/components/ForgotPasswordExpired.vue
@@ -0,0 +1,65 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+ 
+ Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-card
+    width="350px"
+    max-width="100%"
+    class="mx-auto"
+    flat>
+    <v-card-title class="primary--text subtitle-1 px-0">
+      <span class="mx-auto text-center">{{ $t('forgotpassword.linkExpired') }}</span>
+    </v-card-title>
+    <div class="d-flex ma-0 flex-column">
+      <div class="pa-0">
+        <v-row class="mx-0 mt-8 pa-0">
+          <v-btn
+            :aria-label="$t('forgotpassword.goToLogin')"
+            :href="formUrl"
+            color="primary"
+            width="222"
+            max-width="100%"
+            tabindex="4"
+            class="mx-auto login-button"
+            elevation="0">
+            <span class="text-capitalize mx-auto">
+              {{ $t('forgotpassword.go') }}
+            </span>
+          </v-btn>
+        </v-row>
+      </div>
+    </div>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    params: {
+      type: Object,
+      default: null,
+    },
+  },
+  computed: {
+    formUrl() {
+      return window.location.pathname;
+    },
+  },
+};
+</script>
