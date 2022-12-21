bd6805d680bdd943fec7d6a836614a4e45ff0c44
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-21 13:52:45 +0100

    feat: Enhance Forgot Password UI by using Vue & Vuetify - MEED-1304 - Meeds-io/MIPs#29 (#2067)
    Prior to this change, the UI of forgot Password was retrieved using jsp
    Server Rendering which is not convenient to new FrontEnd tools used for
    other UIs. This change will rework the UI to use Vue & Vuetify
    applications for this handler.

---
+++ webapp/portlet/src/main/webapp/vue-apps/login-forgot-password/initComponents.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+import ForgotPassword from './components/ForgotPassword.vue';
+import ForgotPasswordMain from './components/ForgotPasswordMain.vue';
+import ForgotPasswordEmailForm from './components/ForgotPasswordEmailForm.vue';
+import ForgotPasswordExpired from './components/ForgotPasswordExpired.vue';
+import ForgotPasswordResetForm from './components/ForgotPasswordResetForm.vue';
+
+const components = {
+  'portal-forgot-password': ForgotPassword,
+  'portal-forgot-password-main': ForgotPasswordMain,
+  'portal-forgot-password-expired': ForgotPasswordExpired,
+  'portal-forgot-password-email-form': ForgotPasswordEmailForm,
+  'portal-forgot-password-reset-form': ForgotPasswordResetForm,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
+}
