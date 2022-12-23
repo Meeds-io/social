976cfdd1fe1c620d4c686bef2517172014c991f0
Arfaoui Mohamed Amine <101107377+ArfaouiMohamedAmine@users.noreply.github.com>
2022-12-23 13:50:48 +0100

    feat: Activity Stream Implement placeholder for activity stream - MEED-941 Meeds-io/meeds#468 (#2076)
    this change is going to alter the old placeholder for activity stream
    with a new one .

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageSpace.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageSpace.vue
@@ -1,3 +1,19 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
 <template>
   <div class="d-flex flex-column white border-radius">
     <img
@@ -23,18 +39,18 @@ export default {
   },
   computed: {
     welcomeTitle() {
-      return this.$t && this.$t('UIActivity.label.Welcome_Space', {
+      return this.$t('UIActivity.label.Welcome_Space', {
         'space name': `<strong>${eXo.env.portal.spaceDisplayName}</strong>`,
       });
     },
     welcomeSpaceParagraph1() {
-      return this.$t && this.$t('UIActivity.label.empty_space_stream_paragraph_one');
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_one');
     },
     welcomeSpaceParagraph2() {
-      return this.$t && this.$t('UIActivity.label.empty_space_stream_paragraph_two');
+      return this.$t('UIActivity.label.empty_space_stream_paragraph_two');
     },
     conclusionParagraph() {
-      return this.$t && this.$t('UIActivity.label.empty_stream_lets_begin');
+      return this.$t('UIActivity.label.empty_stream_lets_begin');
     },
   },
 };
---
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/empty-stream/ActivityStreamEmptyMessageSpaceIconsColumn.vue
@@ -0,0 +1,57 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-col class="mx-3">
+    <v-row>
+      <v-icon
+        class="uiIcon fa mx-auto" 
+        :size="iconSize"
+        :color="iconColor"
+        :class="iconRef && iconRef || ''" />
+    </v-row>
+    <v-row class="text-center mt-1">
+      <v-card
+      flat
+      max-width="150"
+      class="mx-auto">
+      {{ infoMessage }}
+      </v-card>
+    </v-row>
+  </v-col>
+</template>
+<script>
+export default {
+  props: {
+    iconRef: {
+      type: String,
+      default: 'fa-exclamation-circle'
+    },
+    iconSize: {
+      type: String,
+      default: '50'
+    },
+    iconColor: {
+      type: String,
+      default: 'primary'
+    },
+    infoMessage: {
+      type: String,
+      default: null
+    },
+  },
+};
+</script>
