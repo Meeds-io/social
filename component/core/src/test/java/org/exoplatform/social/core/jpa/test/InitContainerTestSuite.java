cee466abfb099f01538d7085cda4d74d1f4383da
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-30 20:55:03 +0100

    feat: Move OAuth Modules from GateIn to centralize them in Social - MEED-1308 - Meeds-io/MIPs#42
    Prior to this change, the oAuth Modules are defined in three artifacts
    and was managed by overrides in order to access users Social Identity.
    This change will allow to centralize oAuth Module in one single place to
    avoid overrides and complicated maintenance & evolutions. In addition,
    the Unit Tests has been configured to make it possible to call
    PortalContainer.getInstance() in addons and upper layes without
    exception when having the good Maven Modules added into it using
    <type>test-jar</type> for test artifacts.

--- component/core/src/test/java/org/exoplatform/social/core/jpa/test/InitContainerTestSuite.java
+++ component/core/src/test/java/org/exoplatform/social/core/jpa/test/InitContainerTestSuite.java
@@ -24,24 +24,70 @@ import org.exoplatform.commons.testing.BaseExoContainerTestSuite;
 import org.exoplatform.commons.testing.ConfigTestCase;
 import org.exoplatform.social.core.jpa.storage.*;
 import org.exoplatform.social.core.jpa.storage.dao.*;
+import org.exoplatform.social.core.listeners.ActivityMetadataListenerTest;
+import org.exoplatform.social.core.listeners.ActivityTagMetadataListenerTest;
+import org.exoplatform.social.core.listeners.SocialUserProfileEventListenerImplTest;
+import org.exoplatform.social.core.manager.ActivityManagerTest;
+import org.exoplatform.social.core.manager.IdentityManagerTest;
+import org.exoplatform.social.core.manager.RelationshipManagerTest;
+import org.exoplatform.social.core.metadata.thumbnail.ImageThumbnailServiceImplTest;
+import org.exoplatform.social.core.processor.I18NActivityProcessorTest;
+import org.exoplatform.social.core.processor.MetadataActivityProcessorTest;
+import org.exoplatform.social.core.processor.OSHtmlSanitizerProcessorTest;
+import org.exoplatform.social.core.processor.TemplateParamsProcessorTest;
+import org.exoplatform.social.core.search.SearchServiceTest;
+import org.exoplatform.social.core.search.SortingTest;
+import org.exoplatform.social.core.service.LinkProviderTest;
+import org.exoplatform.social.core.space.SpaceLifeCycleTest;
+import org.exoplatform.social.core.space.SpaceUtilsTest;
+import org.exoplatform.social.core.space.spi.SpaceServiceTest;
+import org.exoplatform.social.core.space.spi.SpaceTemplateServiceTest;
+import org.exoplatform.social.metadata.MetadataServiceTest;
+import org.exoplatform.social.metadata.favorite.FavoriteServiceTest;
+import org.exoplatform.social.metadata.tag.TagServiceTest;
+import org.exoplatform.social.core.application.SpaceActivityPublisherTest;
+import org.exoplatform.social.core.binding.spi.GroupSpaceBindingServiceTest;
 import org.exoplatform.social.core.binding.spi.RDBMSGroupSpaceBindingStorageTest;
 
 @SuiteClasses({
-    ActivityDAOTest.class,
-    IdentityDAOTest.class,
-    StreamItemDAOTest.class,
-    ActivityManagerRDBMSTest.class,
-    SpaceActivityRDBMSPublisherTest.class,
-    RelationshipStorageTest.class,
-    RDBMSRelationshipManagerTest.class,
-    IdentityStorageTest.class,
-    RDBMSSpaceStorageTest.class,
-    SpaceMemberDAOTest.class,
-    SpaceExternalInvitationDAOTest.class,
-    SpaceDAOTest.class,
-    SpaceDAOTest.class
+  ActivityDAOTest.class,
+  IdentityDAOTest.class,
+  StreamItemDAOTest.class,
+  ActivityManagerRDBMSTest.class,
+  SpaceActivityRDBMSPublisherTest.class,
+  RelationshipStorageTest.class,
+  RDBMSRelationshipManagerTest.class,
+  IdentityStorageTest.class,
+  RDBMSSpaceStorageTest.class,
+  SpaceMemberDAOTest.class,
+  SpaceExternalInvitationDAOTest.class,
+  SpaceDAOTest.class,
+  I18NActivityProcessorTest.class,
+  SearchServiceTest.class,
+  RDBMSActivityStorageImplTest.class,
+  ActivityManagerTest.class,
+  IdentityManagerTest.class,
+  SpaceServiceTest.class,
+  RelationshipManagerTest.class,
+  SpaceUtilsTest.class,
+  SpaceActivityPublisherTest.class,
+  SpaceLifeCycleTest.class,
+  SocialUserProfileEventListenerImplTest.class,
+  OSHtmlSanitizerProcessorTest.class,
+  TemplateParamsProcessorTest.class,
+  LinkProviderTest.class,
+  SpaceTemplateServiceTest.class,
+  SortingTest.class,
+  GroupSpaceBindingServiceTest.class,
+  RDBMSGroupSpaceBindingStorageTest.class,
+  MetadataServiceTest.class,
+  FavoriteServiceTest.class,
+  TagServiceTest.class,
+  ActivityMetadataListenerTest.class,
+  ActivityTagMetadataListenerTest.class,
+  MetadataActivityProcessorTest.class,
+  ImageThumbnailServiceImplTest.class,
 })
-
 @ConfigTestCase(AbstractCoreTest.class)
 public class InitContainerTestSuite extends BaseExoContainerTestSuite {
 
