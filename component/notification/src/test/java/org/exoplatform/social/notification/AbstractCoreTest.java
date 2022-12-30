a2d6b467a4808896e1920e552395d9b70bba8bc3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-30 20:25:15 +0100

    Include Tests Using PortalContainer in Test Suite

--- component/notification/src/test/java/org/exoplatform/social/notification/AbstractCoreTest.java
+++ component/notification/src/test/java/org/exoplatform/social/notification/AbstractCoreTest.java
@@ -25,7 +25,6 @@ import org.exoplatform.component.test.ConfigurationUnit;
 import org.exoplatform.component.test.ConfiguredBy;
 import org.exoplatform.component.test.ContainerScope;
 import org.exoplatform.services.log.ExoLogger;
-import org.exoplatform.services.organization.Group;
 import org.exoplatform.services.organization.OrganizationService;
 import org.exoplatform.services.organization.User;
 import org.exoplatform.services.organization.UserHandler;
