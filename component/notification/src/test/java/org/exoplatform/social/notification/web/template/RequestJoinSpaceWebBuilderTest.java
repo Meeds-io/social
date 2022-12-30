8262acd21bdd750fc1609aaa7e4613e758301adb
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2022-12-30 20:55:03 +0100

    feat: Move OAuth Modules from GateIn to centralize them in Social - MEED-1308 - Meeds-io/MIPs#42
    Prior to this change, the oAuth Modules are defined in three artifacts
    and was managed by overrides in order to access users Social Identity.
    This change will allow to centralize oAuth Module in one single place to
    avoid overrides and complicated maintenance & evolutions. In addition,
    the Unit Tests has been configured to make it possible to call
    PortalContainer.getInstance() in addons and upper layes without
    exception when having the good Maven Modules added into it using
    <type>test-jar</type> for test artifacts.

--- component/notification/src/test/java/org/exoplatform/social/notification/web/template/RequestJoinSpaceWebBuilderTest.java
+++ component/notification/src/test/java/org/exoplatform/social/notification/web/template/RequestJoinSpaceWebBuilderTest.java
@@ -37,7 +37,7 @@ public class RequestJoinSpaceWebBuilderTest extends AbstractPluginTest {
   private ChannelManager manager;
   
   @Override
-  protected void setUp() throws Exception {
+  public void setUp() throws Exception {
     super.setUp();
     manager = getService(ChannelManager.class);
   }
