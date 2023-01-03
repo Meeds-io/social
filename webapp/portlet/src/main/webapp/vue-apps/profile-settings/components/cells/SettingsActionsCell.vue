94852d4c297c2a1c1c562c7caa6b0b4019b1c5c0
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-01-03 14:50:00 +0100

    feat: Add profile page settings table - EXO-60717 - Meeds-io/MIPs#38
    Add new "Profile Settings Portlet"

---
+++ webapp/portlet/src/main/webapp/vue-apps/profile-settings/components/cells/SettingsActionsCell.vue
@@ -0,0 +1,103 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2022 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <div
+    class="d-flex flex-nowrap">
+    <div
+      :id="`setting-action-menu-cel-${setting.id}`">
+      <v-tooltip bottom>
+        <template #activator="{ on, attrs }">
+          <v-btn
+            icon
+            small
+            v-bind="attrs"
+            v-on="on">
+            <v-icon
+              :size="isMobile ? 14 : 18"
+              class="clickable text-sub-title button-settings-action"
+              @click="displayActionMenu()">
+              mdi-dots-vertical
+            </v-icon>
+          </v-btn>
+          <v-menu
+            v-model="menuDisplayed"
+            :attach="`#setting-action-menu-cel-${setting.id}`"
+            transition="slide-x-reverse-transition"
+            :content-class="isMobile ? 'settingsActionMenuMobile' : 'settingsActionMenu'"
+            offset-y
+            offset-x
+            close-on-click
+            absolute>
+            <profile-settings-action-menu
+              :setting="setting"  :settings="settings"/>
+          </v-menu>
+        </template>
+        <span>
+          {{ menuActionTooltip }}
+        </span>
+      </v-tooltip>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+
+  props: {
+    setting: {
+      type: Object,
+      default: null,
+    },
+    settings: {
+      type: Object,
+      default: null,
+    }
+  },
+  data: () => ({
+    loading: false,
+    menuDisplayed: false,
+    waitTimeUntilCloseMenu: 200,
+  }),
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'sm';
+    },
+    menuActionTooltip() {
+      return this.$t('profileSettings.label.menu.action.tooltip');
+    },
+  },
+  created() {
+    $(document).on('mousedown', () => {
+      if (this.menuDisplayed) {
+        window.setTimeout(() => {
+          $(`#setting-action-menu-cel-${this.setting.id}`).parent().parent().parent().parent().css('background', '#fff');
+          this.menuDisplayed = false;
+        }, this.waitTimeUntilCloseMenu);
+      }
+    });
+  },
+  methods: {
+    displayActionMenu() {
+      if (this.isMobile){
+        this.$root.$emit('open-setting-action-menu', this.setting);
+      } else {
+        this.menuDisplayed = true;
+        $(`#setting-action-menu-cel-${this.setting.id}`).parent().parent().parent().parent().css('background', '#eee');
+      }
+    },
+  }
+};
+</script>
