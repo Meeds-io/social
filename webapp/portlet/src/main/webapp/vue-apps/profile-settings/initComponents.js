97ebbcfcce02207f8d05773d33e9d09513875e89
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-01-03 14:50:00 +0100

    feat: Add profile page settings table - EXO-60717 - Meeds-io/MIPs#38
    Add new "Profile Settings Portlet"

---
+++ webapp/portlet/src/main/webapp/vue-apps/profile-settings/initComponents.js
@@ -0,0 +1,44 @@
+/*
+    This file is part of the Meeds project (https://meeds.io/).
+    Copyright (C) 2022 Meeds Association
+    contact@meeds.io
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+import ProfileSettings from './components/ProfileSettings.vue';
+import ProfileSettingsHeader from './components/ProfileSettingsHeader.vue';
+import ProfileSettingsTable from './components/ProfileSettingsTable.vue';
+import ProfileSettingsActionMenu from './components/menu/ProfileSettingsActionMenu.vue';
+import SettingsActionsCell from './components/cells/SettingsActionsCell.vue';
+import ProfileSettingFormDrawer from './components/drawers/ProfileSettingFormDrawer.vue';
+
+import * as ProfileSettingsService from '../common/js/ProfileSettingsService.js';
+
+const components = {
+  'profile-settings': ProfileSettings,
+  'profile-settings-header': ProfileSettingsHeader,
+  'profile-settings-table': ProfileSettingsTable,
+  'profile-settings-action-menu': ProfileSettingsActionMenu,
+  'profile-settings-actions-cell': SettingsActionsCell,
+  'profile-setting-form-drawer': ProfileSettingFormDrawer,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
+}
+
+if (!Vue.prototype.$profileSettingsService) {
+  window.Object.defineProperty(Vue.prototype, '$profileSettingsService', {
+    value: ProfileSettingsService,
+  });
+}
