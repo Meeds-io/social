cda9b550e899c8520c2147086e0de1f8f42bbe2b
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-01-03 14:50:00 +0100

    feat: Add profile page settings table - EXO-60717 - Meeds-io/MIPs#38
    Add new "Profile Settings Portlet"

---
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/storage/dao/ProfilePropertySettingDAO.java
@@ -0,0 +1,34 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * Copyright (C) 2020 - 2022 Meeds Association contact@meeds.io
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+
+package org.exoplatform.social.core.jpa.storage.dao;
+
+import org.exoplatform.commons.persistence.impl.GenericDAOJPAImpl;
+import org.exoplatform.social.core.jpa.storage.entity.ProfilePropertySettingEntity;
+import javax.persistence.TypedQuery;
+
+public class ProfilePropertySettingDAO extends GenericDAOJPAImpl<ProfilePropertySettingEntity, Long> {
+    public ProfilePropertySettingEntity findProfileSettingByName(String name) {
+        TypedQuery<ProfilePropertySettingEntity> query = getEntityManager().createNamedQuery("SocProfileSettingEntity.findProfileSettingByName",ProfilePropertySettingEntity.class)
+        .setParameter("name", name);
+        try {
+            return query.getSingleResult();
+        } catch (Exception e) {
+            return null;
+        }
+
+    }
+}
---
+++ webapp/portlet/src/main/webapp/vue-apps/profile-settings/main.js
@@ -0,0 +1,49 @@
+/*
+    This file is part of the Meeds project (https://meeds.io/).
+    Copyright (C) 2022 Meeds Association
+    contact@meeds.io
+    This program is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 3 of the License, or (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with this program; if not, write to the Free Software Foundation,
+    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
+import './initComponents.js';
+
+// get overridden components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('profileSettings');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+//getting language of user
+const lang = eXo && eXo.env.portal.language || 'en';
+
+const url = `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Portlets-${lang}.json`;
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify(eXo.env.portal.vuetifyPreset);
+
+const appId = 'profileSettings';
+
+export function init() {
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+    // init Vue app when locale resources are ready
+    Vue.createApp({
+      template: `<profile-settings id="${appId}" />`,
+      vuetify,
+      i18n
+    }, `#${appId}`, 'Profile settings');
+  });
+}
