66e9abf0226213f6dd894724e7d9facd8dfa3e2e
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-01-04 10:51:50 +0100

    feat: Add profile page settings table - EXO-60717 - Meeds-io/MIPs#38 (#2094)

--- component/core/src/main/java/org/exoplatform/social/core/jpa/storage/entity/ProfilePropertySettingEntity.java
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/storage/entity/ProfilePropertySettingEntity.java
@@ -48,7 +48,7 @@ public class ProfilePropertySettingEntity implements Serializable {
   @Column(name="PARENT_ID")
   private Long parentId;
 
-  @Column(name = "ORDER")
+  @Column(name = "PROPERTY_ORDER")
   private Long            order;
 
   @Column(name = "ACTIVE")
