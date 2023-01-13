79fba1ff8d01747bf3f09b403d20253f017c215b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-13 15:44:38 +0100

    feat: Define General Settings Portlet to replace Branding application - MEED-1499 - Meeds-io/MIPs#33 (#2154)
    This change will remove Branding Portlet to replace it by General
    Settings which defines more Site customizations.

--- webapp/portlet/src/main/webapp/vue-apps/general-settings/js/brandingService.js
+++ webapp/portlet/src/main/webapp/vue-apps/general-settings/js/brandingService.js
@@ -1,5 +1,23 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 export function updateBrandingInformation(branding){
-  return fetch(`/${eXo.env.portal.rest}/v1/platform/branding`, {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/platform/branding`, {
     method: 'PUT',
     credentials: 'include',
     headers: {
@@ -14,23 +32,10 @@ export function updateBrandingInformation(branding){
 }
 
 export function getBrandingInformation() {
-  return fetch(`/${eXo.env.portal.rest}/v1/platform/branding`, {
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/platform/branding`, {
     method: 'GET',
     credentials: 'include',
   }).then(resp => resp.json());
-}
-
-export function getBrandingDefaultLogo() {
-  return fetch(`/${eXo.env.portal.rest}/v1/platform/branding/defaultLogo?defaultLogo=404`, {
-    method: 'GET',
-    credentials: 'include',
-  }).then(resp => {
-    if (resp.ok) {
-      return resp.arrayBuffer();
-    } else {
-      return null;
-    }
-  });
 }
 
 export function throwErrorFromServerCall(serverResponse, defaultErrorMessage) {
