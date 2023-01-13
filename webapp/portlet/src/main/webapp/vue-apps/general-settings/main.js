7617e9d5d8e7c523c9d6b316009f4aa70cd6e142
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-13 15:44:38 +0100

    feat: Define General Settings Portlet to replace Branding application - MEED-1499 - Meeds-io/MIPs#33 (#2154)
    This change will remove Branding Portlet to replace it by General
    Settings which defines more Site customizations.

---
+++ webapp/portlet/src/main/webapp/vue-apps/general-settings/main.js
@@ -0,0 +1,55 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import './initComponents.js';
+import * as brandingService from './js/brandingService.js';
+
+window.Object.defineProperty(Vue.prototype, '$brandingService', {
+  value: brandingService,
+});
+
+// get overrided components if exists
+const components = extensionRegistry.loadComponents('GeneralSettings');
+if (components && components.length > 0) {
+  components.forEach(cmp => {
+    Vue.component(cmp.componentName, cmp.componentOptions);
+  });
+}
+
+const appId = 'generalSettings';
+const lang = window.eXo?.env?.portal?.language || 'en';
+
+// Should expose the locale ressources as REST API 
+const urls = [
+  `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.Portlets-${lang}.json`,
+  `${eXo.env.portal.context}/${eXo.env.portal.rest}/i18n/bundle/locale.portlet.GeneralSettings-${lang}.json`,
+];
+
+export function init(params) {
+  exoi18n.loadLanguageAsync(lang, urls)
+    .then(i18n =>
+      Vue.createApp({
+        data: {
+          params: params,
+        },
+        template: `<portal-general-settings id="${appId}" :params="params" />`,
+        vuetify: Vue.prototype.vuetifyOptions,
+        i18n
+      }, `#${appId}`, 'General Settings')
+    );
+}
