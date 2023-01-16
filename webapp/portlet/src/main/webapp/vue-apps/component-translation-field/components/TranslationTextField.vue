0a747eca540af535f83c949b45a8f0224cc95788
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-16 17:15:01 +0100

    feat: Improve General Settings Portlet by adding Login Background & l10n for Login Title & Subtitle - MEED-1501 - Meeds-io/MIPs#33 This change will define two re-usable component that will serve for General Settings: - Translation Drawer component - Image Cropper Drawer
    Both components will be used in General Settings to make it possible to change Login Page UI for better customizations.

---
+++ webapp/portlet/src/main/webapp/vue-apps/component-translation-field/components/TranslationTextField.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="translation-text-field">
+    <v-text-field
+      v-model="defaultLanguageValue"
+      :id="id"
+      :name="id"
+      :placeholder="placeholder"
+      :required="required"
+      :aria-required="required"
+      append-icon="fas fa-language primary--text"
+      class="border-box-sizing pt-0"
+      type="text"
+      outlined
+      dense
+      @click:append="openDrawer" />
+    <translation-drawer
+      ref="translationDrawer"
+      v-model="valuesPerLanguage"
+      :drawer-title="drawerTitle"
+      :default-language="defaultLanguage"
+      :supported-languages="supportedLanguages"
+      @input="$emit('input', $event)" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: null,
+    },
+    value: {
+      type: Object,
+      default: null,
+    },
+    placeholder: {
+      type: String,
+      default: null,
+    },
+    drawerTitle: {
+      type: String,
+      default: null,
+    },
+    defaultLanguage: {
+      type: String,
+      default: null,
+    },
+    supportedLanguages: {
+      type: Array,
+      default: null,
+    },
+    required: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  data: () => ({
+    defaultLanguageValue: null,
+    valuesPerLanguage: {},
+  }),
+  watch: {
+    value: {
+      immediate: true,
+      handler: function() {
+        this.valuesPerLanguage = this.value && JSON.parse(JSON.stringify(this.value)) || {};
+        this.defaultLanguageValue = this.valuesPerLanguage[this.defaultLanguage] || '';
+      },
+    },
+    defaultLanguageValue() {
+      if (this.defaultLanguageValue !== this.valuesPerLanguage[this.defaultLanguage]) {
+        this.valuesPerLanguage[this.defaultLanguage] = this.defaultLanguageValue;
+        this.$emit('input', this.valuesPerLanguage);
+      }
+    },
+  },
+  methods: {
+    openDrawer() {
+      this.$refs.translationDrawer.open();
+    },
+  },
+};
+</script>
