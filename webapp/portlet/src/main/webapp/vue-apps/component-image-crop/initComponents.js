d8e2d46d66c1e7917f27763cce609064b106e265
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-16 17:15:01 +0100

    feat: Improve General Settings Portlet by adding Login Background & l10n for Login Title & Subtitle - MEED-1501 - Meeds-io/MIPs#33 This change will define two re-usable component that will serve for General Settings: - Translation Drawer component - Image Cropper Drawer
    Both components will be used in General Settings to make it possible to change Login Page UI for better customizations.

--- webapp/portlet/src/main/webapp/vue-apps/component-image-crop/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/component-image-crop/initComponents.js
@@ -16,22 +16,10 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import GeneralSettings from './components/GeneralSettings.vue';
-import SiteBranding from './components/branding/SiteBranding.vue';
-import LoginBranding from './components/branding/LoginBranding.vue';
-import ColorPicker from './components/branding/form/ColorPicker.vue';
-import CompanyLogo from './components/branding/form/CompanyLogo.vue';
-import CompanyFavicon from './components/branding/form/CompanyFavicon.vue';
-import LoginBackgroundSelector from './components/branding/form/LoginBackgroundSelector.vue';
+import ImageCropDrawer from './components/ImageCropDrawer.vue';
 
 const components = {
-  'portal-general-settings': GeneralSettings,
-  'portal-general-settings-branding-site': SiteBranding,
-  'portal-general-settings-branding-login': LoginBranding,
-  'portal-general-settings-color-picker': ColorPicker,
-  'portal-general-settings-company-logo': CompanyLogo,
-  'portal-general-settings-company-favicon': CompanyFavicon,
-  'portal-general-settings-login-background-selector': LoginBackgroundSelector,
+  'image-crop-drawer': ImageCropDrawer,
 };
 
 for (const key in components) {
