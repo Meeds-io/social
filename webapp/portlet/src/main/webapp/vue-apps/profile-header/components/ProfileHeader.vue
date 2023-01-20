2582f25a96a9447ce91e86067a2954ddf6ca2210
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-20 17:58:35 +0100

    feat: Use Image cropper for Profile and Space avatars and banners - Meeds-io/meeds#555 (#2203)
    - This change will improve image cropper re-usable component to allow
    cropping an image into circle and use it for Profile avatar image upload
    process in Both portlets: Profile Header and Profile contact
    Information.
    
    - This change will improve image cropper re-usable component to allow
    cropping an image into rounded and use it for Space avatar image upload
    process.
    
    - This change will allow to use image cropper for Space Header image
    upload process.
    
    - This change will allow to use image cropper for Profile Banner image
    upload process. At the same time, this will fix Profile Header actions
    position in Desktop.
    
    - This change will as well fix the screen breakpoints to use same values
    as defined in CSS.

--- webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeader.vue
+++ webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeader.vue
@@ -1,70 +1,85 @@
 <template>
-  <v-app 
-    :class="owner && 'profileHeaderOwner' || 'profileHeaderOther'">
+  <v-app :class="owner && 'profileHeaderOwner' || 'profileHeaderOther'">
     <v-hover>
-      <v-img
-        slot-scope="{ hover }"
-        :lazy-src="user && user.banner"
-        :src="user && user.banner"
-        transition="none"
-        class="profileBannerImg d-flex"
-        min-height="240px"
-        height="240px"
-        max-height="240px"
-        eager>
-        <v-flex fill-height>
-          <v-layout>
-            <v-flex class="d-flex profileHeaderTitle">
-              <exo-confirm-dialog
-                ref="errorUploadDialog"
-                :message="errorMessage"
-                :title="$t('profileHeader.title.errorUploadingImage')"
-                :ok-label="$t('profileHeader.label.ok')" />
-              <v-hover>
-                <profile-header-avatar
-                  slot-scope="{ profileHover }"
-                  :user="user"
-                  :max-upload-size="maxUploadSizeInBytes"
-                  :owner="owner"
-                  :hover="hover || profileHover"
-                  save
-                  @refresh="avatarUpdated"
-                  @error="handleError" />
-              </v-hover>
-              <div class="profileHeader">
-                <div class="profileHeaderText align-start d-flex flex-grow-0">
-                  <profile-header-text
-                    :user="user"
-                    :skeleton="skeleton"
-                    :class="skeleton && 'skeleton-text' || ''"
-                    class="ma-auto pb-0" />
-                </div>
-                <div class="flex-grow-1"></div>
-                <div v-if="!skeleton" class="d-flex flex-row justify-end pe-6">
-                  <profile-header-banner-button
-                    v-if="owner"
-                    :user="user"
-                    :max-upload-size="maxUploadSizeInBytes"
-                    :owner="owner"
-                    :hover="hover"
-                    @refresh="refresh"
-                    @error="handleError" />
-                  <profile-header-actions
-                    v-else
-                    :user="user"
-                    :owner="owner"
-                    :hover="hover"
-                    @refresh="refresh" />
-                </div>
-              </div>
-            </v-flex>
-          </v-layout>
-        </v-flex>
-      </v-img>
+      <div slot-scope="{ hover }" class="white">
+        <v-card
+          height="13vw"
+          max-height="175"
+          class="d-flex position-relative overflow-hidden"
+          tile
+          flat>
+          <img
+            :src="user && user.banner"
+            alt=""
+            width="100%"
+            height="auto"
+            class="profileBannerImg absolute-vertical-center"
+            lazy>
+          <profile-header-banner-button
+            v-if="owner"
+            :user="user"
+            :hover="hover"
+            class="justify-end full-width position-absolute t-0 r-3 pt-3"
+            @edit="editBanner"
+            @refresh="refresh" />
+        </v-card>
+        <v-card
+          color="white"
+          class="d-flex flex-column flex-md-row border-color px-4"
+          flat
+          tile>
+          <v-card
+            :width="imageSize"
+            :max-width="165"
+            max-height="70"
+            height="11vw"
+            class="flex-shrink-0 position-relative me-2"
+            flat
+            tile>
+            <v-card
+              class="position-absolute z-index-two b-0 mb-2 mb-md-2"
+              color="transparent"
+              flat
+              tile>
+              <profile-header-avatar
+                :user="user"
+                :owner="owner"
+                :hover="hover"
+                :size="imageSize"
+                @edit="editAvatar" />
+            </v-card>
+          </v-card>
+          <v-card
+            min-height="70"
+            class="d-flex flex-column flex-sm-row flex-grow-1"
+            flat
+            tile>
+            <profile-header-text
+              :user="user"
+              class="d-flex flex-grow-0 text-truncate" />
+            <profile-header-actions
+              v-if="!owner"
+              :user="user"
+              :hover="hover"
+              class="profileHeader flex-grow-1 flex-shrink-0 d-flex flex-row justify-start justify-sm-end my-auto"
+              @refresh="refresh" />
+          </v-card>
+        </v-card>
+      </div>
     </v-hover>
+    <image-crop-drawer
+      v-if="owner"
+      ref="imageCropDrawer"
+      :src="imageCropperSrc"
+      :circle="imageCropperCircle"
+      :crop-options="imageCropperOptions"
+      :drawer-title="imageCropperDrawerTitle"
+      :max-file-size="maxUploadSizeInBytes"
+      :max-image-width="maxImageWidth"
+      @input="uploadImage" />
+    <alert-notifications v-if="owner" />
   </v-app>    
 </template>
-
 <script>
 export default {
   props: {
@@ -77,13 +92,54 @@ export default {
     user: null,
     owner: eXo.env.portal.profileOwner === eXo.env.portal.userName,
     errorMessage: null,
+    imageType: null,
   }),
   computed: {
-    mobile() {
-      return this.$vuetify.breakpoint.name === 'xs';
+    small() {
+      return this.$vuetify.breakpoint.mdAndDown;
+    },
+    large() {
+      return this.$vuetify.breakpoint.lgAndUp;
+    },
+    xlarge() {
+      return this.$vuetify.breakpoint.xlAndUp;
+    },
+    imageSize() {
+      return '15vw';
+    },
+    maxImageViewHeight() {
+      return this.large && '175px' || 'calc(16.6vw - 40px)';
     },
     maxUploadSizeInBytes() {
       return this.maxUploadSize * 1024 * 1024;
+    },
+    imageCropperDrawerTitle() {
+      return this.imageType === 'avatar' && this.$t('UIChangeAvatarContainer.label.ChangeAvatar') || this.$t('UIPopupBannerUploader.title.ChangeBanner');
+    },
+    imageCropperSrc() {
+      return this.user && (this.imageType === 'avatar' && `${this.user.avatar}&size=0` || `${this.user.banner}&size=0`);
+    },
+    imageCropperCircle() {
+      return this.imageType === 'avatar';
+    },
+    imageCropperOptions() {
+      return this.imageType === 'avatar' && {
+        aspectRatio: 1,
+        viewMode: 1,
+      } || {
+        aspectRatio: 1280 / 175,
+        viewMode: 1,
+      };
+    },
+    maxImageWidth() {
+      return this.imageType === 'avatar' && 350 || 1280;
+    },
+  },
+  watch: {
+    errorMessage() {
+      if (this.errorMessage) {
+        this.$root.$emit('alert-message', this.errorMessage, 'error');
+      }
     },
   },
   mounted() {
@@ -96,10 +152,52 @@ export default {
     });
   },
   methods: {
+    editAvatar() {
+      this.imageType = 'avatar';
+      this.$nextTick()
+        .then(() => this.$refs.imageCropDrawer.open());
+    },
+    editBanner() {
+      this.imageType = 'banner';
+      this.$nextTick()
+        .then(() => this.$refs.imageCropDrawer.open());
+    },
+    uploadImage(uploadId) {
+      if (this.imageType === 'avatar') {
+        this.uploadAvatar(uploadId);
+      } else if (this.imageType === 'banner') {
+        this.uploadBanner(uploadId);
+      }
+    },
+    uploadAvatar(uploadId) {
+      if (uploadId) {
+        return this.$userService.updateProfileField(eXo.env.portal.userName, 'avatar', uploadId)
+          .then(() => this.avatarUpdated())
+          .catch(this.handleError)
+          .finally(() => this.imageType = null);
+      }
+    },
+    uploadBanner(uploadId) {
+      if (uploadId) {
+        return this.$userService.updateProfileField(eXo.env.portal.userName, 'banner', uploadId)
+          .then(() => this.bannerUpdated())
+          .catch(this.handleError)
+          .finally(() => this.imageType = null);
+      }
+    },
     avatarUpdated() {
-      this.refresh().then(() => {
-        document.dispatchEvent(new CustomEvent('userModified', {detail: this.user}));
-      });
+      return this.refresh()
+        .then(() => {
+          document.dispatchEvent(new CustomEvent('userModified', {detail: this.user}));
+          this.$root.$emit('alert-message', this.$t('UIChangeAvatarContainer.label.AvatarUpdated') ,'success');
+        });
+    },
+    bannerUpdated() {
+      return this.refresh()
+        .then(() => {
+          document.dispatchEvent(new CustomEvent('userModified', {detail: this.user}));
+          this.$root.$emit('alert-message', this.$t('UIPopupBannerUploader.title.BannerUpdated') ,'success');
+        });
     },
     refresh() {
       return this.$userService.getUser(eXo.env.portal.profileOwner, 'relationshipStatus')
@@ -121,7 +219,6 @@ export default {
         } else {
           this.errorMessage = String(error);
         }
-        this.$refs.errorUploadDialog.open();
       }
     },
   },
