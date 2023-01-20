5edc8c2caa4475a706ae6491abc0d3f0f0d0f598
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-01-20 17:58:35 +0100

    feat: Use Image cropper for Profile and Space avatars and banners - Meeds-io/meeds#555 (#2203)
    - This change will improve image cropper re-usable component to allow
    cropping an image into circle and use it for Profile avatar image upload
    process in Both portlets: Profile Header and Profile contact
    Information.
    
    - This change will improve image cropper re-usable component to allow
    cropping an image into rounded and use it for Space avatar image upload
    process.
    
    - This change will allow to use image cropper for Space Header image
    upload process.
    
    - This change will allow to use image cropper for Profile Banner image
    upload process. At the same time, this will fix Profile Header actions
    position in Desktop.
    
    - This change will as well fix the screen breakpoints to use same values
    as defined in CSS.

--- webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderAvatar.vue
+++ webapp/portlet/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderAvatar.vue
@@ -1,24 +1,38 @@
 <template>
-  <v-avatar
-    :class="owner && hover && 'profileHeaderAvatarHoverEdit'"
-    :size="size"
-    class="align-start flex-grow-0 ms-3 my-3 profileHeaderAvatar">
-    <v-img
-      :lazy-src="(avatarData || user && `${user.avatar}&size=165x165`) || ''"
-      :src="(avatarData || user && `${user.avatar}&size=165x165`) || ''"
-      transition="none"
-      eager
-      role="presentation" />
-    <v-file-input
-      v-if="owner && !sendingImage"
-      v-show="hover"
-      ref="avatarInput"
-      prepend-icon="mdi-camera"
-      class="changeAvatarButton"
-      accept="image/*"
-      clearable
-      @change="uploadAvatar" />
-  </v-avatar>
+  <div class="d-flex">
+    <v-avatar
+      :class="owner && hover && 'profileHeaderAvatarHoverEdit'"
+      :size="size"
+      min-width="44"
+      min-height="44"
+      max-width="165"
+      max-height="165"
+      class="align-start flex-grow-0 border-color profileHeaderAvatar">
+      <v-img
+        :lazy-src="(avatarData || user && `${user.avatar}&size=165x165`) || ''"
+        :src="(avatarData || user && `${user.avatar}&size=165x165`) || ''"
+        transition="none"
+        min-width="44"
+        min-height="44"
+        max-width="165"
+        max-height="165"
+        eager
+        role="presentation" />
+      <v-btn
+        v-if="owner"
+        v-show="hover"
+        ref="avatarInput"
+        :title="$t('UIChangeAvatarContainer.label.ChangeAvatar')"
+        class="changeAvatarButton"
+        icon
+        outlined
+        dark
+        @click="$emit('edit')">
+        <v-icon size="18">fas fa-camera</v-icon>
+      </v-btn>
+    </v-avatar>
+    <slot></slot>
+  </div>
 </template>
 
 <script>
@@ -28,13 +42,9 @@ export default {
       type: Object,
       default: () => null,
     },
-    maxUploadSize: {
-      type: Number,
-      default: () => 0,
-    },
-    save: {
-      type: Boolean,
-      default: () => false,
+    avatarData: {
+      type: Array,
+      default: null,
     },
     owner: {
       type: Boolean,
@@ -47,62 +57,6 @@ export default {
     size: {
       type: Number,
       default: () => 165,
-    },
-    value: {
-      type: String,
-      default: () => null,
-    },
-  },
-  data: () => ({
-    sendingImage: false,
-    avatarData: null,
-  }),
-  watch: {
-    sendingImage() {
-      if (this.sendingImage) {
-        document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
-      } else {
-        document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
-      }
-    },
-  },
-  methods: {
-    reset() {
-      this.avatarData = null;
-      this.sendingImage = false;
-    },
-    uploadAvatar(file) {
-      if (file && file.size) {
-        if (file.type && file.type.indexOf('image/') !== 0) {
-          this.$emit('error', this.$t('profile.warning.message.fileType'));
-          return;
-        }
-        if (file.size > this.maxUploadSize) {
-          this.$emit('error', this.$uploadService.avatarExcceedsLimitError);
-          return;
-        }
-        this.sendingImage = true;
-        const thiss = this;
-        return this.$uploadService.upload(file)
-          .then(uploadId => {
-            if (this.save) {
-              return this.$userService.updateProfileField(eXo.env.portal.userName, 'avatar', uploadId);
-            } else {
-              const reader = new FileReader();
-              reader.onload = (e) => {
-                thiss.avatarData = e.target.result;
-                thiss.$forceUpdate();
-              };
-              reader.readAsDataURL(file);
-              this.$emit('input', uploadId);
-            }
-          })
-          .then(() => this.$emit('refresh'))
-          .catch(error => this.$emit('error', error))
-          .finally(() => {
-            this.sendingImage = false;
-          });
-      }
     },
   },
 };
