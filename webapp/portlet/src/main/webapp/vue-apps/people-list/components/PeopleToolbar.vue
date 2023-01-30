0b88500b6ca45d9d9ead978b84134ceae4110a8c
sofien haj chedli <shajchedli@exoplatform.com>
2023-01-30 11:17:09 +0100

    feat: Implement mobile view for people search - EXO-61919 - Meeds-io/MIPs#39 (#2262)

--- webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleToolbar.vue
+++ webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleToolbar.vue
@@ -3,10 +3,14 @@
     <div class="showingPeopleText text-sub-title ms-3 d-none d-sm-flex">
       {{ $t('peopleList.label.peopleCount', {0: peopleCount}) }}
     </div>
+    <span>
+      <v-icon v-if="showMobileFilter" left size="20" @click="mobileFilter">fa-arrow-left</v-icon>
+    </span>
     <v-spacer class="d-none d-sm-flex" />
     <v-scale-transition>
       <v-text-field
-        v-model="keyword"
+          v-show="isMobile && showMobileFilter || !isMobile"
+          v-model="keyword"
         :placeholder="$t('peopleList.label.filterPeople')"
         prepend-inner-icon="fa-filter"
         class="inputPeopleFilter pa-0 me-3 my-auto" />
@@ -23,13 +27,9 @@
         </option>
       </select>
     </v-scale-transition>
-    <v-icon
-      class="d-sm-none"
-      @click="openBottomMenu">
-      fa-filter
-    </v-icon>
-    <v-scale-transition>
+    <v-scale-transition >
       <v-btn
+          v-show="isMobile && showMobileFilter || !isMobile"
           class="btn px-2 btn-primary"
           outlined
           @click="openPeopleAdvancedFilterDrawer()"
@@ -41,44 +41,13 @@
           </span>
       </v-btn>
     </v-scale-transition>
-    <v-bottom-sheet v-model="bottomMenu" class="pa-0">
-      <v-sheet class="text-center" height="169px">
-        <v-toolbar
-          color="primary"
-          dark
-          class="border-box-sizing">
-          <v-btn text @click="bottomMenu = false">
-            {{ $t('peopleList.label.cancel') }}
-          </v-btn>
-          <v-spacer />
-          <v-toolbar-title>
-            <v-icon>fa-filter</v-icon>
-            {{ $t('peopleList.label.filter') }}
-          </v-toolbar-title>
-          <v-spacer />
-          <v-btn text @click="changeFilterSelection">
-            {{ $t('peopleList.label.confirm') }}
-          </v-btn>
-        </v-toolbar>
-        <v-list>
-          <v-list-item
-            v-for="peopleFilter in peopleFilters"
-            :key="peopleFilter"
-            @click="filterToChange = peopleFilter.value">
-            <v-list-item-title class="align-center d-flex">
-              <v-icon v-if="filterToChange === peopleFilter.value">fa-check</v-icon>
-              <span v-else class="me-6"></span>
-              <v-spacer />
-              <div>
-                {{ peopleFilter.text }}
-              </div>
-              <v-spacer />
-              <span class="me-6"></span>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-sheet>
-    </v-bottom-sheet>
+    <v-icon
+        size="24"
+        class="text-sub-title pa-1 my-auto mt-2 ml-auto"
+        v-show="isMobile && !showMobileFilter"
+        @click="mobileFilter()">
+        mdi-filter-outline
+    </v-icon>
   </v-toolbar>
 </template>
 
@@ -107,6 +76,7 @@ export default {
     startTypingKeywordTimeout: 0,
     typing: false,
     advancedFilterCount: 0,
+    showMobileFilter: false,
   }),
   created() {
     this.$root.$on('advanced-filter-count', (filterCount) => this.advancedFilterCount = filterCount );
@@ -127,7 +97,10 @@ export default {
     },
     advancedFilterCountDisplay() {
       return this.advancedFilterCount || 0;
-    }
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.width < 768;
+    },
   },
   watch: {
     keyword() {
@@ -163,6 +136,9 @@ export default {
         }
       }, this.endTypingKeywordTimeout);
     },
+    mobileFilter() {
+      this.showMobileFilter = !this.showMobileFilter;
+    }
   }
 };
 </script>
