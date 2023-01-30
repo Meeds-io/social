68261ca017e42df21d3e9c5de1bbbccf6dd5a0d5
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-01-30 14:44:58 +0100

    feat: Add an enhancement for the people item - MEED-1576  (#2265)
    This change will enhance the people item display in the peoples page and space members page.

--- webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleCardFront.vue
+++ webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleCardFront.vue
@@ -66,7 +66,7 @@
       <div v-if="isMobile && !isSameUser">
         <v-icon 
           size="14" 
-          class="my-3" 
+          class="my-1" 
           @click="openBottomMenu">
           fas fa-ellipsis-v
         </v-icon>
@@ -183,21 +183,20 @@
       </a>
     </div>
 
-    <v-card-text class="peopleCardBody align-center py-1">
+    <v-card-text class="peopleCardBody align-center py-0 py-sm-1">
       <a
         :href="url"
         :title="user.fullname"
         :class="usernameClass"
         class="userFullname font-weight-bold">
         {{ user.fullname }}
-        <span v-if="user.external == 'true' " class="externalFlagClass">
+        <span v-if="externalUser" class="externalFlagClass">
           {{ $t('peopleList.label.external') }}
         </span>
       </a>
       <v-card-subtitle 
-        class="userPositionLabel text-truncate py-0 mt-0 mt-sm-auto">
-        {{ user.position || '&nbsp;' }}
-      </v-card-subtitle>
+        class="userPositionLabel text-truncate py-0 mt-0 mt-sm-auto"
+        v-sanitized-html="userPosition" />
     </v-card-text>
 
     <v-card-actions v-if="!isSameUser && !isMobile" class="peopleCardActions">
@@ -388,7 +387,13 @@ export default {
     },
     outgoingUser() {
       return this.user?.relationshipStatus === 'OUTGOING';
-    }
+    },
+    userPosition() {
+      return this.user?.position || (this.isMobile ? '--' : '&nbsp;'); 
+    },
+    externalUser() {
+      return this.user.external === 'true';
+    },
   },
   created() {
     $(document).on('mousedown', () => {
