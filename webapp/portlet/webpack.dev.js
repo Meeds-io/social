85c04e17e8b661afed50761e68c441d1b82713f3
Mohamed Amine Krout <mkrout@exoplatform.com>
2023-02-20 20:53:18 +0100

    fix: No alert when adding an invalid URL to profile - EXO-62604 - meeds-io/meeds#737 (#2340)
    Prior to this change when the user edits his profile information and add
    or edit a URL field with less than 10 chars, nothing happens, the
    profile is not updated since the URL should be more than 10 chars, and
    no alert is displayed.
    After this modification the limit of 10 chars is removed, and the url
    should be valid, if the text added is not a valid url, the user will get
    an info alert telling that the URL won't be considered as it is
    not an actual link
    In addition, this change will also apply npm run lint formatting not
    formatted vue files.

--- webapp/portlet/webpack.dev.js
+++ webapp/portlet/webpack.dev.js
@@ -6,7 +6,7 @@ const webpackCommonConfig = require('./webpack.common.js');
 const app = 'social-portlet';
 
 // add the server path to your server location path
-const exoServerPath = "/exo-server";
+const exoServerPath = "D:\\\eXo\\servers\\platform-6.4.0-RC01";
 
 let config = merge(webpackCommonConfig, {
   output: {
