9f458d46a0b78b5af9132ca4f1a152267c3f366c
Ali Hamdi <ahamdi@exoplatform.com>
2023-03-07 14:54:38 +0100

    fix: stabilize social build for SocialUserProfileEventListenerImplTest

--- component/core/src/test/java/org/exoplatform/social/core/listeners/SocialUserProfileEventListenerImplTest.java
+++ component/core/src/test/java/org/exoplatform/social/core/listeners/SocialUserProfileEventListenerImplTest.java
@@ -20,7 +20,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.exoplatform.commons.utils.PropertyManager;
 import org.exoplatform.container.PortalContainer;
 import org.exoplatform.container.component.RequestLifeCycle;
 import org.exoplatform.services.log.ExoLogger;
