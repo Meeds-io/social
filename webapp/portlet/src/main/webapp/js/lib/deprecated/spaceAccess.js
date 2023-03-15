ce53352ee12fa11b4c9a9fc6c36fd0b0a498006b
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-03-15 13:41:52 +0100

    feat: Improve SpaceAccess Application UX - MEED-1899 - Meeds-io/meeds#784

--- webapp/portlet/src/main/webapp/js/lib/deprecated/spaceAccess.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  window.ajaxGet = function(url, callback) {
-    return fetch(url, {
-      method: 'GET',
-      credentials: 'include',
-    }).then(resp => resp && resp.ok && callback && callback());
-  };
-})();
