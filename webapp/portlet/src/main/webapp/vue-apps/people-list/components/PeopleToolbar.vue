c17020ed441fe8e176337f100c42eebcec4e9a41
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-03-27 11:31:28 +0100

    feat: People page enhancement - MEED-1838 - Meeds-io/meeds#746 (#2477)
    This change allows to: 
     - Reduce the filter dropdown width to 100px max
    - Remove the filter label in advanced filters and Keep the number of filters applied if applied

--- webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleToolbar.vue
+++ webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleToolbar.vue
@@ -1,55 +1,58 @@
 <template>
   <v-toolbar id="peopleListToolbar" flat>
-    <div class="showingPeopleText text-sub-title ms-3 d-none d-sm-flex">
-      {{ $t('peopleList.label.peopleCount', {0: peopleCount}) }}
+    <div>
+      <div class="showingPeopleText text-sub-title ms-3 d-none d-sm-flex">
+        {{ $t('peopleList.label.peopleCount', {0: peopleCount}) }}
+      </div>
+      <span v-show="mobileFilter" class="hidden-sm-and-up">
+        <v-icon
+          left
+          size="20"
+          @click="showMobileFilter">fa-arrow-left</v-icon>
+      </span>
     </div>
-    <span v-show="mobileFilter" class="hidden-sm-and-up">
+    <div class="d-flex align-center flex-grow-1">
+      <v-col>
+        <v-text-field
+          v-show="isMobile && mobileFilter || !isMobile"
+          v-model="keyword"
+          :placeholder="$t('peopleList.label.filterPeople')"
+          prepend-inner-icon="fa-filter"
+          class="inputPeopleFilter pa-0 mb-n2 my-auto ms-auto" />
+      </v-col>
+      <v-scale-transition>
+        <select
+          v-model="filter"
+          class="selectPeopleFilter my-auto me-2 subtitle-1 ignore-vuetify-classes d-none d-sm-inline">
+          <option
+            v-for="peopleFilter in peopleFilters"
+            :key="peopleFilter.value"
+            :value="peopleFilter.value">
+            {{ peopleFilter.text }}
+          </option>
+        </select>
+      </v-scale-transition>
+      <v-scale-transition>
+        <v-btn
+          v-show="isMobile && mobileFilter || !isMobile"
+          class="btn px-2 btn-primary"
+          :min-width="iconWidth"
+          outlined
+          @click="openPeopleAdvancedFilterDrawer()">
+          <v-icon small class="primary--text me-lg-1">fa-sliders-h</v-icon>
+          <span class="d-none font-weight-regular caption d-lg-inline me-1">
+            {{ $t('profile.label.search.openSearch') }} </span>
+          <span class="font-weight-regular caption ms-1"> {{ advancedFilterCountDisplay }} </span>
+        </v-btn>
+      </v-scale-transition>
       <v-icon
-        left
-        size="20"
-        @click="showMobileFilter">fa-arrow-left</v-icon>
-    </span>
-    <v-spacer class="d-none d-sm-flex" />
-    <v-col>
-      <v-text-field
-        v-show="isMobile && mobileFilter || !isMobile"
-        v-model="keyword"
-        :placeholder="$t('peopleList.label.filterPeople')"
-        prepend-inner-icon="fa-filter"
-        class="inputPeopleFilter pa-0 mb-n2 my-auto" />
-    </v-col>
-    <v-scale-transition>
-      <select
-        v-model="filter"
-        class="selectPeopleFilter my-auto me-2 subtitle-1 ignore-vuetify-classes d-none d-sm-inline">
-        <option
-          v-for="peopleFilter in peopleFilters"
-          :key="peopleFilter.value"
-          :value="peopleFilter.value">
-          {{ peopleFilter.text }}
-        </option>
-      </select>
-    </v-scale-transition>
-    <v-scale-transition>
-      <v-btn
-        v-show="isMobile && mobileFilter || !isMobile"
-        class="btn px-2 btn-primary"
-        outlined
-        @click="openPeopleAdvancedFilterDrawer()">
-        <v-icon small class="primary--text mr-1">fa-sliders-h</v-icon>
-        <span class="d-none font-weight-regular caption d-sm-inline mr-1">
-          {{ $t('profile.label.search.openSearch') }}
-          {{ advancedFilterCountDisplay }}
-        </span>
-      </v-btn>
-    </v-scale-transition>
-    <v-icon
-      size="24"
-      class="text-sub-title pa-1 my-auto mt-2 ml-auto"
-      v-show="isMobile && !mobileFilter"
-      @click="showMobileFilter()">
-      mdi-filter-outline
-    </v-icon>
+        size="24"
+        class="text-sub-title pa-1 my-auto mt-2 ms-auto"
+        v-show="isMobile && !mobileFilter"
+        @click="showMobileFilter()">
+        mdi-filter-outline
+      </v-icon>
+    </div>
   </v-toolbar>
 </template>
 
@@ -79,6 +82,7 @@ export default {
     typing: false,
     advancedFilterCount: 0,
     mobileFilter: false,
+    iconWidth: '24px'
   }),
   created() {
     this.$root.$on('advanced-filter-count', (filterCount) => this.advancedFilterCount = filterCount);
@@ -101,7 +105,7 @@ export default {
     },
     isMobile() {
       return this.$vuetify.breakpoint.width < 768;
-    },
+    }
   },
   watch: {
     keyword() {
