f66938d5e736724ac6813e92dffa09682926cdfe
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-05-10 19:32:38 +0100

    feat: Attach an image to an activity MEED-2030- Meeds-io/MIPs#53 (#2516)
    This task add a new ckeditor plugin allows to attach images to an
    activity from the rich text editor.
    
    ---------
    
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>

--- extension/war/src/main/webapp/WEB-INF/conf/social-extension/ckeditor/config.js
+++ extension/war/src/main/webapp/WEB-INF/conf/social-extension/ckeditor/config.js
@@ -22,7 +22,6 @@ CKEDITOR.editorConfig = function(config) {
   CKEDITOR.plugins.addExternal('embedsemantic', '/commons-extension/eXoPlugins/embedsemantic/', 'plugin.js');
   CKEDITOR.plugins.addExternal('tagSuggester', '/commons-extension/eXoPlugins/tagSuggester/', 'plugin.js');
   CKEDITOR.plugins.addExternal('formatOption', '/commons-extension/eXoPlugins/formatOption/', 'plugin.js');
-  CKEDITOR.plugins.addExternal('attachImage', '/commons-extension/eXoPlugins/attachImage/', 'plugin.js');
   CKEDITOR.plugins.addExternal('googleDocPastePlugin', '/commons-extension/eXoPlugins/googleDocPastePlugin/', 'plugin.js')
 
   const embedBaseApiEndpoint = '@JVMProp{io.meeds.iframely.url://ckeditor.iframe.ly/api/oembed?omit_script=1}';
