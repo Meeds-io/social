2de28b16bacd5ff3523ed473d9514b24e8b3f5e5
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-05-10 19:32:38 +0100

    feat: Attach an image to an activity MEED-2030- Meeds-io/MIPs#53 (#2516)
    This task add a new ckeditor plugin allows to attach images to an
    activity from the rich text editor.
    
    ---------
    
    Co-authored-by: Boubaker Khanfir <boubaker.khanfir@exoplatform.com>

---
+++ webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachedItems.vue
@@ -0,0 +1,44 @@
+<template>
+  <card-carousel v-if="attachmentsCount" parent-class="activity-files-parent">
+    <attached-item
+      v-for="(attachment, index) in attachments"
+      :key="attachment.id"
+      :activity="activity"
+      :index="index"
+      :count="attachmentsCount"
+      :attachment="attachment"
+      :preview-width="previewWidth"
+      :preview-height="previewHeight"
+      class="activity-file-item" />
+  </card-carousel>
+</template>
+
+<script>
+export default {
+  props: {
+    activity: {
+      type: Object,
+      default: null,
+    },
+    previewHeight: {
+      type: String,
+      default: () => '152px',
+    },
+    previewWidth: {
+      type: String,
+      default: () => '250px',
+    },
+  },
+  computed: {
+    attachments() {
+      return this.activity?.metadatas?.attachments?.map(metadata => ({
+        id: metadata.name,
+        thumbnailUrl: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/attachments/activity/${this.activity.id}/${metadata.name}?size=120x120`,
+      })) || [];
+    },
+    attachmentsCount() {
+      return this.attachments.length;
+    },
+  }
+};
+</script>
