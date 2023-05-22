e8dcc374985069c01689cfb8787201576211527d
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-05-22 15:38:54 +0100

    feat: View images -MEED-2039 - Meeds-io/MIPs#53 (#2539)
    This PR allows to add the preview and to download options to activity attached images.

--- webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachedItems.vue
+++ webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachedItems.vue
@@ -1,16 +1,16 @@
 <template>
-  <card-carousel v-if="attachmentsCount" parent-class="activity-files-parent">
-    <attached-item
-      v-for="(attachment, index) in attachments"
-      :key="attachment.id"
-      :activity="activity"
-      :index="index"
-      :count="attachmentsCount"
-      :attachment="attachment"
-      :preview-width="previewWidth"
-      :preview-height="previewHeight"
-      class="activity-file-item" />
-  </card-carousel>
+  <div v-if="attachmentsCount">
+    <card-carousel parent-class="activity-files-parent mx-n2">
+      <attached-item
+        v-for="attachment in attachments"
+        :key="attachment.id"
+        :attachment="attachment"
+        :preview-width="previewWidth"
+        :preview-height="previewHeight"
+        class="activity-file-item"
+        @preview-attachment="openPreview(attachment.id)" />
+    </card-carousel>
+  </div>
 </template>
 
 <script>
@@ -29,6 +29,9 @@ export default {
       default: () => '250px',
     },
   },
+  data: () => ({
+    objectType: 'activity',
+  }),
   computed: {
     attachments() {
       return this.activity?.metadatas?.attachments?.map(metadata => ({
@@ -38,6 +41,23 @@ export default {
     },
     attachmentsCount() {
       return this.attachments.length;
+    },
+    objectId() {
+      return this.activity?.id;
+    },
+  },
+  methods: {
+    openPreview(attachmentId) {
+      const previewAttachments = this.activity?.metadatas?.attachments?.map(metadata => ({
+        id: metadata.name,
+        filename: metadata.properties.fileName,
+        fileSize: metadata.properties.fileSize,
+        fileMimeType: metadata.properties.fileMimeType,
+        fileUpdateDate: metadata.properties.fileUpdateDate,
+        thumbnailUrl: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/attachments/activity/${this.activity.id}/${metadata.name}?size=0x0&download=true`,
+      })) || [];
+
+      this.$root.$emit('open-attachments-preview', this.objectType, this.objectId, previewAttachments, attachmentId);
     },
   }
 };
