743f151e8409f984792a1991c24b085158a07d4c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-05-22 15:08:06 +0100

    feat: Add Confeti Animation Re-usable component - MEED-1863 - Meeds-io/MIPs#49

--- webapp/portlet/src/main/webapp/vue-apps/animations/components/ConfetiAnimation.vue
+++ webapp/portlet/src/main/webapp/vue-apps/animations/components/ConfetiAnimation.vue
@@ -1,61 +1,102 @@
 <template>
   <v-card
     color="transparent"
-    min-width="250px"
+    min-width="400px"
     width="100%"
     height="250px"
-    class="d-flex align-end justify-end"
+    class="confeti-animation-parent d-flex align-end justify-center pb-2"
     flat>
-    <div class="confeti-animation"></div>
+    <div class="confeti-animation">
+      <div
+        v-for="index in 400"
+        :key="index"
+        class="particle"></div>
+    </div>
   </v-card>
 </template>
 <style lang="scss">
-  $particles: 180;
-  $width: 250;
+  $length: 400;
+  $particles: 400;
+  $width: 400;
   $height: 250;
-
-  // Array of colours to randomly populate the particle colours.
+  $timing: 1.3s;
+  $repeat: 1;
   $colors: 
     #4ad66d, #ffe169, #ff8fa3, #20a8ea, #C155F4, #F7A35B, #A0C7FF, #FD6A6A, #059d98, #b7efc5,
     #dbb42c, #c9184a, #1273d4, #E65ABC, #00FF56, #B1F6FF, #FFFF46, #26a855, #f10000, #208b3a,
     #c9a227, #ffccd5, #134d9b, #E66CDC, #58D68B, #5CE6D3, #f16a27, #ac1c1e, #eda3ff, #1a7431,
     #a47e1b, #ff4d6d, #62b0de, #FF97D0, #92e03a, #f44336, #3d6d8a, #E0A5FF, #FF9DB8, #808080;
 
-  // Get the random colour
   @function random-color() {
     @return nth($colors, random(40));
   }
 
-  // Create the explosion...
+  @mixin create-path() {
+    $end-point-w: random($length) - ($length / 2);
+    $end-point-h: random($height) - ($height / 2);
+    $bezier-w: random($length) - ($length / 2);
+    $bezier-h: random($height) - ($height / 2);
+    offset-path: path(
+      "M 0, 0 " + 
+      "q #{$bezier-w}, #{$bezier-h}, #{$end-point-w}, #{$end-point-h} "
+    );
+  }
+
   $box-shadow: ();
   @for $i from 0 through $particles {
     $box-shadow: $box-shadow,
-      random($width)-$width /
-        2 +
-        px
-        random($height)-$height /
-        2 +
-        px
-        #{random-color()};
+      random($width)- $width / 2 + px
+      random($height)- $height / 2 + px
+      #{random-color()};
+  }
+
+  .confeti-animation-parent {
+    transform: translateY(50%);
   }
 
   .confeti-animation::before {
     position: absolute;
     content: '';
-    width: 4px;
-    height: 7px;
-    animation: 1.5s explode ease-out 120 backwards,
-      1.5s gravity ease-in 120 backwards;
+    transform: translateY(-50%, -50%);
   }
 
-  @keyframes explode {
-    to {
-      box-shadow: $box-shadow;
+  .particle {
+    width: 10px;
+    height: 4px;
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    pointer-events: none;
+    offset-distance: 0;
+    opacity: 0;
+
+    @for $i from 0 through $particles {
+      &:nth-of-type(#{$i}) {
+        @include create-path();
+        $col-amount: 500;
+        animation: $timing move ease-out $repeat backwards,
+                   $timing fade ease-in-out $repeat backwards;
+        background: #{random-color()};
+      }
     }
   }
 
-  @keyframes gravity {
+  @keyframes move {
+    0% {
+      offset-distance: 0%;
+    }
+    100% {
+      offset-distance: 100%;
+    }
+  }
+
+  @keyframes fade {
+    from {
+      opacity: 1;
+    }
     to {
+      width: 0;
+      height: 0;
       transform: translateY(-75px);
       opacity: 0;
     }
