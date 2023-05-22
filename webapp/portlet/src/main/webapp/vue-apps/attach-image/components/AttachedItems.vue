6bd2345ba1ff4f88f14ec0d6b49d70d7e3414b66
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-05-22 15:38:54 +0100

    feat: View images -MEED-2039 - Meeds-io/MIPs#53 (#2539)
    This PR allows to add the preview and to download options to activity attached images.

--- webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachedItems.vue
+++ webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachedItems.vue
@@ -1,16 +1,21 @@
 <template>
-  <card-carousel v-if="attachmentsCount" parent-class="activity-files-parent">
-    <attached-item
-      v-for="(attachment, index) in attachments"
-      :key="attachment.id"
-      :activity="activity"
-      :index="index"
-      :count="attachmentsCount"
-      :attachment="attachment"
-      :preview-width="previewWidth"
-      :preview-height="previewHeight"
-      class="activity-file-item" />
-  </card-carousel>
+  <div v-if="attachmentsCount">
+    <card-carousel parent-class="activity-files-parent">
+      <attached-item
+        v-for="(attachment, index) in attachments"
+        :key="attachment.id"
+        :activity="activity"
+        :index="index"
+        :attachment="attachment"
+        :preview-width="previewWidth"
+        :preview-height="previewHeight"
+        class="activity-file-item clickable"
+        @preview-attachment="openPreview(attachment.id)" />
+    </card-carousel>
+    <attachment-preview-dialog 
+      ref="attachmentPreview"
+      :activity="activity" /> 
+  </div>
 </template>
 
 <script>
@@ -29,6 +34,9 @@ export default {
       default: () => '250px',
     },
   },
+  data: () => ({
+    previewAttachments: []
+  }),
   computed: {
     attachments() {
       return this.activity?.metadatas?.attachments?.map(metadata => ({
@@ -38,6 +46,19 @@ export default {
     },
     attachmentsCount() {
       return this.attachments.length;
+    },
+  },
+  methods: {
+    openPreview(attachmentId) {
+      this.previewAttachments = this.activity?.metadatas?.attachments?.map(metadata => ({
+        id: metadata.name,
+        filename: metadata.properties.fileName,
+        fileSize: metadata.properties.fileSize,
+        fileMimeType: metadata.properties.fileMimeType,
+        fileUpdateDate: metadata.properties.fileUpdateDate,
+        thumbnailUrl: `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/attachments/activity/${this.activity.id}/${metadata.name}?size=0x0&download=true`,
+      })) || [];
+      this.$refs.attachmentPreview.open(this.previewAttachments, attachmentId);
     },
   }
 };
---
+++ webapp/portlet/src/main/webapp/vue-apps/attach-image/components/AttachmentPreviewDialog.vue
@@ -0,0 +1,131 @@
+<!--
+  This file is part of the Meeds project (https://meeds.io/).
+  Copyright (C) 2023 Meeds Association
+  contact@meeds.io
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+<template>
+  <v-dialog
+    v-model="dialog"
+    :persistent="false"
+    width="84vw"
+    overlay-opacity="0.9"
+    content-class="uiPopup overflow-y-initial "
+    max-width="80vw">
+    <div class="ignore-vuetify-classes ClearFix preview-attachment-action d-flex justify-end">
+      <v-btn 
+        :href="attachmentURL" 
+        :download="attachmentFilename"
+        icon
+        class="icon-large-size white--text"
+        aria-hidden="true">
+        <i class="fas fa-download"></i>
+      </v-btn>
+      <v-btn
+        icon
+        class="icon-large-size white--text ml-4"
+        aria-hidden="true"
+        @click="close">
+        <i class="fas fa-times"></i>
+      </v-btn>
+    </div>
+    <v-card 
+      flat
+      max-height="80vh"
+      class="transparent">
+      <v-carousel 
+        v-model="currentAttchmentId"
+        :id="`previewCarousel-${activityId}`"
+        :touchless="true"
+        :value="currentAttchmentId"
+        :show-arrows-on-hover="!isMobile"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+        hide-delimiters                        
+        height="80vh"
+        class="AttachmentCarouselPreview white border-radius">
+        <v-carousel-item
+          v-for="attachment in attachments"
+          :key="attachment.id"
+          :value="attachment.id">
+          <v-img 
+            :src="attachment.thumbnailUrl" 
+            :aspect-ratio="2"
+            height="80vh"
+            contain />
+        </v-carousel-item>
+      </v-carousel>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    activity: {
+      type: Object,
+      default: null,
+    }
+  },
+  data: () => ({
+    dialog: false,
+    currentAttchmentId: 0, 
+    filename: '',
+    objectType: 'activity'
+  }),
+  watch: {
+    dialog() {
+      if (this.dialog) {
+        this.$emit('dialog-opened');
+        document.dispatchEvent(new CustomEvent('modalOpened'));
+      } else {
+        this.$emit('dialog-closed');
+        document.dispatchEvent(new CustomEvent('modalClosed'));
+      }
+    }
+  },
+  computed: {
+    activityId() {
+      return this.activity?.id;
+    },
+    attachmentURL() {
+      return `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/attachments/activity/${this.activity.id}/${this.currentAttchmentId}?size=0x0&download=true`;
+    },
+    attachmentFilename() {
+      return  this.attachments?.length && this.attachments.filter(attachment => attachment.id === this.currentAttchmentId).finename || this.filename;
+    }, 
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'sm' || this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'md';
+    }
+  },
+  created() {
+    document.addEventListener('keydown', (event) => {
+      if (event.key === 'Escape') {
+        this.dialog = false;
+      }
+    });
+  },
+  methods: {
+    open(attachments, id) {
+      this.attachments = attachments;
+      this.currentAttchmentId = id;
+      this.filename = this.attachments.filter(attachment => attachment.id === id)[0].filename;
+      this.dialog = true;
+    },
+    close() {
+      this.dialog = false;
+    }
+  }
+};
+</script>
+
