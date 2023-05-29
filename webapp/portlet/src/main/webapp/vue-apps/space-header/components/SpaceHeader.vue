3f4b0e12db083ba2f651c94052d526e8d5761cde
GouadriaHanen <hgouadria@exoplatform.com>
2023-05-29 17:38:05 +0100

    feat: Add redirection for link with http  - EXO-64521 - Meeds-io/MIPs#51

--- webapp/portlet/src/main/webapp/vue-apps/space-header/components/SpaceHeader.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-header/components/SpaceHeader.vue
@@ -197,7 +197,7 @@ export default {
         });
     },
     urlVerify(url) {
-      if (!(url.indexOf('javascript:') === 0 && url.indexOf('portal') === 0)) {
+      if (!url.match(/^((https?:\/\/))|(javascript:)|(\/portal\/)/)) {
         url = `//${url}`;
       }
       return url ;
