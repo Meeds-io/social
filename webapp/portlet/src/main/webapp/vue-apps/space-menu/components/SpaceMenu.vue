d20c68d4c4eaa26a1132b276d38a2883eec9abca
GouadriaHanen <49310007+GouadriaHanen@users.noreply.github.com>
2023-05-29 17:40:03 +0100

    feat: Add redirection for link with http  - EXO-64521 - Meeds-io/MIPs#51 (#2568)

--- webapp/portlet/src/main/webapp/vue-apps/space-menu/components/SpaceMenu.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-menu/components/SpaceMenu.vue
@@ -113,7 +113,7 @@ export default {
       });
     },
     urlVerify(url) {
-      if (!(url.indexOf('javascript:') === 0 || url.indexOf('/portal') === 0)) {
+      if (!url.match(/^((https?:\/\/))|(javascript:)|(\/portal\/)/)) {
         url = `//${url}`;
       }
       return url ;
