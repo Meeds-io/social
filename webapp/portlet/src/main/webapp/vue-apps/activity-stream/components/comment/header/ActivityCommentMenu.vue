aa17b7d93f9387604fcc627b0954073b79d621ca
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-13 16:56:47 +0100

    fix: Fix Avatar Fullname Truncation Display - MEED-2249 - Meeds-io/meeds#988 (#2611)
    Prior to this change, the user full name wasn't truncated in Activity
    Comments drawer to let three dots button displayed. This change will
    truncate the name when it overflows the drawer width and let the avatar
    and three dots button displayed without truncation.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/header/ActivityCommentMenu.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/comment/header/ActivityCommentMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-list-item-icon v-show="enabledActions && enabledActions.length" class="mx-0 mb-0 mt-n1">
+  <v-list-item-icon v-show="enabledActions && enabledActions.length" class="mx-0 mb-0 mt-n1 flex-shrink-0">
     <v-menu
       v-model="menu"
       content-class="white"
