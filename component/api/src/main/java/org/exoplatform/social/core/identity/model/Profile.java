851bc92cf988b3e0543d55d9714779ed17fe011f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-14 13:21:17 +0100

    feat: Include modifier username in profile update operation - MEED-2206 - Meeds-io/MIPs#50 This change will include modifier username in triggered event of profile update when it's made using REST API.

--- component/api/src/main/java/org/exoplatform/social/core/identity/model/Profile.java
+++ component/api/src/main/java/org/exoplatform/social/core/identity/model/Profile.java
@@ -174,30 +174,30 @@ public class Profile {
     BANNER,
     TECHNICAL;
 
-    public void updateActivity(ProfileLifeCycle profileLifeCycle, Profile profile) {
+    public void updateActivity(ProfileLifeCycle profileLifeCycle, Profile profile, String modifierUsername) {
       switch (this) {
       case ABOUT_ME: {
-        profileLifeCycle.aboutMeUpdated(profile.getIdentity().remoteId, profile);
+        profileLifeCycle.aboutMeUpdated(profile.getIdentity().remoteId, profile, modifierUsername);
         break;
       }
       case CONTACT: {
-        profileLifeCycle.contactUpdated(profile.getIdentity().getRemoteId(), profile);
+        profileLifeCycle.contactUpdated(profile.getIdentity().getRemoteId(), profile, modifierUsername);
         break;
       }
       case EXPERIENCES: {
-        profileLifeCycle.experienceUpdated(profile.getIdentity().getRemoteId(), profile);
+        profileLifeCycle.experienceUpdated(profile.getIdentity().getRemoteId(), profile, modifierUsername);
         break;
       }
       case AVATAR: {
-        profileLifeCycle.avatarUpdated(profile.getIdentity().getRemoteId(), profile);
+        profileLifeCycle.avatarUpdated(profile.getIdentity().getRemoteId(), profile, modifierUsername);
         break;
       }
       case BANNER: {
-        profileLifeCycle.bannerUpdated(profile.getIdentity().getRemoteId(), profile);
+        profileLifeCycle.bannerUpdated(profile.getIdentity().getRemoteId(), profile, modifierUsername);
         break;
       }
       case TECHNICAL: {
-        profileLifeCycle.technicalUpdated(profile.getIdentity().getRemoteId(), profile);
+        profileLifeCycle.technicalUpdated(profile.getIdentity().getRemoteId(), profile, modifierUsername);
         break;
       }
       default:
