44cfd540b4059a3f4034f0419399db8f03b4556f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-20 18:08:12 +0100

    feat: Make Image Attachment Form & View Components Generic & re-usable - MEED-2038 - Meeds-io/MIPs#53 (#2597)

--- webapp/portlet/src/main/webapp/vue-apps/common/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/common/main.js
@@ -19,7 +19,6 @@ import * as brandingService from './js/brandingService.js';
 import * as navigationService from '../common/js/NavigationService.js';
 import * as profileSettingsService from '../common/js/ProfileSettingsService.js';
 import * as profileLabelService from '../common/js/ProfileLabelService.js';
-import * as fileAttachmentService from '../common/js/FileAttachmentService.js';
 
 // get overrided components if exists
 if (extensionRegistry) {
@@ -92,9 +91,6 @@ window.Object.defineProperty(Vue.prototype, '$profileLabelService', {
 });
 window.Object.defineProperty(Vue.prototype, '$navigationService', {
   value: navigationService,
-});
-window.Object.defineProperty(Vue.prototype, '$fileAttachmentService', {
-  value: fileAttachmentService,
 });
 
 if (eXo.env.portal.userIdentityId) {
