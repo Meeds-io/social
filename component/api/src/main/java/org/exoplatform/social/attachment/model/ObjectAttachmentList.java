689264874c6812c18b26be68fb7ea0be706ef4e3
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-20 13:55:00 +0100

    feat: Refactor Backend To allow Defining Specific Attachment Storage - MEED-2038 - Meeds-io/MIPs#53 (#2597)

---
+++ component/api/src/main/java/org/exoplatform/social/attachment/model/ObjectAttachmentList.java
@@ -0,0 +1,43 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2023 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.exoplatform.social.attachment.model;
+
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ObjectAttachmentList implements Cloneable {
+
+  private List<ObjectAttachmentDetail> attachments;
+
+  private String                       objectType;
+
+  private String                       objectId;
+
+  @Override
+  public ObjectAttachmentList clone() { // NOSONAR
+    return new ObjectAttachmentList(attachments, objectType, objectId);
+  }
+
+}
