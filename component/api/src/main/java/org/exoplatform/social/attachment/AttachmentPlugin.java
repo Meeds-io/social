21034b2d4f3a6f436dbcfa15477a857410c805c9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-06-20 13:55:00 +0100

    feat: Refactor Backend To allow Defining Specific Attachment Storage - MEED-2038 - Meeds-io/MIPs#53 (#2597)

--- component/api/src/main/java/org/exoplatform/social/attachment/AttachmentPlugin.java
+++ component/api/src/main/java/org/exoplatform/social/attachment/AttachmentPlugin.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package org.exoplatform.social.metadata;
+package org.exoplatform.social.attachment;
 
 import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.container.component.BaseComponentPlugin;
