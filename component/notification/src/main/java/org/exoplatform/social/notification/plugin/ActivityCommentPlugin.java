3446625008a96c2e2d4658908a0ffe845fed6b58
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-07-04 16:43:02 +0100

    feat: Allow to disable Activity Notifications by plugin - MEED-2290 - Meeds-io/MIPs#50 (#2644)
    Prior to this change, the notifications of activity types were disabled
    by a property value, which isn't extensible and can't be defined in each
    addon apart. This change will allow to disable activity notifications by
    component plugin instead.
    In addition, this change will add a generic upgrade plugin that allows
    to setup default notification settings for existing users.

--- component/notification/src/main/java/org/exoplatform/social/notification/plugin/ActivityCommentPlugin.java
+++ component/notification/src/main/java/org/exoplatform/social/notification/plugin/ActivityCommentPlugin.java
@@ -16,25 +16,25 @@
  */
 package org.exoplatform.social.notification.plugin;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.commons.lang.StringUtils;
+
 import org.exoplatform.commons.api.notification.NotificationContext;
 import org.exoplatform.commons.api.notification.model.NotificationInfo;
 import org.exoplatform.commons.api.notification.plugin.BaseNotificationPlugin;
 import org.exoplatform.container.xml.InitParams;
 import org.exoplatform.social.core.activity.model.ExoSocialActivity;
 import org.exoplatform.social.core.identity.model.Identity;
-import org.exoplatform.social.core.identity.provider.SpaceIdentityProvider;
 import org.exoplatform.social.notification.Utils;
-
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
 
 public class ActivityCommentPlugin extends BaseNotificationPlugin {
 
-  public static final String ID = "ActivityCommentPlugin";
+  public static final String ID           = "ActivityCommentPlugin";
 
-  protected boolean isSubComment = false;
+  protected boolean          isSubComment = false;
 
   public ActivityCommentPlugin(InitParams initParams) {
     super(initParams);
@@ -45,7 +45,7 @@ public class ActivityCommentPlugin extends BaseNotificationPlugin {
     ExoSocialActivity comment = ctx.value(SocialNotificationUtils.ACTIVITY);
     ExoSocialActivity activity = Utils.getActivityManager().getParentActivity(comment);
     String spaceId = activity.getSpaceId();
-    Set<String> receivers = new HashSet<String>();
+    Set<String> receivers = new HashSet<>();
     if (StringUtils.isNotBlank(comment.getParentCommentId())) {
       ExoSocialActivity parentComment = Utils.getActivityManager().getActivity(comment.getParentCommentId());
       String parentCommentUserPosterId = Utils.getUserId(parentComment.getPosterId());
@@ -53,14 +53,15 @@ public class ActivityCommentPlugin extends BaseNotificationPlugin {
         // Send notification to parent comment poster
         Utils.sendToActivityPoster(receivers, parentComment.getPosterId(), comment.getPosterId(), spaceId);
       } else {
-        // Send notification to all others users who have commented on this activity
+        // Send notification to all others users who have commented on this
+        // activity
         // except parent comment poster
         Utils.sendToCommeters(receivers, activity.getCommentedIds(), comment.getPosterId(), spaceId);
         Utils.sendToStreamOwner(receivers, activity.getStreamOwner(), comment.getPosterId());
         Utils.sendToActivityPoster(receivers, activity.getPosterId(), comment.getPosterId(), spaceId);
         receivers.remove(parentCommentUserPosterId);
         Utils.sendToLikers(receivers, activity.getLikeIdentityIds(), activity.getPosterId(), spaceId);
-        receivers.remove( Utils.getUserId(comment.getPosterId()));
+        receivers.remove(Utils.getUserId(comment.getPosterId()));
       }
     } else {
       // Send notification to all others users who have comment on this activity
@@ -68,18 +69,16 @@ public class ActivityCommentPlugin extends BaseNotificationPlugin {
       Utils.sendToStreamOwner(receivers, activity.getStreamOwner(), comment.getPosterId());
       Utils.sendToActivityPoster(receivers, activity.getPosterId(), comment.getPosterId(), spaceId);
       Utils.sendToLikers(receivers, activity.getLikeIdentityIds(), activity.getPosterId(), spaceId);
-      receivers.remove( Utils.getUserId(comment.getPosterId()));
+      receivers.remove(Utils.getUserId(comment.getPosterId()));
 
     }
-    //
     return NotificationInfo.instance()
-           .to(new ArrayList<String>(receivers))
-           .with(SocialNotificationUtils.ACTIVITY_ID.getKey(), activity.getId())
-           .with(SocialNotificationUtils.COMMENT_ID.getKey(), comment.getId())
-           .with(SocialNotificationUtils.POSTER.getKey(), Utils.getUserId(comment.getUserId()))
-           .key(getId());
+                           .to(new ArrayList<>(receivers))
+                           .with(SocialNotificationUtils.ACTIVITY_ID.getKey(), activity.getId())
+                           .with(SocialNotificationUtils.COMMENT_ID.getKey(), comment.getId())
+                           .with(SocialNotificationUtils.POSTER.getKey(), Utils.getUserId(comment.getUserId()))
+                           .key(getId());
   }
-
 
   @Override
   public String getId() {
@@ -89,18 +88,16 @@ public class ActivityCommentPlugin extends BaseNotificationPlugin {
   @Override
   public boolean isValid(NotificationContext ctx) {
     ExoSocialActivity comment = ctx.value(SocialNotificationUtils.ACTIVITY);
+    if (!Utils.getActivityManager().isNotificationEnabled(comment)
+        || (isSubComment && comment.getParentCommentId() == null)) {
+      return false;
+    }
     ExoSocialActivity activity = Utils.getActivityManager().getParentActivity(comment);
 
-    if (isSubComment && comment.getParentCommentId() == null) {
-      return false;
-    }
-
-    Identity spaceIdentity = Utils.getIdentityManager().getOrCreateIdentity(SpaceIdentityProvider.NAME, activity.getStreamOwner(), false);
-    //if the space is not null and it's not the default activity of space, then it's valid to make notification 
-    if (spaceIdentity != null && activity.getPosterId().equals(spaceIdentity.getId())) {
-      return false;
-    }
-    return true;
+    Identity spaceIdentity = Utils.getIdentityManager().getOrCreateSpaceIdentity(activity.getStreamOwner());
+    // if the space is not null and it's not the default activity of space, then
+    // it's valid to make notification
+    return spaceIdentity == null || !activity.getPosterId().equals(spaceIdentity.getId());
   }
 
 }
