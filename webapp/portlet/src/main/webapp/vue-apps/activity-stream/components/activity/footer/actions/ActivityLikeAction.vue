e934d6f4dd8148c1e24a02e9237fb7631caf6630
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-08-15 09:51:27 +0100

    fix: Mobile view of activity actions - meed-2275 - Meeds-io/MIPs#54 (#2718)
    This change allows to hide the activity actions label when mobile view.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/footer/actions/ActivityLikeAction.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/footer/actions/ActivityLikeAction.vue
@@ -18,10 +18,10 @@
             <v-icon
               :class="likeColorClass"
               class="baseline-vertical-align"
-              size="14">
+              :size="isMobile && '20' || '14'">
               fa-thumbs-up
             </v-icon>
-            <span class="mx-auto mt-1 mt-lg-0 ms-lg-2">
+            <span v-if="!isMobile" class="mx-auto mt-1 mt-lg-0 ms-lg-2">
               {{ $t('UIActivity.msg.LikeActivity') }}
             </span>
           </div>
@@ -58,6 +58,9 @@ export default {
     },
     likeButtonTitle() {
       return this.hasLiked && this.$t('UIActivity.msg.UnlikeActivity') || this.$t('UIActivity.msg.LikeActivity');
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'sm' || this.$vuetify.breakpoint.name === 'xs';
     },
   },
   created() {
