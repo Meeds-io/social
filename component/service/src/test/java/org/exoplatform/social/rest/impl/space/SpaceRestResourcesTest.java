6411ea8c2cf6269aa9872679cc14ceb162e8c975
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-08-27 12:56:21 +0100

    feat: Allow to choose default spaces when registering a user - MEED-2425 - Meeds-io/MIPs#63
    This change will introduce a UI to allow choosing default spaces that
    will be added to a newly registered user.

--- component/service/src/test/java/org/exoplatform/social/rest/impl/space/SpaceRestResourcesTest.java
+++ component/service/src/test/java/org/exoplatform/social/rest/impl/space/SpaceRestResourcesTest.java
@@ -32,6 +32,7 @@ import org.exoplatform.social.core.manager.ActivityManager;
 import org.exoplatform.social.core.manager.IdentityManager;
 import org.exoplatform.social.core.mock.MockUploadService;
 import org.exoplatform.social.core.service.LinkProvider;
+import org.exoplatform.social.core.space.SpaceUtils;
 import org.exoplatform.social.core.space.impl.DefaultSpaceApplicationHandler;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
@@ -593,6 +594,30 @@ public void testSpaceDisplayNameUpdateWithDifferentCases () throws Exception {
 
     // Get space by its pretty name
     response = service("GET", getURLResource("spaces/byPrettyName/" + space.getPrettyName()), "", null, null);
+    assertNotNull(response);
+    assertEquals(200, response.getStatus());
+
+    spaceEntity = getBaseEntity(response.getEntity(), SpaceEntity.class);
+    assertNotNull(spaceEntity);
+    assertEquals("test space", spaceEntity.getDisplayName());
+    EntityTag eTag = (EntityTag) response.getHttpHeaders().getFirst("ETAG");
+    assertNotNull(eTag);
+  }
+
+  public void testGetSpaceByGroupSuffix() throws Exception {
+    startSessionAs("root");
+    String input = "{\"displayName\":\"test space\",\"visibility\":\"hidden\",\"subscription\":\"open\"}";
+    // root creates a space
+    ContainerResponse response = getResponse("POST", getURLResource("spaces/"), input);
+    assertNotNull(response);
+    assertEquals(200, response.getStatus());
+
+    SpaceEntity spaceEntity = getBaseEntity(response.getEntity(), SpaceEntity.class);
+    Space space = spaceService.getSpaceById(spaceEntity.getId());
+    assertNotNull(space);
+
+    // Get space by its groupId
+    response = service("GET", getURLResource("spaces/byGroupSuffix/" + space.getGroupId().replace(SpaceUtils.SPACE_GROUP + "/", "")), "", null, null);
     assertNotNull(response);
     assertEquals(200, response.getStatus());
 
