754a5b687e05eb4365a4ac619cc459aef0ee71b0
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-08-31 12:24:12 +0100

    feat: Allow to add embedded content in Rich Editor contents - MEED-2439 - Meeds-io/meeds#1082 (#2779)
    Prior to this change, it was possible to embed content from external
    sites only in Activity stream using TemplateParams. This change will
    allow to store the html of embedded content inside the saved content
    itself so this can be genralized in all content that are based on
    'rich-editor' component.

---
+++ webapp/portlet/src/main/webapp/js/sanitize-html-directive.js
@@ -0,0 +1,36 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+(function() {
+  window.Vue.directive('sanitized-html', function (el, binding) {
+    let content = binding.value;
+    content = content.replace(/]]&gt;/g, ']]>');
+    content = content.replace(/&lt;!\[CDATA\[/g, '<![CDATA[');
+    if (content && content.includes('<oembed>') && content.includes('</oembed>')) {
+      content = content.replace(/<oembed>(.*)<\/oembed>/g, '');
+    }
+    if (content && content.includes('<div><![CDATA[') && content.includes(']]></div>')) {
+      try {
+        content = content.replace(/<div><!\[CDATA\[(.*)]]><\/div>/g, window.decodeURIComponent(content.match(/<div><!\[CDATA\[(.*)]]><\/div>/i)[1]));
+      } catch(e) {
+        content = content.replace(/<div><!\[CDATA\[(.*)]]><\/div>/g, content.match(/<div><!\[CDATA\[(.*)]]><\/div>/i)[1]);
+      }
+    }
+    el.innerHTML = content && ExtendedDomPurify.purify(content) || '';
+  });
+})();
