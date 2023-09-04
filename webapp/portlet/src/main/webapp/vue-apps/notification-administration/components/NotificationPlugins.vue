0bf775f7ac7c0348a9a83d470aafa23ffce47e1e
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-04 19:41:39 +0100

    feat: Move Notification UI from Commons - MEED-2445 - Meeds-io/MIPs#79 (#2791)
    This change will move Notification Administration UI from Commons into
    Social, same as the REST API endpoint. The new portlet is defined using
    Vue instead of Juzu.

---
+++ webapp/portlet/src/main/webapp/vue-apps/notification-administration/components/NotificationPlugins.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card flat>
+    <div class="pt-8 subtitle-1">{{ $t('NotificationAdmin.allowedNotificationChannels.title') }}</div>
+    <notification-administration-plugin-group
+      v-for="group in settings.groups"
+      :settings="settings"
+      :key="group.groupId"
+      :group="group"
+      @edit="openDrawer" />
+    <notification-administration-plugin-drawer
+      ref="drawer"
+      :settings="settings" />
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    settings: {
+      type: Object,
+      default: null,
+    },
+  },
+  methods: {
+    openDrawer(plugin, group) {
+      this.$nextTick(() => this.$refs.drawer.open(plugin, group));
+    },
+  },
+};
+</script>
