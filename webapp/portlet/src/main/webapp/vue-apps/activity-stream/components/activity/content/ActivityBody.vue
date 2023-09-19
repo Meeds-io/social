421285094c0d959f9003997225cb8ab7b4e45196
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-19 09:32:50 +0100

    fix: Reset Style of all CKEditor Contents to have WYSIWYG effect after editing - MEED-2534 - Meeds-io/meeds#1102 (#2889)
    This change will define 'reset-style-box' CSS class to add to all
    contents displayed in paged that will use 'v-sanitized-html' directive.
    By adding this class, HTML contents will be reverted to its original CSS
    content to have exactly the same content as in Rich Editor.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/content/ActivityBody.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/content/ActivityBody.vue
@@ -4,7 +4,7 @@
     :child="bodyElement"
     :element="element"
     :class="bodyClass"
-    class="reset-style-box text-break overflow-hidden"
+    class="reset-style-box rich-editor-content text-break overflow-hidden"
     dir="auto" />
 </template>
 
