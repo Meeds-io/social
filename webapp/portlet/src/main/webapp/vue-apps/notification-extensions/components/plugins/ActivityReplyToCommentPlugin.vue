de6a778e8cca6e8fb042a8a1aba166a4a02bde8c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-09-20 17:59:39 +0100

    fix: Fix Rendering Old Grouped Comments - MEED-2533 - Meeds-io/meeds#1101 (#2905)
    Prior to this change, the old grouped comments wasn't displayed names of
    commenters. This change will consider grouping of old comments by
    retrieving poster identities.

--- webapp/portlet/src/main/webapp/vue-apps/notification-extensions/components/plugins/ActivityReplyToCommentPlugin.vue
+++ webapp/portlet/src/main/webapp/vue-apps/notification-extensions/components/plugins/ActivityReplyToCommentPlugin.vue
@@ -1,7 +1,9 @@
 <template>
   <user-notification-activity-base
+    :loading="loading"
     :notification="notification"
-    message-key="Notification.intranet.message.one.ActivityReplyToCommentPlugin"
+    :message-text="message"
+    :from-identity="fromIdentity"
     icon="fa-comment"
     reply />
 </template>
@@ -11,6 +13,55 @@ export default {
     notification: {
       type: Object,
       default: null,
+    },
+  },
+  data: () => ({
+    loading: true,
+    posterIdentities: [],
+  }),
+  computed: {
+    fromIdentity() {
+      return this.notification?.from
+        || (this.posterIdentities?.length && this.posterIdentities[0]);
+    },
+    posterUsernames() {
+      return this.notification?.parameters?.poster
+        && this.notification.parameters.poster.split(',')
+        || [this.notification.parameters.poster];
+    },
+    message() {
+      if (!this.posterIdentities?.length) {
+        return 'Notification.intranet.message.one.ActivityReplyToCommentPlugin';
+      } else if (this.posterUsernames.length < 2) {
+        return this.$t('Notification.intranet.message.one.ActivityReplyToCommentPlugin', {
+          0: `<a class="user-name font-weight-bold">${this.posterIdentities[0].fullname}</a>`,
+        });
+      } else if (this.posterUsernames.length < 3) {
+        return this.$t('Notification.intranet.message.two.ActivityReplyToCommentPlugin', {
+          0: `<a class="user-name font-weight-bold">${this.posterIdentities[0].fullname}</a>`,
+          1: `<a class="user-name font-weight-bold">${this.posterIdentities[1].fullname}</a>`,
+        });
+      } else {
+        return this.$t('Notification.intranet.message.more.ActivityReplyToCommentPlugin', {
+          0: `<a class="user-name font-weight-bold">${this.posterIdentities[0].fullname}</a>`,
+          1: `<a class="user-name font-weight-bold">${this.posterIdentities[1].fullname}</a>`,
+          2: `<strong>${this.posterUsernames.length - 2}</strong>`,
+        });
+      }
+    },
+  },
+  created() {
+    this.loadIdentities().finally(() => this.loading = false);
+  },
+  methods: {
+    loadIdentities() {
+      if (this.posterUsernames?.length && this.posterUsernames?.length > 1) {
+        return Promise.all(this.posterUsernames.slice(0, 2).map(u => this.$identityService.getIdentityByProviderIdAndRemoteId('organization', u)))
+          .then(identities => this.posterIdentities = identities.map(i => i?.profile).filter(p => !!p));
+      } else {
+        this.posterIdentities = this.notification.from && [this.notification.from] || [];
+        return Promise.resolve();
+      }
     },
   },
 };
