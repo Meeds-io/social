2bcbc94cd596819deb2e359e03dbe252b8375000
IlhemEssaadi <93516534+IlhemEssaadi@users.noreply.github.com>
2023-09-22 18:16:50 +0100

    feat: Implement getSiteById rest service - EXO-66263 - Meeds-io/MIPs#72 (#2928)

--- webapp/portlet/src/main/webapp/vue-apps/common/js/SiteService.js
+++ webapp/portlet/src/main/webapp/vue-apps/common/js/SiteService.js
@@ -33,3 +33,23 @@ export function getSites(siteType, excludedSiteType, excludedSiteName, excludeEm
     }
   });
 }
+
+export function getSiteById(siteId, expandNavigations, excludeEmptyNavigationSites, lang) {
+  const formData = new FormData();
+  formData.append('lang', lang);
+  formData.append('excludeEmptyNavigationSites', excludeEmptyNavigationSites);
+  formData.append('expandNavigations', expandNavigations);
+
+  const params = new URLSearchParams(formData).toString();
+
+  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/sites/${siteId}?${params}`, {
+    method: 'GET',
+    credentials: 'include',
+  }).then(resp => {
+    if (resp?.ok) {
+      return resp.json();
+    } else {
+      throw new Error('Error while getting site by id');
+    }
+  });
+}
