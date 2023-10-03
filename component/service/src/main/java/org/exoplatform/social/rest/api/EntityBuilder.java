9888f1270cdf8cbac22fce1ed3dde335b2380a9a
IlhemEssaadi <93516534+IlhemEssaadi@users.noreply.github.com>
2023-10-03 09:38:18 +0100

    feat: Return root navigation node from site entity - EXO-66035 - Meeds-io/MIPs#72 (#2956)

--- component/service/src/main/java/org/exoplatform/social/rest/api/EntityBuilder.java
+++ component/service/src/main/java/org/exoplatform/social/rest/api/EntityBuilder.java
@@ -1875,6 +1875,7 @@ public class EntityBuilder {
                           site.getName(),
                           !StringUtils.isBlank(displayName) ? displayName : site.getName(),
                           StringUtils.isNotBlank(translateSiteDescription) ? translateSiteDescription : site.getDescription(),
+                          new UserNodeRestEntity(rootNode),
                           accessPermissions,
                           editPermission,
                           site.isDisplayed(),
