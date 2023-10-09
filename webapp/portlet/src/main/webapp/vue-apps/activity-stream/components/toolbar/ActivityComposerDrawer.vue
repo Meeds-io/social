fbeaa7a743ff7a0a5ba26bb5a081727767239252
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2023-10-09 17:06:50 +0100

    fix: Fix the edit message from general stream - MEED-2665 -Meeds-io/meeds#1159 (#3023)
    This change will fix the edit message from general stream when the post to network property is set o false.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/toolbar/ActivityComposerDrawer.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/toolbar/ActivityComposerDrawer.vue
@@ -124,6 +124,7 @@
             :object-id="metadataObjectId"
             :max-file-size="$root.maxFileSize"
             :suggester-space-u-r-l="spaceURL"
+            :activity-id="activityId"
             class="activityRichEditor"
             ck-editor-type="activityContent"
             context-name="activityComposer"
@@ -223,7 +224,7 @@ export default {
       return this.drawer && this.$refs.activityContent || null;
     },
     postDisabled() {
-      return (!this.messageLength && !this.activityBodyEdited && !this.activityAttachmentsEdited) || this.messageLength > this.MESSAGE_MAX_LENGTH || this.loading || (!!this.activityId && !this.activityBodyEdited && !this.attachments?.length) || (!this.attachments?.length && !this.messageLength && !this.activityBodyEdited) || (this.postInYourSpacesChoice && !this.audience) || (!this.postToNetwork && !eXo.env.portal.spaceId && !this.audience);
+      return (!this.messageLength && !this.activityBodyEdited && !this.activityAttachmentsEdited) || this.messageLength > this.MESSAGE_MAX_LENGTH || this.loading || (!!this.activityId && !this.activityBodyEdited && !this.attachments?.length) || (!this.attachments?.length && !this.messageLength && !this.activityBodyEdited) || (this.postInYourSpacesChoice && !this.audience) || (!this.postToNetwork && !eXo.env.portal.spaceId && !this.audience && !this.messageEdited);
     },
     metadataObjectId() {
       return this.templateParams?.metadataObjectId || this.activityId;
