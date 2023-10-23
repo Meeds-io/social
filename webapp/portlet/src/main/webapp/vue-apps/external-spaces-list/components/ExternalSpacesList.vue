2b6683a08704d674107f30a5aa41bb0a76f6c112
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-10-23 14:24:52 +0100

    feat: Apply Generic UI to existing portlet (Social) - MEED-2573-Meeds-io/MIPs#81 (#3088)
    This PR allows to add new reusable component to define a unique ui/ux for all the widget the platform

--- webapp/portlet/src/main/webapp/vue-apps/external-spaces-list/components/ExternalSpacesList.vue
+++ webapp/portlet/src/main/webapp/vue-apps/external-spaces-list/components/ExternalSpacesList.vue
@@ -1,40 +1,17 @@
 <template>
-  <div>
-    <v-app v-if="isShown">
-      <v-flex
-        d-flex
-        xs12
-        sm12>
-        <v-layout
-          row
-          wrap
-          mx-0>
-          <v-flex
-            d-flex
-            xs12>
-            <v-card
-              flat
-              class="flex">
-              <v-card-title class="external-spaces-list-title subtitle-1 text-uppercase pb-2">
-                <span class="body-1 text-sub-title">
-                  {{ $t('externalSpacesList.title.yourSpaces') }}
-                </span>
-              </v-card-title>
-              <v-list dense>
-                <template>
-                  <external-space-item
-                    v-for="space in spacesList"
-                    :key="space.id"
-                    :space="space" />
-                  <external-spaces-requests-items @invitationReplied="refreshSpaces" />
-                </template>
-              </v-list>
-            </v-card>
-          </v-flex>
-        </v-layout>
-      </v-flex>
-    </v-app>
-  </div>
+  <v-app v-if="isShown">
+    <widget-wrapper :title="$t('externalSpacesList.title.yourSpaces')">
+      <v-list dense class="py-0">
+        <template>
+          <external-space-item
+            v-for="space in spacesList"
+            :key="space.id"
+            :space="space" />
+          <external-spaces-requests-items @invitationReplied="refreshSpaces" />
+        </template>
+      </v-list>
+    </widget-wrapper>
+  </v-app>
 </template>
 <script>
 import * as externalSpacesListService from '../externalSpacesListService.js';
