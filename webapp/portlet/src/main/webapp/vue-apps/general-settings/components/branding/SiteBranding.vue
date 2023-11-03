9c7e5fcd3641e57928f739a7fa6e9ec8801edcd5
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-11-03 16:24:51 +0100

    feat: Preview of branding changes - MEED-2760 -Meeds-io/MIPs#81 (#3129)
    This PR allows to add a preview section of the home page in the branding app that displays changes made by the user
    
    (cherry picked from commit 77711670f13e6bed4c7d0f699c7de34bd45dd3cf)

--- webapp/portlet/src/main/webapp/vue-apps/general-settings/components/branding/SiteBranding.vue
+++ webapp/portlet/src/main/webapp/vue-apps/general-settings/components/branding/SiteBranding.vue
@@ -27,7 +27,6 @@
     </v-col>
     <v-col
       cols="12"
-      lg="6"
       class="pa-0">
       <h4 class="mb-0 mt-4">
         {{ $t('generalSettings.companyNameTitle') }}
@@ -55,7 +54,6 @@
     </v-col>
     <v-col
       cols="12"
-      lg="6"
       class="pa-0">
       <h4 class="mb-0 mt-4">
         {{ $t('generalSettings.companyLogoTitle') }}
@@ -71,7 +69,6 @@
     </v-col>
     <v-col
       cols="12"
-      lg="6"
       class="pa-0">
       <h4 class="mb-0 mt-4">
         {{ $t('generalSettings.themeColorsTitle') }}
@@ -99,7 +96,6 @@
     </v-col>
     <v-col
       cols="12"
-      lg="6"
       class="pa-0">
       <h4 class="mb-0 mt-4">
         {{ $t('generalSettings.companyFaviconTitle') }}
@@ -112,9 +108,8 @@
         v-model="faviconUploadId"
         :branding="branding" />
     </v-col>
-    <v-col
+    <v-col 
       cols="12"
-      lg="6"
       class="pa-0">
       <h4 class="mb-0 mt-4">
         {{ $t('generalSettings.widgetAndAppStyle.title') }}
@@ -130,7 +125,7 @@
     <v-col
       cols="12"
       class="pa-0">
-      <div class="d-flex my-12 justify-end">
+      <div :class="!isMobile && 'position-absolute b-0 r-0' || ''" class="d-flex justify-end pb-5">
         <v-btn
           :aria-label="$t('generalSettings.cancel')"
           :disabled="loading"
@@ -191,6 +186,9 @@ export default {
     defaultBorderRadius() {
       return this.branding?.themeStyle?.borderRadius && Number(this.branding.themeStyle.borderRadius.split('px')[0]);
     },
+    isMobile() {
+      return this.$vuetify.breakpoint.name === 'sm' || this.$vuetify.breakpoint.name === 'xs' || this.$vuetify.breakpoint.name === 'md';
+    },
     validForm() {
       return this.changed && this.isValidForm;
     },
@@ -230,6 +228,41 @@ export default {
     changed() {
       this.$emit('changed', this.changed);
     },
+    primaryColor() {
+      this.$root.$emit('refresh-style-property', {
+        detail: {
+          propertyName: '--allPagesPrimaryColor',
+          propertyValue: this.primaryColor
+        }
+      });
+    },
+    secondaryColor() {
+      this.$root.$emit('refresh-style-property', {
+        detail: {
+          propertyName: '--allPagesSecondaryColor',
+          propertyValue: this.secondaryColor
+        }
+      });
+    },
+    tertiaryColor() {
+      this.$root.$emit('refresh-style-property', {
+        detail: {
+          propertyName: '--allPagesTertiaryColor',
+          propertyValue: this.tertiaryColor
+        }
+      });
+    },
+    borderRadius() {
+      this.$root.$emit('refresh-style-property', {
+        detail: {
+          propertyName: '--allPagesBorderRadius',
+          propertyValue: `${this.borderRadius}px`
+        }
+      });
+    },
+    companyName() {
+      this.$root.$emit('refresh-company-name', this.companyName);
+    }
   },
   mounted() {
     this.init();
@@ -272,7 +305,10 @@ export default {
         .then(() => this.$emit('saved'))
         .then(() => this.$root.$emit('alert-message', this.$t('generalSettings.savedSuccessfully'), 'success'))
         .catch(e => this.errorMessage = String(e))
-        .finally(() => this.$root.loading = false);
+        .finally(() => {
+          this.$root.$emit('refresh-iframe');
+          this.$root.loading = false;
+        });
     },
   }
 };
---
+++ webapp/portlet/src/main/webapp/vue-apps/general-settings/components/branding/preview/SiteBrandingPreview.vue
@@ -0,0 +1,85 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div class="d-none d-lg-block">
+    <div>
+      <h4 class="font-weight-bold mt-0">
+        {{ $t('generalSettings.preview.title') }}
+      </h4>
+    </div>
+    <div class="mt-4 position-relative border-color full-width overflow-hidden border-radius" style="height:450px;">
+      <iframe 
+        id="previewIframe" 
+        :title="$t('generalSettings.preview.title')"
+        :src="pageHomeLink" 
+        width="250%"
+        height="250%"
+        style="transform: scale(0.4); transform-origin: 0 0;"
+        class="no-border">
+      </iframe>
+      <div class="position-absolute full-width full-height t-0"></div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data: ()=> ({
+    pageHomeLink: `${eXo.env.portal.context}/${eXo.env.portal.defaultPortal}/stream?sticky=true`,
+  }),
+  mounted() {
+    this.$root.$on('refresh-style-property', this.setStyleProperty);
+    this.$root.$on('refresh-iframe', this.reloadURL);
+    this.$root.$on('refresh-company-name', this.refreshCompanyName);
+    this.$root.$on('refresh-company-logo', this.refreshCompanyLogo);
+  },
+  beforeDestroy() {
+    this.$root.$off('refresh-style-property', this.setStyleProperty);
+    this.$root.$off('refresh-iframe', this.reloadURL);
+    this.$root.$off('refresh-company-name', this.refreshCompanyName);
+    this.$root.$off('refresh-company-logo', this.refreshCompanyLogo);
+  },
+  methods: {
+    reloadURL() {
+      document.getElementById('previewIframe').src = this.pageHomeLink;
+    },
+    setStyleProperty(event) {
+      if (event?.detail && event.detail?.propertyName && event.detail?.propertyValue) {
+        const propertyName = event.detail.propertyName;
+        const propertyValue = event.detail.propertyValue;
+        const iframeDoc = document.getElementById('previewIframe').contentWindow.document.documentElement;
+        iframeDoc.style.setProperty(propertyName, propertyValue);
+      }
+    },
+    refreshCompanyName(event) {
+      if (event) {
+        const companyNameElement =  document.getElementById('previewIframe').contentWindow.document.getElementsByClassName('logoTitle')[0];
+        companyNameElement.innerHTML = event;
+      }
+    },
+    refreshCompanyLogo(event) {
+      if (event) {
+        const companyNameElement =  document.getElementById('previewIframe').contentWindow.document.getElementById('UserHomePortalLink').getElementsByTagName('img')[0];
+        companyNameElement.src = event;
+      }
+    }
+  }
+};
+</script>
