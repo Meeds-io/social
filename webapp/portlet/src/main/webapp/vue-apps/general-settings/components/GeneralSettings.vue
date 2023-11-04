c5f236d62d9b28a4285abcb7ca651b9197a936db
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2023-11-04 07:00:24 +0100

    fix: Add workaround for Viewport definition in Mobile - MEED-2819 - Meeds-io/meeds#1232 (#3135)
    This change will allow to define a custom value for viewport definition
    to workaround the problem of viewport real size in Mobile Devices.

--- webapp/portlet/src/main/webapp/vue-apps/general-settings/components/GeneralSettings.vue
+++ webapp/portlet/src/main/webapp/vue-apps/general-settings/components/GeneralSettings.vue
@@ -22,7 +22,7 @@
   <v-app>
     <v-main>
       <v-card
-        min-height="calc(100vh - 160px)"
+        min-height="calc(var(--100vh, 100vh) - 160px)"
         class="px-6 card-border-radius overflow-hidden"
         flat>
         <v-list-item dense class="px-0 mb-4">
