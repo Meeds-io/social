800ec1953c52ccebe4d8e8d951529a90274d689c
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-11-14 15:00:54 +0100

    feat: Margins cleanup - Remove extra-margins - MEED-2895-Meeds-io/MIPs#103 (#3180)
    All Vue applications parents no longer have margins due to these changes.
    The margin will be added as a CSS variable inside the less variables, so we can change this variable using the branding UI.
    
    (cherry picked from commit e140c795347bbe5d8c2c32cfbc53de032f5db848)

--- webapp/portlet/src/main/webapp/vue-apps/user-setting-security/components/UserSettingSecurity.vue
+++ webapp/portlet/src/main/webapp/vue-apps/user-setting-security/components/UserSettingSecurity.vue
@@ -6,7 +6,7 @@
         @back="closeSecurityDetail" />
       <v-card
         v-else
-        class="my-3 card-border-radius"
+        class="card-border-radius"
         flat>
         <v-list>
           <v-list-item>
