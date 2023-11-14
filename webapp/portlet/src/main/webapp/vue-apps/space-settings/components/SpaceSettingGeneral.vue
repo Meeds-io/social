6c6923e0060fb08211dd960635c75cdf2db5d0f6
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-11-14 13:18:41 +0100

    feat: Label style not consistent in space settings - MEED-2858-Meeds-io/MIPs#81 (#3151)

--- webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettingGeneral.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-settings/components/SpaceSettingGeneral.vue
@@ -1,18 +1,27 @@
 <template>
   <div v-if="displayed">
-    <widget-wrapper :title="$t('SpaceSettings.general')">
-      <template #action>
-        <v-btn
-          small
-          icon
-          @click="openDrawer">
-          <v-icon size="18" class="icon-default-color">fa-edit</v-icon>
-        </v-btn>
-      </template>
-    </widget-wrapper>
-    <space-setting-form-drawer
-      ref="spaceFormDrawer"
-      :max-upload-size="maxUploadSize" />
+    <v-card class="card-border-radius" flat>
+      <v-list two-line class="transparent">
+        <v-list-item>
+          <v-list-item-content>
+            <v-list-item-title class="title text-color">
+              {{ $t('SpaceSettings.general') }}
+            </v-list-item-title>
+          </v-list-item-content>
+          <v-list-item-action>
+            <v-btn
+              small
+              icon
+              @click="openDrawer">
+              <v-icon size="18" class="icon-default-color">fa-edit</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </v-list>
+      <space-setting-form-drawer
+        ref="spaceFormDrawer"
+        :max-upload-size="maxUploadSize" />
+    </v-card>
   </div>
 </template>
 
