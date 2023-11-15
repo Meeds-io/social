a84ad879b286b0840c5bdb83ef2bafd7954707e1
Romain Dénarié <romain.denarie@exoplatform.com>
2023-11-15 17:03:22 +0100

    Revert "fix: Correctly display the space after space selection in composer - EXO-67423 - meeds-io/meeds#1273"
    This reverts commit a2c1ae4ff604970dbf0ff6b5e41b4dd425d2c70c.

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/toolbar/ActivityComposerDrawer.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/toolbar/ActivityComposerDrawer.vue
@@ -224,13 +224,7 @@ export default {
       return this.drawer && this.$refs.activityContent || null;
     },
     postDisabled() {
-      return (!this.messageLength && !this.activityBodyEdited && !this.activityAttachmentsEdited)
-        || this.messageLength > this.MESSAGE_MAX_LENGTH
-        || this.loading
-        || (!!this.activityId && !this.activityBodyEdited && !this.attachments?.length)
-        || (!this.attachments?.length && !this.messageLength && !this.activityBodyEdited)
-        || (this.postInYourSpacesChoice && !this.audience)
-        || (!this.postToNetwork && !eXo.env.portal.spaceId && !this.audience && !this.messageEdited);
+      return (!this.messageLength && !this.activityBodyEdited && !this.activityAttachmentsEdited) || this.messageLength > this.MESSAGE_MAX_LENGTH || this.loading || (!!this.activityId && !this.activityBodyEdited && !this.attachments?.length) || (!this.attachments?.length && !this.messageLength && !this.activityBodyEdited) || (this.postInYourSpacesChoice && !this.audience) || (!this.postToNetwork && !eXo.env.portal.spaceId && !this.audience && !this.messageEdited);
     },
     metadataObjectId() {
       return this.templateParams?.metadataObjectId || this.activityId;
@@ -297,9 +291,8 @@ export default {
     document.addEventListener('activity-composer-drawer-open', this.open);
     document.addEventListener('activity-composer-edited', this.isActivityBodyEdited);
     document.addEventListener('activity-composer-closed', this.close);
-    if (!eXo.env.portal.spaceId) {
-      this.resetAudienceChoice();
-    }
+
+    this.resetAudienceChoice();
   },
   methods: {
     isActivityBodyEdited(event) {
