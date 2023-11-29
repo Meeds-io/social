2e614c5464be69b7b684a77ef9649709c7dde9e6
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-11-29 15:36:27 +0100

    feat: Add options to balloon toolbar- MEED-2983 - Meeds-io/MIPs#107 (#3250)
    This PR allows to add the unlink option only when the selected text is a link
    
    (cherry picked from commit 7699d758c4829dda2eb17ab6f3a408e5995b6154)

--- webapp/portlet/src/main/webapp/js/ckeditorPlugins/linkBalloon/plugin.js
+++ webapp/portlet/src/main/webapp/js/ckeditorPlugins/linkBalloon/plugin.js
@@ -6,16 +6,6 @@ CKEDITOR.plugins.add( 'linkBalloon', {
     editor.on('instanceReady', function( ) {
       const balloonToolbar = new CKEDITOR.ui.balloonToolbar(editor);
       balloonToolbar.addItems({
-        link: new CKEDITOR.ui.button({
-          command: 'link',
-          toolbar: 'links,10',
-          label: editor.lang.linkBalloon.link
-        }),
-        unlink: new CKEDITOR.ui.button({
-          command: 'unlink',
-          toolbar: 'links,20',
-          label: editor.lang.linkBalloon.unlink
-        }),
         bold: new CKEDITOR.ui.button({
           command: 'bold',
           toolbar: 'basicstyles,1',
@@ -26,10 +16,25 @@ CKEDITOR.plugins.add( 'linkBalloon', {
           toolbar: 'basicstyles,2',
           label: editor.lang.linkBalloon.italic
         }),
+        link: new CKEDITOR.ui.button({
+          command: 'link',
+          toolbar: 'links,10',
+          label: editor.lang.linkBalloon.link
+        }),
       });
       // Attach the balloon toolbar on selected text
-      editor.document.on( 'mouseup', function() {
+      editor.document.on( 'mouseup', function(evt) {
         const selectedElem = editor.getSelection();
+        if (!balloonToolbar.getItem('unlink')) {
+          if (isLink(evt)) {
+            addUnlink(balloonToolbar);
+          } 
+        } else {
+          if (!isLink(evt)) {
+            balloonToolbar.deleteItem('unlink');
+          } 
+        }
+        
         if ( selectedElem && selectedElem.getSelectedText().length > 0) {
           balloonToolbar.attach( selectedElem );
         } else {
@@ -38,10 +43,17 @@ CKEDITOR.plugins.add( 'linkBalloon', {
       });
       // Attach the balloon toolbar when the clicked text is a link
       editor.document.on( 'click', function( evt ) {
-        if (evt.data.$.target.nodeName.toLowerCase() === 'a') {
+        if (isLink(evt)) {
+          addUnlink(balloonToolbar);
           balloonToolbar.attach( editor.getSelection() );
         }
       });    
     });
-  } 
+    function isLink(e) {
+      return e.data.$.target.nodeName.toLowerCase() === 'a';
+    }
+    function addUnlink(toolbar) {
+      toolbar.addItem('unlink', new CKEDITOR.ui.button({command: 'unlink',toolbar: 'links,20',label: editor.lang.linkBalloon.unlink}));
+    }
+  },
 } );
