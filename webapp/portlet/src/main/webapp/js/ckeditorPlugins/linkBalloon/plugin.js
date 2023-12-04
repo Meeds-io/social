dc099cefb0f056a5d224b8c5c189147e0605a66f
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2023-12-04 10:30:31 +0100

    feat: Add Ckeditor balloon toolbar - MEED-2982- Meeds-io/MIPs#107 (#3263)

--- webapp/portlet/src/main/webapp/js/ckeditorPlugins/linkBalloon/plugin.js
+++ webapp/portlet/src/main/webapp/js/ckeditorPlugins/linkBalloon/plugin.js
@@ -24,36 +24,51 @@ CKEDITOR.plugins.add( 'linkBalloon', {
       });
       // Attach the balloon toolbar on selected text
       editor.document.on( 'mouseup', function(evt) {
-        const selectedElem = editor.getSelection();
-        if (!balloonToolbar.getItem('unlink')) {
-          if (isLink(evt)) {
-            addUnlink(balloonToolbar);
-          } 
-        } else {
-          if (!isLink(evt)) {
-            balloonToolbar.deleteItem('unlink');
-          } 
-        }
-        
-        if ( selectedElem && selectedElem.getSelectedText().length > 0) {
-          balloonToolbar.attach( selectedElem );
-        } else {
-          balloonToolbar.hide();
-        }
+        setupMouseObserver(evt,balloonToolbar);
       });
       // Attach the balloon toolbar when the clicked text is a link
       editor.document.on( 'click', function( evt ) {
-        if (isLink(evt)) {
-          addUnlink(balloonToolbar);
-          balloonToolbar.attach( editor.getSelection() );
-        }
-      });    
+        setupClickObserver(evt, balloonToolbar);
+      });
+
+      editor.on('contentDom', function () {
+        const editable = editor.editable();
+        editable.attachListener(editable, 'mouseup', function (evt) {
+          setupMouseObserver(evt,balloonToolbar);
+        });
+        editable.attachListener(editable, 'click', function (evt) {
+          setupClickObserver(evt, balloonToolbar);
+        });
+      });  
     });
     function isLink(e) {
       return e.data.$.target.nodeName.toLowerCase() === 'a';
     }
     function addUnlink(toolbar) {
       toolbar.addItem('unlink', new CKEDITOR.ui.button({command: 'unlink',toolbar: 'links,20',label: editor.lang.linkBalloon.unlink}));
+    }
+    function setupMouseObserver(evt, toolbar) {
+      const selectedElem = editor.getSelection();
+      if (!toolbar.getItem('unlink')) {
+        if (isLink(evt)) {
+          addUnlink(toolbar);
+        } 
+      } else {
+        if (!isLink(evt)) {
+          toolbar.deleteItem('unlink');
+        } 
+      }
+      if ( selectedElem && selectedElem.getSelectedText().length > 0) {
+        toolbar.attach( selectedElem );
+      } else {
+        toolbar.hide();
+      }
+    }
+    function setupClickObserver(evt, toolbar) {
+      if (isLink(evt)) {
+        addUnlink(toolbar);
+        toolbar.attach( editor.getSelection() );
+      }
     }
   },
 } );
