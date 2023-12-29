a674fadda200066097899f022182dcb435055d77
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2023-12-29 15:12:10 +0100

    fix: Suggest isn't displaying users - EXO-68574 - Meeds-io/meeds#1503 . (#3368)
    Before this change, when create user1 & user2, add user & user2 in
    spaceX, on spaceX, user1 Sends kudos and on suggestor types user2 name,
    suggestor isn't displaying any results. To resolve this problem, added
    the default profile.fullName value to the itemText props. After this
    change, suggestor should display results related to typed string.
    
    (cherry picked from commit a5c9041971011e28f620c26385253f5b434c9c34)

--- webapp/portlet/src/main/webapp/vue-apps/common/components/ExoIdentitySuggester.vue
+++ webapp/portlet/src/main/webapp/vue-apps/common/components/ExoIdentitySuggester.vue
@@ -67,8 +67,8 @@
           </v-avatar>
           <span class="text-truncate">
             {{ item.profile.external
-              ? (itemText ? item[itemText] : item.profile.fullName).concat(' (').concat($t('userAvatar.external.label')).concat(')')
-              : (itemText) ? item[itemText] : item.profile.fullName
+              ? (itemText !== 'profile.fullName' ? item[itemText] : item.profile.fullName).concat(' (').concat($t('userAvatar.external.label')).concat(')')
+              : itemText !== 'profile.fullName' ? item[itemText] : item.profile.fullName
             }}          
           </span>
         </v-chip>
