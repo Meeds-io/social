e59f39ea661b2416c31f62dec45c1dea54ffb527
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-01-16 20:48:37 +0100

    feat: UI improvement of the people list in mobile - EXO-68642 - Meeds-io/MIPs#110 (#3402)
    UI improvement of the people list in mobile

--- webapp/portlet/src/main/webapp/vue-apps/people-list/initComponents.js
+++ webapp/portlet/src/main/webapp/vue-apps/people-list/initComponents.js
@@ -7,6 +7,7 @@ import PeopleCardReverse from './components/PeopleCardReverse.vue';
 import PeopleAdvancedFilterDrawer from './components/PeopleAdvancedFilterDrawer.vue';
 import AdvancedFilterInputItem from './components/AdvancedFilterInputItem.vue';
 import PeopleConnectionOptionItem from './components/PeopleConnectionOptionItem.vue';
+import PeopleCardFrontCompact from './components/PeopleCardFrontCompact.vue';
 
 const components = {
   'people-list': PeopleList,
@@ -17,7 +18,8 @@ const components = {
   'people-card-reverse': PeopleCardReverse,
   'people-advanced-filter-drawer': PeopleAdvancedFilterDrawer,
   'people-advanced-filter-input-item': AdvancedFilterInputItem,
-  'people-connection-option-item': PeopleConnectionOptionItem
+  'people-connection-option-item': PeopleConnectionOptionItem,
+  'people-card-front-compact': PeopleCardFrontCompact,
 };
 
 for (const key in components) {
