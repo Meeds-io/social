9055e0e54dfcae8ae2398125b09ef6866b504991
saraBoutej <sboutej@exoplatform.com>
2024-02-21 16:56:56 +0100

    feat: Review Invitation gadget UI when tablet - MEED-3376 - Meeds-io/MIPs#113

--- webapp/portlet/src/main/webapp/vue-apps/people-overview/components/PeopleOverview.vue
+++ webapp/portlet/src/main/webapp/vue-apps/people-overview/components/PeopleOverview.vue
@@ -1,20 +1,30 @@
 <template>
   <v-app>
-    <widget-wrapper>
-      <v-card class="border-box-sizing d-flex flex-row justify-center ma-0" flat>
-        <people-overview-card
-          id="peopleInvitationsOverview"
-          :title="$t('peopleOverview.label.invitations')"
-          :count="invitations"
-          :class="invitations === '-' && 'text-sub-title'"
-          @click="$refs.peopleDrawer.open('invitations', $t('peopleOverview.label.invitations'))" />
-        <v-divider class="peopleOverviewVertivalSeparator ma-auto" vertical />
-        <people-overview-card
-          id="peoplePendingOverview"
-          :title="$t('peopleOverview.label.pending')"
-          :count="pending"
-          :class="pending === '-' && 'text-sub-title'"
-          @click="$refs.peopleDrawer.open('pending', $t('peopleOverview.label.pending'))" />
+    <widget-wrapper :title="$t('peopleOverview.label.title')">
+      <v-card flat>
+        <div class="d-flex flex-row justify-space-around" v-if="invitations > 0 || pending > 0">
+          <people-overview-card
+              id="peopleInvitationsOverview"
+              :title="$t('peopleOverview.label.invitations')"
+              :count="invitations"
+              icon="fas fa-user-plus"
+              :class="invitations === '-' && 'text-sub-title'"
+              @click="$refs.peopleDrawer.open('invitations', $t('peopleOverview.label.invitations'))" />
+          <people-overview-card
+              id="peoplePendingOverview"
+              :title="$t('peopleOverview.label.pending')"
+              :count="pending"
+              icon="fas fa-user-clock"
+              :class="pending === '-' && 'text-sub-title'"
+              @click="$refs.peopleDrawer.open('pending', $t('peopleOverview.label.pending'))" />
+        </div>
+        <div v-else class="d-flex align-center justify-center">
+          <v-icon size="24" class="tertiary--text me-2">fas fa-user-plus</v-icon>
+          <div class="d-flex flex-column">
+            <span class="subtitle-1 text-color text-left">{{ $t('peopleOverview.label.network') }}</span>
+            <span class="subtitle-1 text-color text-left">{{ $t('peopleOverview.label.connect') }}</span>
+          </div>
+        </div>
       </v-card>
     </widget-wrapper>
     <people-overview-drawer ref="peopleDrawer" @refresh="refresh()" />
@@ -26,7 +36,7 @@ export default {
   data: () => ({
     invitations: '-',
     pending: '-',
-    initialized: false,
+    initialized: false
   }),
   watch: {
     initialized(newVal, oldVal) {
