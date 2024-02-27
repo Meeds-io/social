350267b06c138ef92d5b31e9fc4b327762db521e
Ali Hamdi <ahamdi@exoplatform.com>
2024-02-27 21:41:59 +0100

    feat: update mobile breakpoint - Meeds-io/MIPs#110 - EXO-69902

--- webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleCard.vue
+++ webapp/portlet/src/main/webapp/vue-apps/people-list/components/PeopleCard.vue
@@ -69,7 +69,7 @@ export default {
   },
   computed: {
     isMobile() {
-      return this.$vuetify.breakpoint.name === 'xs';
+      return this.$vuetify.breakpoint.smAndDown;
     },
     userAvatarUrl() {
       let userAvatarUrl;
