cc4ebe13f2ee3e0f78d464e21d25b762bb8a1770
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-03-13 14:05:40 +0100

    fix: quicksearch result is not correct - EXO-70143 - Meeds-io/MIPs#110 (#3610)
    prior to this change when using the advanced filter api in quicksearch
    the returned result are not exact because of how the api was designed.
    This PR adjusts the filter part that based on profile settings to fit
    only the exact request setting keyword
    
    (cherry picked from commit fef87b581890c5048b214dbb8860726b0f9e4bd9)

--- component/core/src/main/java/org/exoplatform/social/core/jpa/search/ProfileSearchConnector.java
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/search/ProfileSearchConnector.java
@@ -175,9 +175,7 @@ public class ProfileSearchConnector {
 
         esSubQuery.append("    \"filter\": [\n");
         esSubQuery.append("      {");
-        esSubQuery.append("          \"query_string\": {\n");
-        esSubQuery.append("            \"query\": \"" + expEsForAdvancedFilter + "\"\n");
-        esSubQuery.append("          }\n");
+        esSubQuery.append(expEsForAdvancedFilter);
         esSubQuery.append("      }\n");
         esSubQuery.append("    ],\n");
 
@@ -489,28 +487,26 @@ public class ProfileSearchConnector {
   }
 
   private String buildAdvancedFilterExpression(ProfileFilter filter) {
-    StringBuilder esExp = new StringBuilder();
+    StringBuilder query = new StringBuilder();
     Map<String, String> settings = filter.getProfileSettings();
-    esExp.append("( ");
-    int settingsCount = 0 ;
-    for (Map.Entry<String, String> entry : settings.entrySet()){
-      String inputKey = entry.getKey().replace(" ", "\\\\ ");
-      String inputValue = entry.getValue().replace(StorageUtils.ASTERISK_STR, StorageUtils.EMPTY_STR);
-      if (inputValue.startsWith("\"") && inputValue.endsWith("\"")) {
-        inputValue = inputValue.replace("\"", "");
-      }
-      esExp.append("( ")
-           .append(inputKey)
-           .append(":")
-           .append(StorageUtils.ASTERISK_STR)
-           .append(removeAccents(inputValue))
-           .append(StorageUtils.ASTERISK_STR);
-      esExp.append(")");
-      if ( settingsCount != settings.size()- 1 ) esExp.append(" AND ") ;
-      settingsCount += 1 ;
-    }
-    esExp.append(" )");
-    return esExp.toString();
+    query.append("""
+            "bool": {
+              "must": [""");
+    settings.forEach((key, value) -> query.append("""
+                    {
+                    "term": {
+                       "%s.raw": "%s"
+                     }
+                   },
+                """.formatted(key, value)));
+    int lastComma = query.lastIndexOf(",");
+    query.replace(lastComma, lastComma + 1, "");
+    query.append("""
+               ]
+            }
+            """);
+
+    return query.toString();
   }
 
   private static String removeAccents(String string) {
