f8cbe495f7b62928ee344e3051175e6833b6c47f
Helmi Akermi <hakermi@exoplatform.com>
2024-04-04 12:02:38 +0100

    feat: Add download chart option - EXO-69790 - Meeds-io/MIPs#112
    Add download chart option

--- webapp/portlet/src/main/webapp/vue-apps/organizational-chart/components/view/OrganizationalChartHeader.vue
+++ webapp/portlet/src/main/webapp/vue-apps/organizational-chart/components/view/OrganizationalChartHeader.vue
@@ -45,19 +45,33 @@
       {{ configuredTitle }}
     </p>
     <div
-      v-if="isAdmin"
       class="ms-auto d-flex">
-      <v-btn
-        v-if="hover || isMobile"
-        class="ms-auto my-0 icon-default-color"
-        small
-        icon
-        @click="openChartSettingsDrawer">
-        <v-icon
-          size="20">
-          fas fa-cog
-        </v-icon>
-      </v-btn>
+      <div>
+        <v-btn
+          class="ms-auto my-0 icon-default-color"
+          :loading="isPrintingPdf"
+          small
+          icon
+          @click="downloadChart">
+          <v-icon
+            size="20">
+            fas fa-download
+          </v-icon>
+        </v-btn>
+      </div>
+      <div v-if="isAdmin">
+        <v-btn
+          v-if="hover || isMobile"
+          class="ms-auto my-0 icon-default-color"
+          small
+          icon
+          @click="openChartSettingsDrawer">
+          <v-icon
+            size="20">
+            fas fa-cog
+          </v-icon>
+        </v-btn>
+      </div>
     </div>
   </v-sheet>
 </template>
@@ -88,11 +102,18 @@ export default {
     hasHeaderTitle: {
       type: Boolean,
       default: false
+    },
+    isPrintingPdf: {
+      type: Boolean,
+      default: false
     }
   },
   methods: {
     openChartSettingsDrawer() {
       this.$emit('open-chart-settings');
+    },
+    downloadChart() {
+      this.$emit('download-chart');
     }
   }
 };
