51748be75b50355ddcf2fb6bb92f5e86d286e040
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2024-04-17 20:13:09 +0100

    feat: re-use application toolbar in space members - EXO-70257 - Meeds-io/MIPs#127 (#3696)
    re-use application toolbar in space members

--- webapp/portlet/src/main/webapp/vue-apps/space-members/components/SpaceMembersToolbar.vue
+++ webapp/portlet/src/main/webapp/vue-apps/space-members/components/SpaceMembersToolbar.vue
@@ -1,83 +1,40 @@
 <template>
-  <v-toolbar id="peopleListToolbar" flat>
-    <v-toolbar-title v-if="isManager">
-      <v-btn
-        class="btn pe-2 ps-0 me-4 inviteUserToSpaceButton"
-        @click="$emit('invite-users')">
-        <i class="uiIconInviteUser ms-2 me-1"></i>
-        <span class="d-none d-sm-inline">
-          {{ $t('peopleList.button.inviteUsers') }}
-        </span>
-      </v-btn>
-    </v-toolbar-title>
-    <div
-      class="showingPeopleText text-sub-title d-none d-sm-flex">
-      {{ $t('peopleList.label.peopleCount', {0: peopleCount}) }}
-    </div>
-    <v-spacer class="d-none d-sm-flex" />
-    <v-scale-transition>
-      <v-text-field
-        v-model="keyword"
-        :placeholder="$t('peopleList.label.filterPeople')"
-        prepend-inner-icon="fa-filter"
-        class="inputPeopleFilter pa-0 mt-0 mb-n2 me-3 my-auto" />
-    </v-scale-transition>
-    <v-scale-transition>
-      <select
-        v-model="filter"
-        class="selectPeopleFilter my-auto me-2 subtitle-1 ignore-vuetify-classes d-none d-sm-inline">
-        <option
-          v-for="peopleFilter in peopleFilters"
-          :key="peopleFilter.value"
-          :value="peopleFilter.value">
-          {{ peopleFilter.text }}
-        </option>
-      </select>
-    </v-scale-transition>
-    <v-icon
-      class="d-sm-none"
-      @click="openBottomMenu">
-      fa-filter
-    </v-icon>
-    <v-bottom-sheet v-model="bottomMenu" class="pa-0">
-      <v-sheet :height="bottomNavigationHeight" class="text-center">
-        <v-toolbar
-          color="primary"
-          dark
-          class="border-box-sizing">
-          <v-btn text @click="bottomMenu = false">
-            {{ $t('peopleList.label.cancel') }}
-          </v-btn>
-          <v-spacer />
-          <v-toolbar-title>
-            <v-icon>fa-filter</v-icon>
-            {{ $t('peopleList.label.filter') }}
-          </v-toolbar-title>
-          <v-spacer />
-          <v-btn text @click="changeFilterSelection">
-            {{ $t('peopleList.label.confirm') }}
-          </v-btn>
-        </v-toolbar>
-        <v-list>
-          <v-list-item
-            v-for="peopleFilter in peopleFilters"
-            :key="peopleFilter"
-            @click="filterToChange = peopleFilter.value">
-            <v-list-item-title class="align-center d-flex">
-              <v-icon v-if="filterToChange === peopleFilter.value">fa-check</v-icon>
-              <span v-else class="me-6"></span>
-              <v-spacer />
-              <div>
-                {{ peopleFilter.text }}
-              </div>
-              <v-spacer />
-              <span class="me-6"></span>
-            </v-list-item-title>
-          </v-list-item>
-        </v-list>
-      </v-sheet>
-    </v-bottom-sheet>
-  </v-toolbar>
+  <application-toolbar
+    id="peopleListToolbar"
+    :right-text-filter="{
+      minCharacters: 3,
+      placeholder: $t('peopleList.label.filterPeople'),
+      tooltip: $t('peopleList.label.filterPeople')
+    }"
+    :right-select-box="{
+      selected: filter,
+      items: peopleFilters,
+    }"
+    @filter-text-input-end-typing="keyword = $event"
+    @filter-select-change="filterValue = $event"
+    @toggle-select="updateFilter($event)">
+    <template
+      #left>
+      <div class="d-flex">
+        <v-btn
+          v-if="isManager"
+          class="btn pe-2 ps-0 me-4 inviteUserToSpaceButton"
+          @click="$emit('invite-users')">
+          <v-icon
+            size="18"
+            class="primary--text ms-2 me-1">
+            fas fa-user-plus
+          </v-icon>
+          <span class="d-none d-sm-inline">
+            {{ $t('peopleList.button.inviteUsers') }}
+          </span>
+        </v-btn>
+        <div class="showingPeopleText text-sub-title d-none my-auto d-sm-flex">
+          {{ $t('peopleList.label.peopleCount', {0: peopleCount}) }}
+        </div>
+      </div>
+    </template>
+  </application-toolbar>
 </template>
 
 <script>
@@ -108,10 +65,11 @@ export default {
     endTypingKeywordTimeout: 50,
     startTypingKeywordTimeout: 0,
     typing: false,
+    filterValue: null
   }),
   computed: {
-    bottomNavigationHeight() {
-      return this.isManager && '255px' || '169px';
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
     },
     peopleFilters() {
       if (this.isManager) {
@@ -160,17 +118,18 @@ export default {
       }
     },
     filter() {
-      this.$emit('filter-changed', this.filter);
+      this.filterValue = this.filter;
     },
+    filterValue() {
+      this.updateFilter();
+    }
+  },
+  created() {
+    this.filterValue = this.filter;
   },
   methods: {
-    openBottomMenu() {
-      this.filterToChange = this.filter;
-      this.bottomMenu = true;
-    },
-    changeFilterSelection() {
-      this.bottomMenu = false;
-      this.filter = this.filterToChange;
+    updateFilter() {
+      this.$emit('filter-changed', this.filterValue);
     },
     waitForEndTyping() {
       window.setTimeout(() => {
