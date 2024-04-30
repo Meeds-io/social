00449327aa88175e762265785611bbc223163196
Sara Boutej <40363501+SaraBoutej@users.noreply.github.com>
2024-04-30 10:09:34 +0100

    feat: Comment display when access to one activity - MEED-5945 - Meeds-io/MIPs#124 (#3737)

--- webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/ActivityStreamActivity.vue
+++ webapp/portlet/src/main/webapp/vue-apps/activity-stream/components/activity/ActivityStreamActivity.vue
@@ -207,7 +207,7 @@ export default {
       return this.activity?.activityStream?.space?.id || '';
     },
     activityCommented() {
-      return this.hasNewComment || !this.isActivityDetail;
+      return this.hasNewComment || this.isActivityDetail;
     }
   },
   watch: {
