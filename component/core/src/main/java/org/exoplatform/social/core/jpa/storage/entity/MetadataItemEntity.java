c6ec99894d84880a896c6e34c053b06ccba448dd
sofyenne <shajchedli@exoplatform.com>
2024-05-27 15:32:35 +0100

    feat : Add 'updatedDate' filed to the metadataItem entity and enable sorting by this field - EXO-71874 - Meeds-io/MIPs#130

--- component/core/src/main/java/org/exoplatform/social/core/jpa/storage/entity/MetadataItemEntity.java
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/storage/entity/MetadataItemEntity.java
@@ -226,6 +226,9 @@ public class MetadataItemEntity implements Serializable {
   @Column(name = "CREATED_DATE", nullable = false)
   private Date                createdDate;
 
+  @Column(name = "UPDATED_DATE", nullable = false)
+  private Date                updatedDate;
+
   @ElementCollection(fetch = FetchType.EAGER)
   @MapKeyColumn(name = "NAME")
   @Column(name = "VALUE")
@@ -295,6 +298,10 @@ public class MetadataItemEntity implements Serializable {
   public void setCreatedDate(Date createdDate) {
     this.createdDate = createdDate;
   }
+
+  public Date getUpdatedDate() { return updatedDate; }
+
+  public void setUpdatedDate(Date updatedDate) { this.updatedDate = updatedDate; }
 
   public Map<String, String> getProperties() {
     return properties;
