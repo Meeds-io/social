7e86be9b221d8ce0dee6ea4a3888dc2bc8210520
saraBoutej <sboutej@exoplatform.com>
2024-05-29 12:27:15 +0100

    PR review

--- extension/war/src/main/webapp/WEB-INF/conf/social-extension/ckeditor/embed-base-plugin.js
+++
@@ -1,2 +0,0 @@
-﻿CKEDITOR.plugins.addExternal('embedbaseOnlyVideo', '/commons-extension/eXoPlugins/embedbaseOnlyVideo/', 'plugin.js');
-CKEDITOR.plugins.addExternal('embedsemanticOnlyVideo', '/commons-extension/eXoPlugins/embedsemanticOnlyVideo/', 'plugin.js');
