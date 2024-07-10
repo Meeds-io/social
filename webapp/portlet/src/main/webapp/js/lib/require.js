97de128d449a3af886432f25d3483ae2849eeed0
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-07-10 17:50:19 +0100

    feat: Retrieve JS file using content hash instead of bundle version - MEED-7237 - Meeds-io/MIPs#134 This change will apply a modification made on portal module where the url doesn't finish by '.js' all time. Thus, this will remove the 'systematic' concatenation of js extension.

--- webapp/portlet/src/main/webapp/js/lib/require.js
+++ webapp/portlet/src/main/webapp/js/lib/require.js
@@ -1594,7 +1594,7 @@ var requirejs, require, define;
                     }
 
                     //Join the path parts together, then figure out if baseUrl is needed.
-                    url = syms.join('/') + (ext || '.js');
+                    url = syms.join('/') + (ext || '');
                     url = (url.charAt(0) === '/' || url.match(/^[\w\+\.\-]+:/) ? '' : config.baseUrl) + url;
                 }
 
