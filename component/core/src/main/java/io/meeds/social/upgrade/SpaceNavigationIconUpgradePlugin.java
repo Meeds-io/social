6f92315f6caeaaf5c2c8c9e873bb77b0e0324fb7
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-09-11 16:01:00 +0100

    feat: Upgrade Existing Space Settings Page Permissions - MEED-7457 - Meeds-io/meeds#2377 This change will introduce a new Upgrade plugin to change space 'settings' page permissions to be accessible to 'manager' role only instead of '*' which was the case using a workaround in Space Menu Portlet and not embedded in Page permissions set.

--- component/core/src/main/java/io/meeds/social/upgrade/SpaceNavigationIconUpgradePlugin.java
+++ component/core/src/main/java/io/meeds/social/upgrade/SpaceNavigationIconUpgradePlugin.java
@@ -14,7 +14,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-package io.meeds.social.core.upgrade;
+package io.meeds.social.upgrade;
 
 import java.util.Arrays;
 import java.util.HashMap;
