97aae5eed619410e488ba9af9c1076f93a31ba71
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-09-30 18:27:31 +0100

    feat: Add CTA Space buttons in Space Public Site - MEED-7545 - Meeds-io/MIPs#151 (#4076)

--- webapp/portlet/src/main/webapp/vue-apps/top-bar-login/components/TopBarLogin.vue
+++ webapp/portlet/src/main/webapp/vue-apps/top-bar-login/components/TopBarLogin.vue
@@ -33,7 +33,7 @@
       <v-btn
         id="topBarLoginButton"
         :icon="$vuetify.breakpoint.mobile"
-        :href="authenticated && '#' || '/portal/login'"
+        :href="authenticated && '#' || loginUrl"
         :class="$vuetify.breakpoint.mobile && 'rounded'"
         class="primary me-1"
         outlined>
@@ -42,9 +42,9 @@
       </v-btn>
     </div>
     <v-btn
-      v-else-if="authenticated"
+      v-else-if="!isSpace && authenticated"
       id="topBarAccessButton"
-      href="/portal"
+      href="/"
       target="_blank"
       class="primary"
       outlined>
@@ -53,19 +53,120 @@
       </v-avatar>
       <span class="text-none">{{ $t('publicAccess.access') }}</span>
     </v-btn>
+    <v-btn
+      v-else-if="isSpace && $root.isMember"
+      id="topBarAccessButton"
+      :href="`/portal/s/${spaceId}`"
+      :title="$t('publicAccess.enter.tooltip')"
+      class="primary"
+      outlined>
+      <v-avatar size="20" class="me-2">
+        <v-img :src="$root.avatarUrl" eager />
+      </v-avatar>
+      <span class="text-none">{{ $t('publicAccess.enter') }}</span>
+    </v-btn>
+    <div v-else-if="isSpace && authenticated && $root.isInvitedUser" class="d-flex">
+      <v-btn
+        id="topBarAccessButtonAccept"
+        :title="$t('publicAccess.acceptJoin.tooltip')"
+        :loading="loading"
+        class="success me-2"
+        outlined
+        @click="acceptToJoin">
+        <v-avatar size="20" class="me-2">
+          <v-img :src="$root.avatarUrl" eager />
+        </v-avatar>
+        <span class="text-none success--text">{{ $t('publicAccess.acceptJoin') }}</span>
+      </v-btn>
+      <v-btn
+        id="topBarAccessButtonRefuse"
+        :title="$t('publicAccess.refuseJoin.tooltip')"
+        :loading="loading"
+        class="error"
+        outlined
+        @click="refuseToJoin">
+        <span class="text-none error--text">{{ $t('publicAccess.refuseJoin') }}</span>
+      </v-btn>
+    </div>
+    <v-btn
+      v-else-if="isSpace && authenticated && $root.isPendingUser"
+      id="topBarAccessButton"
+      :title="$t('publicAccess.cancelRequest.tooltip')"
+      :loading="loading"
+      class="primary"
+      outlined
+      @click="cancelRequest">
+      <v-avatar size="20" class="me-2">
+        <v-img :src="$root.avatarUrl" eager />
+      </v-avatar>
+      <span class="text-none">{{ $t('publicAccess.cancelRequest') }}</span>
+    </v-btn>
+    <v-btn
+      v-else-if="isSpace && authenticated && spaceRegistration === 'open'"
+      id="topBarAccessButton"
+      :loading="loading"
+      :title="$t('publicAccess.join.tooltip')"
+      class="primary"
+      outlined
+      @click="join">
+      <v-avatar size="20" class="me-2">
+        <v-img :src="$root.avatarUrl" eager />
+      </v-avatar>
+      <span class="text-none">{{ $t('publicAccess.join') }}</span>
+    </v-btn>
+    <v-btn
+      v-else-if="isSpace && authenticated && spaceRegistration === 'validation'"
+      id="topBarAccessButton"
+      :title="$t('publicAccess.requestJoin.tooltip')"
+      :loading="loading"
+      class="primary"
+      outlined
+      @click="requestJoin">
+      <v-avatar size="20" class="me-2">
+        <v-img :src="$root.avatarUrl" eager />
+      </v-avatar>
+      <span class="text-none">{{ $t('publicAccess.requestJoin') }}</span>
+    </v-btn>
+    <div
+      v-else-if="isSpace && authenticated && spaceRegistration === 'closed'"
+      :title="$t('publicAccess.restrictedAccess.tooltip')">
+      <v-btn
+        id="topBarAccessButton"
+        class="primary"
+        disabled
+        outlined>
+        <v-avatar size="20" class="me-2">
+          <v-img :src="$root.avatarUrl" eager />
+        </v-avatar>
+        <span class="text-none">{{ $t('publicAccess.restrictedAccess') }}</span>
+      </v-btn>
+    </div>
   </v-app>
 </template>
 <script>
 export default {
   data: () => ({
     previewMode: false,
+    loading: false,
   }),
   computed: {
     publicMode() {
       return this.previewMode || !this.authenticated;
     },
+    spaceRegistration() {
+      return this.$root.spaceRegistration;
+    },
+    isSpace() {
+      return !!this.spaceId?.length;
+    },
+    spaceId() {
+      return eXo?.env?.portal?.spaceId;
+    },
     authenticated() {
       return eXo?.env?.portal?.userIdentityId?.length;
+    },
+    loginUrl() {
+      return this.isSpace ? `/portal/login?initialURI=${window.location.pathname}` : '/portal/login';
     },
   },
   created() {
@@ -81,6 +182,51 @@ export default {
     },
     switchToEdit() {
       this.previewMode = false;
+    },
+    async acceptToJoin() {
+      this.loading = true;
+      try {
+        await this.$spaceService.accept(this.spaceId);
+        this.$root.isMember = true;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async refuseToJoin() {
+      this.loading = true;
+      try {
+        await this.$spaceService.deny(this.spaceId);
+        this.$root.isInvitedUser = false;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async join() {
+      this.loading = true;
+      try {
+        await this.$spaceService.join(this.spaceId);
+        this.$root.isMember = true;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async requestJoin() {
+      this.loading = true;
+      try {
+        await this.$spaceService.requestJoin(this.spaceId);
+        this.$root.isPendingUser = true;
+      } finally {
+        this.loading = false;
+      }
+    },
+    async cancelRequest() {
+      this.loading = true;
+      try {
+        await this.$spaceService.cancel(this.spaceId);
+        this.$root.isPendingUser = false;
+      } finally {
+        this.loading = false;
+      }
     },
   },
 };
