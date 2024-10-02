9dbcee54bf0d6f4dc0df6067d6744d775d654386
Boubaker Khanfir <bkhanfir@exoplatform.com>
2024-10-02 06:36:13 +0100

    feat: Add Space Public Site URL in Description Widget - MEED-7542 - Meeds-io/MIPs#151

--- webapp/portlet/src/main/webapp/vue-apps/space-widget-description/main.js
+++ webapp/portlet/src/main/webapp/vue-apps/space-widget-description/main.js
@@ -34,7 +34,7 @@ if (extensionRegistry) {
 const appId = 'SpaceDescriptionApplication';
 const url = `/social-portlet/i18n/locale.portlet.social.SpaceInfosPortlet?lang=${lang}`;
 
-export async function init(spaceId, isManager, spaceDescription) {
+export async function init(spaceId, isManager, publicSiteUrl, spaceDescription) {
   if (!isManager && (!spaceDescription.length || !Vue.prototype.$utils.htmlToText(spaceDescription).length)) {
     Vue.prototype.$updateApplicationVisibility(false, document.querySelector(`#${appId}`));
     return;
@@ -44,6 +44,7 @@ export async function init(spaceId, isManager, spaceDescription) {
     data: {
       spaceId,
       spaceDescription,
+      publicSiteUrl,
       isManager,
     },
     template: `<space-description-application id="${appId}" />`,
