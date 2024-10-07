0e69236cbb8ef91712285d2f80e96a03706df22a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-07 17:57:01 +0100

    feat: Enhance Space Card buttons disposition - MEED-7581 - Meeds-io/MIPs#158 (#4089)

--- webapp/portlet/src/main/webapp/vue-apps/spaces-list/components/SpaceCard.vue
+++ webapp/portlet/src/main/webapp/vue-apps/spaces-list/components/SpaceCard.vue
@@ -1,3 +1,25 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
 <template>
   <v-hover v-model="hoverCard">
     <v-card
@@ -59,19 +81,7 @@
             class="flex-shrink-0 text-subtitle"></div>
         </div>
         <v-spacer />
-        <space-favorite-action
-          v-if="space.isMember"
-          :is-favorite="space.isFavorite"
-          :space-id="space.id"
-          :icon-size="20"
-          class="ms-1" />
-        <space-card-button
-          v-for="(extension, i) in enabledSpaceActionExtensions"
-          :key="i"
-          :extension="extension"
-          :space="space"
-          class="ms-1" />
-        <space-card-membership-options
+        <space-card-menu
           :space="space"
           class="ms-1" />
       </v-card>
