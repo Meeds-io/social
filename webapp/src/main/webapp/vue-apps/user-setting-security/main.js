f0b408b019871f71d407912bfe18b23bfcad8058
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-14 13:56:36 +0100

    feat: Combine Social Web Archives to one single social.war - Meeds-io/MIPs#150 (#4114)
    Social Module has two Web Archives, social-extension.war and
    social-portlet.war. both Web Archives can be combined into a single one
    social.war which will be the main Spring context of Social Module and at
    the same time, used for extension configuration and the Web Archive to
    use for Portlet Definitions. The webapp module name has to be social.war
    in order to have clean URLs for REST Endpoints which will be implemented
    through Spring, with format /social/rest/ instead of
    /social-portlet/rest.

---
+++ webapp/src/main/webapp/vue-apps/user-setting-security/main.js
@@ -0,0 +1,36 @@
+import './initComponents.js';
+// get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('UserSettingSecurity');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+//getting language of user
+const lang = eXo && eXo.env.portal.language || 'en';
+
+//should expose the locale ressources as REST API 
+const url = `/social/i18n/locale.portlet.social.UserSettings?lang=${lang}`;
+
+const appId = 'UserSettingSecurity';
+
+document.dispatchEvent(new CustomEvent('displayTopBarLoading'));
+
+export function init() {
+  exoi18n.loadLanguageAsync(lang, url).then(i18n => {
+    const appElement = document.createElement('div');
+    appElement.id = appId;
+
+    Vue.createApp({
+      mounted() {
+        document.dispatchEvent(new CustomEvent('hideTopBarLoading'));
+      },
+      template: `<user-setting-security id="${appId}" v-cacheable />`,
+      i18n,
+      vuetify: Vue.prototype.vuetifyOptions,
+    }, appElement, 'User Settings Security');
+  });
+}
