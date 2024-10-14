3f5e120a12309114e0bb89ee397ef5f3bb0d9ca7
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-14 13:56:36 +0100

    feat: Combine Social Web Archives to one single social.war - Meeds-io/MIPs#150 (#4114)
    Social Module has two Web Archives, social-extension.war and
    social-portlet.war. both Web Archives can be combined into a single one
    social.war which will be the main Spring context of Social Module and at
    the same time, used for extension configuration and the Web Archive to
    use for Portlet Definitions. The webapp module name has to be social.war
    in order to have clean URLs for REST Endpoints which will be implemented
    through Spring, with format /social/rest/ instead of
    /social-portlet/rest.

---
+++ webapp/src/main/webapp/vue-apps/spaces-administration/main.js
@@ -0,0 +1,40 @@
+import './initComponents.js';
+import './services.js';
+
+// getting language of the PLF 
+const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
+
+// should expose the locale ressources as REST API 
+const urls = [
+  `/social/i18n/locale.portlet.social.SpacesAdministrationPortlet?lang=${lang}`,
+  `/social/i18n/locale.portal.webui?lang=${lang}`,
+];
+
+// get overrided components if exists
+if (extensionRegistry) {
+  const components = extensionRegistry.loadComponents('SpacesAdministration');
+  if (components && components.length > 0) {
+    components.forEach(cmp => {
+      Vue.component(cmp.componentName, cmp.componentOptions);
+    });
+  }
+}
+
+const appId = 'spacesAdministration';
+
+// getting locale ressources
+export function init(applicationsByCategory) {
+  exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
+    const appElement = document.createElement('div');
+    appElement.id = appId;
+
+    Vue.createApp({
+      data: {
+        applicationsByCategory: applicationsByCategory,
+      },
+      template: `<exo-spaces-administration-spaces v-cacheable id="${appId}" :applications-by-category="applicationsByCategory"></exo-spaces-administration-spaces>`,
+      i18n,
+      vuetify: Vue.prototype.vuetifyOptions,
+    }, appElement, 'Spaces Administration');
+  });
+}
