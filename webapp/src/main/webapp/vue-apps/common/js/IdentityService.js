3beb1b8cc07ead0343d66e24e34926afdbbc168a
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-17 16:55:36 +0100

    feat: Add Space Template Form Drawers - MEED-7612 - Meeds-io/MIPs#150 (#4121)
    This change will add Two drawers to allow creating and editing a Space
    Template.

--- webapp/src/main/webapp/vue-apps/common/js/IdentityService.js
+++ webapp/src/main/webapp/vue-apps/common/js/IdentityService.js
@@ -12,7 +12,10 @@ export function getIdentityById(identityId, expand) {
 }
 
 export function getIdentityByProviderIdAndRemoteId(providerId, remoteId) {
-  return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/${providerId}/${remoteId}`, {
+  const url = remoteId.includes('/') ?
+    `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/byParams?providerId=${providerId}&remoteId=${remoteId}`
+    : `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/${providerId}/${remoteId}`;
+  return fetch(url, {
     method: 'GET',
     credentials: 'include',
   }).then(resp => {
---
+++ webapp/src/main/webapp/vue-apps/space-templates-management/components/form/SpaceTemplateVisibility.vue
@@ -0,0 +1,82 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div class="d-flex flex-column">
+    <div class="font-weight-bold">
+      {{ $t('spaceTemplate.defaultSpaceConfigurationStepVisibility') }}
+    </div>
+    <v-radio-group
+      v-model="visibility"
+      class="mt-2 ms-n1"
+      mandatory
+      inset>
+      <v-radio
+        value="PRIVATE"
+        class="mb-6">
+        <template #label>
+          <div class="d-flex flex-column">
+            <div class="text-body">
+              {{ $t('spaceTemplate.defaultSpaceConfigurationStepVisibilityListed') }}
+            </div>
+            <div class="text-subtitle position-absolute line-height-normal mt-5">
+              {{ $t(`spaceTemplate.defaultSpaceConfigurationStepVisibilityListedDescription`) }}
+            </div>
+          </div>
+        </template>
+      </v-radio>
+      <v-radio
+        value="HIDDEN"
+        class="mt-0 mb-8">
+        <template #label>
+          <div class="d-flex flex-column">
+            <div class="text-body">
+              {{ $t('spaceTemplate.defaultSpaceConfigurationStepVisibilityUnlisted') }}
+            </div>
+            <div class="text-subtitle position-absolute line-height-normal mt-5">
+              {{ $t(`spaceTemplate.defaultSpaceConfigurationStepVisibilityUnlistedDescription`) }}
+            </div>
+          </div>
+        </template>
+      </v-radio>
+    </v-radio-group>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  data: () => ({
+    visibility: null,
+  }),
+  watch: {
+    visibility() {
+      this.$emit('input', this.visibility);
+    },
+  },
+  created() {
+    this.visibility = this.value;
+  },
+};
+</script>
