2c84f7d1815dc6e6d40f128e02767b6cef2bea2f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-21 15:19:27 +0100

    feat: Replace StringUtils package name - Meeds-io/MIPs#150

--- component/service/src/test/java/io/meeds/social/observer/rest/ObserverRestTest.java
+++ component/service/src/test/java/io/meeds/social/observer/rest/ObserverRestTest.java
@@ -24,7 +24,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.apache.commons.codec.binary.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.Test;
 import org.mockito.MockedStatic;
 
