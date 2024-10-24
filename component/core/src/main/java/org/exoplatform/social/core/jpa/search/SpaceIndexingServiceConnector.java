c385958af4d6840d3ced7f08f85e1677a5a5ae32
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-10-24 18:17:02 +0100

    feat: Allow To search partial matching part of a word in spaces - MEED-7667 - Meeds-io/meeds#2523 (#4132)
    This change will reindex Spaces in order to allow using ngram tokenizer
    on display name to allow partial match search.

--- component/core/src/main/java/org/exoplatform/social/core/jpa/search/SpaceIndexingServiceConnector.java
+++ component/core/src/main/java/org/exoplatform/social/core/jpa/search/SpaceIndexingServiceConnector.java
@@ -154,6 +154,7 @@ public class SpaceIndexingServiceConnector extends ElasticIndexingServiceConnect
                                                .append("    \"prettyName\" : {\"type\" : \"keyword\"},\n")
                                                .append("    \"displayName\" : {")
                                                .append("      \"type\" : \"text\",")
+                                               .append("      \"analyzer\": \"ngram_analyzer\",")
                                                .append("      \"index_options\": \"offsets\",")
                                                .append("      \"fields\": {")
                                                .append("        \"raw\": {")
