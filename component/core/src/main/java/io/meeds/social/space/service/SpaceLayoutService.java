d99e0eaaef01e2e853b3e638e8aef2ec0107c5f9
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-28 06:29:10 +0100

    feat: Rename social.core to social package - Meeds-io/MIPs#165

--- component/core/src/main/java/io/meeds/social/space/service/SpaceLayoutService.java
+++ component/core/src/main/java/io/meeds/social/space/service/SpaceLayoutService.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package io.meeds.social.core.space.service;
+package io.meeds.social.space.service;
 
 import java.util.List;
 
