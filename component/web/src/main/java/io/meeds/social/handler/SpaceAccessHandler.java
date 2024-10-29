90bdbd31bcad7892e7f9a36a5114fa250d2f7c33
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-29 13:18:46 +0100

    feat: Rename Packages to use io.meeds - Meeds-io/MIPs#165

--- component/web/src/main/java/io/meeds/social/handler/SpaceAccessHandler.java
+++ component/web/src/main/java/io/meeds/social/handler/SpaceAccessHandler.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package org.exoplatform.social.core.space.impl;
+package io.meeds.social.handler;
 
 import static org.exoplatform.portal.application.PortalRequestHandler.REQUEST_SITE_NAME;
 import static org.exoplatform.portal.application.PortalRequestHandler.REQUEST_SITE_TYPE;
