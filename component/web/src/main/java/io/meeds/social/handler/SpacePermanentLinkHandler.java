9ffcbbe7afacf7e4687b73920757212b930c096f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-10-29 13:18:46 +0100

    feat: Rename Packages to use io.meeds - Meeds-io/MIPs#165

--- component/web/src/main/java/io/meeds/social/handler/SpacePermanentLinkHandler.java
+++ component/web/src/main/java/io/meeds/social/handler/SpacePermanentLinkHandler.java
@@ -16,7 +16,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
-package io.meeds.social.portlet;
+package io.meeds.social.handler;
 
 import static io.meeds.social.permlink.plugin.SpacePermanentLinkPlugin.APPLICATION_URI;
 import static io.meeds.social.permlink.plugin.SpacePermanentLinkPlugin.OBJECT_TYPE;
