43be8e10aa8c738d9e6bbab9fcf6bab90921d4b8
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-11-06 10:17:22 +0100

    feat: Rework Space Administration Table - MEED-7493 - Meeds-io/MIPs#160 (#4164)

--- webapp/src/main/webapp/vue-apps/spaces-administration/main.js
+++ webapp/src/main/webapp/vue-apps/spaces-administration/main.js
@@ -1,16 +1,24 @@
+/*
+ * This file is part of the Meeds project (https://meeds.io/).
+ * 
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ * 
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
 import './initComponents.js';
 import './services.js';
 
-// getting language of the PLF 
-const lang = typeof eXo !== 'undefined' ? eXo.env.portal.language : 'en';
-
-// should expose the locale ressources as REST API 
-const urls = [
-  `/social/i18n/locale.portlet.social.SpacesAdministrationPortlet?lang=${lang}`,
-  `/social/i18n/locale.portal.webui?lang=${lang}`,
-];
-
-// get overrided components if exists
 if (extensionRegistry) {
   const components = extensionRegistry.loadComponents('SpacesAdministration');
   if (components && components.length > 0) {
@@ -20,21 +28,30 @@ if (extensionRegistry) {
   }
 }
 
-const appId = 'spacesAdministration';
-
-// getting locale ressources
-export function init(applicationsByCategory) {
-  exoi18n.loadLanguageAsync(lang, urls).then(i18n => {
-    const appElement = document.createElement('div');
-    appElement.id = appId;
-
-    return Vue.createApp({
+export function init() {
+  const appId = 'spacesAdministration';
+  const lang = eXo?.env?.portal?.language || 'en';
+  exoi18n.loadLanguageAsync(lang, [
+    `/social/i18n/locale.portlet.social.SpacesAdministrationPortlet?lang=${lang}`,
+    `/social/i18n/locale.portlet.social.SpacesListApplication?lang=${lang}`,
+    `/social/i18n/locale.portal.webui?lang=${lang}`,
+  ])
+    .then(i18n => Vue.createApp({
       data: {
-        applicationsByCategory: applicationsByCategory,
+        spaceTemplates: null,
+        collator: new Intl.Collator(eXo.env.portal.language, {numeric: true, sensitivity: 'base'}),
       },
-      template: `<exo-spaces-administration-spaces v-cacheable id="${appId}" :applications-by-category="applicationsByCategory"></exo-spaces-administration-spaces>`,
+      created() {
+        this.refreshSpaceTemplates();
+      },
+      methods: {
+        async refreshSpaceTemplates() {
+          this.spaceTemplates = await this.$spaceTemplateService.getSpaceTemplates(true);
+        },
+      },
+      template: `<spaces-administration id="${appId}" />`,
       i18n,
       vuetify: Vue.prototype.vuetifyOptions,
-    }, appElement, 'Spaces Administration');
-  }).finally(() => Vue.prototype.$utils.includeExtensions('ManageSpacesExtension'));
+    }, `#${appId}`, 'Spaces Administration'))
+    .finally(() => Vue.prototype.$utils.includeExtensions('ManageSpacesExtension'));
 }
