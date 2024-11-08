0d2c93f23fd649586fbe8ec5a20e73b71303dc20
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-11-08 05:42:48 +0100

    feat: Allow to apply a Template on a Space - MEED-7735 - Meeds-io/MIPs#160 (#4171)
    This change will allow to a platform administrator to apply a template's
    characteristics on a chosen space.

--- component/core/src/test/java/io/meeds/social/space/administration/service/SpaceAdministrationServiceTest.java
+++ component/core/src/test/java/io/meeds/social/space/administration/service/SpaceAdministrationServiceTest.java
@@ -18,12 +18,8 @@
  */
 package io.meeds.social.space.administration.service;
 
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
 
 import java.util.Collections;
 
@@ -37,16 +33,43 @@ import org.exoplatform.commons.exception.ObjectNotFoundException;
 import org.exoplatform.social.core.space.model.Space;
 import org.exoplatform.social.core.space.spi.SpaceService;
 
-import io.meeds.social.space.administration.model.SpacePermissions;
+import io.meeds.social.space.constant.Registration;
+import io.meeds.social.space.constant.Visibility;
+import io.meeds.social.space.model.SpacePermissions;
+import io.meeds.social.space.model.SpaceTemplatePatch;
+import io.meeds.social.space.service.SpaceAdministrationServiceImpl;
+import io.meeds.social.space.service.SpaceLayoutService;
+import io.meeds.social.space.template.model.SpaceTemplate;
+import io.meeds.social.space.template.service.SpaceTemplateService;
 
 @RunWith(MockitoJUnitRunner.class)
 public class SpaceAdministrationServiceTest {
+
+  private static final String    TESTUSER_REDACTOR = "testuserRedactor";
+
+  private static final String    TESTUSER_MANAGER  = "testuserManager";
+
+  long                           spaceId           = 2l;
+
+  long                           templateId        = 3l;
 
   @Mock
   SpaceService                   spaceService;
 
   @Mock
+  SpaceLayoutService             spaceLayoutService;
+
+  @Mock
+  SpaceTemplateService           spaceTemplateService;
+
+  @Mock
   Space                          space;
+
+  @Mock
+  SpaceTemplate                  spaceTemplate;
+
+  @Mock
+  SpaceTemplatePatch             templatePatch;
 
   SpaceAdministrationServiceImpl spaceAdministrationService;
 
@@ -54,16 +77,18 @@ public class SpaceAdministrationServiceTest {
   public void setup() {
     spaceAdministrationService = new SpaceAdministrationServiceImpl();
     spaceAdministrationService.setSpaceService(spaceService);
+    spaceAdministrationService.setSpaceLayoutService(spaceLayoutService);
+    spaceAdministrationService.setSpaceTemplateService(spaceTemplateService);
   }
 
   @Test
   public void testGetSpacePermissions() throws ObjectNotFoundException {
-    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.getSpacePermissions(2l));
-    when(spaceService.getSpaceById(2l)).thenReturn(space);
+    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.getSpacePermissions(spaceId));
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
     when(space.getLayoutPermissions()).thenReturn(Collections.singletonList("layoutPermissions"));
     when(space.getDeletePermissions()).thenReturn(Collections.singletonList("deletePermissions"));
     when(space.getPublicSitePermissions()).thenReturn(Collections.singletonList("publicSitePermissionsPermissions"));
-    SpacePermissions spacePermissions = spaceAdministrationService.getSpacePermissions(2l);
+    SpacePermissions spacePermissions = spaceAdministrationService.getSpacePermissions(spaceId);
     assertNotNull(spacePermissions);
     assertEquals(space.getDeletePermissions(), spacePermissions.getDeletePermissions());
     assertEquals(space.getLayoutPermissions(), spacePermissions.getLayoutPermissions());
@@ -77,13 +102,114 @@ public class SpaceAdministrationServiceTest {
     when(spacePermissions.getDeletePermissions()).thenReturn(Collections.singletonList("deletePermissions"));
     when(spacePermissions.getPublicSitePermissions()).thenReturn(Collections.singletonList("publicSitePermissionsPermissions"));
 
-    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.updateSpacePermissions(2l, spacePermissions));
-    when(spaceService.getSpaceById(2l)).thenReturn(space);
-    spaceAdministrationService.updateSpacePermissions(2l, spacePermissions);
+    assertThrows(ObjectNotFoundException.class,
+                 () -> spaceAdministrationService.updateSpacePermissions(spaceId, spacePermissions));
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    spaceAdministrationService.updateSpacePermissions(spaceId, spacePermissions);
     verify(space).setDeletePermissions(spacePermissions.getDeletePermissions());
     verify(space).setLayoutPermissions(spacePermissions.getLayoutPermissions());
     verify(space).setPublicSitePermissions(spacePermissions.getPublicSitePermissions());
     verify(spaceService).updateSpace(space);
+  }
+
+  @Test
+  public void testApplySpaceTemplate() throws ObjectNotFoundException {
+    when(spaceTemplateService.getSpaceTemplate(anyLong())).thenReturn(spaceTemplate);
+    when(spaceTemplate.isDeleted()).thenReturn(false);
+    when(spaceTemplate.isEnabled()).thenReturn(true);
+    when(spaceTemplate.isSpaceAllowContentCreation()).thenReturn(true);
+    when(spaceTemplate.getSpaceDefaultRegistration()).thenReturn(Registration.OPEN);
+    when(spaceTemplate.getSpaceDefaultVisibility()).thenReturn(Visibility.HIDDEN);
+
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(space.getManagers()).thenReturn(new String[] { TESTUSER_MANAGER });
+
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+    when(templatePatch.isAccessRules()).thenReturn(true);
+    when(templatePatch.isDeletePermissions()).thenReturn(true);
+    when(templatePatch.isLayoutPermissions()).thenReturn(true);
+    when(templatePatch.isPublicSitePermissions()).thenReturn(true);
+    when(templatePatch.isEditorialMode()).thenReturn(true);
+
+    spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch);
+
+    verify(spaceService).updateSpace(space);
+    verify(spaceLayoutService).updateSpaceSite(space);
+    verify(spaceService).addRedactor(any(), any());
+    verify(space).setRegistration(spaceTemplate.getSpaceDefaultRegistration().name().toLowerCase());
+    verify(space).setVisibility(spaceTemplate.getSpaceDefaultVisibility().name().toLowerCase());
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenSpaceNotFound() {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(null);
+    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch));
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenSpaceTemplateNotFound() {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+    when(spaceTemplateService.getSpaceTemplate(templateId)).thenReturn(null);
+
+    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch));
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenSpaceTemplateIsDeleted() {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+    when(spaceTemplateService.getSpaceTemplate(templateId)).thenReturn(spaceTemplate);
+    when(spaceTemplate.isDeleted()).thenReturn(true);
+
+    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch));
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenSpaceTemplateIsDisabled() {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+    when(spaceTemplateService.getSpaceTemplate(templateId)).thenReturn(spaceTemplate);
+    when(spaceTemplate.isEnabled()).thenReturn(false);
+
+    assertThrows(ObjectNotFoundException.class, () -> spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch));
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenEditorialModeAndAllowContentCreation() throws ObjectNotFoundException {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(space.getRedactors()).thenReturn(new String[0]);
+    when(space.getManagers()).thenReturn(new String[] { TESTUSER_MANAGER });
+
+    when(spaceTemplateService.getSpaceTemplate(templateId)).thenReturn(spaceTemplate);
+    when(spaceTemplate.isSpaceAllowContentCreation()).thenReturn(true);
+    when(spaceTemplate.isDeleted()).thenReturn(false);
+    when(spaceTemplate.isEnabled()).thenReturn(true);
+
+    when(templatePatch.isEditorialMode()).thenReturn(true);
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+
+    spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch);
+
+    verify(spaceService).addRedactor(any(), any());
+  }
+
+  @Test
+  public void testApplySpaceTemplateWhenEditorialModeAndNotAllowContentCreation() throws ObjectNotFoundException {
+    when(spaceService.getSpaceById(spaceId)).thenReturn(space);
+    when(space.getRedactors()).thenReturn(new String[] { TESTUSER_REDACTOR });
+
+    when(spaceTemplateService.getSpaceTemplate(anyLong())).thenReturn(spaceTemplate);
+    when(spaceTemplate.isSpaceAllowContentCreation()).thenReturn(false);
+    when(spaceTemplate.isDeleted()).thenReturn(false);
+    when(spaceTemplate.isEnabled()).thenReturn(true);
+
+    when(templatePatch.isEditorialMode()).thenReturn(true);
+    when(templatePatch.getTemplateId()).thenReturn(templateId);
+
+    spaceAdministrationService.applySpaceTemplate(spaceId, templatePatch);
+
+    verify(spaceService).removeRedactor(any(), any());
   }
 
 }
