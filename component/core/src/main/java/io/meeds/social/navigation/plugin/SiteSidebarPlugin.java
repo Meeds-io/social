2a409cf15fecd6bfdcd0c8eec44ce49149e84045
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-11-20 19:02:28 +0100

    feat: Prepare Tobar and Sidebar Navigation Service - MEED-7776 - Meeds-io/MIPs#159 (#4196)
    This change will add Backend and Basic Frontend for Topbar and Sidebar
    configuration.

---
+++ component/core/src/main/java/io/meeds/social/navigation/plugin/SiteSidebarPlugin.java
@@ -0,0 +1,234 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.social.navigation.plugin;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.ResourceBundle;
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import org.exoplatform.commons.utils.ExpressionUtil;
+import org.exoplatform.container.ExoContainerContext;
+import org.exoplatform.portal.config.UserPortalConfigService;
+import org.exoplatform.portal.config.model.PortalConfig;
+import org.exoplatform.portal.mop.SiteFilter;
+import org.exoplatform.portal.mop.SiteKey;
+import org.exoplatform.portal.mop.SiteType;
+import org.exoplatform.portal.mop.Visibility;
+import org.exoplatform.portal.mop.navigation.NodeContext;
+import org.exoplatform.portal.mop.navigation.NodeData;
+import org.exoplatform.portal.mop.navigation.NodeState;
+import org.exoplatform.portal.mop.service.LayoutService;
+import org.exoplatform.portal.mop.service.NavigationService;
+import org.exoplatform.services.organization.Group;
+import org.exoplatform.services.organization.OrganizationService;
+import org.exoplatform.services.resources.LocaleContextInfo;
+import org.exoplatform.services.resources.ResourceBundleManager;
+import org.exoplatform.services.resources.ResourceBundleService;
+
+import io.meeds.social.navigation.constant.SidebarItemType;
+import io.meeds.social.navigation.model.SidebarItem;
+import io.meeds.social.translation.service.TranslationService;
+
+import lombok.SneakyThrows;
+
+@Component
+@Order(10)
+public class SiteSidebarPlugin implements SidebarPlugin {
+
+  private static final String     SITE_EXPAND_PAGES_PROP_NAME = "expandPages";
+
+  private static final String     SITE_NAME_PROP_NAME         = "siteName";
+
+  private static final String     SITE_ID_PROP_NAME           = "siteId";
+
+  private static final String     SITE_TYPE_PROP_NAME         = "siteType";
+
+  private static final String     SITE_OBJECT_TYPE            = "site";
+
+  private static final String     SITE_LABEL_FIELD_NAME       = "label";
+
+  @Autowired
+  private TranslationService      translationService;
+
+  @Autowired
+  private UserPortalConfigService userPortalConfigService;
+
+  @Autowired
+  private LayoutService           layoutService;
+
+  @Autowired
+  private NavigationService       navigationService;
+
+  @Autowired
+  private ResourceBundleManager   resourceBundleManager;
+
+  @Override
+  public SidebarItemType getType() {
+    return SidebarItemType.SITE;
+  }
+
+  @Override
+  public SidebarItem resolveProperties(SidebarItem item, Locale locale) {
+    String siteId = item.getProperties().get(SITE_ID_PROP_NAME);
+    String label = translationService.getTranslationLabelOrDefault(SITE_OBJECT_TYPE,
+                                                                   Long.parseLong(siteId),
+                                                                   SITE_LABEL_FIELD_NAME,
+                                                                   locale);
+    if (StringUtils.isBlank(label)) {
+      String siteType = item.getProperties().get(SITE_TYPE_PROP_NAME);
+      String siteName = item.getProperties().get(SITE_NAME_PROP_NAME);
+      item.setName(getSiteLabel(new SiteKey(siteType, siteName), locale));
+    } else {
+      item.setName(label);
+    }
+
+    NodeContext<NodeContext<Object>> rootNode = navigationService.loadNode(new SiteKey(item.getProperties()
+                                                                                           .get(SITE_TYPE_PROP_NAME),
+                                                                                       item.getProperties()
+                                                                                           .get(SITE_NAME_PROP_NAME)));
+    if (rootNode != null && rootNode.getSize() > 0) {
+      NodeContext<NodeContext<Object>> firstNode = rootNode.get(0);
+      if (firstNode.getData() != null
+          && firstNode.getData().getState() != null
+          && firstNode.getData().getState().getIcon() != null) {
+        item.setIcon(firstNode.getData().getState().getIcon());
+      }
+    }
+    return item;
+  }
+
+  @Override
+  public List<SidebarItem> getDefaultItems() {
+    SiteFilter siteFilter = new SiteFilter();
+    siteFilter.setDisplayed(true);
+    siteFilter.setSiteType(SiteType.PORTAL);
+    siteFilter.setExcludedSiteName(UserPortalConfigService.DEFAULT_GLOBAL_PORTAL);
+    siteFilter.setExcludeSpaceSites(true);
+    siteFilter.setSortByDisplayOrder(true);
+    siteFilter.setFilterByDisplayed(true);
+    List<PortalConfig> sites = layoutService.getSites(siteFilter);
+    return sites.stream()
+                .map(site -> toSidebarItem(SiteKey.portal(site.getName())))
+                .toList();
+  }
+
+  @Override
+  public boolean itemExists(SidebarItem item) {
+    String siteType = item.getProperties().get(SITE_TYPE_PROP_NAME);
+    String siteName = item.getProperties().get(SITE_NAME_PROP_NAME);
+    return layoutService.getPortalConfig(siteType, siteName) != null;
+  }
+
+  private SidebarItem toSidebarItem(SiteKey siteKey) {
+    return new SidebarItem(siteKey.getName(),
+                                 "/portal/" + siteKey.getName(),
+                                 null,
+                                 null,
+                                 getSiteIcon(siteKey),
+                                 SidebarItemType.SITE,
+                                 null,
+                                 buildSiteProperties(siteKey));
+  }
+
+  private String getSiteIcon(SiteKey siteKey) {
+    NodeContext<NodeContext<Object>> rootNode = navigationService.loadNode(siteKey);
+    if (rootNode != null && rootNode.getSize() > 0) {
+      Collection<NodeContext<Object>> nodes = rootNode.getNodes();
+      return nodes.stream().map(node -> {
+        NodeData data = node.getData();
+        NodeState state = data.getState();
+        if ((state.getVisibility() == Visibility.DISPLAYED
+             || state.getVisibility() == Visibility.TEMPORAL)
+            && state.getPageRef() != null
+            && StringUtils.isNotBlank(state.getIcon())) {
+          return state.getIcon();
+        } else {
+          return null;
+        }
+      }).filter(Objects::nonNull).findFirst().orElse(null);
+    }
+    return null;
+  }
+
+  private Map<String, String> buildSiteProperties(SiteKey siteKey) {
+    PortalConfig site = layoutService.getPortalConfig(siteKey);
+    long siteId = Long.parseLong((site.getStorageId().split("_"))[1]);
+    boolean isMetaSite = StringUtils.equals(userPortalConfigService.getMetaPortal(), siteKey.getName());
+
+    Map<String, String> properties = new HashMap<>();
+    properties.put(SITE_TYPE_PROP_NAME, siteKey.getTypeName());
+    properties.put(SITE_NAME_PROP_NAME, siteKey.getName());
+    properties.put(SITE_EXPAND_PAGES_PROP_NAME, String.valueOf(isMetaSite));
+    properties.put(SITE_ID_PROP_NAME, String.valueOf(siteId));
+    return properties;
+  }
+
+  @SneakyThrows
+  private String getSiteLabel(SiteKey siteKey, Locale locale) {
+    PortalConfig site = layoutService.getPortalConfig(siteKey);
+    String label = site == null ?
+                                siteKey.getName() :
+                                StringUtils.firstNonBlank(site.getLabel(),
+                                                          site.getName(),
+                                                          siteKey.getName());
+    if (siteKey.getType() == SiteType.PORTAL) {
+      return StringUtils.firstNonBlank(getLabel(siteKey, label, locale),
+                                       siteKey.getName());
+    } else if (siteKey.getType() == SiteType.GROUP) {
+      Group siteGroup = ExoContainerContext.getService(OrganizationService.class)
+                                           .getGroupHandler()
+                                           .findGroupById(siteKey.getName());
+      if (siteGroup != null) {
+        return siteGroup.getLabel();
+      }
+    }
+    return label;
+  }
+
+  private String getLabel(SiteKey siteKey, String label, Locale locale) {
+    if (ExpressionUtil.isResourceBindingExpression(label)) {
+      return Stream.of(locale, ResourceBundleService.DEFAULT_CROWDIN_LOCALE)
+                   .map(l -> getBundle(siteKey.getTypeName(), siteKey.getName(), locale))
+                   .filter(Objects::nonNull)
+                   .map(b -> ExpressionUtil.getExpressionValue(b, label))
+                   .filter(StringUtils::isNotBlank)
+                   .findFirst()
+                   .orElse(label);
+    } else {
+      return label;
+    }
+  }
+
+  private ResourceBundle getBundle(String siteType, String siteName, Locale locale) {
+    return resourceBundleManager.getNavigationResourceBundle(LocaleContextInfo.getLocaleAsString(locale),
+                                                             siteType,
+                                                             siteName);
+  }
+
+}
---
+++ component/core/src/main/java/io/meeds/social/navigation/plugin/SpaceTemplateSidebarPlugin.java
@@ -0,0 +1,93 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.social.navigation.plugin;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.annotation.Order;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Component;
+
+import io.meeds.social.navigation.constant.SidebarItemType;
+import io.meeds.social.navigation.model.SidebarItem;
+import io.meeds.social.space.template.model.SpaceTemplate;
+import io.meeds.social.space.template.service.SpaceTemplateService;
+
+@Component
+@Order(30)
+public class SpaceTemplateSidebarPlugin implements SidebarPlugin {
+
+  private static final String  SPACE_TEMPLATE_ID_PROP_NAME = "spaceTemplateId";
+
+  @Autowired
+  private SpaceTemplateService spaceTemplateService;
+
+  @Override
+  public SidebarItemType getType() {
+    return SidebarItemType.SPACE_TEMPLATE;
+  }
+
+  @Override
+  public SidebarItem resolveProperties(SidebarItem item, Locale locale) {
+    String spaceTemplateId = item.getProperties().get(SPACE_TEMPLATE_ID_PROP_NAME);
+    SpaceTemplate spaceTemplate = spaceTemplateService.getSpaceTemplate(Long.parseLong(spaceTemplateId), locale, true);
+    if (spaceTemplate != null) {
+      item.setName(spaceTemplate.getName());
+      item.setIcon(spaceTemplate.getIcon());
+    }
+    return item;
+  }
+
+  @Override
+  public List<SidebarItem> getDefaultItems() {
+    return spaceTemplateService.getSpaceTemplates(null, Pageable.unpaged(), true)
+                               .stream()
+                               .filter(t -> t.isEnabled() && !t.isDeleted())
+                               .map(this::toSidebarItem)
+                               .toList();
+  }
+
+  @Override
+  public boolean itemExists(SidebarItem item) {
+    String spaceTemplateId = item.getProperties().get(SPACE_TEMPLATE_ID_PROP_NAME);
+    SpaceTemplate spaceTemplate = spaceTemplateService.getSpaceTemplate(Long.parseLong(spaceTemplateId));
+    return spaceTemplate != null && spaceTemplate.isEnabled() && !spaceTemplate.isDeleted();
+  }
+
+  private SidebarItem toSidebarItem(SpaceTemplate spaceTemplate) {
+    return new SidebarItem(spaceTemplate.getName(),
+                           null,
+                           null,
+                           null,
+                           spaceTemplate.getIcon(),
+                           SidebarItemType.SPACE_TEMPLATE,
+                           null,
+                           buildSpaceTemplateProperties(spaceTemplate));
+  }
+
+  private Map<String, String> buildSpaceTemplateProperties(SpaceTemplate spaceTemplate) {
+    return Collections.singletonMap(SPACE_TEMPLATE_ID_PROP_NAME,
+                                    String.valueOf(spaceTemplate.getId()));
+  }
+
+}
