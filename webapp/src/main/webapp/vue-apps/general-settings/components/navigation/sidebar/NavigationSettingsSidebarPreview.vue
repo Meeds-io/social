0cbde1090332b29c390e1a2db26173c0089368a6
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-11-21 06:56:41 +0100

    feat: Add Sidebar Preview in Main settings UI - MEED-7777 - Meeds-io/MIPs#159

--- webapp/src/main/webapp/vue-apps/general-settings/components/navigation/sidebar/NavigationSettingsSidebarPreview.vue
+++ webapp/src/main/webapp/vue-apps/general-settings/components/navigation/sidebar/NavigationSettingsSidebarPreview.vue
@@ -20,8 +20,63 @@
 
 -->
 <template>
-  <div class="d-flex">
-  </div>
+  <v-card
+    class="d-none d-sm-flex flex-column overflow-hidden"
+    min-width="420"
+    min-height="700"
+    height="fit-content"
+    max-height="900"
+    flat>
+    <v-list class="d-flex flex-column overflow-hidden pb-0 flex-grow-1 flex-shrink-1" dense>
+      <div class="flex-grow-0 flex-shrink-0">
+        <v-list-item class="d-flex" dense>
+          <v-list-item-avatar size="36" tile>
+            <img
+              src="/portal/rest/v1/platform/branding/logo"
+              height="36px"
+              width="auto"
+              alt="">
+          </v-list-item-avatar>
+          <v-list-item-content>
+            <v-list-item-title class="logoTitle font-weight-bold menu-text-color text-truncate">
+              {{ $root.branding?.companyName }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </div>
+      <div class="flex-grow-1 flex-shrink-1 overflow-x-hidden overflow-y-auto">
+        <portal-general-settings-navigation-settings-sidebar-preview-item
+          v-for="(item, index) in menuItems"
+          :key="index"
+          :settings="settings"
+          :item="item" />
+      </div>
+      <div class="flex-grow-0 flex-shrink-0">
+        <v-list-item class="d-flex" dense>
+          <v-list-item-action class="me-auto font-weight-bold">
+            {{ productName }}
+          </v-list-item-action>
+          <v-list-item-action class="ms-auto d-flex flex-row">
+            <v-avatar
+              class="userAvatar mx-3"
+              size="36">
+              <v-img :src="avatarUrl" contain />
+            </v-avatar>
+            <v-btn
+              class="userSettingsLink me-3"
+              icon>
+              <v-icon size="20">fa-sliders-h</v-icon>
+            </v-btn>
+            <v-btn
+              class="logoutLink me-n3"
+              icon>
+              <v-icon size="20">fa-power-off</v-icon>
+            </v-btn>
+          </v-list-item-action>
+        </v-list-item>
+      </div>
+    </v-list>
+  </v-card>
 </template>
 <script>
 export default {
@@ -29,6 +84,17 @@ export default {
     settings: {
       type: Object,
       default: null,
+    },
+  },
+  data: () => ({
+    productName: eXo.env.portal.productName,
+  }),
+  computed: {
+    menuItems() {
+      return this.settings?.items || [];
+    },
+    avatarUrl() {
+      return `/portal/rest/v1/social/users/${eXo.env.portal.userIdentityId}/avatar?byId=true`;
     },
   },
 };
