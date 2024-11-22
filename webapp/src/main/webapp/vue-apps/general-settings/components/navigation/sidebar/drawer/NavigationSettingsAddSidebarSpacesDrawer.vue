75a01ac7b11c7513126a4ac77894fd05d44a3fb9
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-11-22 09:07:02 +0100

    feat: Allow to add Spaces in Sidebar Menus - MEED-7799 - Meeds-io/MIPs#159

--- webapp/src/main/webapp/vue-apps/general-settings/components/navigation/sidebar/drawer/NavigationSettingsAddSidebarSpacesDrawer.vue
+++ webapp/src/main/webapp/vue-apps/general-settings/components/navigation/sidebar/drawer/NavigationSettingsAddSidebarSpacesDrawer.vue
@@ -30,6 +30,76 @@
       {{ $t(isNew && 'generalSettings.addSideBarItemSpaces.drawerTitle' || 'generalSettings.updateSideBarItemSpaces.drawerTitle') }}
     </template>
     <template v-if="drawer" #content>
+      <div class="pa-4">
+        <v-radio-group
+          v-model="option"
+          class="mt-0 mb-4 ms-n1 text-no-wrap flex-grow-1 flex-shrink-0"
+          mandatory>
+          <v-radio
+            value="SPACE_TEMPLATE"
+            class="mx-0 mt-0 mb-1">
+            <template #label>
+              <span class="text-body">{{ $t('generalSettings.sidebar.spaceTemplateOption') }}</span>
+            </template>
+          </v-radio>
+          <v-radio
+            value="SPACES"
+            class="mx-0 mt-0 mb-1">
+            <template #label>
+              <span class="text-body">{{ $t('generalSettings.sidebar.spacesOption') }}</span>
+            </template>
+          </v-radio>
+        </v-radio-group>
+        <v-autocomplete
+          v-if="option === 'SPACE_TEMPLATE' && spaceTemplates"
+          v-model="spaceTemplateId"
+          :items="spaceTemplates"
+          :placeholder="$t('generalSettings.sidebar.searchTemplatePlaceholder')"
+          item-text="name"
+          item-value="id"
+          class="mt-0 mb-4 mx-0 pa-0 elevation-0 no-border"
+          hide-no-data
+          hide-selected
+          hide-details
+          outlined
+          dense />
+        <div v-if="option === 'SPACES'" class="mb-4">
+          <div class="mb-2">
+            {{ $t('generalSettings.sidebar.spacesUpdateNameLabel') }}
+          </div>
+          <translation-text-field
+            v-model="names"
+            :placeholder="$t('generalSettings.sidebar.spacesNamesPlaceHolder')"
+            drawer-title="generalSettings.sidebar.spacesNamesDrawerTitle" />
+        </div>
+        <div class="d-flex align-center mb-2">
+          <div>{{ $t('generalSettings.sidebar.spacesSortItemsBy') }}</div>
+          <v-spacer />
+          <select
+            v-model="sortBy"
+            :aria-label="$t('generalSettings.sidebar.spacesSortItemsBy')"
+            class="ignore-vuetify-classes py-2 height-auto width-auto text-truncate ma-0">
+            <option value="TITLE">
+              {{ $t('generalSettings.sidebar.spacesSortItemsByTitle') }}
+            </option>
+            <option value="FAVORITE">
+              {{ $t('generalSettings.sidebar.spacesSortItemsByFavorite') }}
+            </option>
+            <option value="LAST_ACCESS">
+              {{ $t('generalSettings.sidebar.spacesSortItemsByRecentlyVisited') }}
+            </option>
+          </select>
+        </div>
+        <div class="d-flex align-center mb-4">
+          <div>{{ $t('generalSettings.sidebar.spacesSelectLimit') }}</div>
+          <v-spacer />
+          <number-input
+            v-model="limit"
+            step="1"
+            min="1"
+            max="10" />
+        </div>
+      </div>
     </template>
     <template #footer>
       <div class="d-flex">
@@ -55,12 +125,27 @@
 export default {
   data: () => ({
     drawer: false,
+    isNew: false,
+    names: {},
     settings: null,
+    option: null,
     item: null,
+    spaceTemplates: null,
+    spaceTemplateId: null,
+    icon: 'fa-layers-group',
+    sortBy: 'TITLE',
+    limit: 4,
   }),
   computed: {
-    isNew() {
-      return this.settings?.sidebar?.items?.indexOf?.(this.item) < 0;
+    spaceTemplate() {
+      return this.spaceTemplateId && this.spaceTemplates?.find?.(t => Number(t.id) === Number(this.spaceTemplateId)) || null;
+    },
+  },
+  watch: {
+    option() {
+      if (this.drawer) {
+        this.reset();
+      }
     },
   },
   created() {
@@ -82,9 +167,29 @@ export default {
         icon: null,
         type: null,
         items: null,
-        properties: null,
+        properties: {
+          sortBy: 'TITLE',
+          limit: 4,
+        },
       };
+      this.isNew = !item;
+      this.reset(item);
       this.$refs.drawer.open();
+      this.refreshSpaceTemplates();
+    },
+    async refreshSpaceTemplates() {
+      if (!this.spaceTemplates) {
+        this.spaceTemplates = await this.$spaceTemplateService.getSpaceTemplates();
+      }
+    },
+    reset(item) {
+      if (item) {
+        this.option = this.isNew ? 'SPACE_TEMPLATE' : item.type;
+      }
+      this.spaceTemplateId = this.option === 'SPACE_TEMPLATE' && item?.properties?.spaceTemplateId || null;
+      this.icon = this.option === 'SPACE_TEMPLATE' && (this.spaceTemplate?.icon || item?.icon) || 'fa-layers-group';
+      this.sortBy = item?.properties?.sortBy || this.sortBy || 'TITLE';
+      this.limit = item?.properties?.limit || this.limit || 4;
     },
     apply() {
       this.close();
