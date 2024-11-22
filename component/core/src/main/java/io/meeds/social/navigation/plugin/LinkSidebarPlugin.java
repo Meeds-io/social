dd2679c66f72d47dc49cd934ea20a1742813255f
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-11-22 19:01:58 +0100

    feat: Allow to Configure a Link in Left Menu - MEED-7799 - Meeds-io/MIPs#159 (#4199)
    This change will allow to create a static link in left menu / sidebar

---
+++ component/core/src/main/java/io/meeds/social/navigation/plugin/LinkSidebarPlugin.java
@@ -0,0 +1,86 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.social.navigation.plugin;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.exoplatform.services.resources.LocaleConfigService;
+
+import io.meeds.social.navigation.constant.SidebarItemType;
+import io.meeds.social.navigation.model.SidebarItem;
+
+public class LinkSidebarPlugin implements SidebarPlugin {
+
+  private static final String LINK_NAMES = "names";
+
+  @Autowired
+  private LocaleConfigService localeConfigService;
+
+  @Override
+  public SidebarItemType getType() {
+    return SidebarItemType.LINK;
+  }
+
+  @Override
+  public SidebarItem resolveProperties(SidebarItem item, String username, Locale locale) {
+    String names = item.getProperties().get(LINK_NAMES);
+    JSONObject jsonObject = new JSONObject(names);
+    String lang = getLang(locale);
+    Object name = getName(jsonObject, lang);
+    if (name == null) {
+      lang = getLang(localeConfigService.getDefaultLocaleConfig().getLocale());
+      name = getName(jsonObject, lang);
+      if (name == null) {
+        lang = jsonObject.keys().next();
+        name = getName(jsonObject, lang);
+      }
+    }
+    item.setName(String.valueOf(name));
+    return item;
+  }
+
+  @Override
+  public List<SidebarItem> getDefaultItems() {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public boolean itemExists(SidebarItem item, String username) {
+    return true;
+  }
+
+  private String getLang(Locale locale) {
+    return locale.toLanguageTag().replace("-", "_");
+  }
+
+  private Object getName(JSONObject jsonObject, String lang) {
+    try {
+      return jsonObject.get(lang);
+    } catch (JSONException e) {
+      return null;
+    }
+  }
+
+}
