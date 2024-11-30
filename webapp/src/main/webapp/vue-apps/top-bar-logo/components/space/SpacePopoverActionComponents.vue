dd4a8d61cba0c8841f30c28dad25bbf3b35f8a26
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-11-30 12:33:32 +0100

    feat: Adjust Topbar Site and Company display switch config - MEED-7782 - Meeds-io/MIPs#159
    This change will reuse the Admin UI configuration in order to display or not the company and site names with associated image/icon for each element.

--- webapp/src/main/webapp/vue-apps/top-bar-logo/components/space/SpacePopoverActionComponents.vue
+++ webapp/src/main/webapp/vue-apps/top-bar-logo/components/space/SpacePopoverActionComponents.vue
@@ -9,7 +9,7 @@
   </div>
 </template>
 <script>
-import { spaceTitleActionComponents } from '../extension.js';
+import { spaceTitleActionComponents } from '../../extension.js';
 export default {
   computed: {
     enabledComponents() {
