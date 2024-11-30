acad89165c83df6a585b962d3b0f9b573ab17b65
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-11-30 12:33:32 +0100

    feat: Adjust Topbar Site and Company display switch config - MEED-7782 - Meeds-io/MIPs#159
    This change will reuse the Admin UI configuration in order to display or not the company and site names with associated image/icon for each element.

---
+++ webapp/src/main/webapp/vue-apps/top-bar-logo/initComponents.js
@@ -0,0 +1,23 @@
+import TopBarLogo from './components/TopBarLogo.vue';
+import CompanyName from './components/CompanyName.vue';
+import SiteName from './components/SiteName.vue';
+
+import SpaceFavoriteAction from '../spaces-list/components/SpaceFavoriteAction.vue';
+import SpaceLogoBanner from './components/space/SpaceLogoBanner.vue';
+import SpacePopoverActionComponents from './components/space/SpacePopoverActionComponents.vue';
+import SpaceHostsDrawer from './components/space/SpaceHostsDrawer.vue';
+
+
+const components = {
+  'top-bar-logo': TopBarLogo,
+  'top-bar-logo-company-name': CompanyName,
+  'top-bar-logo-site-name': SiteName,
+  'space-logo-banner': SpaceLogoBanner,
+  'space-popover-action-component': SpacePopoverActionComponents,
+  'space-hosts-drawer': SpaceHostsDrawer,
+  'space-favorite-action': SpaceFavoriteAction,
+};
+
+for (const key in components) {
+  Vue.component(key, components[key]);
+}
