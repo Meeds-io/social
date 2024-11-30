bd33044f6f77cb8ec2f4829844eff6966a15d06d
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-11-30 12:33:32 +0100

    feat: Adjust Topbar Site and Company display switch config - MEED-7782 - Meeds-io/MIPs#159
    This change will reuse the Admin UI configuration in order to display or not the company and site names with associated image/icon for each element.

---
+++ webapp/src/main/webapp/vue-apps/top-bar-logo/components/SiteName.vue
@@ -0,0 +1,33 @@
+<template>
+  <space-logo-banner v-if="$root.displaySite && $root.spaceId" />
+  <div v-else-if="$root.displaySite" class="d-inline-flex">
+    <a
+      v-if="$root.displaySiteLogo"
+      :href="$root.portalPath"
+      :aria-label="$t('space.avatar.href.title',{0: $root.logoTitle})">
+      <v-list-item-avatar 
+        id="UserHomePortalLink"
+        size="36"
+        class="ma-0"
+        tile>
+        <v-icon size="28">{{ $root.siteIcon || 'fa-globe' }}</v-icon>
+      </v-list-item-avatar>
+    </a>
+    <a
+      v-if="$root.displaySiteTitle"
+      :href="$root.portalPath"
+      :class="$root.displaySiteLogo && 'ms-4'"
+      class="align-self-center brandingContainer">
+      <div class="siteTitle text-body menu-text-color font-weight-bold text-truncate">
+        {{ $root.siteTitle }}
+      </div>
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  computed: {
+    
+  },
+};
+</script>
