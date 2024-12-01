cbd7326159d16370f924914a92aa4bed3bc06a23
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-12-01 05:53:42 +0100

    feat: Rename Hamburger Menu to Sidebar Portlet - Meeds-io/MIPs#159

--- webapp/webpack.common.js
+++ webapp/webpack.common.js
@@ -30,7 +30,7 @@ let config = {
     register: './src/main/webapp/vue-apps/user-register/main.js',
     registerOnboarding: './src/main/webapp/vue-apps/user-register-onboarding/main.js',
     spacesAdministration: './src/main/webapp/vue-apps/spaces-administration/main.js',
-    hamburgerMenu: './src/main/webapp/vue-apps/hamburger-menu/main.js',
+    sidebar: './src/main/webapp/vue-apps/sidebar/main.js',
     topBarNotification: './src/main/webapp/vue-apps/notification-top-bar/main.js',
     gettingStarted: './src/main/webapp/vue-apps/getting-started/main.js',
     externalSpacesList: './src/main/webapp/vue-apps/external-spaces-list/main.js',
