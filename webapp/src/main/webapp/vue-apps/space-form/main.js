4160831b945e51cbb6925bf1c23c9c25b01f578c
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2024-12-03 11:18:33 +0100

    fix: Display Space creation button in Sidebar for allowed users only - MEED-7883 - Meeds-io/MIPs#159

--- webapp/src/main/webapp/vue-apps/space-form/main.js
+++ webapp/src/main/webapp/vue-apps/space-form/main.js
@@ -17,7 +17,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 import './initComponents.js';
-import './services.js';
 
 // get overridden components if exists
 if (extensionRegistry) {
