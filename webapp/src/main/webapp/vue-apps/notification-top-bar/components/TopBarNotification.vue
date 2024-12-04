3344ef823f70df0deeefb977e1b8304c46d48960
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-12-04 10:52:07 +0100

    fix: Refresh Notification Extensions when loading is deferred - MEED-7896 - Meeds-io/MIPs#159 (#4230)
    Prior to this change, when the Notification Extensions are loaded in a
    deferred way, the associated extension of a notification type didn't
    refresh to get the right associated Notification Extension due to
    missing Vue Listener on Vue Object attributes.
    This change will force the refresh by introducing a listener event
    triggered when the extensions list is refreshed.
    In addition, a loading visual effect is added to notification button to
    enhance user UX while opening the notifications Drawer.

--- webapp/src/main/webapp/vue-apps/notification-top-bar/components/TopBarNotification.vue
+++ webapp/src/main/webapp/vue-apps/notification-top-bar/components/TopBarNotification.vue
@@ -23,9 +23,10 @@
     <v-flex>
       <v-layout>
         <v-btn
+          :title="$t('UIIntranetNotificationsPortlet.title.notifications')"
+          :loading="loading"
           icon
           class="text-xs-center"
-          :title="$t('UIIntranetNotificationsPortlet.title.notifications')"
           @click="openDrawer">
           <v-badge
             :value="badge > 0"
@@ -50,6 +51,7 @@ export default {
   data: () => ({
     badge: 0,
     open: false,
+    loading: false,
   }),
   watch: {
     open() {
@@ -73,12 +75,14 @@ export default {
   methods: {
     openDrawer() {
       this.$root.initialized = false;
+      this.loading = true;
       this.$root.lastLoadedNotificationIndex = 0;
       window.require(['SHARED/notificationExtensions'], () => {
-        Promise.all([
-          this.$utils.includeExtensions('NotificationPopoverExtension'),
-          this.$utils.includeExtensions('NotificationExtension')
-        ]).then(() => this.open = true);
+        Promise.resolve(this.$utils.includeExtensions('NotificationExtension'))
+          .then(() => {
+            this.open = true;
+            this.loading = false;
+          });
       });
     },
     updateBadgeByEvent(event) {
