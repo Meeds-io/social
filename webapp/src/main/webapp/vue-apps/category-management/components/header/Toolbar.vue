afa6c70568cfee3735f9c329e276342b03699779
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-12-18 16:35:55 +0100

    feat: Allow to Add/Update/Delete a category - MEED-7923 - Meeds-io/MIPs#170 (#4280)
    this change will add Vue Components to edit, add and delete categories

--- webapp/src/main/webapp/vue-apps/category-management/components/header/Toolbar.vue
+++ webapp/src/main/webapp/vue-apps/category-management/components/header/Toolbar.vue
@@ -8,6 +8,7 @@
   modify it under the terms of the GNU Lesser General Public
   License as published by the Free Software Foundation; either
   version 3 of the License, or (at your option) any later version.
+
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
@@ -33,7 +34,7 @@
         :aria-label="$t('catagoryManagement.add')"
         :class="$root.isMobile && 'px-0'"
         class="btn btn-primary text-truncate"
-        @click="$root.$emit('space-templates-name-open')">
+        @click="$root.$emit('category-form-open')">
         <v-icon
           size="18">
           fa-plus
