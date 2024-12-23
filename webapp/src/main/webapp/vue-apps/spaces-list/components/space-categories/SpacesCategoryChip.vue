c89807778e34d77e57678d6a2bee0d7852b241e4
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2024-12-23 18:56:49 +0100

    feat: Add Spaces Filtering by Category - MEED-7977 - Meeds-io/MIPs#170 (#4298)
    This change will allow to display spaces list by filtering by category
    tree.

---
+++ webapp/src/main/webapp/vue-apps/spaces-list/components/space-categories/SpacesCategoryChip.vue
@@ -0,0 +1,51 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2024 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-chip
+    :outlined="!selected"
+    color="primary"
+    class="text-truncate"
+    @click="$root.selectedCategoryId = category.id">
+    <v-card
+      class="text-truncate"
+      :class="selected && 'white--text' || 'text-color'"
+      max-width="150"
+      flat>
+      {{ category.name }}
+    </v-card>
+  </v-chip>
+</template>
+<script>
+export default {
+  props: {
+    category: {
+      type: Object,
+      default: null,
+    },
+    selected: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
