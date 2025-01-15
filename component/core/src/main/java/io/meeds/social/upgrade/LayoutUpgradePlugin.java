f0902c05f3a4fdec0e5ac230b9602e2ea6585dd9
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-01-15 06:23:21 +0100

    feat: Allow to upgrade a site properties without upgrading layout - MEED-8089 - Meeds-io/MIPs#173

--- component/core/src/main/java/io/meeds/social/upgrade/LayoutUpgradePlugin.java
+++ component/core/src/main/java/io/meeds/social/upgrade/LayoutUpgradePlugin.java
@@ -115,7 +115,7 @@ public class LayoutUpgradePlugin extends UpgradeProductPlugin {
                  portalName);
       } else {
         boolean fullyUpgraded = true;
-        if (upgrade.isUpdatePortalConfig()) {
+        if (upgrade.isUpdatePortalConfig() || upgrade.isUpdatePortalProperties()) {
           fullyUpgraded = upgradePortalLayout(upgrade, portalType, portalName) && fullyUpgraded;
         }
         if (upgrade.isUpdatePageLayout()) {
