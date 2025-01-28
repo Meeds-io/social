5f042109d6e8f37210f9179ae7ecd500d9d21066
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-01-28 19:28:23 +0100

    feat: Move System Addon Containers as UIPortalApplication extensions - MEED-8281 - Meeds-io/MIPs#175 (#4385)
    This change will allow to centralize the system apps and divs to be
    served by `UIPortalApplication` rather than AddonContainers added inside
    the `sharedlayout.xml` and `portal.xml` of standalone sites.

--- webapp/src/main/webapp/js/BodyScrollListener.js
+++ webapp/src/main/webapp/js/BodyScrollListener.js
@@ -26,21 +26,6 @@ function() {
       siteBody.classList.add('overflow-x-hidden');
       siteBody.style.maxHeight = bodyMaxHeight;
       siteBody.setAttribute('scroll-control', 'true');
-      const middleBar = document.querySelector('.MiddleToolBarTDContainer .MiddleToolBar') || document.querySelector('#MiddleToolBarChildren');
-      if (middleBar) {
-        const shadowBox = document.createElement('div');
-        shadowBox.id = 'TopBarBoxShadow';
-        shadowBox.style.boxShadow = '0 6px 4px -4px rgb(0 0 0 / 30%)';
-        shadowBox.style.position = 'absolute';
-        shadowBox.style.width = '100vw';
-        shadowBox.style.height = '56px';
-        shadowBox.style.top = 0;
-        shadowBox.style.left = 0;
-        shadowBox.style.right = 0;
-        shadowBox.style.zIndex = -1;
-        shadowBox.style.visibility = 'hidden';
-        middleBar.appendChild(shadowBox);
-      }
       siteBody.addEventListener('scroll', controlBodyScrollClass, false);
       controlBodyScrollClass();
     }
@@ -48,16 +33,9 @@ function() {
 
   function controlBodyScrollClass() {
     const siteBody = document.querySelector(getScrollableSelector()) || document.querySelector('#UIPageBody');
-    const topBarBoxShadow = document.querySelector('#TopBarBoxShadow');
     if(siteBody.scrollTop) {
-      if (topBarBoxShadow && topBarBoxShadow.style.visibility === 'hidden') {
-        document.querySelector('#TopBarBoxShadow').style.visibility = '';
-      }
       siteBody.classList.add('site-scroll-top');
     } else {
-      if (topBarBoxShadow && topBarBoxShadow.style.visibility !== 'hidden') {
-        document.querySelector('#TopBarBoxShadow').style.visibility = 'hidden';
-      }
       siteBody.classList.remove('site-scroll-top');
     }
   }
