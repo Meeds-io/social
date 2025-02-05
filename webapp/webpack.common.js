dbd5039ec56ef29338953eca068fdba33de9d8a6
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2025-02-05 11:39:18 +0100

    feat: Add a new portlet for listing admins - MEED-8235 - Meeds-io/MIPs#177 (#4406)
    This change will add a new portlet for listing admins.

--- webapp/webpack.common.js
+++ webapp/webpack.common.js
@@ -95,7 +95,8 @@ let config = {
     breadcrumb: './src/main/webapp/vue-apps/breadcrumb/main.js',
     complementaryFilter: './src/main/webapp/vue-apps/complementary-filter/main.js',
     organizationalChart: './src/main/webapp/vue-apps/organizational-chart/main.js',
-    organizationalChartExtension: './src/main/webapp/vue-apps/organizational-chart/extensions/main.js'
+    organizationalChartExtension: './src/main/webapp/vue-apps/organizational-chart/extensions/main.js',
+    admins: './src/main/webapp/vue-apps/platformAdminsWidget/main.js',
   },
   module: {
     rules: [
