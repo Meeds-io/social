b2d3534df67b0fb7d5b71de3528c0b50072c022a
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2025-02-06 10:42:46 +0100

    feat: Move access admin page - MEED-8236 - Meeds-io/MIPs#177   (#4419)
    This change will move access admin page.

--- webapp/src/main/webapp/vue-apps/platform-access/services.js
+++ webapp/src/main/webapp/vue-apps/platform-access/services.js
@@ -1,8 +1,8 @@
 /*
  * This file is part of the Meeds project (https://meeds.io/).
- * 
+ *
  * Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
- * 
+ *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
@@ -11,22 +11,16 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-import * as languageSettingService from './js/LanguageSettingService.js';
-import * as navigationConfigurationService from './js/NavigationConfigurationService.js';
+import * as registrationService from './js/RegistrationService.js';
 
-if (!Vue.prototype.$languageSettingService) {
-  window.Object.defineProperty(Vue.prototype, '$languageSettingService', {
-    value: languageSettingService,
-  });
-}
-if (!Vue.prototype.$navigationConfigurationService) {
-  window.Object.defineProperty(Vue.prototype, '$navigationConfigurationService', {
-    value: navigationConfigurationService,
+if (!Vue.prototype.$registrationService) {
+  window.Object.defineProperty(Vue.prototype, '$registrationService', {
+    value: registrationService,
   });
 }
