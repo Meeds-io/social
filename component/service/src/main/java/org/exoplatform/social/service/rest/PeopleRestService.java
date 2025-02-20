7b84cedce535e66f9b54859af7e509760b8c377f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-02-20 14:42:37 +0100

    feat: Move Utils Component from Portal to Commons - Meeds-io/MIPs#181

--- component/service/src/main/java/org/exoplatform/social/service/rest/PeopleRestService.java
+++ component/service/src/main/java/org/exoplatform/social/service/rest/PeopleRestService.java
@@ -21,6 +21,7 @@ import org.apache.commons.text.StringEscapeUtils;
 import org.apache.commons.lang3.StringUtils;
 
 import org.exoplatform.commons.utils.ListAccess;
+import org.exoplatform.commons.utils.TimeConvertUtils;
 import org.exoplatform.container.ExoContainer;
 import org.exoplatform.container.ExoContainerContext;
 import org.exoplatform.container.PortalContainer;
@@ -52,7 +53,6 @@ import org.exoplatform.social.rest.impl.user.UserRest;
 import org.exoplatform.social.service.rest.api.models.IdentityNameList;
 import org.exoplatform.social.service.rest.api.models.IdentityNameList.Option;
 import org.exoplatform.social.service.rest.api.models.PeopleInfo;
-import org.exoplatform.webui.utils.TimeConvertUtils;
 
 import javax.annotation.security.RolesAllowed;
 import jakarta.servlet.http.HttpServletRequest;
