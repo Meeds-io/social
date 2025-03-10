42ec2355eeb0b4c442eda9daf8e1f503a8d8dac7
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-03-10 11:12:44 +0100

    feat: Rework Space Banner Portlet - MEED-8543 - Meeds-io/meeds#3073 This change will rework the Space Banner portlet in order to use the same behavior as Image Portlet with landscape mode.

--- webapp/src/main/webapp/vue-apps/space-banner/initComponents.js
+++ webapp/src/main/webapp/vue-apps/space-banner/initComponents.js
@@ -1,7 +1,9 @@
 import SpaceBanner from './components/SpaceBanner.vue';
+import SpaceBannerSettingButtons from './components/SpaceBannerSettingButtons.vue';
 
 const components = {
   'space-banner': SpaceBanner,
+  'space-banner-setting-buttons': SpaceBannerSettingButtons,
 };
 
 for (const key in components) {
