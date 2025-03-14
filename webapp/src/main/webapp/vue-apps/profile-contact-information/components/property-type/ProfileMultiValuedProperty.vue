bee3bfc54e8442588e2fc1b7dafb92d2e8f63fc9
MayTekayaa <mtekaya@exoplatform.com>
2025-03-14 12:08:35 +0100

    MEED-8588 Fix the display of the profile attributes label

--- webapp/src/main/webapp/vue-apps/profile-contact-information/components/property-type/ProfileMultiValuedProperty.vue
+++ webapp/src/main/webapp/vue-apps/profile-contact-information/components/property-type/ProfileMultiValuedProperty.vue
@@ -122,6 +122,9 @@ export default {
       default: false,
     },
   },
+  data: () => ({
+    lang: eXo?.env.portal.language
+  }),
   computed: {
     userProperty() {
       return this.property.propertyType === 'user';
