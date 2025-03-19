6e2aff848ba113993f51ca4b9f3e7973221d93f3
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-03-19 12:37:13 +0100

    fix: Ensure to update unread badge in sidebar - MEED-8598 - Meeds-io/meeds#3085 Prior to this change, the unread badge is sometimes not updated in UI. This is due to the Vue binding listeners not initialized on space.unread attribute when it's empty. This change will ensure to use a centralized variable which displays the space badge in all sidebar components.

--- webapp/src/main/webapp/vue-apps/sidebar/components/SidebarButton.vue
+++ webapp/src/main/webapp/vue-apps/sidebar/components/SidebarButton.vue
@@ -45,41 +45,5 @@ export default {
         && Object.values(this.$root.unreadPerSpace).reduce((sum, v) => sum += v, 0) > 0;
     },
   },
-  mounted() {
-    document.addEventListener('notification.unread.item', this.handleUpdatesFromWebSocket);
-    document.addEventListener('notification.read.item', this.handleUpdatesFromWebSocket);
-    document.addEventListener('notification.read.allItems', this.handleUpdatesFromWebSocket);
-  },
-  beforeDestroy() {
-    document.removeEventListener('notification.unread.item', this.handleUpdatesFromWebSocket);
-    document.removeEventListener('notification.read.item', this.handleUpdatesFromWebSocket);
-    document.removeEventListener('notification.read.allItems', this.handleUpdatesFromWebSocket);
-  },
-  methods: {
-    handleUpdatesFromWebSocket(event) {
-      const data = event?.detail;
-      const wsEventName = data?.wsEventName || '';
-      let spaceWebNotificationItem = data?.message?.spaceWebNotificationItem || data?.message?.spacewebnotificationitem;
-      if (spaceWebNotificationItem?.length) {
-        spaceWebNotificationItem = JSON.parse(spaceWebNotificationItem);
-      }
-      const spaceId = spaceWebNotificationItem?.spaceId;
-      if (wsEventName === 'notification.unread.item') {
-        if (spaceId && this.$root.unreadPerSpace[spaceId]) {
-          this.$root.unreadPerSpace[spaceId]++;
-        } else {
-          this.$set(this.$root.unreadPerSpace, spaceId, 1);
-        }
-      }  else if (wsEventName === 'notification.read.item') {
-        if (spaceId && this.$root.unreadPerSpace[spaceId] > 0) {
-          this.$root.unreadPerSpace[spaceId]--;
-        }
-      } else if (wsEventName === 'notification.read.allItems') {
-        if (spaceId && this.$root.unreadPerSpace[spaceId] > 0) {
-          this.$root.unreadPerSpace[spaceId] = 0;
-        }
-      }
-    },
-  }
 };
 </script>
