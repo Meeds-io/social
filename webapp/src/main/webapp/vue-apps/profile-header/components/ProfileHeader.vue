20297b37533fc660ed7db830dd72330a92d4dc64
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2025-03-21 14:22:59 +0100

    feat: Allow setting fixed banner height in profile header settings - MEED-8643 - Meeds-io/MIPs#184 (#4573)
    Allow setting fixed banner height in profile header settings

--- webapp/src/main/webapp/vue-apps/profile-header/components/ProfileHeader.vue
+++ webapp/src/main/webapp/vue-apps/profile-header/components/ProfileHeader.vue
@@ -1,18 +1,22 @@
 <template>
-  <v-app :class="{ 'profileHeaderOwner': owner, 'profileHeaderOther': !owner }">
+  <v-app
+    :class="{ 'profileHeaderOwner': owner, 'profileHeaderOther': !owner }">
     <v-hover>
       <div slot-scope="{ hover }" class="application-body">
         <v-card
           :max-height="bannerMaxHeight"
+          :height="bannerHeight"
+          min-height="60"
           class="d-flex position-relative overflow-hidden"
           tile
           flat>
           <v-img
             :src="user?.banner"
             :max-height="bannerMaxHeight"
+            :height="bannerHeight"
+            min-height="60"
             :alt="$t('profileHeader.banner.alt')"
             width="100%"
-            height="auto"
             class="profileBannerImg application-border-radius-top"
             lazy />
           <profile-header-banner-button
@@ -26,7 +30,8 @@
             @open-settings="openHeaderSettings" />
         </v-card>
         <v-card
-          class="d-flex flex-column flex-md-row border-color px-4" 
+          class="d-flex flex-column border-color px-4"
+          :class="{ 'flex-md-row': !containerMD && containerBasedBreakpoints || !containerBasedBreakpoints }"
           flat
           tile>
           <v-card
@@ -53,7 +58,8 @@
             </v-card>
           </v-card>
           <v-card
-            class="d-flex flex-column flex-sm-row mb-2 flex-grow-1"
+            class="d-flex flex-column mb-2 flex-grow-1"
+            :class="{'flex-sm-row': !containerSM && containerBasedBreakpoints || !containerBasedBreakpoints}"
             flat
             tile>
             <profile-header-text
@@ -82,12 +88,13 @@
       @input="uploadImage" />
     <profile-header-settings-drawer
       v-if="isAdmin"
-      :save-settings-url="$root.saveSettingsUrl"
+      :save-settings-url="$root.settings.saveSettingsUrl"
       :saved-settings="{
         avatarMaxSize: this.avatarMaxSize,
         avatarMinSize: this.avatarMinSize,
         bannerMaxHeight: this.bannerMaxHeight,
-        displayOption: this.displayOption
+        displayOption: this.displayOption,
+        bannerHeight: this.bannerHeight
       }"
       @updated="headerSettingsUpdated"
       ref="headerSettingsDrawer" />
@@ -106,29 +113,36 @@ export default {
     owner: eXo.env.portal.profileOwner === eXo.env.portal.userName,
     errorMessage: null,
     imageType: null,
-    avatarSize: '15vw'
+    avatarSize: '15vw',
+    appWidth: null
   }),
   computed: {
-    small() {
-      return this.$vuetify.breakpoint.mdAndDown;
+    containerSM() {
+      return this.appWidth < this.$vuetify.breakpoint.thresholds.md;
     },
-    large() {
-      return this.$vuetify.breakpoint.lgAndUp;
+    containerMD() {
+      return this.appWidth < this.$vuetify.breakpoint.thresholds.sm;
     },
     avatarMaxSize() {
-      return this.$root?.avatarMaxSize;
+      return this.$root?.settings?.avatarMaxSize;
     },
     avatarMinSize() {
-      return this.$root?.avatarMinSize;
+      return this.$root?.settings?.avatarMinSize;
     },
     bannerMaxHeight() {
-      return this.$root?.bannerMaxHeight;
+      return this.$root?.settings?.bannerMaxHeight;
+    },
+    bannerHeight() {
+      return this.$root?.settings?.bannerHeight;
     },
     useActions() {
-      return this.$root?.useActions;
+      return this.$root?.settings?.useActions;
+    },
+    containerBasedBreakpoints() {
+      return this.$root?.settings?.containerBasedBreakpoints;
     },
     displayOption() {
-      return this.$root.displayOption;
+      return this.$root?.settings?.displayOption;
     },
     maxUploadSizeInBytes() {
       return this.maxUploadSize * 1024 * 1024;
@@ -165,7 +179,11 @@ export default {
       }
     },
   },
+  created() {
+    window.addEventListener('resize', this.calculateAppWidth);
+  },
   mounted() {
+    this.calculateAppWidth();
     this.refresh();
     document.addEventListener('userModified', event => {
       if (event && event.detail && event.detail !== this.user) {
@@ -248,11 +266,15 @@ export default {
       this.$refs.headerSettingsDrawer.open();
     },
     headerSettingsUpdated(settings) {
-      this.$root.displayOption = settings.displayOption;
-      this.$root.avatarMinSize = settings.avatarMinSize;
-      this.$root.avatarMaxSize = settings.avatarMaxSize;
-      this.$root.bannerMaxHeight = settings.bannerMaxHeight;
+      this.$root.settings.displayOption = settings.displayOption;
+      this.$root.settings.avatarMinSize = settings.avatarMinSize;
+      this.$root.settings.avatarMaxSize = settings.avatarMaxSize;
+      this.$root.settings.bannerMaxHeight = settings.bannerMaxHeight;
+      this.$root.settings.bannerHeight = settings.bannerHeight;
       this.$refs.headerSettingsDrawer.close();
+    },
+    calculateAppWidth() {
+      this.appWidth = document.getElementById('ProfileHeader')?.clientWidth;
     }
   },
 };
