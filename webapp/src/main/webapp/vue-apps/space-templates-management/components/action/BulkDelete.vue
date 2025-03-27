cc74ff2065e51ba25ec432f5f7dbc077a7d4b386
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-03-27 15:42:13 +0100

    feta: Delete multiple space templates - MEED-8414 - Meeds-io/MIPs#180 (#4593)
    This change will allow administrators to bulk delete space templates.

---
+++ webapp/src/main/webapp/vue-apps/space-templates-management/components/action/BulkDelete.vue
@@ -0,0 +1,76 @@
+<!--
+
+   This file is part of the Meeds project (https://meeds.io/).
+
+   Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 3 of the License, or (at your option) any later version.
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public License
+   along with this program; if not, write to the Free Software Foundation,
+   Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+ -->
+<template>
+  <div class="d-inline">
+    <v-tooltip :disabled="!$root.systemSelectedSpaceTemplates" bottom>
+      <template #activator="{ on, attrs }">
+        <div
+          v-on="on"
+          v-bind="attrs">
+          <v-btn
+            :disabled="$root.isBulkProcessing || $root.systemSelectedSpaceTemplates"
+            color="error"
+            elevation="0"
+            outlined
+            @click="openConfirmDialog">
+            <v-icon size="16" class="me-2">fa-trash</v-icon>
+            {{ $t('spaceTemplate.label.delete') }}
+          </v-btn>
+        </div>
+      </template>
+      <span>{{ $t('spaceTemplate.label.system.noDelete') }}</span>
+    </v-tooltip>
+    <confirm-dialog
+      ref="dialog"
+      :title="$t('spaceTemplate.label.bulk.deleteConfirmTitle')"
+      :message="$t('spaceTemplate.label.bulk.deleteConfirmMessage')"
+      :ok-label="$t('spaceTemplate.label.confirm')"
+      :cancel-label="$t('spaceTemplate.label.cancel')"
+      @ok="deleteSpaceTemplates"
+      @closed="close" />
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    openConfirmDialog() {
+      window.setTimeout(() => this.$refs.dialog.open(), 200);
+    },
+    deleteSpaceTemplates() {
+      this.$root.applyOperationInBulk(
+        spaceTemplate => {
+          if (!spaceTemplate.system) {
+            return this.$spaceTemplateService.deleteSpaceTemplate(spaceTemplate.id);
+          }
+          return Promise.resolve();
+        },
+        null,
+        () => {
+          this.$root.$emit('alert-message', this.$root.$t('spaceTemplate.label.bulk.delete.success'), 'success');
+          this.$root.$emit('space-templates-list-refresh');
+        });
+    },
+    close() {
+      window.setTimeout(() => this.dialog = false, 200);
+    },
+  },
+};
+</script>
