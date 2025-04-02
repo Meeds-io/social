fea55d4f1debab2acc208aa1928dc2d47a301696
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-04-02 14:04:46 +0200

    feat: Import / Export Space templates - MEED-8423 - Meeds-io/MIPs#180 (#4600)

--- webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
+++ webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
@@ -147,7 +147,7 @@ export default {
           uploadId: this.uploadId,
         });
         this.importFinished = true;
-        this.$root.$emit('space-template-saved');
+        this.$root.$emit('space-templates-created');
       } catch (error) {
         if (error.message === 'databind.notMatchType') {
           this.$root.$emit('alert-message', this.$t('spaceTemplate.label.exception.notMatchType'), 'error');
