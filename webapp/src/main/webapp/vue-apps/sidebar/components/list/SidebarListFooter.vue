8a86972c8299df5012a5216e1625b42dcb6d737a
Jihed Chabbeh <31882754+Jihed525@users.noreply.github.com>
2025-04-11 15:05:07 +0100

    fix: Set an empty alt to the profile picture - MEED-8753 - Meeds-io/meeds#3076  (#4639)
    Accessibility- Images C 1.2 : The Profile picture is a decoration image
    which must have an empty alternative text

--- webapp/src/main/webapp/vue-apps/sidebar/components/list/SidebarListFooter.vue
+++ webapp/src/main/webapp/vue-apps/sidebar/components/list/SidebarListFooter.vue
@@ -67,7 +67,7 @@
                 size="24">
                 <img
                   :src="avatarUrl"
-                  :alt="userName"
+                  alt=""
                   height="24"
                   width="24"
                   contain>
