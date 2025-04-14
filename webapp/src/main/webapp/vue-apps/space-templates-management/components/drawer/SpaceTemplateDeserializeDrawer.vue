916ad57df3e6f11cfc5cb6a4a6a9dc938f756e77
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-04-14 14:07:19 +0200

    fix: Fix opening the deserialize drawer without reloading the page - MEED-8756 - Meeds-io/MIPs#180 (#4644)
    This PR will fix opening the deserialize drawer without reloading the
    page

--- webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
+++ webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
@@ -23,7 +23,8 @@
     ref="deserializeDrawer"
     body-classes="hide-scroll decrease-z-index-more"
     allow-expand
-    right>
+    right
+    @closed="close">
     <template #title>
       {{ $t('spaceTemplate.label.templateToImport') }}
     </template>
@@ -134,6 +135,7 @@ export default {
       this.importFinished = false;
       this.fileName = '';
       this.uploadId = '';
+      this.$root.$emit('reset-uploaded-file');
       this.$refs.deserializeDrawer.close();
     },
     async deserializeSpaceTemplates() {
