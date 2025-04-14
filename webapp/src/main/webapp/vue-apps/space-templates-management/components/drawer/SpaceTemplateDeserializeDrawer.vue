dc24968ccea34c001fec656b49a84adf068e4a6d
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-04-14 14:36:08 +0200

    fix: Display warning when admin importing wrong template object type - MEED-8802 - Meeds-io/MIPs#180 (#4645)

--- webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
+++ webapp/src/main/webapp/vue-apps/space-templates-management/components/drawer/SpaceTemplateDeserializeDrawer.vue
@@ -151,8 +151,10 @@ export default {
         this.importFinished = true;
         this.$root.$emit('space-templates-list-refresh');
       } catch (error) {
-        if (error.message === 'databind.notMatchType') {
+        if (String(error).indexOf('databind.notMatchType') >= 0) {
           this.$root.$emit('alert-message', this.$t('spaceTemplate.label.exception.notMatchType'), 'error');
+        } else {
+          this.$root.$emit('alert-message', String(error) , 'error');
         }
       } finally {
         this.loading = false;
