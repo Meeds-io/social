55157de6286036bbff7b0b7235d4649ad7eb8ed1
MayTekayaa <mtekaya@exoplatform.com>
2025-04-15 15:22:41 +0100

    MEED-8760 Fix the bad mention user display

--- webapp/src/main/webapp/js/ExtendedDomPurify.js
+++ webapp/src/main/webapp/js/ExtendedDomPurify.js
@@ -22,7 +22,7 @@
   ExtendedDomPurify.prototype.purify = function(content) {
     content = content.replace(/<div> <\/div>/g, '<div><br><\/div>');
     content = content.trim().replace(/>[ \n]+</g, '> <').replace(/  /g, '&nbsp;&nbsp;');
-    const pureHtml = DOMPurify.sanitize(Autolinker.link(content, {
+    let outputHtml = /<a\s/i.test(content) ? content : Autolinker.link(content, {
       email: false,
       replaceFn : function (match) {
         switch(match.getType()) {
@@ -39,7 +39,8 @@
             return true;
         }
       }
-    }), {
+    });
+    let pureHtml = DOMPurify.sanitize(outputHtml, {
       USE_PROFILES: {
         html: true,
         SAFE_FOR_TEMPLATES: true,
