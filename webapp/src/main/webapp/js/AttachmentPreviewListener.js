a4894cd076cf8fd1b269f6ca9ebfe8d7acd9b7fe
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-04-18 15:00:18 +0100

    feat: Make meeds attachment preview extensible - EXO-77966
    Meeds attachment preview only supports images.
    this change will make it extensible to support other content types and create a default extension for images.

--- webapp/src/main/webapp/js/AttachmentPreviewListener.js
+++ webapp/src/main/webapp/js/AttachmentPreviewListener.js
@@ -1,6 +1,5 @@
 function() {
     document.addEventListener('open-attachments-preview', event => {
-      console.log('open-attachments-preview', event);
       window.require(["SHARED/AttachmentPreview"], preview => {
           preview.init(event);
       });
