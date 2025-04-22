f9e574437e8dc5faefaee2d2a740c43c5e7c42a7
Ali HAMDI <ahamdi@exoplatform.com>
2025-04-22 16:00:12 +0100

    feat: Use Image preview dialog for image messages - MEED-8681 - Meeds-io/MIPs#163 (#4677)
    Use thumbnail url as a property of the attachment otherwise the download
    url

--- webapp/src/main/webapp/vue-apps/attachments-preview/components/preview/AttachmentsPreviewDialog.vue
+++ webapp/src/main/webapp/vue-apps/attachments-preview/components/preview/AttachmentsPreviewDialog.vue
@@ -31,7 +31,7 @@
         :max-height="!isMobile && '80vh' || '75vh'"
         class="transparent">
         <v-carousel
-          :id="`previewCarousel-${objectType}`"
+          :id="`previewCarousel`"
           ref="attachmentsCarousel"
           v-model="currentAttachmentId"
           :show-arrows-on-hover="!isMobile"
@@ -128,8 +128,6 @@ export default {
     },
     openPreview(event) {
       const attachment = event?.detail;
-      this.objectType = attachment.objectType;
-      this.objectId = attachment.objectType;
       this.attachments = attachment.attachments;
       this.currentAttachmentId = attachment.id;
       this.filename = this.attachments.find(file => file.id === attachment.id).filename;
@@ -138,8 +136,6 @@ export default {
     },
     close() {
       this.dialog = false;
-      this.objectType = null;
-      this.objectId = null;
       this.attachments = null;
       this.currentAttachmentId = null;
       this.filename = null;
