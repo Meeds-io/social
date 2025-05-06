1fc186ab9c91e36a2b745abc0b3fb5fa0ab29dc9
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-05-06 14:44:33 +0100

    feat: Add Content Link Backend - MEED-8962 - Meeds-io/MIPs#192

--- component/core/src/test/java/io/meeds/social/navigation/plugin/LinkSidebarPluginTest.java
+++ component/core/src/test/java/io/meeds/social/navigation/plugin/LinkSidebarPluginTest.java
@@ -18,6 +18,10 @@
  */
 package io.meeds.social.navigation.plugin;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
---
+++ component/service/src/main/java/io/meeds/social/cms/rest/ContentLinkRest.java
@@ -0,0 +1,108 @@
+/**
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ */
+package io.meeds.social.cms.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import io.meeds.social.cms.model.ContentLink;
+import io.meeds.social.cms.model.ContentLinkList;
+import io.meeds.social.cms.model.ContentObject;
+import io.meeds.social.cms.service.ContentLinkService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+
+@RestController
+@RequestMapping("/contentLinks")
+@Tag(name = "/social/rest/contentLinks", description = "Manage Content Links")
+public class ContentLinkRest {
+
+  @Autowired
+  private ContentLinkService contentLinkService;
+
+  @GetMapping(value = "{objectType}/{objectId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @Operation(summary = "Retrieves Linked objects to a content", method = "GET")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "200", description = "Request fulfilled"),
+    @ApiResponse(responseCode = "403", description = "Forbidden"),
+  })
+  public List<ContentLink> getLinks(HttpServletRequest request,
+                                    @Parameter(description = "Object Type")
+                                    @PathVariable(name = "objectType")
+                                    String objectType,
+                                    @Parameter(description = "Object Id")
+                                    @PathVariable(name = "objectId")
+                                    String objectId,
+                                    @Parameter(description = "Field Name")
+                                    @RequestParam(name = "fieldName", required = false)
+                                    String fieldName) {
+    try {
+      return contentLinkService.getLinks(new ContentObject(objectType, objectId, fieldName),
+                                         request.getRemoteUser());
+    } catch (IllegalAccessException e) {
+      throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+    }
+  }
+
+  @PutMapping(value = "{objectType}/{objectId}", consumes = MediaType.APPLICATION_JSON_VALUE)
+  @ResponseStatus(value = HttpStatus.NO_CONTENT)
+  @Operation(summary = "Save Linked objects to a content", method = "PUT")
+  @ApiResponses(value = {
+    @ApiResponse(responseCode = "204", description = "Request fulfilled"),
+    @ApiResponse(responseCode = "403", description = "Forbidden"),
+  })
+  public void saveLinks(HttpServletRequest request,
+                        @Parameter(description = "Object Type")
+                        @PathVariable(name = "objectType")
+                        String objectType,
+                        @Parameter(description = "Object Id")
+                        @PathVariable(name = "objectId")
+                        String objectId,
+                        @Parameter(description = "Field Name")
+                        @RequestParam(name = "fieldName", required = false)
+                        String fieldName,
+                        @RequestBody
+                        ContentLinkList linkList) {
+    try {
+      contentLinkService.saveLinks(new ContentObject(objectType, objectId, fieldName),
+                                   linkList.getLinks(),
+                                   request.getRemoteUser());
+    } catch (IllegalAccessException e) {
+      throw new ResponseStatusException(HttpStatus.FORBIDDEN, e.getMessage());
+    }
+  }
+
+}
