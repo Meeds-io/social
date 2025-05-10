bee64ebf57437dcd6a8a5e5c9c284c8ac535484f
Boubaker Khanfir <boubaker.khanfir@gmail.com>
2025-05-10 12:36:05 +0100

    feat: Introduce Web Component for ContentLink - MEED-8937 - Meeds-io/MIPs#192

--- webapp/src/main/webapp/js/ExtendedDomPurify.js
+++ webapp/src/main/webapp/js/ExtendedDomPurify.js
@@ -45,7 +45,7 @@
         SAFE_FOR_TEMPLATES: true,
         svg: true
       },
-      ADD_TAGS: ["iframe"],
+      ADD_TAGS: ["iframe", "content-link"],
       ADD_ATTR: ['target', 'allow', 'allowfullscreen', 'frameborder', 'scrolling', 'v-identity-popover'],
     });
     DOMPurify.addHook('afterSanitizeAttributes', function(node) {
