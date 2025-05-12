5cd3c22b14d0df6475458dc3fcb6e52e5fa552e6
Anwar khanfir <akhanfir@exoplatform.com>
2025-05-12 10:42:45 +0100

    fix: Providing an alternative text for project manager avatars - MEED-8968 - Meeds-io/meeds#3259 Before this change, when accessing projects board, the Avatar of the project manager contains that contain aria-label= Open Full name profile. To fix this problem, add an aria-label props to the user-avatar component and pass the requested value. After this change, avatars provide information about project managers they are contains an aria-label equal to Open list of managers if there is more than one project manager. Resolves https://github.com/Meeds-io/meeds/issues/3259

--- webapp/src/main/webapp/vue-apps/common/components/UserAvatarsList.vue
+++ webapp/src/main/webapp/vue-apps/common/components/UserAvatarsList.vue
@@ -19,7 +19,7 @@
             :compact="compact"
             :margin-left="index > 0 && marginLeft || ''"
             :class="{ 'mt-n1 z-index-two': hover && compact }"
-            :alt="user?.alt"
+            :aria-label="user?.ariaLabel"
             avatar />
         </v-hover>
       </div>
@@ -39,7 +39,7 @@
             :compact="compact"
             :margin-left="index > 0 && marginLeft || ''"
             :class="{ 'mt-n1 z-index-two': hover && compact }"
-            :alt="user?.alt"
+            :aria-label="user?.ariaLabel"
             avatar />
         </v-hover>
       </div>
@@ -49,6 +49,7 @@
       :height="iconSize"
       :width="iconSize"
       :class="{ 'mt-n1 z-index-two': compact && showAnimation, 'ml-n4': compact }"
+      :aria-label="usersToDisplay[usersToDisplay.length -1]?.ariaLabel"
       fab
       depressed
       @click="$emit('open-detail')"
