2f0e3a252c08bca136aee003d6cbcf0968d52812
MayTekayaa <mtekaya@exoplatform.com>
2025-05-15 14:35:47 +0100

    MEED-9037 Hide invite users step when user not connected

--- webapp/src/main/webapp/vue-apps/space-form/components/SpaceFormDrawer.vue
+++ webapp/src/main/webapp/vue-apps/space-form/components/SpaceFormDrawer.vue
@@ -296,7 +296,8 @@ export default {
       return this.spaceTemplate?.spaceFields?.includes?.('access');
     },
     includeInvitation() {
-      return this.spaceTemplate?.spaceFields?.includes?.('invitation');
+      console.warn('eXo.env.portal.userName', !!eXo.env.portal.userName, this.spaceTemplate?.spaceFields?.includes?.('invitation'));
+      return !!eXo.env.portal.userName && this.spaceTemplate?.spaceFields?.includes?.('invitation');
     },
     propertiesStep() {
       return (this.includeName ? 1 : 0) + 1;
