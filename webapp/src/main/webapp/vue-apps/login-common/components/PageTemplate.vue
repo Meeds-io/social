4783ff968ec97a37006bb471f379333ea9b9904c
Anwar khanfir <93767376+akhanfir@users.noreply.github.com>
2025-05-15 08:42:22 +0100

    fix: Make the login background image focusable - MEED-9051 - Meeds-io/meeds#3291. (#4768)
    Before this change, when accessing the login page, the login background
    image isn't focusable so its alternative text can't be rendered by
    screen reader. To fix this problem, add a tabindex and tootltip props to
    the image. After this change, the login background image is focusable by
    adding tooltip.
    Resolves https://github.com/Meeds-io/meeds/issues/3291

--- webapp/src/main/webapp/vue-apps/login-common/components/PageTemplate.vue
+++ webapp/src/main/webapp/vue-apps/login-common/components/PageTemplate.vue
@@ -29,17 +29,23 @@
         class="d-none d-sm-flex flex-shrink-0 full-height position-relative"
         flat
         tile>
-        <v-tooltip bottom>
+        <v-tooltip bottom v-if="tooltipMessage">
           <template v-slot:activator="{ on }">
             <img
               v-on="on"
               :src="background"
               style="height: 100%;"
               tabindex="0"
-              :alt="params.loginBackgroundAltText">
+              :alt="tooltipMessage">
           </template>
-          <span>{{ params.loginBackgroundAltText || $t('UILoginForm.tooltip.backgroundImage')}}</span>
+          <span>{{ tooltipMessage }}</span>
         </v-tooltip>
+        <img
+          v-else
+          class
+          :src="background"
+          style="height: 100%;"
+          alt="">
         <v-card
           :class="background && 'position-absolute t-0'"
           min-width="100%"
@@ -121,6 +127,9 @@ export default {
     },
   },
   computed: {
+    tooltipMessage() {
+      return this.params?.loginBackgroundAltText;
+    },
     companyName() {
       return this.params?.companyName;
     },
