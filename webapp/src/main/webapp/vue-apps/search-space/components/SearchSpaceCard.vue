7f2b38b93b9b58530467349615c25e211e06e5ee
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-15 11:42:15 +0100

    feat: Review spaces search result display - MEED-8984 - Meeds-io/MIPs#193 (#4770)

--- webapp/src/main/webapp/vue-apps/search-space/components/SearchSpaceCard.vue
+++ webapp/src/main/webapp/vue-apps/search-space/components/SearchSpaceCard.vue
@@ -1,9 +1,16 @@
 <template>
   <div class="space-search-card">
     <space-card
+      :min-width="cardMinWidth"
+      :max-width="cardWidth"
+      :height="cardHeight"
+      :min-height="cardHeight"
+      :class="{'full-width': isMobile}"
       :space="result"
       :space-action-extensions="spaceActionExtensions"
-      embedded
+      display-members-count
+      hide-space-description
+      class="pa-0"
       @refresh="$emit('refresh')" />
   </div>
 </template>
@@ -21,7 +28,17 @@ export default {
   },
   data: () => ({
     spaceActionExtensions: [],
+    cardWidth: 400,
+    cardHeight: 130,
   }),
+  computed: {
+    cardMinWidth() {
+      return  this.isMobile && 220 || this.cardWidth;
+    },
+    isMobile() {
+      return this.$vuetify.breakpoint.smAndDown;
+    }
+  },
   created() {
     document.addEventListener('extension-profile-extension-action-updated', this.refreshExtensions);
     this.refreshExtensions();
