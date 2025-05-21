c68681f062440b166e6fd5c33ce046ae20b56ac9
sofyenne <shajchedli@exoplatform.com>
2025-05-21 09:55:37 +0100

    feat: Review activities search result display - MEED-8982 - Meeds-io/MIPs#193

--- webapp/src/main/webapp/vue-apps/search-activity/initComponents.js
+++ webapp/src/main/webapp/vue-apps/search-activity/initComponents.js
@@ -1,7 +1,6 @@
 import SearchActivityCard from './components/SearchActivityCard.vue';
 import ActivityFavoriteAction from '../activity-stream/components/activity/footer/actions/ActivityFavoriteAction.vue';
 
-
 const components = {
   'activity-search-result-card': SearchActivityCard,
 };
