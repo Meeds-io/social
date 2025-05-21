91bc91a9acdb5bcd94beb92f16243c2ce7c1c1a7
sofyenne <shajchedli@exoplatform.com>
2025-05-21 11:02:43 +0100

    Address review feedback

--- webapp/src/main/webapp/vue-apps/search-activity/initComponents.js
+++ webapp/src/main/webapp/vue-apps/search-activity/initComponents.js
@@ -1,10 +1,8 @@
 import SearchActivityCard from './components/SearchActivityCard.vue';
-import ActivityFavoriteAction from '../activity-stream/components/activity/footer/actions/ActivityFavoriteAction.vue';
 
 const components = {
   'activity-search-result-card': SearchActivityCard,
 };
-components['activity-favorite-action'] = ActivityFavoriteAction;
 
 
 for (const key in components) {
