10d7af59bc62b0c0a03dd31d1e37abe83438094d
Ali Hamdi <ahamdi@exoplatform.com>
2025-05-22 23:09:17 +0100

    fix: add missing expand parameter in JS identityservice - MEED-9161 - Meeds-io/MIPs#163

--- webapp/src/main/webapp/vue-apps/common/js/IdentityService.js
+++ webapp/src/main/webapp/vue-apps/common/js/IdentityService.js
@@ -11,10 +11,10 @@ export function getIdentityById(identityId, expand) {
   });
 }
 
-export function getIdentityByProviderIdAndRemoteId(providerId, remoteId) {
+export function getIdentityByProviderIdAndRemoteId(providerId, remoteId, expand) {
   const url = remoteId.includes('/') ?
-    `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/byParams?providerId=${providerId}&remoteId=${remoteId}`
-    : `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/${providerId}/${remoteId}`;
+    `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/byParams?providerId=${providerId}&remoteId=${remoteId}&expand=${expand || ''}`
+    : `${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/social/identities/${providerId}/${remoteId}?expand=${expand || ''}`;
   return fetch(url, {
     method: 'GET',
     credentials: 'include',
