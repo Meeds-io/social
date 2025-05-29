de229a708e8baf8103b9aee2c67efabdd604f4ad
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-29 10:02:37 +0100

    feat: Unify the font size of the search options filter - MEED-9033 - Meeds-io/MIPs#193 (#4845)

--- webapp/src/main/webapp/vue-apps/search/components/SearchTagSelector.vue
+++ webapp/src/main/webapp/vue-apps/search/components/SearchTagSelector.vue
@@ -4,7 +4,7 @@
     <v-chip
       v-for="(selectedTag, index) in selectedTags"
       :key="selectedTag"
-      class="text-header-color mx-1"
+      class="text-body text-header-color mx-1"
       color="primary">
       <v-icon size="16" class="pe-2">
         fas fa-hashtag
