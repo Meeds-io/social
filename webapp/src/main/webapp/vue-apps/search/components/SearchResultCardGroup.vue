ac9cbd0283a60430ec61c10e147b24e92ac5344a
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-05-30 11:55:58 +0100

    feat: Remove text transformation from the search filter options - MEED-9205 - Meeds-io/MIPs#193 (#4860)

--- webapp/src/main/webapp/vue-apps/search/components/SearchResultCardGroup.vue
+++ webapp/src/main/webapp/vue-apps/search/components/SearchResultCardGroup.vue
@@ -36,7 +36,7 @@
           <v-list-item-title class="d-flex flex-row full-width align-center pt-2">
             <p
               :title="connectorName"
-              class="flex-grow-1 title font-weight-bold pt-1 mb-0 ps-0 my-auto align-center text-start text-truncate"
+              class="flex-grow-1 title pt-1 mb-0 ps-0 my-auto align-center text-start text-truncate"
               v-sanitized-html="connectorName">
             </p>
           </v-list-item-title>
