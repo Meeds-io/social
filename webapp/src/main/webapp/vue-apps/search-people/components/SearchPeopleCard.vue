3a3a65e9313267a554b6888e7889fab14cf604c8
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-06-10 13:06:08 +0100

    feat: Add the ability to open activity/people search results in a new tab - MEED-9241 - Meeds-io/MIPs#193 (#4895)

--- webapp/src/main/webapp/vue-apps/search-people/components/SearchPeopleCard.vue
+++ webapp/src/main/webapp/vue-apps/search-people/components/SearchPeopleCard.vue
@@ -1,13 +1,13 @@
 <template>
   <v-hover v-slot="{ hover }">
     <v-card
-      @click="openUserProfile()"
       class="border-box-sizing fill-height"
       width="350"
       :height="80"
       :min-height="80"
       :max-height="80"
       :elevation="hover && 3 || 0"
+      :href="userProfileUrl"
       outlined
       flat
       rounded>
@@ -42,16 +42,16 @@ export default {
     isMobile() {
       return this.$vuetify.breakpoint.mobile;
     },
+    userProfileUrl() {
+      if (this.result?.username) {
+        return `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/profile/${this.result.username}`;
+      } else {
+        return '#';
+      }
+    }
   },
   created() {
     this.profileActionExtensions = extensionRegistry.loadExtensions('profile-extension', 'action') || [];
   },
-  methods: {
-    openUserProfile() {
-      if (this.result?.username) {
-        window.location.href = `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/profile/${this.result.username}`;
-      }
-    }
-  }
 };
 </script>
