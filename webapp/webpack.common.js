5d1be113e0e2b853ff42d31e0a2e192956c1b20a
Romain Dénarié <romain.denarie@exoplatform.com>
2025-07-01 17:56:17 +0200

    feat: Add portlet Platform name - meeds-io/MIPs#203 - MEED-9364
    This commit add the portlet Platform Name
    This portlet display the branding name as a title

--- webapp/webpack.common.js
+++ webapp/webpack.common.js
@@ -99,6 +99,7 @@ let config = {
     simpleStorage: './src/main/webapp/vue-apps/simple-storage/main.js',
     admins: './src/main/webapp/vue-apps/platformAdminsWidget/main.js',
     platformAccess: './src/main/webapp/vue-apps/platform-access/main.js',
+    platformName: './src/main/webapp/vue-apps/platform-name/main.js',
     contentLink: './src/main/webapp/vue-apps/content-link/main.js',
     spaceCreation: './src/main/webapp/vue-apps/space-creation/main.js',
     emojiPicker: './src/main/webapp/vue-apps/emoji-picker/main.js',
