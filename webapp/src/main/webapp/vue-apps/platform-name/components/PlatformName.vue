9e27ec58b858fd191cace4d62ace9bd1fc080508
Romain Dénarié <romain.denarie@exoplatform.com>
2025-07-01 17:56:17 +0200

    feat: Add portlet Platform name - meeds-io/MIPs#203 - MEED-9364
    This commit add the portlet Platform Name
    This portlet display the branding name as a title

---
+++ webapp/src/main/webapp/vue-apps/platform-name/components/PlatformName.vue
@@ -0,0 +1,39 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-card-title
+    class="text-title text-h4 primary--text text-center justify-center full-height py-0">
+    <span class="text-truncate-2 full-width">
+      {{ platformName }}
+    </span>
+  </v-card-title>
+</template>
+
+<script>
+export default {
+  props: {
+    platformName: {
+      type: String,
+      default: null,
+    },
+  },
+};
+</script>
