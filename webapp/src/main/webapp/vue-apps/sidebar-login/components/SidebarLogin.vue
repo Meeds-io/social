f40a496f8f717f0b5f679433b7819309d3adff5c
Romain Dénarié <romain.denarie@exoplatform.com>
2025-07-08 15:03:40 +0200

    feat: Add portlet Login Sidebar - meeds-io/MIPs#203 - MEED-9366
    This commit add the portlet Login Sidebar

---
+++ webapp/src/main/webapp/vue-apps/sidebar-login/components/SidebarLogin.vue
@@ -0,0 +1,85 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+
+ Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-card
+    flat
+    :color="this.backgroundPath && 'transparent' || 'primary'"
+    class="full-height d-flex justify-center align-center">
+    <img v-if="backgroundPath"
+         :src="backgroundPath"
+         :alt="backgroundAlt"
+         class="full-height full-witdh object-fit-cover">
+    <v-card
+      flat
+      :color="this.backgroundPath && 'transparent' || 'primary'"
+      class="full-height d-flex justify-center align-center">
+      <img v-if="backgroundPath"
+           :src="backgroundPath"
+           :alt="backgroundAlt || ''"
+           class="full-height full-witdh object-fit-cover rounded-0">
+      <v-card
+        :class="backgroundPath && 'position-absolute t-0'"
+        tile
+        flat
+        class="fill-height transparent">
+        <v-card-title class="d-flex flex-column py-0 fill-height justify-center">
+          <span class="text-title  mx-auto text-wrap text-break">
+            {{ this.title }}
+          </span>
+          <span class="text-body pa-4 text-wrap mx-auto text-break">
+            {{ this.subtitle }}
+          </span>
+        </v-card-title>
+      </v-card>
+    </v-card>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    backgroundPath: {
+      type: String,
+      default: null,
+    },
+    backgroundAlt: {
+      type: String,
+      default: null,
+    },
+    authenticationTitle: {
+      type: String,
+      default: null,
+    },
+    authenticationSubtitle: {
+      type: String,
+      default: null,
+    }
+  },
+  computed: {
+    title() {
+      return this.$t(this.authenticationTitle);
+    },
+    subtitle() {
+      return this.$t(this.authenticationSubtitle);
+    },
+  },
+};
+</script>
