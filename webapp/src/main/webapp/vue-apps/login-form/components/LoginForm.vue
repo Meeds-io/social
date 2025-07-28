8c17dedf6e1e18d2b891c736c4514689723ea5f4
Romain Dénarié <romain.denarie@exoplatform.com>
2025-07-28 15:55:12 +0200

    feat: Add settings for LoginForm Portlet - meeds-io/MIPs#203 - MEED-9368
    This commit add settings for loginForm Portlet.

--- webapp/src/main/webapp/vue-apps/login-form/components/LoginForm.vue
+++ webapp/src/main/webapp/vue-apps/login-form/components/LoginForm.vue
@@ -20,116 +20,135 @@
 -->
 <template>
   <v-app class="mx-auto" v-show="this.init">
-    <v-card
-      class="rounded-0 transparent pa-5"
-      flat>
-      <div v-if="registerEnabled" class="widget-text-header align-center">
-        {{ $t('UILoginForm.label.registerNewAccount') }}
-        <a
-          :title="$t('UILoginForm.button.registerNewAccount')"
-          href="/portal/register"
-          class="text-decoration-underline">
-          {{ $t('UILoginForm.button.registerNewAccount') }}
-        </a>
-      </div>
-      <div v-else class="widget-text-header align-center">{{ $t('portal.login.WelcomeBack') }}</div>
-
-      <div v-if="!registerEnabled && !this.displayForm" class="widget-text-header align-center">{{ $t('portal.login.chooseSignin') }}</div>
-
-      <portal-login-providers
-        :params="this.values"
-        :rememberme="rememberme"
-        ref="loginProvidersComponent"
-        v-show="!this.displayForm"/>
-
-      <form
-        ref="form"
-        name="form"
-        action="/portal/login"
-        method="post"
-        autocomplete="off"
-        class="d-flex ma-0 flex-column"
-        @submit="validateForm()"
-        v-if="this.displayForm">
-        <input
-          v-if="initialUri"
-          type="hidden"
-          name="initialURI"
-          :value="initialUri">
-        <div class="pa-0">
-          <v-row class="ma-0 pa-0">
-            <v-text-field
-              id="username"
-              v-model="username"
-              :title="$t('portal.login.Username')"
-              :placeholder="$t('portal.login.Username')"
-              :autofocus="'autofocus'"
-              prepend-inner-icon="fas fa-user ms-n2 grey--text text--lighten-1"
-              class="login-username border-box-sizing"
-              name="username"
-              aria-required="true"
-              type="text"
-              tabindex="0"
-              required="required"
-              outlined
-              dense
-              background-color="white"/>
-          </v-row>
-          <v-row class="ma-0 pa-0">
-            <v-text-field
-              id="password"
-              v-model="password"
-              :title="$t('portal.login.Password')"
-              :placeholder="$t('portal.login.Password')"
-              :type="passwordType"
-              :append-icon="showPassword ? 'fas fa-eye-slash text-font-size mt-0' : 'fas fa-eye text-font-size mt-0'"
-              prepend-inner-icon="fas fa-lock ms-n2 grey--text text--lighten-1"
-              class="login-password border-box-sizing"
-              name="password"
-              aria-required="true"
-              required="required"
-              outlined
-              dense
-              @click:append="toggleShow"
-              background-color="white" />
-          </v-row>
-          <v-row class="d-flex flex-column flex-sm-row ma-0 py-0 px-3 px-sm-0" flat>
-            <v-checkbox
-              v-model="rememberme"
-              id="rememberme"
-              :label="$t('portal.login.RememberOnComputer')"
-              :value="rememberme"
-              name="rememberme"
-              on-icon="fas fa-check-square"
-              class="mx-0 my-3 my-sm-0"
-              dense />
-            <v-spacer />
-            <a
-              :href="forgotPasswordPath"
-              :title="$t('portal.login.forgotPassword')"
-              class="text-decoration-underline d-flex">
-              <span class="v-label theme--light pb-2px my-auto">
-                {{ $t('portal.login.forgotPassword') }}
-              </span>
-            </a>
-          </v-row>
-          <v-row class="mx-0 mt-8 pa-0 loginButton">
+    <v-hover v-slot="{ hover }">
+      <v-card
+        class="rounded-0 transparent pa-5"
+        flat>
+        <div
+          v-if="values.canEdit && hover"
+          class="position-absolute t-0 r-0"
+          :class="{
+            'l-0': $vuetify.rtl,
+            'r-0': !$vuetify.rtl,
+          }">
+          <v-fab-transition hide-on-leave>
             <v-btn
-              id="loginButton"
-              :aria-label="$t('portal.login.Signin')"
-              :type="'submit'"
-              :loading="loading"
-              width="222"
-              max-width="100%"
-              color="primary"
-              class="mx-auto login-button text-none"
-              elevation="0">
-              {{ $t('portal.login.Signin') }}
+              :title="$t('loginForm.settings.editTooltip')"
+              class="z-index-two me-2 mt-2"
+              small
+              icon
+              @click="$root.$emit('login-form-settings')">
+              <v-icon size="18">fa-cog</v-icon>
             </v-btn>
-          </v-row>
+          </v-fab-transition>
         </div>
-      </form>
-      <div v-else class="mt-4 center text-body">
+        <div v-if="registerEnabled" class="widget-text-header align-center">
+          {{ newHere }}
+          <a
+            :title="createAccount"
+            href="/portal/register"
+            class="text-decoration-underline">
+            {{ createAccount }}
+          </a>
+        </div>
+        <div v-else class="widget-text-header align-center">{{ welcomeBack }}</div>
+
+        <portal-login-providers
+          :params="values"
+          :rememberme="rememberme"
+          ref="loginProvidersComponent"
+          v-show="!displayForm"/>
+
+        <portal-login-separator class="mt-5" v-if="!displayForm && signinOption !== 'noform' "/>
+
+        <form
+          ref="form"
+          name="form"
+          action="/portal/login"
+          method="post"
+          autocomplete="off"
+          class="d-flex ma-0 flex-column"
+          @submit="validateForm()"
+          v-if="displayForm || signinOption === 'loginform'">
+          <input
+            v-if="initialUri"
+            type="hidden"
+            name="initialURI"
+            :value="initialUri">
+          <div class="pa-0">
+            <v-row class="ma-0 pa-0">
+              <v-text-field
+                id="username"
+                v-model="username"
+                :title="$t('portal.login.Username')"
+                :placeholder="$t('portal.login.Username')"
+                :autofocus="'autofocus'"
+                prepend-inner-icon="fas fa-user ms-n2 grey--text text--lighten-1"
+                class="login-username border-box-sizing"
+                name="username"
+                aria-required="true"
+                type="text"
+                tabindex="0"
+                required="required"
+                outlined
+                dense
+                background-color="white"/>
+            </v-row>
+            <v-row class="ma-0 pa-0">
+              <v-text-field
+                id="password"
+                v-model="password"
+                :title="$t('portal.login.Password')"
+                :placeholder="$t('portal.login.Password')"
+                :type="passwordType"
+                :append-icon="showPassword ? 'fas fa-eye-slash text-font-size mt-0' : 'fas fa-eye text-font-size mt-0'"
+                prepend-inner-icon="fas fa-lock ms-n2 grey--text text--lighten-1"
+                class="login-password border-box-sizing"
+                name="password"
+                aria-required="true"
+                required="required"
+                outlined
+                dense
+                @click:append="toggleShow"
+                background-color="white" />
+            </v-row>
+            <v-row class="d-flex flex-column flex-sm-row ma-0 py-0 px-3 px-sm-0" flat>
+              <v-checkbox
+                v-model="rememberme"
+                id="rememberme"
+                :label="$t('portal.login.RememberOnComputer')"
+                :value="rememberme"
+                name="rememberme"
+                on-icon="fas fa-check-square"
+                class="mx-0 my-3 my-sm-0"
+                dense />
+              <v-spacer />
+              <a
+                :href="forgotPasswordPath"
+                :title="$t('portal.login.forgotPassword')"
+                class="text-decoration-underline d-flex">
+                <span class="v-label theme--light pb-2px my-auto">
+                  {{ $t('portal.login.forgotPassword') }}
+                </span>
+              </a>
+            </v-row>
+            <v-row class="mx-0 mt-8 pa-0 loginButton">
+              <v-btn
+                id="loginButton"
+                :aria-label="$t('portal.login.Signin')"
+                :type="'submit'"
+                :loading="loading"
+                width="222"
+                max-width="100%"
+                color="primary"
+                class="mx-auto login-button text-none"
+                elevation="0">
+                {{ $t('portal.login.Signin') }}
+              </v-btn>
+            </v-row>
+          </div>
+        </form>
+        <div v-else-if="!displayForm && signinOption !== 'noform'" class="mt-4 center text-body">
           <v-btn
             color="primary"
             class="elevation-0"
@@ -137,13 +156,19 @@
             style="background-color:white"
             @click="clickDisplayForm()">
             <span class="text-body">
-              <v-icon class="me-2">fas fa-envelope</v-icon>
-              {{ $t('portal.login.SigninUsingEmail') }}
+              <v-icon class="me-2" v-if="displaySigninEmailButtonIcon">fas fa-envelope</v-icon>
+              {{ signinEmailButton }}
             </span>
           </v-btn>
-      </div>
-    </v-card>
-
+        </div>
+      </v-card>
+    </v-hover>
+    <login-form-settings-drawer
+      :translation-identifier="values.translationIdentifier"
+      :register-enabled="values.registerEnabled"
+      :signin-option="signinOption"
+      :providersCount="providersCount"
+      :display-signin-email-button-icon="displaySigninEmailButtonIcon" />
   </v-app>
 </template>
 <script>
@@ -161,6 +186,13 @@ export default {
     showPassword: false,
     displayForm: false,
     init: false,
+    welcomeBack: '',
+    newHere: '',
+    createAccount: '',
+    signinEmailButton: '',
+    signinOption: 'loginform',
+    providersCount: 0,
+    displaySigninEmailButtonIcon: true,
   }),
   watch: {
     errorMessage: {
@@ -174,8 +206,28 @@ export default {
   },
   created() {
     this.values = JSON.parse(this.params);
+    this.signinOption = this.values?.signinOption || 'loginform';
+    this.welcomeBack = this.values?.welcomeBack || this.$t('portal.login.WelcomeBack');
+    this.newHere = this.values?.newHere || this.$t('UILoginForm.label.registerNewAccount');
+    this.createAccount = this.values?.createAccount || this.$t('UILoginForm.button.registerNewAccount');
+    this.displaySigninEmailButtonIcon = this.values?.displaySigninEmailButtonIcon;
+    this.signinEmailButton = this.values?.signinEmailButton || this.$t('portal.login.SigninUsingEmail');
+    this.$root.$on('login-form-settings-updated', (welcomeBackTranslations,
+      newHereTranslations,
+      createAccountTranslations,
+      signinEmailButtonTranslations,
+      signinOption,
+      displaySigninEmailButtonIcon) => {
+      this.welcomeBack = welcomeBackTranslations?.[eXo.env.portal.language] || this.$t('portal.login.WelcomeBack');
+      this.newHere = newHereTranslations?.[eXo.env.portal.language] || this.$t('UILoginForm.label.registerNewAccount');
+      this.createAccount = createAccountTranslations?.[eXo.env.portal.language] || this.$t('UILoginForm.button.registerNewAccount');
+      this.signinEmailButton = signinEmailButtonTranslations?.[eXo.env.portal.language] || this.$t('portal.login.SigninUsingEmail');
+      this.signinOption = signinOption || 'loginform';
+      this.displaySigninEmailButtonIcon = displaySigninEmailButtonIcon;
+    });
     this.$root.$on('login-providers-refreshed', (providers) => {
       this.displayForm = providers.length === 0;
+      this.providersCount = providers.length;
       const t = this;
       setTimeout(() => {
         t.init = true;
@@ -218,7 +270,6 @@ export default {
     this.setupUserName();
   },
   methods: {
-
     clickDisplayForm() {
       this.displayForm = !this.displayForm;
     },
