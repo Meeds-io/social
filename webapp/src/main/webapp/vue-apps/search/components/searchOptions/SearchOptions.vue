3088f40538ac725327167635a4b65b0dabcb0739
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2025-07-29 10:56:48 +0100

    feat: add search sort selection - MEED-9498 - Meeds-io/MIPs#212 (#5075)

--- webapp/src/main/webapp/vue-apps/search/components/searchOptions/SearchOptions.vue
+++ webapp/src/main/webapp/vue-apps/search/components/searchOptions/SearchOptions.vue
@@ -99,6 +99,7 @@ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
     </div>
     <search-tag-selector @tags-changed="$emit('select-tags', $event)" />
     <search-favorites-selector :favorites="favorites" />
+    <search-sort-selector class="align-end ml-auto" />
   </div>
 </template>
 
