30597a59c0ccb1c8cdb02661ee42470f8a97fc0c
Boubaker Khanfir <boubaker.khanfir@exoplatform.com>
2025-08-22 13:38:31 +0100

    feat: Improve Password changing UX - MEED-8566 - Meeds-io/MIPs#214 (#5125)
    This change will allow to end user to clearly identify what is missing
    in term of pattern when adding a new password.

--- webapp/src/main/webapp/vue-apps/common/js/UserService.js
+++ webapp/src/main/webapp/vue-apps/common/js/UserService.js
@@ -346,13 +346,16 @@ export function updateProfileFields(username, obj, fields) {
 }
 
 export function changePassword(username, currentPassword, newPassword) {
+  const formData = new FormData();
+  formData.append('currentPassword', currentPassword);
+  formData.append('newPassword', newPassword);
   return fetch(`${eXo.env.portal.context}/${eXo.env.portal.rest}/v1/users/${username}/changePassword`, {
     method: 'PATCH',
     credentials: 'include',
     headers: {
       'Content-Type': 'application/x-www-form-urlencoded',
     },
-    body: `currentPassword=${currentPassword}&newPassword=${newPassword}`
+    body: new URLSearchParams(formData).toString(),
   }).then(resp => {
     if (!resp || !resp.ok) {
       return resp.text();
