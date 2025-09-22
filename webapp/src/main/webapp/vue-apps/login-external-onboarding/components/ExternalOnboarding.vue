5479f49c5981c6c05ac2827b7fb3f7b3c185e236
Romain Dénarié <romain.denarie@exoplatform.com>
2025-09-22 11:26:16 +0200

    feat : Make External Onboarding page modifiable in layout - MEED-9700 - meeds-io/MIPs#203
    This commit update External Onboarding page to transform the existing handler and application into a portlet which can be added on a page, and configured with the layout editor.

--- webapp/src/main/webapp/vue-apps/login-external-onboarding/components/ExternalOnboarding.vue
+++ webapp/src/main/webapp/vue-apps/login-external-onboarding/components/ExternalOnboarding.vue
@@ -19,13 +19,7 @@
 
 -->
 <template>
-  <portal-login-template
-    :params="params"
-    branding-image
-    hide-success-message
-    center>
-    <portal-external-onboarding-main :params="params" />
-  </portal-login-template>
+  <portal-external-onboarding-main :params="jsonParams" />
 </template>
 <script>
 export default {
@@ -35,8 +29,13 @@ export default {
       default: null,
     },
   },
+  data() {
+    return {
+      jsonParams: null,
+    };
+  },
   created() {
-    document.title = this.$t('externalOnboarding.pagetitle');
+    this.jsonParams = JSON.parse(this.params);
   },
   mounted() {
     this.$root.$applicationLoaded();
