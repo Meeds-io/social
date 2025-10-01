201072658f528e46951efb7008d381bedec38c3e
Romain Dénarié <romain.denarie@exoplatform.com>
2025-10-01 17:12:55 +0200

    fix: Make settings drawer accessible by keyboard navigation on login portlet - MEED-9785 - meeds-io/MIPs#203
    Before this fix, the edit settings button in login porlet is not accessible by keyboard,
    This commit ensure that the button appear when navigate with keyboard.
    In addition, the ExoDrawer component was modified to autofocus the first input of the drawer if exists.

--- webapp/src/main/webapp/vue-apps/login-form/components/LoginForm.vue
+++ webapp/src/main/webapp/vue-apps/login-form/components/LoginForm.vue
@@ -51,7 +51,7 @@
           </v-btn>
         </div>
         <div
-          v-if="values.canEdit && hover"
+          v-if="values.canEdit"
           class="position-absolute t-0"
           :class="{
             'l-0': $vuetify.rtl,
@@ -63,7 +63,11 @@
               class="z-index-two me-2 mt-2"
               small
               icon
-              @click="$root.$emit('login-form-settings')">
+              @click="$root.$emit('login-form-settings')"
+              taxindex="0"
+              @focus="focus = true"
+              @focusout="focus = false"
+              :style="{ opacity: (focus || hover) ? 1 : 0 }">
               <v-icon size="18">fa-cog</v-icon>
             </v-btn>
           </v-fab-transition>
@@ -223,6 +227,7 @@ export default {
     displayProvidersIcons: true,
     displayBackButton: false,
     displayWelcomeMessage: true,
+    focus: false,
   }),
   watch: {
     errorMessage: {
