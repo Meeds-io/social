d07e7c9f702cf91f4fdcf143ebd04e8d0ca35e53
Romain Dénarié <romain.denarie@exoplatform.com>
2025-10-02 15:27:30 +0200

    fix: Clean up code after feature completion - MEED-9753 - meeds-io/MIPs#203
    Remove uneeded vue components

--- webapp/src/main/webapp/vue-apps/login/initComponents.js
+++ webapp/src/main/webapp/vue-apps/login/initComponents.js
@@ -17,10 +17,6 @@
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
-import Login from './components/Login.vue';
-import LoginMain from './components/LoginMain.vue';
-import LoginMainTopExtensions from './components/LoginMainTopExtensions.vue';
-import LoginMainBottomExtensions from './components/LoginMainBottomExtensions.vue';
 import LoginSeparator from './components/LoginSeparator.vue';
 import LoginProviders from './components/provider/LoginProviders.vue';
 import LoginProvidersMenu from './components/provider/LoginProvidersMenu.vue';
@@ -28,10 +24,6 @@ import LoginProviderLink from './components/provider/LoginProviderLink.vue';
 import LoginProviderMenuLink from './components/provider/LoginProviderMenuLink.vue';
 
 const components = {
-  'portal-login': Login,
-  'portal-login-main': LoginMain,
-  'portal-login-main-top-extensions': LoginMainTopExtensions,
-  'portal-login-main-bottom-extensions': LoginMainBottomExtensions,
   'portal-login-providers': LoginProviders,
   'portal-login-providers-menu': LoginProvidersMenu,
   'portal-login-provider-link': LoginProviderLink,
