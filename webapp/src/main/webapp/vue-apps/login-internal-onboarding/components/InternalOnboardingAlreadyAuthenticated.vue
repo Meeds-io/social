5f824cc3fba34a69d7ef59e58096b8123f39734a
Romain Dénarié <romain.denarie@exoplatform.com>
2025-10-23 15:18:59 +0200

    fix: Change portlets InternalRegistration and Register to not automatically redirect if user is already loggued - MEED-9827 - meeds-io/meeds#3739
    Before this fix, portlets InternalRegistration and Register cannot be displayed in edit mode if user is authenticated.
    This commit ensure to display a message "Already authenticated", based on message displayed in ExternalRegistration, in order to be able to see portlets in editLayout context
    
    Resolves meeds-io/meeds#3739

---
+++ webapp/src/main/webapp/vue-apps/login-internal-onboarding/components/InternalOnboardingAlreadyAuthenticated.vue
@@ -0,0 +1,45 @@
+<!--
+
+ This file is part of the Meeds project (https://meeds.io/).
+ 
+ Copyright (C) 2020 - 2023 Meeds Association contact@meeds.io
+ 
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 3 of the License, or (at your option) any later version.
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+ 
+ You should have received a copy of the GNU Lesser General Public License
+ along with this program; if not, write to the Free Software Foundation,
+ Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-card flat class="transparent">
+    <v-card-title class="text-body px-0">
+      <span class="mx-auto text-center full-width">{{ $t('onboarding.alreadyLoggedInPart1') }}</span>
+      <span class="mx-auto text-center mt-4 full-width">{{ $t('onboarding.alreadyLoggedInPart2') }}</span>
+    </v-card-title>
+    <div class="d-flex ma-0 flex-column">
+      <div class="pa-0">
+        <v-row class="mx-0 mt-8 pa-0">
+          <v-btn
+            :aria-label="$t('forgotpassword.backToLogin')"
+            href="/portal/login"
+            color="primary"
+            width="222"
+            max-width="100%"
+            tabindex="0"
+            class="mx-auto login-button text-none"
+            elevation="0">
+            {{ $t('forgotpassword.go') }}
+          </v-btn>
+        </v-row>
+      </div>
+    </div>
+  </v-card>
+</template>
