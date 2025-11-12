4a0a8adad756c60f37ab68f11c6bee923724397c
Helmi Akermi <hakermi@exoplatform.com>
2025-11-12 16:32:59 +0100

    fix: responsive design issue on the Profile page - MEED-9962 - Meeds-io/meeds#3830
     responsive design issue on the Profile page

---
+++ webapp/src/main/webapp/vue-apps/profile-header/components/ProfileHeaderRelationButton.vue
@@ -0,0 +1,87 @@
+<!--
+ * This file is part of the Meeds project (https://meeds.io/).
+ *
+ * Copyright (C) 2025 Meeds Association contact@meeds.io
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+-->
+
+<template>
+  <v-btn
+    :loading="loading"
+    :disabled="disabled"
+    :class="extraButtonClass"
+    :outlined="outlined"
+    :icon="iconButton"
+    class="ma-1 transparent"
+    @click="$emit('click')">
+    <v-icon
+      :class="extraIconClass"
+      :color="iconButton && 'primary'"
+      class="ma-auto"
+      size="16">
+      {{ icon }}
+    </v-icon>
+    <span
+      v-if="!iconButton"
+      :class="extraTextClass"
+      class="ms-1">
+      {{ label }}
+    </span>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: false
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    icon: {
+      type: String,
+      default: ''
+    },
+    outlined: {
+      type: Boolean,
+      default: false
+    },
+    extraButtonClass: {
+      type: String,
+      default: ''
+    },
+    extraTextClass: {
+      type: String,
+      default: ''
+    },
+    extraIconClass: {
+      type: String,
+      default: ''
+    },
+    iconButton: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
