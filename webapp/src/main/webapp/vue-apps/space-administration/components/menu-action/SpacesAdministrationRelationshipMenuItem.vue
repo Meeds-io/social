1012527621de6e19856461031da5aaf87d9633ca
Azmi TOUIL <42934070+AzmiTouil@users.noreply.github.com>
2025-11-18 09:55:30 +0100

    feat: Manage relations between spaces as platform admin - MEED-9850 - Meeds-io/MIPs#227 (#5389)
    This change will allow to manage relations between spaces as platform
    admin

---
+++ webapp/src/main/webapp/vue-apps/space-administration/components/menu-action/SpacesAdministrationRelationshipMenuItem.vue
@@ -0,0 +1,46 @@
+<!--
+
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2020 - 2025 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <v-list-item
+    dense
+    @click="$root.$emit('space-administration-manage-relationships-drawer-open', space)">
+    <v-card
+      class="d-flex full-height justify-center"
+      color="transparent"
+      min-width="20"
+      flat>
+      <v-icon size="16">fas fa-link</v-icon>
+    </v-card>
+    <v-list-item-title class="ps-2">
+      {{ $t('social.spaces.administration.manageSpaces.manageRelationships') }}
+    </v-list-item-title>
+  </v-list-item>
+</template>
+<script>
+export default {
+  props: {
+    space: {
+      type: Object,
+      default: null,
+    },
+  },
+};
+</script>
