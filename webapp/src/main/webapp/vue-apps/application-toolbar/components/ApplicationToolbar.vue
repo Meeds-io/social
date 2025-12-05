f4d1bd8ca12c8859cb7f408fe1c0b21d4a9ecf49
Mohamed Amine Krout <mkrout@exoplatform.com>
2025-12-05 18:22:57 +0100

    feat: Allow to set auto cols on application toolbar when not in compact mode  - EXO-82846 (#5437)
    This commit allows setting auto cols when there is no center content and
    not in compact mode

--- webapp/src/main/webapp/vue-apps/application-toolbar/components/ApplicationToolbar.vue
+++ webapp/src/main/webapp/vue-apps/application-toolbar/components/ApplicationToolbar.vue
@@ -491,6 +491,7 @@ export default {
     } else {
       document.addEventListener('keydown', this.closeFilter);
     }
+    console.log('dddddddddddddddddddddddddddddddddddddddddd');
   },
   beforeDestroy() {
     this.$root.$off('reset-filter', this.reset);
