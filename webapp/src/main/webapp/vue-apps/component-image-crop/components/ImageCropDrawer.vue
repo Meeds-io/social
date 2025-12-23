2cc4bdacb69daad7232be9a113b7d87411cbf3a1
MayTekayaa <100131287+MayTekayaa@users.noreply.github.com>
2025-12-23 09:35:56 +0100

    fix: Add some accessibility requirements for switch option - MEED-10041 - Meeds-io/MIPs#232  (#5481)
    This change will add some accessibility requirements for switch option.

--- webapp/src/main/webapp/vue-apps/component-image-crop/components/ImageCropDrawer.vue
+++ webapp/src/main/webapp/vue-apps/component-image-crop/components/ImageCropDrawer.vue
@@ -223,13 +223,14 @@
             outlined
             dense />
           <div class="d-flex mb-2">
-            <div class="d-flex align-center flex-grow-1 flex-shrink-1 text-truncate text-color">
+            <label for="OpenSameTab" class="d-flex align-center flex-grow-1 flex-shrink-1 text-truncate text-color">
               {{ $t('imageCropDrawer.link.target') }}
-            </div>
+            </label>
             <v-switch
               v-model="linkTarget"
               :true-value="''"
               :false-value="'_blank'"
+              :aria-checked="linkTarget === '' ? 'true' : 'false'"
               class="my-0 me-n3 pa-0"
               dense
               hide-details />
