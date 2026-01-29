81525e12c82b62ddfe31beaa465256de371ca312
Helmi Akermi <hakermi@exoplatform.com>
2026-01-29 09:24:31 +0100

    feat: Add Call attribute type to profile settings list types - MEED-10166 - Meeds-io/MIPs#231
    This feature introduces a new Call attribute type in the profile settings list types, allowing it to be selected and managed like other existing attribute types within profile configuration.

--- webapp/src/main/webapp/vue-apps/profile-settings/components/drawers/ProfileSettingFormDrawer.vue
+++ webapp/src/main/webapp/vue-apps/profile-settings/components/drawers/ProfileSettingFormDrawer.vue
@@ -416,7 +416,8 @@ export default {
     propertyTypes () {
       return !this.isDropdownList && [
         {label: this?.$t('profileSettings.label.text.propertyType'), value: 'text'},
-        {label: this?.$t('profileSettings.label.user.propertyType'), value: 'user'}
+        {label: this?.$t('profileSettings.label.user.propertyType'), value: 'user'},
+        {label: this?.$t('profileSettings.label.call.propertyType'), value: 'call'}
       ] || [
         {label: this?.$t('profileSettings.label.text.propertyType'), value: 'text'}];
     },
