1d9057fa7dcb5ba3c2ee5f3d51a2b28a77d9ddac
sofyenne <shajchedli@exoplatform.com>
2026-02-25 14:23:54 +0100

    fix: Fix advanced profile filter to support partial single-word matching - MEED-10273 - Meeds-io/meeds#3970

--- component/core/src/test/java/org/exoplatform/social/core/jpa/search/ProfileSearchConnectorTest.java
+++ component/core/src/test/java/org/exoplatform/social/core/jpa/search/ProfileSearchConnectorTest.java
@@ -553,8 +553,8 @@ public class ProfileSearchConnectorTest {
                 }
           ,
             {
-              "match_phrase": {
-                "testProperty": "valueProperty"
+              "query_string": {
+                "query": "testProperty.whitespace:*valueProperty*"
               }
            }
                 ]
