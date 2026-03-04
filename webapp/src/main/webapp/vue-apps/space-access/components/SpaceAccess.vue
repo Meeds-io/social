7aa825ce9bb9d03dd5e5a00b92908f8c04536fd5
Helmi Akermi <70575401+hakermi@users.noreply.github.com>
2026-03-04 10:08:42 +0100

    feat: Enhance placeholder message and button label in case of space access to subspace - MEED-10281 - Meeds-io/MIPs#244 (#5632)
    Change placeholder message and button label in case of space access to
    subspace depends on parent registration type

--- webapp/src/main/webapp/vue-apps/space-access/components/SpaceAccess.vue
+++ webapp/src/main/webapp/vue-apps/space-access/components/SpaceAccess.vue
@@ -35,7 +35,8 @@
         class="text-body"
         v-sanitized-html="secondLabel" />
       <v-card-actions class="justify-center py-5">
-        <template v-if="spaceAccessTypeLabel === 'INVITED_SPACE'">
+        <span v-if="isSubSpace && !isParentSpaceMember"></span>
+        <template v-else-if="spaceAccessTypeLabel === 'INVITED_SPACE'">
           <v-btn
             :disabled="sendingRefuse"
             :loading="sendingAction"
@@ -104,6 +105,12 @@ export default {
     spacesLink() {
       return `${eXo.env.portal.context}/${eXo.env.portal.metaPortalName}/spaces`;
     },
+    isSubSpace() {
+      return 'isParentSpaceMember' in (this.parameters || {});
+    },
+    isParentSpaceMember() {
+      return this.parameters?.isParentSpaceMember;
+    },
     spaceLink() {
       return this.parameters?.originalUri;
     },
@@ -139,6 +146,9 @@ export default {
       return null;
     },
     secondLabel() {
+      if (this.isSubSpace && !this.isParentSpaceMember) {
+        return this.$t('UISpaceAccess.subspace.memberRestrict');
+      }
       switch (this.spaceAccessTypeLabel) {
       case 'INVITED_SPACE':
         return this.$t('UISpaceAccess.invited-space').replace('{0}', `<strong>${this.spaceDisplayName}</strong>`);
