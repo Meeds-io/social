e1bc1dc71fc66810f400622443a4e3d1f1567cd5
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2026-05-04 11:28:32 +0100

    feat: Review administration groups management UX page - MEED-10383 - Meeds-io/MIPs#237 (#5702)

--- webapp/src/main/webapp/vue-apps/idm-groups-management/components/sidebar/GroupsManagementTreeItem.vue
+++ webapp/src/main/webapp/vue-apps/idm-groups-management/components/sidebar/GroupsManagementTreeItem.vue
@@ -1,57 +1,57 @@
 <template>
-  <div>
-    <v-list-item
-      :class="selectedClass"
-      selectable
-      dense
-      @click="$root.$emit('selectGroup', group)">
-      <v-list-item-action class="me-4">
-        <v-btn
-          v-if="displayIcon"
-          icon
-          @click="toogleOpenTree">
-          <v-progress-circular
-            v-if="loadingChildren"
-            indeterminate
-            size="32" />
-          <v-icon v-else size="32">{{ icon }}</v-icon>
-        </v-btn>
-        <div v-else class="ms-5 me-4"></div>
-      </v-list-item-action>
-      <v-list-item-content>
-        <v-list-item-title
-          :title="group.label"
-          class="font-weight-bold text-truncate">
-          {{ group.label }}
-        </v-list-item-title>
-        <v-list-item-subtitle
-          :title="group.id"
-          class="caption text-truncate">
-          {{ group.id }}
-        </v-list-item-subtitle>
-      </v-list-item-content>
-      <v-list-item-action>
-        <groups-management-tree-group-menu :group="group" />
-      </v-list-item-action>
-    </v-list-item>
-    <div v-if="displayChildren">
-      <div class="ms-3 d-flex flex-nowrap">
-        <v-pagination
-          v-if="pagesCount > 1"
-          v-model="page"
-          :length="pagesCount"
-          :total-visible="5" />
+  <v-hover v-slot="{ hover }">
+    <div>
+      <v-list-item
+        class="ps-0"
+        :class="selectedClass"
+        selectable
+        dense
+        @click="$root.$emit('selectGroup', group)">
+        <v-list-item-action class="ps-1 my-1 me-0">
+          <v-btn
+            v-if="displayIcon"
+            icon
+            @click="toogleOpenTree">
+            <v-progress-circular
+              v-if="loadingChildren"
+              indeterminate
+              size="18" />
+            <v-icon v-else size="32">{{ icon }}</v-icon>
+          </v-btn>
+        </v-list-item-action>
+        <v-list-item-content>
+          <v-list-item-title
+            :title="group.label"
+            :class="{ 'font-weight-bold': loading >=0 && selected, 'ms-3': !displayIcon }"
+            class="text-body text-truncate">
+            {{ group.label }}
+          </v-list-item-title>
+        </v-list-item-content>
+        <v-list-item-action class="my-1">
+          <v-sheet :min-height="36">
+            <groups-management-tree-group-menu v-show="hover" :group="group" />
+          </v-sheet>
+        </v-list-item-action>
+      </v-list-item>
+      <div v-if="displayChildren">
+        <div class="ms-3 d-flex flex-nowrap">
+          <v-pagination
+            v-if="pagesCount > 1"
+            v-model="page"
+            :length="pagesCount"
+            :total-visible="5" />
+        </div>
+        <groups-management-tree-item
+          v-for="child in children"
+          :key="child.id"
+          :group="child"
+          :page-size="pageSize"
+          :loading="loading"
+          :open-all="openAll"
+          class="ms-3" />
       </div>
-      <groups-management-tree-item
-        v-for="child in children"
-        :key="child.id"
-        :group="child"
-        :page-size="pageSize"
-        :loading="loading"
-        :open-all="openAll"
-        class="ms-3" />
     </div>
-  </div>
+  </v-hover>
 </template>
 
 <script>
@@ -124,6 +124,10 @@ export default {
   created() {
     this.$root.$on('selectGroup', group => {
       this.selected = group && this.group.id === group.id;
+    });
+    this.$root.$on('searchingGroup', () => {
+      this.open = false;
+      this.selected = false;
     });
   },
   methods: {
