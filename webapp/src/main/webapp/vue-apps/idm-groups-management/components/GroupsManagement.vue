2ba479c31b15ef9f0544807900906a25e22d6f0d
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2026-05-04 11:28:32 +0100

    feat: Review administration groups management UX page - MEED-10383 - Meeds-io/MIPs#237 (#5702)

--- webapp/src/main/webapp/vue-apps/idm-groups-management/components/GroupsManagement.vue
+++ webapp/src/main/webapp/vue-apps/idm-groups-management/components/GroupsManagement.vue
@@ -1,17 +1,31 @@
 <template>
   <v-app>
-    <v-card class="d-flex flex py-5 application-body" flat>
-      <v-flex class="sm12 md4" flat>
-        <groups-management-tree-toolbar />
-        <groups-management-tree />
-      </v-flex>
-      <v-divider vertical />
-      <v-flex class="sm12 md8" flat>
-        <groups-management-membership-toolbar />
-        <groups-management-membership-list />
-      </v-flex>
-    </v-card>
+    <main class="application-body position-static">
+      <groups-management-toolbar
+        ref="toolbar"
+        class="application-layout-style" />
+      <v-card
+        class="d-flex flex-column flex-md-row mt-5 transparent"
+        min-width="100%"
+        flat>
+        <v-card
+          class="application-layout-style me-0 me-md-5 mb-5 mb-md-0 flex-shrink-0 flex-grow-0"
+          :width="$root.isMobile ? '100%' : '300px !important'"
+          :max-width="$root.isMobile ? '100%' : '300px !important'"
+          flat>
+          <groups-management-tree-toolbar />
+          <groups-management-tree />
+        </v-card>
+        <div
+          ref="main"
+          class="application-layout-style flex-shrink-1 flex-grow-1 overflow-hidden">
+          <groups-management-membership-toolbar />
+          <groups-management-membership-list />
+        </div>
+      </v-card>
+    </main>
     <groups-management-form-drawer />
     <groups-management-membership-form-drawer />
   </v-app>
 </template>
+
