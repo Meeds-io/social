795d24283147c582f91b54b97feb203cc0819f4e
Sofien Haj Chedhli <shajchedli@exoplatform.com>
2026-05-05 11:16:46 +0100

    feat: add group management placeholder when no group is selected - MEED-10385 - Meeds-io/MIPs#237 (#5705)

---
+++ webapp/src/main/webapp/vue-apps/idm-groups-management/components/GroupManagementPlaceholder.vue
@@ -0,0 +1,35 @@
+<!--
+
+  This file is part of the Meeds project (https://meeds.io/).
+
+  Copyright (C) 2020 - 2026 Meeds Association contact@meeds.io
+
+  This program is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 3 of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, write to the Free Software Foundation,
+  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+-->
+<template>
+  <div class="d-flex flex-column ma-auto">
+    <div class="align-self-center">
+      <v-icon size="42" color="tertiary">fa-users</v-icon>
+    </div>
+    <div class="text-title mb-3">{{ $t('GroupsManagement.placeholder.title') }}</div>
+    <div class="text-body">{{ $t('GroupsManagement.placeholder.description1') }}</div>
+    <div class="text-body">{{ $t('GroupsManagement.placeholder.description2') }}</div>
+    <div class="text-body">{{ $t('GroupsManagement.placeholder.description3') }}</div>
+    <div class="text-body">{{ $t('GroupsManagement.placeholder.description4') }}</div>
+  </div>
+</template>
+<script>
+</script>
