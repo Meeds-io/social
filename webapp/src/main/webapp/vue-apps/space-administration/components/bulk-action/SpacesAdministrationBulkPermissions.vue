af4b632aa8d493a1409928d7cbb5b413f7b97535
Ali HAMDI <ahamdi@exoplatform.com>
2026-06-19 09:05:19 +0100

    feat: add default value for multi selection in spaces administration - Meeds-io/Mips#249 - MEED-10480 (#5815)

--- webapp/src/main/webapp/vue-apps/space-administration/components/bulk-action/SpacesAdministrationBulkPermissions.vue
+++ webapp/src/main/webapp/vue-apps/space-administration/components/bulk-action/SpacesAdministrationBulkPermissions.vue
@@ -41,6 +41,7 @@ export default {
           permissions.layoutPermissions = params.layoutPermissions.map(g => g.replace('spaceAdmin', `manager:${space.groupId}`));
           permissions.publicSitePermissions = params.publicSitePermissions.map(g => g.replace('spaceAdmin', `manager:${space.groupId}`));
           permissions.deletePermissions = params.deletePermissions.map(g => g.replace('spaceAdmin', `manager:${space.groupId}`));
+          permissions.extendedPermissions = params.extendedPermissions;
           await this.$spaceAdministrationService.updateSpacePermissions(space.id, permissions);
         },
         null,
