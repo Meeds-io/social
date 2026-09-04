e32d3f97bbfccd174359232224f391e0bab2c3de
Ayoub Zayati <azayati@exoplatform.com>
2026-09-04 17:07:11 +0200

    feat: Digest email lines for spaces and feed - EXO-89486_EXO-89487 - eXIP7.3.0.22 (#6072)
    (cherry picked from commit 274b5d4b8b4dc06dae8624138cb4fa80e9b83e5c)
    (cherry picked from commit 7c9a929aaf16d6809da87a09bb7aa3360410b766)

--- component/core/src/test/java/org/exoplatform/social/core/test/NoContainerTestSuite.java
+++ component/core/src/test/java/org/exoplatform/social/core/test/NoContainerTestSuite.java
@@ -79,7 +79,6 @@ import io.meeds.social.space.template.service.SpaceTemplateServiceTest;
 import io.meeds.social.space.template.storage.SpaceTemplateStorageTest;
 import io.meeds.social.upgrade.SpaceNavigationIconUpgradePluginTest;
 import io.meeds.social.user.plugin.UserAclPluginTest;
-import io.meeds.social.digest.service.DigestCategoryLabelResolverTest;
 import io.meeds.social.timezone.service.UserTimeZoneServiceTest;
 import io.meeds.social.identity.permission.listener.UserPermissionGroupListenerTest;
 import io.meeds.social.identity.permission.listener.UserPermissionMembershipListenerTest;
@@ -95,7 +94,6 @@ import io.meeds.social.organizationalunit.storage.OrganizationalUnitStorageTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
-    DigestCategoryLabelResolverTest.class,
     UserTimeZoneServiceTest.class,
     ReactionServiceImplTest.class,
     ActivityReportServiceImplTest.class,
